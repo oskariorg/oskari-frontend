9c3cf613efdc88874632ee88e9b9c9e3a3d7ec94
Aapi Hämäläinen <aapi.hamalainen@nomovok.com>
2012-08-24 16:13:54 +0300

    [PORTTISK-836] ...cleanup...

--- proof-of-concepts/oskari/bundle/main/locale/fi.js
+++
@@ -1,8 +0,0 @@
-
-Oskari.registerLocalization({
-	"lang" : "fi",
-	"key" : "SampleLocale",
-	"value" : {
-		"just_a_sample" : "Vain esimerkki"		
-	}
-});
--- samples/example-bundles/bundle/sade3/impl/sade-js-app/SADE/service/NimistoWFS.js
+++
@@ -1,7 +0,0 @@
-Oskari.clazz.define('Oskari.poc.sade3.service.NimistoWFS', function(mapplet, url) {
-	this.mapplet = mapplet;
-	this.url = url;
-
-}, {
-
-});
