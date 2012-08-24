ea2dc41fa74fd346b483848848c35ab7be51aaec
Aapi Hämäläinen <aapi.hamalainen@nomovok.com>
2012-08-24 16:13:36 +0300

    [PORTTISK-836] ...cleanup...

--- packages/deprecated/bundle/layerselection2/build/yui-pack-locale-en.js
+++
@@ -1,9 +0,0 @@
-/* This is a unpacked Oskari bundle (bundle script version Thu May 31 2012 12:23:19 GMT+0300 (Suomen kesäaika)) */ 
-Oskari.registerLocalization({
-	"lang" : "en",
-	"key" : "LayerSelection",
-	"value" : {
-		"title" : "Chosen maplayers",
-		"desc" : ""
-	}
-});
