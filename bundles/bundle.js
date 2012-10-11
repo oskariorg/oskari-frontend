e19850135cdcbc9970c9f1ad76a009deebc4e4e3
Aapi Hämäläinen <aapi.hamalainen@nomovok.com>
2012-10-11 16:31:08 +0300

    [PORTTISK-1094] Test using empty.js to get first-use-guide to start with preloaded

--- bundles/bundle.js
+++ bundles/bundle.js
@@ -960,23 +960,31 @@ Oskari = (function() {
 		}
 	    };
 	    var f = false;
-	    for(var n = 0; n < me.fileList.length; n++) {
-		var fn = me.fileList[n].src;
+	    for (var n = 0; n < me.fileList.length; n++) {
+                var st = null;
+                var fn = '/Oskari/empty.js';
+                if (!preloaded()) {
+		    fn = me.fileList[n].src;
+                }
 		var st = me.buildScriptTag(fn, onFileLoaded);
-		if(st) {
-		    // If this breaks something, revert to using
-		    // the empty.js-hack in buildScriptTag...
-		    if (preloaded()) {
-			onFileLoaded();
-		    } else {
-			fragment.appendChild(st);
-			f = true;
-		    }
-		}
+                if (st) {
+                    fragment.appendChild(st);
+                    f = true;
+                }
+		// if(st) {
+		//     // If this breaks something, revert to using
+		//     // the empty.js-hack in buildScriptTag...
+		//     if (preloaded()) {
+		// 	onFileLoaded();
+		//     } else {
+		// 	fragment.appendChild(st);
+		// 	f = true;
+		//     }
+                //  }
 	    }
-	    if(f)
+	    if (f) {
 		head.appendChild(fragment);
-
+            }
 	},
 	/**
 	 * @method buildScriptTag
