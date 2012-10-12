855fb7a7c5088a4020f13dcfa75b84de5b8d44fe
Aapi Hämäläinen <aapi.hamalainen@nomovok.com>
2012-10-12 15:23:26 +0300

    [PORTTISK-1094] Move guided tour from sample to framework

---
+++ bundles/framework/bundle/guidedtour/locale/en.js
@@ -0,0 +1,68 @@
+Oskari.registerLocalization(
+    {
+        "lang": "fi",
+        "key": "GuidedTour",
+        "button": {
+            "next" : "Seuraava",
+            "previous" : "Edellinen",
+            "start" : "Aloita",
+            "finish" : "Valmis"
+        },
+        "value": {
+            "title": "Tervetuloa",
+            "desc": "",
+            "page1": {
+                "title": "Tervetuloa Paikkatietoikkunan karttanäkymään",
+                "message" : "Jos olet ensimmäistä kertaa Paikkatietoikkunan karttapalvelussamme, pikaoppaamme auttaa sinut alkuun.",
+                "label" : "Älä näytä tätä viestiä uudelleen"
+            },
+            "page2": {
+                "title": "Haku",
+                "message" : "Haun avulla voit siirtyä suoraan etsimääsi paikkaan.",
+                "openLink" : "Näytä haku",
+                "closeLink" : "Piilota haku",
+                "tileText" : "Haku"
+            },
+            "page3": {
+                "title": "Karttatasot",
+                "message" : "Karttasoista valitset näkyviin eri tiedontuottajien aineistoja. Klikkaa kategoria auki ja valitse joku karttataso.",
+                "openLink" : "Näytä karttatasot",
+                "closeLink" : "Piilota karttatasot",
+                "tileText" : "Karttatasot"
+            },
+            "page4": {
+                "title": "Valitut karttatasot",
+                "message" : "Raahaa karttatasoja uuteen järjestykseen ja säädä niiden läpinäkyvyyttä. Näet muutokset karttaikkunassa.",
+                "openLink" : "Näytä valitut karttatasot",
+                "closeLink" : "Piilota valitut karttatasot",
+                "tileText" : "Valitut karttatasot"
+            },
+            "page5": {
+                "title": "Omat tiedot",
+                "message" : "Kaikki lisäämäsi kohteet, tallentamasi näkymät ja julkaisemasi kartat löytyvät täältä kun olet kirjautuneena sisään. Pääset myös muokkaamaan omia tietojasi täältä.",
+                "openLink" : "Näytä omat tiedot",
+                "closeLink" : "Piilota omat tiedot",
+                "tileText" : "Omat tiedot"
+            },
+            "page6": {
+                "title": "Julkaise kartta",
+                "message" : "Kokoamasi kartan voit julkaista helposti internet-sivustollasi. Määrittelet vain ensin kartan koon ja käyttöliitymän, ja kopioit lopuksi rivin HTML-koodia sivustollesi.",
+                "openLink" : "Näytä kartan julkaisu",
+                "closeLink" : "Piilota kartan julkaisu",
+                "tileText" : "Julkaise kartta"
+            },
+            "page7": {
+                "title": "Työkalut",
+                "message" : "Työkalujen avulla mm. lisäät kohteita, tallennat näkymän, lähetät linkin näkymään tai perut muutoksen kartalla. Työkalujen selitteet näet kun pidät hetken hiiren kursoria niiden päällä."
+            },
+            "page8": {
+                "title": "Liikkuminen kartalla",
+                "message" : "Voit siirtää karttaa raahaamalla tai panorointinuolia käyttäen. Palauta kartta oletustilaan nuolten keskellä olevasta painikkeesta. Vinkki: Myös näppäimistön nuolinäppäimet panoroivat karttaa."
+            },
+            "page9": {
+                "title": "Lähentäminen ja loitontaminen",
+                "message" : "Lähennä ja loitonna karttaa mittakaavasäätimestä tai plus- ja miinusmerkeistä säätimen yhteydessä. Vinkki: Voit lähentää ja loitontaa näppäimistön plus- ja miinusnäppäimillä tai lähentää kaksoisosoittamalla karttaa."
+            }
+        }
+    }
+);
