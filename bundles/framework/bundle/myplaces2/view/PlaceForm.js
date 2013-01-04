c2434759a4f0e1d6d5ff20a292acac75a2cb1269
Usman Muhammad <usman.mail11@gmail.com>
2013-01-04 14:52:02 +0200

    AL-608, ie css hacks

--- bundles/framework/bundle/myplaces2/view/PlaceForm.js
+++ bundles/framework/bundle/myplaces2/view/PlaceForm.js
@@ -18,23 +18,12 @@ function(instance) {
     var loc = instance.getLocalization('placeform');
     
     this.template = jQuery('<div class="myplacesform">' +
-            '<div class="field">' + 
-                '<div class="help icon-info" ' + 
-                'title="' + loc.tooltip + '"></div>' + 
-                '<input type="text" name="placename" placeholder="' + loc.placename.placeholder + '"/>' +
-            '</div>' +
-            '<div class="field">' + 
-                '<input type="text" name="placelink" placeholder="' + loc.placelink.placeholder + '"/>' +
-            '</div>' +
-            '<div class="field">' +  
-                '<textarea name="placedesc" placeholder="' + loc.placedesc.placeholder + '">' +
-                '</textarea>' +
-            '</div>' +
             
+            '<div class="field">' + 
                 '<label for="category">' + loc.category.label + '</label><br clear="all" />' +
                 '<select name="category" autofocus>' +
                 '</select>' +
-            
+            '</div>' +
         '</div>');
     this.templateOption = jQuery('<option></option>');
     this.categoryForm = undefined;
