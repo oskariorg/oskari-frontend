af2d72036c6fc5cf25bb29b2f4671500ae20a1d4
Mikko Johansson <mikko.johansson@nls.fi>
2013-01-16 10:52:33 +0200

    [AL-737] Parsing md files into html

--- docs/generator/generator.js
+++ docs/generator/generator.js
@@ -3,7 +3,6 @@ var program = require("commander"),
     path = require("path"),
     wrench = require('wrench'),
     util = require('util'),
-    //marked = require("marked"),
     markx = require('markx'),
     fsutils = require("./fsutils.js");
 
@@ -19,22 +18,6 @@ var cssDirectory = "css";
 var imagesDirectory = "images";
 var destinationDirectory = "release";
 
-/*
-marked.setOptions({
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: false,
-  highlight: function(code, lang) {
-    if (lang === 'js') {
-      return highlighter.javascript(code);
-    }
-    return code;
-  }
-});
-*/
-
 
 /*
  * Parsing command line arguments using commander
@@ -47,8 +30,12 @@ program
 
 
 var parseFile = function(file) {
+    var cwd = process.cwd();
+    var output = file.substr(0, file.lastIndexOf(".")) + ".html";
+    console.log("parse:", path.join(cwd, markdownDirectory, file));
+    console.log("output:", output);
     markx({
-        input: file, //can be either a filepath or a source string
+        input: path.join(markdownDirectory, file), //can be either a filepath or a source string
         template: path.join(layoutDirectory, layoutFile), //can either be filepath or source string
         highlight: true, //parse code snippets for syntax highlighters, default: true
         data: {} //data that gets passed into template
@@ -58,16 +45,18 @@ var parseFile = function(file) {
             console.log("Exiting..");
             process.exit(-1);
         }
-
-        fs.writeFile(path.join(destinationDirectory, "test.html"), html, "utf-8", function (err) {
-            if(err) { 
-                console.log(err);
+        /// TODO: change file extension .md => .html file.
+        console.log("write:", output);
+        fs.writeFile(path.join(destinationDirectory, output), html, "utf-8", function (err2) {
+            if(err2) { 
+                console.log(err2);
                 console.log("Exiting..");
                 process.exit(-1);
             }
         });
 
     });
+
 }
 
 /*
@@ -93,24 +82,35 @@ var run = function() {
             console.log("Exiting..");
             process.exit(-1);
         }
-        if(res) {
-            for (var i=0,len=res.length; i<len; i++) {
-                console.log(res[i]);
-                fs.stat(path.join(markdownDirectory, res[i]), function(err, stat) {
-                    if (stat && stat.isDirectory()) { // folder
-                        console.log("folder", res[i]);
+        if(res && res.length > 0) {
+            var tmp = null;
+            var filePath = null;
+            var folder = null;
+            var file = null;
+            for (var i = 0; i < res.length; i++) {
+                try {
+                    stat = fs.statSync(path.join(markdownDirectory, res[i]));
+                } catch(err) { }
+
+                if (stat && !stat.isDirectory()) { // file
+                    console.log("file:", res[i]);
+                    tmp = res[i].split("\\");
+                    filePath = tmp.slice(0,-1);
+                    console.log(filePath);
+
+                    folder = "";
+                    for (var j = 0; j < filePath.length; j++) {
+                        folder = path.join(folder, filePath[j]);
+                        console.log("folder", folder);
+                        fsutils.createDirStructure(destinationDirectory, [folder]);
                     }
-                    else { // file
-                        console.log("file", res[i]);
-                    }
-                });
+
+                    file = tmp.slice(-1)[0].split(".")[0];
+                    console.log(file);
+                    parseFile(res[i]);
+                }
+                console.log("--------");
             }
-            //console.log(res);
-            var file = res[0].split("\\");
-            if(typeof file !== 'string') {
-                console.log("last", file.slice(-1)[0].split(".")[0]);
-            }
-            //console.log(file);
         }
     });
 }
