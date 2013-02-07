e981dda7e70c08798e11638c1ba9c76ae239b2ec
Usman Muhammad <usman.mail11@gmail.com>
2013-02-07 15:44:45 +0200

    Added metadatalinks to layers in datasource popup.

--- bundles/framework/bundle/mapmodule-plugin/plugin/datasource/DataSourcePlugin.js
+++ bundles/framework/bundle/mapmodule-plugin/plugin/datasource/DataSourcePlugin.js
@@ -16,6 +16,7 @@ function() {
     this.template = null;
     this.element = null;
     this.sandbox = null;
+    this.tool = null;
 }, {
     /** @static @property __name plugin name */
     __name : 'DataSourcePlugin',
@@ -57,7 +58,7 @@ function() {
      * Interface method for the module protocol
      *
      * @param {Oskari.mapframework.sandbox.Sandbox} sandbox
-     * 			reference to application sandbox
+     *          reference to application sandbox
      */
     init : function(sandbox) {
         this.template = jQuery("<div class='datascource'>" +
@@ -83,7 +84,7 @@ function() {
      * Interface method for the plugin protocol
      *
      * @param {Oskari.mapframework.sandbox.Sandbox} sandbox
-     * 			reference to application sandbox
+     *          reference to application sandbox
      */
     startPlugin : function(sandbox) {
         this._sandbox = sandbox;
@@ -101,7 +102,7 @@ function() {
      * Interface method for the plugin protocol
      *
      * @param {Oskari.mapframework.sandbox.Sandbox} sandbox
-     * 			reference to application sandbox
+     *          reference to application sandbox
      */
     stopPlugin : function(sandbox) {
 
@@ -124,7 +125,7 @@ function() {
      * Interface method for the module protocol
      *
      * @param {Oskari.mapframework.sandbox.Sandbox} sandbox
-     * 			reference to application sandbox
+     *          reference to application sandbox
      */
     start : function(sandbox) {
     },
@@ -133,24 +134,45 @@ function() {
      * Interface method for the module protocol
      *
      * @param {Oskari.mapframework.sandbox.Sandbox} sandbox
-     * 			reference to application sandbox
+     *          reference to application sandbox
      */
     stop : function(sandbox) {
     },
-	/** 
-	 * @property {Object} eventHandlers 
-	 * @static 
-	 */
+    /** 
+     * @property {Object} eventHandlers 
+     * @static 
+     */
     eventHandlers : {
     },
 
-	/** 
-	 * @method onEvent
-	 * @param {Oskari.mapframework.event.Event} event a Oskari event object
-	 * Event is handled forwarded to correct #eventHandlers if found or discarded if not.
-	 */
+    /** 
+     * @method onEvent
+     * @param {Oskari.mapframework.event.Event} event a Oskari event object
+     * Event is handled forwarded to correct #eventHandlers if found or discarded if not.
+     */
     onEvent : function(event) {
         return this.eventHandlers[event.getName()].apply(this, [event]);
+    },
+    /**
+     * @method _layerListComparator
+     * Uses the private property #grouping to sort layer objects in the wanted order for rendering
+     * The #grouping property is the method name that is called on layer objects.
+     * If both layers have same group, they are ordered by layer.getName()
+     * @private
+     * @param {Oskari.mapframework.domain.WmsLayer/Oskari.mapframework.domain.WfsLayer/Oskari.mapframework.domain.VectorLayer/Object} a comparable layer 1
+     * @param {Oskari.mapframework.domain.WmsLayer/Oskari.mapframework.domain.WfsLayer/Oskari.mapframework.domain.VectorLayer/Object} b comparable layer 2
+     * @param {String} groupingMethod method name to sort by
+     */
+    _layerListComparator : function(a, b, getOrganizationName) {
+        var nameA = a[getOrganizationName]().toLowerCase();
+        var nameB = b[getOrganizationName]().toLowerCase();
+        if(nameA == nameB) {
+            nameA = a.getName().toLowerCase();
+            nameB = b.getName().toLowerCase();          
+        }
+        if (nameA < nameB) {return -1}
+        if (nameA > nameB) {return 1}
+        return 0;
     },
     
     /**
@@ -159,14 +181,14 @@ function() {
      * Creates logo and terms of use links on top of map
      */
     _createUI : function() {
-    	var me = this;
-		var sandbox = me._sandbox;
+        var me = this;
+        var sandbox = me._sandbox;
         // get div where the map is rendered from openlayers
         var parentContainer = jQuery(this._map.div);
         if(!this.element) {
             this.element = this.template.clone();
         }
-        		
+                
         parentContainer.append(this.element);
         
         var pluginLoc = this.getMapModule().getLocalization('plugin', true);
@@ -177,21 +199,74 @@ function() {
         link.bind('click', function(){
             var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
             var okBtn = dialog.createCloseButton(myLoc.button.close);
+            var infoIcon = jQuery('<div class="icon-info" style="float: right;"></div>');
+            var selectedLayers = sandbox.findAllSelectedMapLayers();  
 
-            var selectedLayers = sandbox.findAllSelectedMapLayers();
-            var content = jQuery('<ul></ul>');
+            //sort the layers
+            selectedLayers.sort(function(a, b) {
+                return me._layerListComparator(a, b, 'getOrganizationName');
+            }); 
+
+            var group = null;
+            var content = jQuery('<div></div>');
+
             for(var i = 0; i < selectedLayers.length; ++i) {
-                var layer = selectedLayers[i];
-                var name = layer.getOrganizationName();
-                if(name) {
-                    var item = jQuery('<li>' + name + '</li>');
-                    content.append(item);
+                var layer = selectedLayers[i];   
+
+                //get organization names
+                var organizationName = layer.getOrganizationName();
+                if(!organizationName && organizationName != 'N/A' ){
+                    organizationName = 'N/A';
+                }
+
+                //compare names for grouping
+                if (!group || group.getTitle() != organizationName) {
+                    var listOrganizations = jQuery('<li><b>' + organizationName + '</b></li>');    
+                    content.append(listOrganizations);
+                }
+                
+                //get layer names
+                var layerName = layer.getName();
+                if(layerName) {
+                        var layerItem = jQuery('<ul>' + layerName + '</ul>');    
+                    //metadata link
+                    var uuid = layer.getMetadataIdentifier();
+                    if(uuid) {    
+                        var layerIcon = infoIcon.clone();
+                        layerIcon.click(function() {
+                            var rn = 'catalogue.ShowMetadataRequest';
+                            var uuid = layer.getMetadataIdentifier();
+                            var additionalUuids = [];
+                            var additionalUuidsCheck = {};
+                            additionalUuidsCheck[uuid] = true; 
+                            var subLayers = layer.getSubLayers(); 
+                                if( subLayers && subLayers.length > 0 ) {
+                                    for( var s = 0 ; s < subLayers.length;s++) {
+                                        var subUuid = subLayers[s].getMetadataIdentifier();
+                                        if( subUuid && subUuid != "" && !additionalUuidsCheck[subUuid] ) { 
+                                            additionalUuidsCheck[subUuid] = true;
+                                            additionalUuids.push({
+                                                uuid: subUuid
+                                            });
+                                             
+                                        }
+                                    }
+                                    
+                                }
+                            sandbox.postRequestByName(rn, [
+                                { uuid : uuid }, additionalUuids
+                            ]);
+                        });
+                        layerItem.append(layerIcon);
+                    }
+                    content.append(layerItem);                 
                 }
             }
+
             dialog.show(myLoc.popup.title, content, [okBtn]);
             dialog.moveTo('div.datascource a', 'top');
             return false;
-	    });
+        });
 
     }
 }, {
