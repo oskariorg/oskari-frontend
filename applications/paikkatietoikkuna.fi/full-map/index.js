a3a8fc18a67f90fbd05254ad7a491d327aa5659f
Matti Pulakka <matti.pulakka@nls.fi>
2013-05-07 16:37:55 +0300

    [AL-1096] Improved cookie handling. Also removed an old parcel test directory.

--- applications/paikkatietoikkuna.fi/full-map/index.js
+++ applications/paikkatietoikkuna.fi/full-map/index.js
@@ -89,25 +89,6 @@ jQuery(document).ready(function() {
         });
     }
 
-    // TODO: handle cookie data in backend
-    // === Look for old mapfull state - cookie set in SaveViewPlugin.js   ===
-    var cookiename = "mymapview1";
-    var cookieviewdata = "";
-    if (document.cookie.length > 0) {
-        var cookieStart = document.cookie.indexOf(cookiename + "=");
-        if (cookieStart != -1) {
-            cookieStart += cookiename.length + 1;
-            var cookieEnd = document.cookie.indexOf(";", cookieStart);
-            if (cookieEnd == -1) {
-                cookieEnd = document.cookie.length;
-            }
-            cookieviewdata = document.cookie.substring(cookieStart, cookieEnd);
-
-        }
-    }
-    var data = {
-        viewData: cookieviewdata
-    };
     jQuery.ajax({
         type: 'POST',
         dataType: 'json',
@@ -117,7 +98,6 @@ jQuery(document).ready(function() {
             }
         },
         url: ajaxUrl + 'action_route=GetAppSetup',
-        data: data,
         success: function(app) {
             if (app.startupSequence && app.configuration) {
               var appSetup = {
