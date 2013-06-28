071b454b76e3a0d1e8be089074f8d321235a16db
Mikko Johansson <mikko.johansson@sc5.io>
2013-06-28 16:59:09 +0300

    [AL-1222] error handling for wfs2

--- bundles/framework/bundle/mapwfs2/locale/fi.js
+++ bundles/framework/bundle/mapwfs2/locale/fi.js
@@ -11,6 +11,21 @@ Oskari.registerLocalization({
         "flyout" : {
             "title" : "MapWfs2",
             "message" : "MapWfs2"
+        },
+        "error" : {
+            "title": "Virhe!",
+            "connection_not_available": "Yhteyttä ei voitu muodostaa WFS-taustapalveluun.",
+            "connection_broken": "Yhteys WFS-taustapalveluun katkesi.",
+            "wfs_no_permissions": "WFS-tasoon ({layer}) ei ole riittäviä oikeuksia",
+            "wfs_configuring_layer_failed": "WFS-tason ({layer}) konfigurointi epäonnistui",
+            "wfs_request_failed": "WFS-tason ({layer}) WFS-kysely epäonnistui",
+            "features_parsing_failed": "WFS-tason ({layer}) featureiden käsittely epäonnistui"
+        },
+        "button": {
+            "cancel": "Peruuta",
+            "show": "Näytä kohteet",
+            "close": "Sulje",
+            "edit" : "Muokkaa"
         }
     }
 });
