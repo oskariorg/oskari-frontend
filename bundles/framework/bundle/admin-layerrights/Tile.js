0cd8432e68a66906714f0cb9fc807fa991ac0cc8
Timo Mikkolainen <timo.mikkolainen@nls.fi>
2013-08-12 14:59:29 +0300

    [AL-1329] Tile and Flyout

---
+++ bundles/framework/bundle/admin-layerrights/Tile.js
@@ -0,0 +1,106 @@
+/*
+ * @class Oskari.framework.bundle.admin-layerrights.Tile
+ * 
+ * Renders the layer rights management tile.
+ */
+Oskari.clazz.define('Oskari.framework.bundle.admin-layerrights.Tile',
+
+	  /**
+	   * @method create called automatically on construction
+	   * @static
+	   * @param {Oskari.mapframework.bundle.search.SearchBundleInstance} instance
+	   * 		reference to component that created the tile
+	   */
+	  function(instance) {
+	    this.instance = instance;
+	    this.container = null;
+	    this.template = null;
+	  }, {
+	    /**
+	     * @method getName
+	     * @return {String} the name for the component 
+	     */
+	    getName : function() {
+	      return 'Oskari.framework.bundle.admin-layerrights.Tile';
+	    },
+	    /**
+	     * @method setEl
+	     * @param {Object} el 
+	     * 		reference to the container in browser
+	     * @param {Number} width 
+	     * 		container size(?) - not used
+	     * @param {Number} height 
+	     * 		container size(?) - not used 
+	     * 
+	     * Interface method implementation
+	     */
+	    setEl : function(el, width, height) {
+	      this.container = jQuery(el);
+	    },
+	    /**
+	     * @method startPlugin
+	     * Interface method implementation, calls #refresh() 
+	     */
+	    startPlugin : function() {
+	      this.refresh();
+	    },
+	    /**
+	     * @method stopPlugin 
+	     * Interface method implementation, clears the container 
+	     */
+	    stopPlugin : function() {
+	      this.container.empty();
+	    },
+	    /**
+	     * @method getTitle 
+	     * @return {String} localized text for the title of the tile 
+	     */
+	    getTitle : function() {
+	      return this.instance.getLocalization('title');
+	    },
+	    /**
+	     * @method getDescription 
+	     * @return {String} localized text for the description of the tile 
+	     */
+	    getDescription : function() {
+	      return this.instance.getLocalization('desc');
+	    },
+	    /**
+	     * @method getOptions 
+	     * Interface method implementation, does nothing atm 
+	     */
+	    getOptions : function() {
+
+	    },
+	    /**
+	     * @method setState 
+	     * @param {Object} state
+	     * 		state that this component should use
+	     * Interface method implementation, does nothing atm 
+	     */
+	    setState : function(state) {
+	    },
+	    /**
+	     * @method refresh
+	     * Creates the UI for a fresh start
+	     */
+	    refresh : function() {
+	      var me = this;
+	      var instance = me.instance;
+	      var cel = this.container;
+	      var tpl = this.template;
+	      var sandbox = instance.getSandbox();
+
+	      var status = cel.children('.oskari-tile-status');
+	      
+//	      status.empty();
+//	      status.append('(' + layers.length + ')');
+
+	    }
+	  }, {
+	    /**
+	     * @property {String[]} protocol
+	     * @static 
+	     */
+	    'protocol' : ['Oskari.userinterface.Tile']
+	  });
