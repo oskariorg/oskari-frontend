f7d6ed53ea5967042cac8a8cfbd25e7769cfc508
Sami Mäkinen <sami.makinen@nls.fi>
2013-09-02 11:33:42 +0300

    [AH-111] Cleaning up example index.js

--- applications/sample/servlet/index.js
+++ applications/sample/servlet/index.js
@@ -1,24 +1,34 @@
 jQuery(document).ready(function() {
-	Oskari.setLang('en');
-	Oskari.setLoaderMode('dev');
-	var appSetup = {};
-	var appConfig;
+
+    var getAppSetupParams = {};
+
+    // populate getappsetup url with possible control parameters
+    if (typeof window.controlParams === 'object') {
+        for (key in window.controlParams) {
+            if (window.controlParams.hasOwnProperty(key)) {
+                getAppSetupParams[key] = window.controlParams[key];
+            }
+        }
+    }
+    // NOTE! expects global variables language and preloaded from encapsulating page
+    if (!(typeof window.language === 'string') || !window.language) {
+        // default to english
+        window.language = 'en';
+    }
+    Oskari.setLang(window.language);
+    if (!(typeof window.preloaded === 'boolean') || !window.preloaded) {
+    	Oskari.setPreloaded(window.preloaded);
+	}
 
 	jQuery.ajax({
 		type : 'GET',
 		dataType : 'json',
+        data : getAppSetupParams,
 		url : '/ajax/?action_route=GetAppSetup',
-		beforeSend : function(x) {
-			if (x && x.overrideMimeType) {
-				x.overrideMimeType("application/j-son;charset=UTF-8");
-			}
-		},
 		success : function(setup) {
-			appSetup.startupSequence = setup.startupSequence;
-			appConfig = setup.configuration;
 			var app = Oskari.app;
-			app.setApplicationSetup(appSetup);
-			app.setConfiguration(appConfig);
+			app.setApplicationSetup(setup);
+			app.setConfiguration(setup.configuration);
 			app.startApplication(function(startupInfos) {
 				// all bundles have been loaded
 			});
