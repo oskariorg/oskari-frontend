93983e17357001fee3b14abe895cff6f7ca671f1
Mikael Blomberg <mikael.blomberg@nls.fi>
2013-09-12 14:34:20 +0300

    Removed unused bundles and moved files

--- bundles/oskari/bundle/map-openlayers/bundle.js
+++
@@ -1,9 +0,0 @@
-define(["oskari",  
-/* openlayers */ 
-"css!_resources_/openlayers/theme/default/style.css", 
-"_libraries_/proj4js-1.0.1/proj4js-compressed", 
-"_libraries_/OpenLayers/OpenLayers-2.12-patched"
-],function(Oskari) {
-	
-	Oskari.bundleCls('oskarimap');
-});
