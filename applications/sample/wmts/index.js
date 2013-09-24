1dfb01e07664ee603019f7c283d0e7f77bae3f93
nls-jajuko <JKORHONEN@HK-PC-034.MMLNET.NLS.FI>
2013-09-24 12:59:50 +0300

    [AH-20] Changed WMTS to use REST urls by default. Added sample

---
+++ applications/sample/wmts/index.js
@@ -0,0 +1,56 @@
+jQuery(document).ready(function() {
+  Oskari.setLang('en');
+  Oskari.setLoaderMode('dev');
+  var appSetup;
+  var appConfig;
+  
+  var downloadConfig = function(notifyCallback) {
+    jQuery.ajax({
+      type : 'GET',
+      dataType : 'json',
+      url : 'config.json',
+      beforeSend: function(x) {
+          if (x && x.overrideMimeType) {
+              x.overrideMimeType("application/j-son;charset=UTF-8");
+          }
+      },
+      success : function(config) {
+         appConfig = config;
+         notifyCallback();
+      }
+    });
+  };
+  var downloadAppSetup = function(notifyCallback) {
+    jQuery.ajax({
+      type : 'GET',
+      dataType : 'json',
+      url : 'appsetup.json',
+      beforeSend: function(x) {
+          if (x && x.overrideMimeType) {
+              x.overrideMimeType("application/j-son;charset=UTF-8");
+          }
+      },
+      success : function(setup) {
+         appSetup = setup;
+         notifyCallback();
+      }
+    });
+  };
+  
+  var startApplication = function() {
+    // check that both setup and config are loaded 
+    // before actually starting the application
+    if(appSetup && appConfig) {
+      var app = Oskari.app;
+      app.setApplicationSetup(appSetup);
+      app.setConfiguration(appConfig);
+      app.startApplication(function(startupInfos) {
+       
+        
+      });
+    }
+  };
+  downloadAppSetup(startApplication);
+  downloadConfig(startApplication);
+  
+});
