85669302c07e069d836ffab6116dd70781d6b592
Timo Mikkolainen <timo.mikkolainen@nls.fi>
2013-10-09 13:58:38 +0300

    [Ah-236] Language selection for ELF demo. Had to add fake localizations to mapwfs2. Locale can be selected in the URL with lang=cs

---
+++ bundles/framework/bundle/mapwfs2/locale/cs.js
@@ -0,0 +1,31 @@
+Oskari.registerLocalization({
+    "lang" : "cs",
+    "key" : "MapWfs2",
+    "value" : {
+        "title" : "MapWfs2",
+        "desc" : "",
+        "object-data": "Object data",
+        "tile" : {
+            "title" : "MapWfs2"
+        },
+        "flyout" : {
+            "title" : "MapWfs2",
+            "message" : "MapWfs2"
+        },
+        "error" : {
+            "title": "Error",
+            "connection_not_available": "Connection to the WFS background service is not available.",
+            "connection_broken": "Connection to the WFS background service broke down.",
+            "wfs_no_permissions": "No permission to use WFS layer ({layer}).",
+            "wfs_configuring_layer_failed": "Configuring WFS layer ({layer}) failed.",
+            "wfs_request_failed": "WFS layer's ({layer}) request failed.",
+            "features_parsing_failed": "Feature parsing of WFS layer ({layer}) failed."
+        },
+        "button": {
+            "cancel": "Cancel",
+            "show": "View places",
+            "close": "Close",
+            "edit" : "Edit"
+        }
+    }
+});
