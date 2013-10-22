c0532e2791721fc7d01247a974c0162e80e5e0da
Timo Mikkolainen <timo.mikkolainen@nls.fi>
2013-10-22 17:15:36 +0300

    JS code cleanup

--- bundles/framework/bundle/statehandler/state-methods.js
+++ bundles/framework/bundle/statehandler/state-methods.js
@@ -1,9 +1,9 @@
 Oskari.clazz.category('Oskari.mapframework.bundle.statehandler.StateHandlerBundleInstance', 'state-methods', {
-	
+
     /**
      * @method useState
      * @param {Object} savedState
-     * 		JSON presentation of application state, created with #getCurrentState()
+     *      JSON presentation of application state, created with #getCurrentState()
      * method.
      *
      * Sends out Oskari.mapframework.request.common.RemoveMapLayerRequest,
@@ -12,23 +12,26 @@ Oskari.clazz.category('Oskari.mapframework.bundle.statehandler.StateHandlerBundl
      * Oskari.mapframework.request.common.MapMoveRequest to control the
      * application state
      */
-    useState : function(state) {
-        if(!state) {
+    useState: function (state) {
+        if (!state) {
             // dont do anything if we dont have a saved state
             return [];
         }
-        var components = this.sandbox.getStatefulComponents();
-        var loopedComponents = [];
-        for(var id in state) {
-            if(components[id] && components[id].setState) {
-                // safety check that we have the component in current config
-                components[id].setState(state[id].state);
+        var components = this.sandbox.getStatefulComponents(),
+            loopedComponents = [],
+            id;
+        for (id in state) {
+            if (state.hasOwnProperty(id)) {
+                if (components[id] && components[id].setState) {
+                    // safety check that we have the component in current config
+                    components[id].setState(state[id].state);
+                }
+                loopedComponents.push(id);
             }
-            loopedComponents.push(id);
         }
         return loopedComponents;
     },
-    
+
     /**
      * @method resetState
      * Used to return the application to its original state.
@@ -39,75 +42,83 @@ Oskari.clazz.category('Oskari.mapframework.bundle.statehandler.StateHandlerBundl
      * All plugins should handle themselves what this means in the plugins
      * implementation.
      */
-    resetState : function() {
-    	var me = this;
-    	me._historyEnabled = false;
+    resetState: function () {
+        var me = this,
+            pluginName;
+        me._historyEnabled = false;
         me._historyPrevious = [];
-	    me._historyNext = [];
+        me._historyNext = [];
 
-    	
-        for(var pluginName in this._pluginInstances) {
-            me.sandbox.printDebug('[' + me.getName() + ']' + ' resetting state on ' + pluginName);
-            me._pluginInstances[pluginName].resetState();
+
+        for (pluginName in this._pluginInstances) {
+            if (this._pluginInstances.hasOwnProperty(pluginName)) {
+                me.sandbox.printDebug('[' + me.getName() + ']' + ' resetting state on ' + pluginName);
+                me._pluginInstances[pluginName].resetState();
+            }
         }
         // reinit with startup params
-     
-		// get initial state from server
-    	me._currentViewId = this._defaultViewId;
-		if(me._startupState) {
+
+        // get initial state from server
+        me._currentViewId = this._defaultViewId;
+        if (me._startupState) {
             me._resetComponentsWithNoStateData(me.useState(this._startupState));
-		}
-		else {
+        } else {
             jQuery.ajax({
-                dataType : "json",
-                type : "GET",
+                dataType: "json",
+                type: "GET",
                 // noSavedState=true parameter tells we dont want the state saved in session
-                url : me.sandbox.getAjaxUrl() + 'action_route=GetAppSetup&noSavedState=true',
-                success : function(data) {
-                	if(data && data.configuration) {
-	                    me._startupState = data.configuration;
-	                    me._resetComponentsWithNoStateData(me.useState(data.configuration));
-	                    me._historyEnabled = true;
-                	}
-                	else
-                	{alert('error in getting configuration');}
+                url: me.sandbox.getAjaxUrl() + 'action_route=GetAppSetup&noSavedState=true',
+                success: function (data) {
+                    if (data && data.configuration) {
+                        me._startupState = data.configuration;
+                        me._resetComponentsWithNoStateData(me.useState(data.configuration));
+                        me._historyEnabled = true;
+                    } else {
+                        alert('error in getting configuration');
+                    }
                 },
-                error : function() {
+                error: function () {
                     alert('error loading conf');
                     me._historyEnabled = true;
                 },
-                complete: function() {
-                	me._historyEnabled = true;
+                complete: function () {
+                    me._historyEnabled = true;
                 }
             });
-		}
-        
+        }
+
         me._historyEnabled = true;
     },
     /**
      * @method _resetComponentsWithNoStateData
-     * Used to return the application to its original state. 
+     * Used to return the application to its original state.
      * Loops through all the stateful components and calls their setState()
-     * with no parameters to reset them. Ignores the components whose IDs are listed in 
+     * with no parameters to reset them. Ignores the components whose IDs are listed in
      * the parameter array.
-     * @param {String[]}  loopedComponents 
+     * @param {String[]}  loopedComponents
      *      list of component IDs that had state data and should not be resetted
      *
      */
-    _resetComponentsWithNoStateData : function(loopedComponents) {
+    _resetComponentsWithNoStateData: function (loopedComponents) {
         // loop all stateful components and reset their state they are not in loopedComponents
-        var components = this.sandbox.getStatefulComponents();
-        for(var cid in components) {
-            var found = false;
-            for(var i = 0; i < loopedComponents.length; ++i) {
-                if(cid == loopedComponents[i]) {
-                    found = true;
-                    break;
+        var components = this.sandbox.getStatefulComponents(),
+            cid,
+            found,
+            i;
+        for (cid in components) {
+            if (components.hasOwnProperty(cid)) {
+                found = false;
+                for (i = 0; i < loopedComponents.length; i += 1) {
+                    // FIXME use ===
+                    if (cid == loopedComponents[i]) {
+                        found = true;
+                        break;
+                    }
                 }
-            }
-            if(!found)  {
-                // set empty state for resetting state
-                components[cid].setState();
+                if (!found) {
+                    // set empty state for resetting state
+                    components[cid].setState();
+                }
             }
         }
     },
@@ -115,17 +126,20 @@ Oskari.clazz.category('Oskari.mapframework.bundle.statehandler.StateHandlerBundl
      * @method saveState
      * @param {Object} view
      * @param {String} pluginName (optional)
-     * 	Calls the saveState method of the given plugin or if not given, calls it
+     *  Calls the saveState method of the given plugin or if not given, calls it
      * for each plugin
      *
      * Used to store the application state though the module/bundle does nothing
      * itself.
      * All actual implementations are done in plugins.
      */
-    saveState : function(view, pluginName) {
-        if(!pluginName) {
-            for(var pluginName in this._pluginInstances) {
-                this.saveState(view, pluginName);
+    saveState: function (view, pluginName) {
+        var plugName;
+        if (!pluginName) {
+            for (plugName in this._pluginInstances) {
+                if (this._pluginInstances.hasOwnProperty(plugName)) {
+                    this.saveState(view, plugName);
+                }
             }
             return;
         }
@@ -137,20 +151,22 @@ Oskari.clazz.category('Oskari.mapframework.bundle.statehandler.StateHandlerBundl
      * @return {Object} JSON object presenting the state of the application at
      * the moment.
      */
-    getCurrentState : function() {
-        var state = {};
-    	var components = this.sandbox.getStatefulComponents();
-    	for(var id in components) {
-            if(components[id].getState) {
-                state[id] = {
-                    // wrap with additional state property so we can use the same json as in startup configuration
-                    'state' : components[id].getState()
-                };
+    getCurrentState: function () {
+        var state = {},
+            components = this.sandbox.getStatefulComponents(),
+            id;
+        for (id in components) {
+            if (components.hasOwnProperty(id)) {
+                if (components[id].getState) {
+                    state[id] = {
+                        // wrap with additional state property so we can use the same json as in startup configuration
+                        'state': components[id].getState()
+                    };
+                } else {
+                    this.sandbox.printWarn('Stateful component ' + id + ' doesnt have getState()');
+                }
             }
-            else {
-                this.sandbox.printWarn('Stateful component ' + id + ' doesnt have getState()');
-            }
-    	}
+        }
         return state;
     },
     /**
@@ -160,11 +176,9 @@ Oskari.clazz.category('Oskari.mapframework.bundle.statehandler.StateHandlerBundl
      * It should return a JSON object created by #getCurrentState on earlier
      * time.
      */
-    getSavedState : function(pluginName) {
+    getSavedState: function (pluginName) {
         return this._pluginInstances[pluginName].getState();
     }
-    
-   
-});
 
-	
+
+});
