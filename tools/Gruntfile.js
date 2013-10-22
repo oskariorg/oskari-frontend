18afe7cfb06906af6357550e549d9167fc5be2b9
Timo Mikkolainen <timo.mikkolainen@nls.fi>
2013-10-22 17:19:16 +0300

    JS code cleanup

--- tools/Gruntfile.js
+++ tools/Gruntfile.js
@@ -331,9 +331,11 @@ module.exports = function (grunt) {
             files = fs.readdirSync(process.cwd());
             file = "";
             for (i in files) {
-                file = files[i];
-                if (file.indexOf('.cfg') !== -1) {
-                    packages.push(file);
+                if (files.hasOwnProperty(i)) {
+                    file = files[i];
+                    if (file.indexOf('.cfg') !== -1) {
+                        packages.push(file);
+                    }
                 }
             }
         } else {
@@ -532,7 +534,7 @@ module.exports = function (grunt) {
                     parsed = JSON.parse(content);
                     languageCode = filepath.substring(filepath.lastIndexOf("/") + 1, filepath.lastIndexOf("."));
                     if (languageCode !== parsed.lang) {
-                        grunt.fail.fatal("Language code mismatch in " + filepath + ":\nExpected " + languageCode + ", found " + parsed.lang +".");
+                        grunt.fail.fatal("Language code mismatch in " + filepath + ":\nExpected " + languageCode + ", found " + parsed.lang + ".");
                     }
                 } catch (err) {
                     grunt.fail.fatal(filepath + ": " + err);
@@ -557,7 +559,7 @@ module.exports = function (grunt) {
                 }
                 grunt.log.writeln("Beautifying " + filepath);
                 contents =  grunt.file.read(filepath);
-                grunt.file.write(filePath, beautify(contents, beautifyOptions));
+                grunt.file.write(filepath, beautify(contents, beautifyOptions));
                 return true;
             });
         });
