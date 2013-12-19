d25e3372471a27496a4f7187278a0609944d5888
eiri <no address>
2013-12-19 16:11:40 +0200

    [AH-490] Fixed empty metadatabox problem

--- bundles/catalogue/bundle/metadataflyout/Flyout.js
+++ bundles/catalogue/bundle/metadataflyout/Flyout.js
@@ -86,7 +86,7 @@ Oskari.clazz.define('Oskari.catalogue.bundle.metadataflyout.Flyout',
                 data,
                 page,
                 panel;
-
+                
             for (p in this.pages) {
                 if (this.pages.hasOwnProperty(p)) {
                     pageInfo = this.pages[p];
@@ -98,9 +98,11 @@ Oskari.clazz.define('Oskari.catalogue.bundle.metadataflyout.Flyout',
                 }
             }
 
-
             for (n = 0; n < allMetadata.length; n++) {
                 data = allMetadata[n];
+                if (data.uuid == null && data.RS_Identifier_Code == null && data.RS_Identifier_CodeSpace ==null){
+                    continue;
+                }
                 page = Oskari.clazz.create('Oskari.catalogue.bundle.metadataflyout.view.MetadataPage', this.instance, this.locale);
                 page.init();
                 panel = page.getPanel();
