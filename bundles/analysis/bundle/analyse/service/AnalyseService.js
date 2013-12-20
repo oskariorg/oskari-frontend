b206e538cac7e8b8d2b80098cd4c29c0b725876e
Tapio Keisteri <tapio.keisteri@nls.fi>
2013-12-20 15:08:21 +0200

    [AH-778] test

--- bundles/analysis/bundle/analyse/service/AnalyseService.js
+++ bundles/analysis/bundle/analyse/service/AnalyseService.js
@@ -66,7 +66,8 @@ Oskari.clazz.define('Oskari.analysis.bundle.analyse.service.AnalyseService',
          * @param {Function} success2 the success callback
          * @param {Function} failure the failure callback
          */
-        _getAnalysisLayers: function (success2, failure) {
+        _getAnalysisLayers: function (mysuccess, failure) {
+
             var url = this.sandbox.getAjaxUrl() + 'action_route=GetAnalysisLayers';
             jQuery.ajax({
                 type: 'GET',
@@ -77,7 +78,7 @@ Oskari.clazz.define('Oskari.analysis.bundle.analyse.service.AnalyseService',
                         x.overrideMimeType("application/j-son;charset=UTF-8");
                     }
                 },
-                success: success2,
+                success: mysuccess,
                 error: failure
             });
         },
