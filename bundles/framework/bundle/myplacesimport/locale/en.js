18bb3b68a9c8ce2e24f0f35ce0db2924a8490e9b
mrLite <aku.peltonen@nls.fi>
2014-03-07 11:14:22 +0200

    Added sv and en locale files to myplacesimport

---
+++ bundles/framework/bundle/myplacesimport/locale/en.js
@@ -0,0 +1,34 @@
+Oskari.registerLocalization({
+    "lang": "en",
+    "key": "MyPlacesImport",
+    "value": {
+        "title": "Omien aineistojen tuonti",
+        "desc": "",
+        "flyout": {
+            "title": "Omien aineistojen tuonti",
+            "description": "Voit tuoda omia aineistoja shape-formaatissa.",
+            "actions": {
+                "cancel": "Peruuta",
+                "next": "Seuraava"
+            },
+            "file": {
+                "submit": "Lähetä"
+            },
+            "layer": {
+                "title": "Tallenna tason tiedot:",
+                "name": "Nimi",
+                "desc": "Kuvaus",
+                "source": "Tietolähde"
+            },
+            "finish": {
+                "success": {
+                    "title": "Tason tuonti onnistui",
+                    "message": "Löydät tason omat tiedot -valikosta"
+                },
+                "failure": {
+                    "title": "Tason tuonti epäonnistui. Yritä myöhemmin uudelleen."
+                }
+            }
+        }        
+    }
+});
