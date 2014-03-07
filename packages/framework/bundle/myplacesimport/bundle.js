ba1375ff706e4ac244feab99717587e3e87d56c6
mrLite <aku.peltonen@nls.fi>
2014-03-07 11:15:40 +0200

    Added the new locale files to myplacesimport package

--- packages/framework/bundle/myplacesimport/bundle.js
+++ packages/framework/bundle/myplacesimport/bundle.js
@@ -29,6 +29,14 @@ Oskari.clazz.define("Oskari.mapframework.bundle.myplacesimport.MyPlacesImportBun
             "lang": "fi",
             "type": "text/javascript",
             "src": "../../../../bundles/framework/bundle/myplacesimport/locale/fi.js"
+        }, {
+            "lang": "sv",
+            "type": "text/javascript",
+            "src": "../../../../bundles/framework/bundle/myplacesimport/locale/sv.js"
+        }, {
+            "lang": "en",
+            "type": "text/javascript",
+            "src": "../../../../bundles/framework/bundle/myplacesimport/locale/en.js"
         }]
     },
     "bundle": {
