b705b7de38fcdf58a59e269a4b1b7fe2d87cdfce
Timo Mikkolainen <timo.mikkolainen@nls.fi>
2014-03-27 16:01:17 +0200

    [AG-1241] Make sure [groupName] exists before inserting stuff in it

--- bundles/framework/bundle/publisher/view/PublisherToolsForm.js
+++ bundles/framework/bundle/publisher/view/PublisherToolsForm.js
@@ -487,6 +487,9 @@ Oskari.clazz.define('Oskari.mapframework.bundle.publisher.view.PublisherToolsFor
                         if (me[configName][groupName]) {
                             me[configName][groupName][toolName] = false;
                         }
+                        if (me.toolbarConfig[groupName] === null || me.toolbarConfig[groupName] === undefined) {
+                            me.toolbarConfig[groupName] = {};
+                        }
                         me.toolbarConfig[groupName][toolName] = false;
                     }
                 };
