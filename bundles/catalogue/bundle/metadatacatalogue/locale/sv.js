8bd4d80eb729504590cfdb4c3fe3ff808ca84400
masapu <matti.pulakka@gmail.com>
2014-04-07 13:39:07 +0300

    Improved metadata search localizations (en, sv).

--- bundles/catalogue/bundle/metadatacatalogue/locale/sv.js
+++ bundles/catalogue/bundle/metadatacatalogue/locale/sv.js
@@ -1,31 +1,45 @@
 Oskari.registerLocalization({
-  "lang": "sv",
-  "key": "catalogue.bundle.metadatacatalogue",
-  "value": {
-    "title": "Ortnamnssökning",
-    "desc": "",
-    "contentErrorMsg": "Sökterm innehåller otillåtna tecken.",
-    "searchDescription": "Sök ortnamn, adress eller fastighetsbeteckning.",
-    "searchButton": "Sök",
-    "searchResultCount": "Sökningen gav ",
-    "searchResultCount2": " resultat.",
-    "searchResultDescriptionMoreResults": "Precisera sökningen för mera kompakta resultat.",
-    "searchResultDescriptionOrdering": "Du kan även ordna resultaten i tabellen nedan genom att klicka på kolumnrubrikerna.",
-    "searchResults": "Resultat: ",
-    "searchResultsDescription": " sökresultat ",
-    "searchservice_search_alert_title": "Fel: ",
-    "searchservice_search_not_found_anything_text": "Sökningen gav inga resultat.",
-    "too_short": "Sökordet är för kort.",
-    "cannot_be_empty": "Sökordet kan inte vara tomt.",
-    "too_many_stars": "För många *-tecken.",
-    "grid": {
-      "name": "Namn",
-      "village": "Kommun",
-      "type": "Typ"
-    },
-    "resultBox" : {
-    	"close" : "Stäng",
-    	"title" : "Sökresultat"
+    "lang": "sv",
+    "key": "catalogue.bundle.metadatacatalogue",
+    "value": {
+        "tabTitle": "Metadatasökning",
+        "metadataCatalogueDescription": "Sök datamängder eller tjänster.",
+        "metadataCatalogueButton": "Sök",
+        "assistance": " ",
+        "showMore": "Mer sökvillkoren",
+        "showLess": "Mindre sökvillkoren",
+        "emptyOption": "* Inte valt *",
+        "type": "Typ",
+        "serviceType": "Typ av tjänst",
+        "Title": "Resursnamn",
+        "OrganisationName": "Ansvarig initiativ",
+        "coverage": "Geografisk läge",
+        "inspireTheme": "Inspire-tema",
+        "Subject": "Nyckelord",
+        "TopicCategory": "Ämneskategori",
+        "searchArea": "Sökområde",
+        "delimitArea": "Avgränsa sökområdet på kartan",
+        "dataset": "Datamängd",
+        "series": "Serie",
+        "service": "Tjänst",
+        "discovery": "Sök",
+        "download": "Nedladdning",
+        "other": "Andra",
+        "transformation": "Omvandling",
+        "view": "Visning",
+        "modifySearch" : "Redigera sök",
+        "showSearch" : "Visa alla resultatet",
+        "metadataCatalogueResults": "Resultatet",
+        "show": "Visa kartlag",
+        "hide": "Ta kartlag bort",
+        "contentErrorMsg": "Sökord kan inte innehålla tecken ",
+        "metadataoptionservice_alert_title": "Fel",
+        "metadataoptionservice_not_found_anything_text": "Sök misslyckats.",
+        "metadatasearchservice_alert_title": "Fel",
+        "metadatasearchservice_not_found_anything_text": "Sök misslyckats.",
+        "searching": "Söker…",
+        "grid": {
+            "name": "Namn"
+        }
     }
-  }
 });
