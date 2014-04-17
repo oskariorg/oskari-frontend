469af91c7bf2227ff992c7a977eb6a4170338bbb
Mikael Blomberg <mikael.blomberg@nls.fi>
2014-04-17 11:47:35 +0300

    AH-1287 added gfiContent field to wmslayer and admin layer settings view.

--- bundles/integration/bundle/admin-layerselector/locale/fi.js
+++ bundles/integration/bundle/admin-layerselector/locale/fi.js
@@ -141,6 +141,7 @@ Oskari.registerLocalization({
             "legendImage": "Legenda-kuvan osoite",
             "legendImageDesc": "Legenda-kuvan osoite",
             "legendImagePlaceholder": "Legenda-kuvan osoite",
+            "gfiContent": "GFI lisäsisältö",
             "gfiResponseType": "GFI vastauksen tyyppi",
             "gfiResponseTypeDesc": "Vastauksen tyyppi eli Get Feature Info (GFI)",
             "gfiStyle": "GFI:n tyyli",
