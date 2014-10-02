9b38e9564cbf96cd1a62c62497d2c56066da6e2a
Mikael Blomberg <mikael.blomberg@nls.fi>
2014-10-02 10:00:12 +0300

    Synced oskari.org and sample servlet index.js

--- applications/sample/servlet/index.js
+++ applications/sample/servlet/index.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function() {
-
     var getAppSetupParams = {};
 
     // populate getappsetup url with possible control parameters
@@ -16,23 +15,26 @@ jQuery(document).ready(function() {
         window.language = 'en';
     }
     Oskari.setLang(window.language);
-    if (!(typeof window.preloaded === 'boolean') || !window.preloaded) {
-    	Oskari.setPreloaded(window.preloaded);
-	}
 
-	jQuery.ajax({
-		type : 'GET',
-		dataType : 'json',
+    if (typeof window.preloaded !== 'boolean') {
+        // default to false
+        window.preloaded = false;
+    }
+    Oskari.setPreloaded(window.preloaded);
+
+    jQuery.ajax({
+        type : 'GET',
+        dataType : 'json',
         data : getAppSetupParams,
         url: ajaxUrl + 'action_route=GetAppSetup',
-		success : function(setup) {
-			var app = Oskari.app;
-			app.setApplicationSetup(setup);
-			app.setConfiguration(setup.configuration);
-			app.startApplication(function(startupInfos) {
-				// all bundles have been loaded
-			});
-		}
-	});
+        success : function(setup) {
+            var app = Oskari.app;
+            app.setApplicationSetup(setup);
+            app.setConfiguration(setup.configuration);
+            app.startApplication(function(startupInfos) {
+                // all bundles have been loaded
+            });
+        }
+    });
 
 });
