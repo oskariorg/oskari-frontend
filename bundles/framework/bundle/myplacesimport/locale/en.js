86ac821e73f7ab38fa62c14b0c5af40c56e1b810
Sami Mäkinen <sami.makinen@nls.fi>
2014-10-15 15:52:47 +0300

    Tuned localizations to not be paikkatietoikkuna specific

--- bundles/framework/bundle/myplacesimport/locale/en.js
+++ bundles/framework/bundle/myplacesimport/locale/en.js
@@ -4,14 +4,14 @@ Oskari.registerLocalization(
     "key": "MyPlacesImport",
     "value": {
         "title": "Dataset Import",
-        "desc": "You can import your own datasets to Paikkatietoikkuna and create your own map layer. Imported datasets can be in the shp (shapefile), zip or kml/kmz file format.",
+        "desc": "You can import your own datasets in shp-, gpx- or mif/mid file format in a zip file or in kmz file format (zipped kml).",
         "help": "Select a file from your computer or give a link to the file that contains your dataset. The file can be in the shp, zip or kml/kmz format. You can create zip files from shapefiles by zipping the files with suffix shp, shx, dbf and prj to the same zip file. Also kml/kmz files from Google maps can be zipped same way.",
         "tool": {
             "tooltip": "Import your own dataset"
         },
         "flyout": {
             "title": "Dataset Import",
-            "description": "You can import your own dataset to Paikkatietoikkuna as a map layer. Datasets can be in shp-, gpx- or mif/mid-format in a zip file or kmz format (zipped kml).",
+            "description": "You can import your own datasets in shp-, gpx- or mif/mid file format in a zip file or in kmz file format (zipped kml).",
             "actions": {
                 "cancel": "Cancel",
                 "next": "Next"
