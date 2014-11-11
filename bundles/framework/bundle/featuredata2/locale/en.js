530ffbda16c1b67c6343f15501d51790d183ce35
Hanna Visuri <hanna.vellonen@nls.fi>
2014-11-11 11:53:37 +0200

    [AH-1763] localizations for filtering with texts NOT TRANSLATED

--- bundles/framework/bundle/featuredata2/locale/en.js
+++ bundles/framework/bundle/featuredata2/locale/en.js
@@ -44,6 +44,103 @@ Oskari.registerLocalization({
                     "tooltip": "Select features"
                 }
             }
+        },
+        "layer": {
+            "style": "Style",
+            "show": "Show",
+            "hide": "Hide",
+            "hidden": "The map layer has been temporarily hidden.",
+            "out-of-scale": "The map layer cannot be shown at this scale level.",
+            "move-to-scale": "Please move to a suitable scale level.",
+            "out-of-content-area": "The map layer contains no data at this location.",
+            "move-to-content-area": "Please move to a suitable location.",
+            "description": "Description",
+            "object-data": "Feature data",
+            "rights": {
+                "notavailable": "Publication prohibited.",
+                "guest": "Please log in to publish the map layer in an embedded map.",
+                "loggedin": "Publication permitted",
+                "official": "Publication permitted only for use by authorities.",
+                "need-login": "You need to log in to publish the map level in an embedded map.",
+                "can_be_published_by_provider": {
+                    "label": "Publication permitted as a data provider",
+                    "tooltip": "Only the data provider can publish the map layer in an embedded map.  If you are a data provider, please contact the support service of Paikkatietoikkuna."
+                },
+                "can_be_published": {
+                    "label": "Publication permitted",
+                    "tooltip": "The map layer can be published in an embedded map. The limit of the usage is not limited."
+                },
+                "can_be_published_map_user": {
+                    "label": "Publication permitted",
+                    "tooltip": "The map layer can be published in an embedded map. The limit of the usage may be limited."
+                },
+                "no_publication_permission": {
+                    "label": "Publication prohibited",
+                    "tooltip": "The map layer cannot be published in an embedded map. The data provider has not granted permission to that."
+                },
+                "can_be_published_by_authority": {
+                    "label": "Publication permitted",
+                    "tooltip": "The map layer can be published in an embedded map. The limit of the usage is not limited."
+                }
+            },
+            "tooltip": {
+                "type-base": "Background map",
+                "type-wms": "Map layer",
+                "type-wfs": "Data product"
+            },
+            "filter": {
+                "title": "NOT TRANSLATED",
+                "description": "NOT TRANSLATED",
+                "cancelButton": "NOT TRANSLATED",
+                "clearButton": "NOT TRANSLATED",
+                "refreshButton": "NOT TRANSLATED",
+                "addFilter": "NOT TRANSLATED",
+                "removeFilter": "NOT TRANSLATED",
+                "bbox": {
+                    "title": "NOT TRANSLATED",
+                    "on": "NOT TRANSLATED",
+                    "off": "NOT TRANSLATED"
+                },
+                "clickedFeatures": {
+                    "title": "NOT TRANSLATED",
+                    "label": "NOT TRANSLATED"
+                },
+                "values": {
+                    "title": "NOT TRANSLATED",
+                    "placeholders": {
+                        "case-sensitive": "NOT TRANSLATED",
+                        "attribute": "NOT TRANSLATED",
+                        "boolean": "NOT TRANSLATED",
+                        "operator": "NOT TRANSLATED",
+                        "attribute-value": "NOT TRANSLATED"
+                    },
+                    "equals": "NOT TRANSLATED",
+                    "like": "NOT TRANSLATED",
+                    "notEquals": "NOT TRANSLATED",
+                    "notLike": "NOT TRANSLATED",
+                    "greaterThan": "NOT TRANSLATED",
+                    "lessThan": "NOT TRANSLATED",
+                    "greaterThanOrEqualTo": "NOT TRANSLATED",
+                    "lessThanOrEqualTo": "NOT TRANSLATED"
+                },
+                "aggregateAnalysisFilter": {
+                    "addAggregateFilter": "NOT TRANSLATED",
+                    "aggregateValueSelectTitle": "NOT TRANSLATED",
+                    "selectAggregateAnalyse": "NOT TRANSLATED",
+                    "selectIndicator": "NOT TRANSLATED",
+                    "selectReadyButton": "NOT TRANSLATED",
+                    "getAggregateAnalysisFailed": "NOT TRANSLATED",
+                    "noAggregateAnalysisPopupTitle": "NOT TRANSLATED",
+                    "noAggregateAnalysisPopupContent": "NOT TRANSLATED"
+                },
+                "validation": {
+                    "title": "NOT TRANSLATED",
+                    "attribute_missing": "NOT TRANSLATED",
+                    "operator_missing": "NOT TRANSLATED",
+                    "value_missing": "NOT TRANSLATED",
+                    "boolean_operator_missing": "NOT TRANSLATED"
+                }
+            }
         }
     }
 });
