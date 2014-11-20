678c7cf2ecb8ac92ab5cd2b608b02163682f6c35
Timo Mikkolainen <timo.mikkolainen@nls.fi>
2014-11-20 15:27:33 +0200

    [AH-2168] En/Disable layout edit mode only if it hasn't already been done...

--- bundles/framework/bundle/publisher/view/BasicPublisher.js
+++ bundles/framework/bundle/publisher/view/BasicPublisher.js
@@ -124,8 +124,8 @@ Oskari.clazz.define('Oskari.mapframework.bundle.publisher.view.BasicPublisher',
                 groupedSiblings: true
             },
 
-            'Oskari.mapframework.bundle.mapmodule.plugin.SearchPlugin': {
-                allowedLocations: ['top left', 'top center', 'top right'],
+            'Oskari.mapframework.bundle.mapmodule.plugin.SearchPlugin': 
+{                allowedLocations: ['top left', 'top center', 'top right'],
                 allowedSiblings: [
                     'Oskari.mapframework.bundle.mapmodule.plugin.PublisherToolbarPlugin',
                     'Oskari.mapframework.bundle.mapmodule.plugin.LayerSelectionPlugin'
@@ -969,6 +969,10 @@ Oskari.clazz.define('Oskari.mapframework.bundle.publisher.view.BasicPublisher',
             var me = this,
                 sandbox = Oskari.getSandbox('sandbox');
 
+            if (me.toolLayoutEditMode) {
+                return;
+            }
+
             me.toolLayoutEditMode = true;
             jQuery('#editModeBtn').val(me.loc.toollayout.usereditmodeoff);
             jQuery('.mapplugins').show();
@@ -1020,6 +1024,10 @@ Oskari.clazz.define('Oskari.mapframework.bundle.publisher.view.BasicPublisher',
         _editToolLayoutOff: function () {
             var me = this,
                 sandbox = Oskari.getSandbox('sandbox');
+
+            if (!me.toolLayoutEditMode) {
+                return;
+            }
 
             me.toolLayoutEditMode = false;
             jQuery('#editModeBtn').val(me.loc.toollayout.usereditmode);
