0855bab51f0869c6b45824458584915f5caea4e9
Marko Kuosmanen <marko.kuosmanen@maanmittauslaitos.fi>
2015-02-06 10:28:12 +0200

    [AH-2328] Oskari2 folder structure changes. Changed bundle locale folder to resources/locale.

---
+++ bundles/admin/admin/resources/locale/sv.js
@@ -0,0 +1,31 @@
+Oskari.registerLocalization({
+    "lang": "sv",
+    "key": "GenericAdmin",
+    "value": {
+        "title": "Upprätthållande",
+        "desc": "",
+        "tile": {
+            "title": "Upprätthållande"
+        },
+        "flyout": {
+            "title": "Upprätthållande",
+            "defaultviews" : {
+                "title" : "Antaglig vy",
+                "desc" : "Välj kartlager, läge och skalanivå. Lagra antagliga vyn genom att klicka 'Lagra'.",
+                "headerName" : "Antaglig vy",
+                "globalViewTitle" : '*Systemens antagliga vy*',
+                "setButton" : "Lagra",
+                "forceButton" : "Uppdatera ändå",
+                "notifications" : {
+                    "errorTitle" : "Fel",
+                    "warningTitle" : "Varning",
+                    "successTitle" : "Lagrat",
+                    "errorLoadingFailed" : "Antagliga vyn kunde inte laddas.",
+                    "errorUpdating" : "Vyn (id=${id}) kunde inte uppdateras.",
+                    "listTitle" : "Guest-anvandare har inte rätt till de här kartlagren:",
+                    "viewUpdated" : "Antaglig vy (id=${id}) uppdaterat."
+                }
+            }
+        }
+    }
+}); 
