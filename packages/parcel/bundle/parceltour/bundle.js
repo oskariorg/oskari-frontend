a87abbcc664bb7763b5cfac37d05dccb0ed37cdd
Sami Mäkinen <sami.makinen@nls.fi>
2015-02-20 16:16:41 +0200

    Parceltour had way too many locales linked which it didnt really have

--- packages/parcel/bundle/parceltour/bundle.js
+++ packages/parcel/bundle/parceltour/bundle.js
@@ -38,97 +38,17 @@ Oskari.clazz.define(
                 "src": "../../../../bundles/parcel/parceltour/resources/css/style.css"
             }],
             "locales": [{
-                "lang": "bg",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/bg.js"
-            }, {
-                "lang": "cs",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/cs.js"
-            }, {
-                "lang": "da",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/da.js"
-            }, {
-                "lang": "de",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/de.js"
-            }, {
                 "lang": "en",
                 "type": "text/javascript",
                 "src": "../../../../bundles/parcel/parceltour/resources/locale/en.js"
-            }, {
-                "lang": "es",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/es.js"
-            }, {
-                "lang": "et",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/et.js"
             }, {
                 "lang": "fi",
                 "type": "text/javascript",
                 "src": "../../../../bundles/parcel/parceltour/resources/locale/fi.js"
             }, {
-                "lang": "ge",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/ge.js"
-            }, {
-                "lang": "gr",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/gr.js"
-            }, {
-                "lang": "hr",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/hr.js"
-            }, {
-                "lang": "hu",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/hu.js"
-            }, {
-                "lang": "lv",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/lv.js"
-            }, {
-                "lang": "nl",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/nl.js"
-            }, {
-                "lang": "pl",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/pl.js"
-            }, {
-                "lang": "pt",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/pt.js"
-            }, {
-                "lang": "ro",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/ro.js"
-            }, {
-                "lang": "rs",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/rs.js"
-            }, {
-                "lang": "sl",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/sl.js"
-            }, {
-                "lang": "sk",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/sk.js"
-            }, {
-                "lang": "sq",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/sq.js"
-            }, {
                 "lang": "sv",
                 "type": "text/javascript",
                 "src": "../../../../bundles/parcel/parceltour/resources/locale/sv.js"
-            }, {
-                "lang": "uk",
-                "type": "text/javascript",
-                "src": "../../../../bundles/parcel/parceltour/resources/locale/uk.js"
             }]
         },
         "bundle": {
