ae71ba05a5bc73dc62809ece4bc2ae8da415f6ee
Rami Linkala <rami.linkala@nls.fi>
2015-05-05 09:48:26 +0300

    localization for findbycordinates icon

---
+++ bundles/framework/findbycoordinates/resources/locale/nn.js
@@ -0,0 +1,18 @@
+Oskari.registerLocalization(
+{
+    "lang": "nn",
+    "key": "findbycoordinates",
+    "value": {
+        "title": "FindByCoordinates",
+        "desc": "FindByCoordinates",
+        "tool": {
+            "tooltip": "Find nearest address by clicking the map"
+        },
+        "resultsTitle": "Search results",
+        "grid": {
+            "address": "Address",
+            "coordinates": "Coordinates"
+        }
+    }
+}
+);
