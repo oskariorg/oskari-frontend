c4a1ed826d463607339b0e224400297d645c8f74
Hanna Visuri <hanna.visuri@nls.fi>
2015-07-06 17:55:35 +0300

    [AH-2608] New tool for giving parameters for the routing service to get route. POC.

---
+++ bundles/framework/routingUI/resources/locale/en.js
@@ -0,0 +1,27 @@
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "routingUI",
+    "value": {
+        "tool": {
+            "tooltip": "Tee reittihaku"
+        },
+        "popup": {
+            "title": "Hae reitti",
+            "instructions": "Hae reitti kahden pisteen välillä antamalla lähtö- ja saapumispiste.",
+            "button": {
+                "cancel": "Sulje",
+                "getRoute": "Hae reitti"
+            },
+            "startingPointTooltip":"Lähtöpiste",
+            "finishingPointTooltip":"Saapumispiste"
+        },
+        "routeInstructions": {
+            "length": "Reitin pituus: ",
+            "duration": "Reitin kesto: ",
+            "seconds": " s",
+            "meters":  " m"
+        }
+    }
+}
+);
---
+++ bundles/framework/routingUI/resources/locale/fi.js
@@ -0,0 +1,27 @@
+Oskari.registerLocalization(
+{
+    "lang": "fi",
+    "key": "routingUI",
+    "value": {
+        "tool": {
+            "tooltip": "Tee reittihaku"
+        },
+        "popup": {
+            "title": "Hae reitti",
+            "instructions": "Hae reitti kahden pisteen välillä klikkaamalla kartalta lähtö- ja saapumispiste.",
+            "button": {
+                "cancel": "Sulje",
+                "getRoute": "Hae reitti"
+            },
+            "startingPointTooltip":"Lähtöpiste",
+            "finishingPointTooltip":"Saapumispiste"
+        },
+        "routeInstructions": {
+            "length": "Reitin pituus: ",
+            "duration": "Reitin kesto: ",
+            "seconds": " s",
+            "meters":  " m"
+        }
+    }
+}
+);
