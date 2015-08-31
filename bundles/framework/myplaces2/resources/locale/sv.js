759df362c0749bf84e1608e90a8856f1013b7b0b
Sami Mäkinen <sami.makinen@nls.fi>
2015-08-31 09:15:05 +0300

    New l10n key for opening the new layer dialog

--- bundles/framework/myplaces2/resources/locale/sv.js
+++ bundles/framework/myplaces2/resources/locale/sv.js
@@ -21,6 +21,7 @@ Oskari.registerLocalization(
                 "privateTooltip": "Detta kartlager är privat. Klicka här för att publicera den.",
                 "publicTooltip": "Detta kartlager är offentlig. Klicka här för att avpublicera det."
             },
+            "addCategoryFormButton": "Ny kartlager",
             "addCategory": "Lägg till kartlagret",
             "editCategory": "Redigera kartlagret",
             "deleteCategory": "Ta bort kartlagret",
