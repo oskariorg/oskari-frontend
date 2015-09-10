d375c68eeab324c3cf9491bd12d0ad26f9b38a2a
Marko Kuosmanen <marko.kuosmanen@dimenteq.fi>
2015-09-10 10:56:49 +0300

    Added tampere bundles to master

---
+++ bundles/tampere/search-from-channels/resources/locale/en.js
@@ -0,0 +1,46 @@
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "search-from-channels",
+    "value": {
+        "tabTitle": "Place search",
+        "searchFromChannelsDescription": "Find a place name, address or register unit identifier",
+        "searchFromChannelsButton": "Search",
+        "assistance": "Write the search termel",
+        "showMore": "Show search options",
+        "showLess": "Hide search options",
+        "advanced_topic" : "Search channels",
+        "channeloptionservice_alert_title" : "Search channels error",
+        "channeloptionservice_not_found_anything_text": "Cannot get search channels",
+        "no_channels_found": "Any search channels not found",
+        "searchResults_header": "Search results",
+        "searchResultCount": "You search return",
+        "searchResultCount2": "results.",
+        "searchResultDescriptionMoreResults": "Define the search to decrease the number of results.",
+        "searchResultDescriptionOrdering": "You can also order results in the table below by clicking the column headings.",
+        "searchResults": "Search results",
+        "searchResultsDescription": "results by search term",
+        "searchservice_search_not_found_anything_text": "No results found.",
+        "searchservice_alert_title": "Service is over-crowded",
+        "too_short": "The search term is too short.",
+        "cannot_be_empty": "The search term must contain at least one character. Allowed characters are the letters a-z as well as å, ä and ö, numbers, backspaces and hyphens.",
+        "too_many_stars": "The search term contains too many stars (*-characters). It may contain at most N *-characters.",
+        "generic_error": "Search failed. Please try again later.",
+        "invalid_characters": "Search term contains illegal characters. Allowed characters are the letters a-z as well as å, ä and ö, numbers, backspaces, question marks (?), stars (*) and hyphens (-).",
+        "show-all-on-map": "Show all on map",
+        "show-selected-on-map": "Show selected on map",
+         "no_selected_rows_alert_title": "Show selected on map error",
+        "no_selected_rows_have_to_select": "No selected rows. Select search results and try again.",
+        "back-to-search": "Back to search",
+         "grid": {
+            "name": "Place name",
+            "village": "Municipality",
+            "type": "Search channel"
+        },
+        "resultBox": {
+            "close": "Close",
+            "title": "Place Search Results"
+        }
+    }
+}
+);
