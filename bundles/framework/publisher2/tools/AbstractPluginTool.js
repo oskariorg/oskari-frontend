d3810866fe0e4c4674656fc370ecb2f29fd72159
kessu <tapio.keisteri@nls.fi>
2015-09-22 11:43:01 +0300

    [AH-2712] new publisher / statsgrid edit state

--- bundles/framework/publisher2/tools/AbstractPluginTool.js
+++ bundles/framework/publisher2/tools/AbstractPluginTool.js
@@ -15,6 +15,7 @@ function(sandbox, mapmodule, localization, instance, handlers) {
     this.__loc = localization[this.group];
     this.__instance = instance;
     this.__plugin = null;
+    this.__tool = null;
     this.__handlers = handlers;
     // This is used to watch tool plugin start/stop changes. If plugin is started then change this value to true, if stopped then change to false.
     // If tool plugin is started then we can call stop plugin if unchecking this tools (otherwise we get error when sopping plugin).
@@ -45,7 +46,6 @@ function(sandbox, mapmodule, localization, instance, handlers) {
     init: function(){
         // override
     },
-
     /**
     * Get tool object.
     * @method getTool
@@ -159,7 +159,30 @@ function(sandbox, mapmodule, localization, instance, handlers) {
     isDefaultTool: function() {
         return false;
     },
+   /**
+    * Check, if nested key exists
+    * @method hasNestedObj
+    * @params {}  object
+    * @params String object path
+    * @public
+    *
+    * @returns {Boolean}
+    */
+    hasNestedObj: function(obj, sobj) {
+       var tmpObj = obj,
+           cnt = 0,
+           splits = sobj.split('.');
 
+       for (i=0; tmpObj && i < splits.length; i++) {
+           if (splits[i] in tmpObj) {
+               tmpObj = tmpObj[splits[i]];
+               cnt++;
+           }
+       }
+       return cnt === splits.length;
+
+
+   },
     /**
     * Whether or not to create a panel and checkbox for the tool in the tools' panel.
     * @method isShownInToolsPanel
