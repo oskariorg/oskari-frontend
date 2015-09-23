82b4d7d84cd8e7e8bafb8f98ce8a2ddbe23325d7
Rami Linkala <rami.linkala@nls.fi>
2015-09-23 11:34:11 +0300

    taxts added to sl localization

---
+++ bundles/elf/elf-metadatafeedback/resources/locale/sl.js
@@ -0,0 +1,59 @@
+Oskari.registerLocalization(
+{
+    "lang": "sl",
+    "key": "catalogue.bundle.metadatafeedback",
+    "value": {
+        "title": "Metapodatki",
+        "desc": "",
+        "userFeedback": {
+            "userFeedback": "Uporabniške povratne informacije",
+            "subject": "Predmet",
+            "subjectPlaceholder": "Naslov za tvojo povratno informacijo",
+            "ratingScore": "Vrednost ocene",
+            "ratingJustification": "Utemeljitev ocene"
+        },
+        "targetSpecification": {
+            "targetSpecification": "Ciljna specifikacija",
+            "targetIdentification": "Ciljna identifikacija",
+            "targetCode": "Ciljna koda",
+            "targetCodeNotDefined": "Ni definirano",
+            "targetCodespace": "Ciljni kodni prostor",
+            "primaryTargetCodeSpaceNotDefined": "Ni definirano",
+            "natureOfTarget": "Značaj cilja"
+        },
+        "userInformation": {
+            "userInformation": "Uporabniška informacija",
+            "userDetails": "Podrobnosti o uporabniku",
+            "userName": "Uporabniško ime",
+            "contactRole": "Vloga uporabnika",
+            "ciRoleUser": "Uporabnik",
+            "ciRoleResourceProvider": "Ponudnik vira",
+            "ciRoleCustodian": "Raziskovalec",
+            "ciRoleOwner": "Lastnik",
+            "ciRoleSponsor": "Pokrovitelj",
+            "ciRoleDistributor": "Distributer",
+            "ciRoleOriginator": "Pobudnik",
+            "ciRolePointOfContact": "Kontaktna točka",
+            "ciRolePrincipalInvestigator": "Glavni preiskovalec",
+            "ciRoleProcessor": "Procesor",
+            "ciRolePublisher": "Založnik",
+            "ciRoleAuthor": "Avtor",
+            "ciRoleCollaborator": "Sodelavec"
+        },
+        "actionButtons": {
+            "save": "Shrani",
+            "cancel": "Prekini"
+        },
+        "errorPopup": {
+            "title": "Napaka!",
+            "savingTheFeedbackFailed": "Shranjevanje povratnih informacij ni uspelo",
+            "formValidationFailed": "Preverjanje forme ni uspela",
+            "okButtonText": "OK"
+        },
+        "successPopup": {
+            "title": "Uspešno!",
+            "savingTheFeedbackSuccesful": "Shranjevanje povratnih informacij je bilo uspešno."
+        }
+    }
+}
+);
