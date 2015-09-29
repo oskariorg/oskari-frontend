9aa6b529a8f2c845f5a8691cdf30a97bad3b69ea
Marko Kuosmanen <marko.kuosmanen@maanmittauslaitos.fi>
2015-09-29 15:04:30 +0300

    Fixed legend url

--- bundles/integration/admin-layerselector/models/layerModel.js
+++ bundles/integration/admin-layerselector/models/layerModel.js
@@ -298,8 +298,18 @@ if (!Function.prototype.bind) {
              */
             getLegendUrl: function() {                
                 var adminBlock = this.getAdmin();
-                if (adminBlock && typeof adminBlock.getCurrentStyle == 'function'){
-                    return adminBlock.getCurrentStyle().getLegend();
+                var currentStyleName = this.getCurrentStyle().getName();
+
+                if (adminBlock && currentStyleName && adminBlock.styles){
+                    var selectedStyle = jQuery.grep(adminBlock.styles ||[], function(style, index){
+                        return style.name === currentStyleName;
+                    });
+
+                    if(selectedStyle.length>0) {
+                        return selectedStyle[0].legend;
+                    } else {
+                        return adminBlock.getCurrentStyle().getLegend();
+                    }
                 }
                 
                 return this.getCurrentStyle().getLegend();
