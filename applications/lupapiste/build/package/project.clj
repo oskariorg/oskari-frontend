22ac867108fc6ddae938b8d7547b7f1fc97c8f39
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-12-08 13:45:19 +0200

    Prepare config for Clojars deploy

--- applications/lupapiste/build/package/project.clj
+++ applications/lupapiste/build/package/project.clj
@@ -1,3 +1,8 @@
-(defproject fi.sito/oskari "0.9.54" ;;; also modify x4 the fullmap.html file
-  :description "Oskari map for Lupapiste"
+(defproject lupapiste/oskari "0.9.54" ;;; also modify x4 the fullmap.html file
+  :description "Oskari map for Lupapiste - fork of http://www.oskari.org/ v1.9.3"
+  :url "https://www.lupapiste.fi"
+  :license {:name "European Union Public Licence (EUPL v.1.1)"
+            :url "https://joinup.ec.europa.eu/community/eupl/og_page/eupl"
+            :distribution :repo}
+  :scm {:url "https://github.com/lupapiste/oskari.git"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
