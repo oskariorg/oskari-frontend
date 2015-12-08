38c47b0ce02b62cccb96de6ee9ce6af2efe4396d
Juha Jokimaki <juha.jokimaki@solita.fi>
2015-12-08 14:42:43 +0200

    fixed version info

--- applications/lupapiste/build/package/project.clj
+++ applications/lupapiste/build/package/project.clj
@@ -1,5 +1,5 @@
 (defproject lupapiste/oskari "0.9.54" ;;; also modify x4 the fullmap.html file
-  :description "Oskari map for Lupapiste - fork of http://www.oskari.org/ v1.9.3"
+  :description "Oskari map for Lupapiste - fork of http://www.oskari.org/ v1.19.3"
   :url "https://www.lupapiste.fi"
   :license {:name "European Union Public Licence (EUPL v.1.1)"
             :url "https://joinup.ec.europa.eu/community/eupl/og_page/eupl"
