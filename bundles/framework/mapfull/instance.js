7f248fcb028038b41d829c9a139b6fa3c8aa4cad
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2016-01-14 14:58:09 +0200

    Fixed a jquery error.

--- bundles/framework/mapfull/instance.js
+++ bundles/framework/mapfull/instance.js
@@ -70,7 +70,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapfull.MapFullBundleInstance',
                     mapHeight = jQuery(window).height(),
                     mapWidth = contentMap.width(),
                     sidebar = jQuery('#sidebar:visible'),
-                    statsgrid = jQuery('.oskari-view.statsgrid:visible'),
+                    statsgrid = jQuery('.statsgrid:visible'),
                     maxWidth = jQuery(window).width()-sidebar.width()-statsgrid.width(),
                     mapTools = jQuery('#maptools:visible');
 
