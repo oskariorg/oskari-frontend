2e18d2f97759dc74832457305175cf11c6092f80
Jaakko Ruutiainen <jruutiai@users.noreply.github.com>
2016-03-01 11:24:36 +0200

    Add new feature needs to have layer

--- bundles/tampere/content-editor/view/SideContentEditor.js
+++ bundles/tampere/content-editor/view/SideContentEditor.js
@@ -193,6 +193,7 @@ Oskari.clazz.define('Oskari.tampere.bundle.content-editor.view.SideContentEditor
             }
             me.getLayerGeometryType();
             me.sendStopDrawRequest(true);
+            var layer = me._getLayerById(me.selectedLayerId);
             var fields = layer.getFields().slice();
             var featureData = [[]];
             for (var i = 0; i < fields.length; i++)
