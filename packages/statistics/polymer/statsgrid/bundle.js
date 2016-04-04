920e6426033139292f672a90ed882b7b4e3a58ea
Sami Mäkinen <sami.makinen@nls.fi>
2016-04-04 16:16:16 +0300

    Removed package json dependencies (copied from tools before)

--- packages/statistics/polymer/statsgrid/bundle.js
+++ packages/statistics/polymer/statsgrid/bundle.js
@@ -14,9 +14,9 @@ Oskari.clazz.define("Oskari.statistics.bundle.statsgrid.StatsGridBundle",
     }, {
         "create": function () {
             return Oskari.clazz.create("Oskari.statistics.bundle.statsgrid.StatsGridBundleInstance",
-                'StatsGrid', 
-                null, 
-                "Oskari.statistics.bundle.statsgrid.Tile", 
+                'StatsGrid',
+                null,
+                "Oskari.statistics.bundle.statsgrid.Tile",
                 "Oskari.statistics.bundle.statsgrid.StatsView");
         },
         "update": function (manager, bundle, bi, info) {
