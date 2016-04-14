0a70017babd65125d5e8fef9127042b1f63d9383
Sami Mäkinen <sami.makinen@nls.fi>
2016-04-14 10:33:31 +0300

    The Oskari core is now provided in minified format. This gives faster loading times and underlines that any changes should be done to files under src instead of the bundles/bundle.js

--- tools/build-core.js
+++ tools/build-core.js
@@ -1,21 +1,39 @@
 /**
  * This script can be used to generate a new version of "Oskari core".
- * Gathers requirejs and files under src, concatenates filecontents to bundles/bundle.js
+ * Gathers requirejs and files under src, minifies file contents to bundles/bundle.js
  */
 var fs = require('fs');
 var os = require('os');
+var UglifyJS = require('uglify-js');
 
-var files = [];
-files.push(fs.readFileSync('../libraries/requirejs/require-2.2.0.min.js'));
-files.push(fs.readFileSync('../libraries/requirejs/text-plugin-2.0.14.js'));
 
-files.push(fs.readFileSync('../src/polyfills.js'));
-files.push(fs.readFileSync('../src/oskari.js'));
-files.push(fs.readFileSync('../src/util.js'));
-files.push(fs.readFileSync('../src/logger.js'));
-files.push(fs.readFileSync('../src/loader.js'));
+var files = [
+	'../libraries/requirejs/require-2.2.0.min.js',
+	'../libraries/requirejs/text-plugin-2.0.14.js',
+	'../src/polyfills.js',
+	'../src/oskari.js',
+	'../src/util.js',
+	'../src/logger.js',
+	'../src/loader.js'
+];
 
-fs.writeFileSync('../bundles/bundle.js', files.join(os.EOL));
+try {
+    var result = UglifyJS.minify(files, {
+        //outSourceMap : "out.js.map",
+        warnings : true,
+        compress : true
+    });
+	fs.writeFileSync('../bundles/bundle.js', result.code);
+} catch (e) {
+    console.log(e);
+    var err = new Error('Uglification failed.');
+    if (e.message) {
+        err.message += '\n' + e.message + '. \n';
+        if (e.line) {
+            err.message += 'Line ' + e.line + ' in ' + src + '\n';
+        }
+    }
+}
 // remove require.js from minified core
 //files.shift();
 //files.shift();
