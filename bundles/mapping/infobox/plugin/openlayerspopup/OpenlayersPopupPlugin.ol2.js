3bead519f96a2a19730ecb26d53b155d87c18575
Hanna Visuri <hanna.visuri@nls.fi>
2016-04-28 16:08:08 +0300

    infobox mobile css changed under infobox css from popups css

--- bundles/mapping/infobox/plugin/openlayerspopup/OpenlayersPopupPlugin.ol2.js
+++ bundles/mapping/infobox/plugin/openlayerspopup/OpenlayersPopupPlugin.ol2.js
@@ -270,7 +270,7 @@ Oskari.clazz.define(
                 popup.dialog.remove();
                 popup.__notifyListeners('close');
             });
-            popup.dialog.addClass('mobile-portrait');
+            popup.dialog.addClass('mobile-infobox');
         },
 
         /**
