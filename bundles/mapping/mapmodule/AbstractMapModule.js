d3d3433d298de86caefe2b9690b444369876d378
Sami Mäkinen <sami.makinen@nls.fi>
2016-06-01 16:58:34 +0300

    Removed annoying warning

--- bundles/mapping/mapmodule/AbstractMapModule.js
+++ bundles/mapping/mapmodule/AbstractMapModule.js
@@ -1447,7 +1447,6 @@ Oskari.clazz.define(
             }
             // marker icon not found
             else {
-                sandbox.printWarn('Not identified marker shape. Not handled getSvg.');
                 return null;
             }
 
