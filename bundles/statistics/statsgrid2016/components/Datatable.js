2faa5821956eb5230c44b5da07fb071dd87930bc
Sami Mäkinen <sami.makinen@nls.fi>
2016-06-14 18:40:53 +0300

    Datagrid eventing

--- bundles/statistics/statsgrid2016/components/Datatable.js
+++ bundles/statistics/statsgrid2016/components/Datatable.js
@@ -1,24 +1,44 @@
 
 Oskari.clazz.define('Oskari.statistics.statsgrid.Datatable', function(sandbox) {
 	this.sb = sandbox;
-	var log = Oskari.log('Oskari.statistics.statsgrid.Datatable');
 	this.service = sandbox.getService('Oskari.statistics.statsgrid.StatisticsService');
-	this.service.on('StatsGrid.IndicatorEvent', function(event) {
-		var src = me.service.getDatasource(event.getDatasource());
-		if(event.isRemoved()) {
-			log.info('Indicator removed',  src, event.getIndicator())
-		} else {
-			log.info('Indicator added', src, event.getIndicator())
-		}
-		log.info('Indicator selections:', event.getSelections());
-	});
 	var me = this;
+	this.service.on('StatsGrid.IndicatorEvent', function(event) {
+		if(event.isRemoved()) {
+			me.handleIndicatorRemoved(event.getDatasource(), event.getIndicator(), event.getSelections());
+		} else {
+			me.handleIndicatorAdded(event.getDatasource(), event.getIndicator(), event.getSelections());
+		}
+	});
 	this.service.on('StatsGrid.RegionsetChangedEvent', function(event) {
-		var regionset = me.service.getRegionsets(event.getRegionset());
-		log.info('Region changed! ', regionset);
+		me.handleRegionsetChanged(event.getRegionset());
 	});
 }, {
+	__templates : {
+		main : _.template('<div class="stats-table"><table></table></div>'),
+		tableHeader : _.template('<th></th>'),
+		tableRow : _.template('<tr></tr>'),
+		tableCell : _.template('<td>${value}</td>')
+	},
 	render : function(el) {
 		el.append('TBD: data');
+	},
+	handleRegionsetChanged: function(setId) {
+		var regionset = this.service.getRegionsets(setId);
+		var log = Oskari.log('Oskari.statistics.statsgrid.Datatable');
+		log.info('Region changed! ', regionset);
+
+	},
+	handleIndicatorAdded: function(datasrc, indId, selections) {
+		var log = Oskari.log('Oskari.statistics.statsgrid.Datatable');
+		var src = this.service.getDatasource(datasrc);
+		log.info('Indicator added ', src, indId, selections);
+
+	},
+	handleIndicatorRemoved: function(datasrc, indId, selections) {
+		var log = Oskari.log('Oskari.statistics.statsgrid.Datatable');
+		var src = this.service.getDatasource(datasrc);
+		log.info('Indicator removed', src, indId, selections)
+
 	}
 });
