6fa0cae7fa86d15c31fe801fa4b32137216ab108
Sami Mäkinen <sami.makinen@nls.fi>
2016-07-08 13:44:23 +0300

    Bundle mediator removed from oskari.js

--- src/oskari.js
+++ src/oskari.js
@@ -54,55 +54,9 @@ Oskari = (function () {
 
     /**
      * singleton instance of the class system
-     *
      */
     var class_singleton = new O2ClassSystem(),
         cs = class_singleton;
-
-    /**
-     * @class Oskari.Bundle_mediator
-     * A mediator class to support bundle to/from bundle manager communication
-     * and initialisation as well as bundle state management
-     *
-     * @param {Object} options Options
-     *
-     */
-    var Bundle_mediator = function (options) {
-        var p;
-        this.manager = null;
-
-        for (p in options) {
-            if (options.hasOwnProperty(p)) {
-                this[p] = options[p];
-            }
-        }
-    };
-
-    Bundle_mediator.prototype = {
-
-        /**
-         * @public @method setState
-         *
-         * @param  {string} state State
-         *
-         * @return {string}       State
-         */
-        setState: function (state) {
-            this.state = state;
-            this.manager.postChange(this.bundle, this.instance, this.state);
-            return this.state;
-        },
-
-        /**
-         * @public @method getState
-         *
-         *
-         * @return {string} State
-         */
-        getState: function () {
-            return this.state;
-        }
-    };
 
     /**
      * @class Oskari.Bundle_trigger
@@ -492,16 +446,6 @@ Oskari = (function () {
                 );
             }
             bundleInstanceId = me._getSerial().toString();
-            bundleInstance.mediator = new Bundle_mediator({
-                bundleId: bid,
-                instanceid: bundleInstanceId,
-                state: 'initial',
-                bundle: bundle,
-                instance: bundleInstance,
-                manager: this,
-                clazz: class_singleton,
-                requestMediator: {}
-            });
 
             this.bundleInstances[bundleInstanceId] = bundleInstance;
 
@@ -518,22 +462,13 @@ Oskari = (function () {
          * @return
          */
         _destroyInstance: function (biid) {
-            var bundleInstance,
-                mediator;
+            var bundleInstance;
 
             if (biid === null || biid === undefined) {
                 throw new TypeError('_destroyInstance(): Missing biid');
             }
 
             bundleInstance = this.bundleInstances[biid];
-            mediator = bundleInstance.mediator;
-
-            mediator.bundle = null;
-            mediator.manager = null;
-            mediator.clazz = null;
-
-            bundleInstance.mediator = null;
-
             this.bundleInstances[biid] = null;
             bundleInstance = null;
 
