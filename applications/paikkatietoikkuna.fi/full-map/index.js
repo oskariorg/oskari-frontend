3daa1ef2f14443469d1f289232353d708e121118
Marko Kuosmanen <marko.kuosmanen@maanmittauslaitos.fi>
2016-10-11 15:53:12 +0300

    Removed hacks

--- applications/paikkatietoikkuna.fi/full-map/index.js
+++ applications/paikkatietoikkuna.fi/full-map/index.js
@@ -70,10 +70,6 @@ jQuery(document).ready(function () {
         data : getAppSetupParams,
         url: ajaxUrl + 'action_route=GetAppSetup',
         success: function (appSetup) {
-var mapfull = appSetup.startupSequence[1].metadata['Import-Bundle'];
-              mapfull.mapstats.bundlePath = "/Oskari/packages/mapping/ol2/";
-              var statsgrid = appSetup.startupSequence[11].metadata['Import-Bundle'];
-              statsgrid.statsgrid.bundlePath = "/Oskari/packages/statistics/";
             var app = Oskari.app;
             if (!appSetup.startupSequence) {
                 jQuery('#mapdiv').append('Unable to start');
