0610d5a2e6736a7db038428e8e43558381178ffd
Sami Mäkinen <sami.makinen@nls.fi>
2016-11-10 14:50:30 +0200

    Translations

--- bundles/framework/feedbackService/resources/locale/fi.js
+++ bundles/framework/feedbackService/resources/locale/fi.js
@@ -6,8 +6,8 @@ Oskari.registerLocalization(
         "display": {
             "publisher": {
                 "apiUrl": "Palvelun baseURL",
-                "urlPlaceholder": "esim. http://dev.ser.fi/open311-test/v2/",
-                "apiKey": "Palvelun avain (api_key)",
+                "urlPlaceholder": "esim. http://dev.ser.fi/open311/v2/",
+                "apiKey": "Palvelun avain (API key)",
                 "keyPlaceholder": "esim. e1301b1ded935fabc5eaa6a2ce975e6",
                 "apiExtensions": "Palvelun laajennukset (optio)",
                 "extensionsPlaceholder": "esim. citysdk,media,geometry"
