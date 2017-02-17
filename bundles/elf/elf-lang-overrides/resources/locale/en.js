7f7bf1cfe94c65079199e89145c5aaffcbee1dfd
Sami Mäkinen <sami.makinen@nls.fi>
2017-02-17 15:59:34 +0200

    Restored ELF-localizations

--- bundles/elf/elf-lang-overrides/resources/locale/en.js
+++ bundles/elf/elf-lang-overrides/resources/locale/en.js
@@ -1,6 +1,162 @@
 Oskari.registerLocalization(
 {
     "lang": "en",
+    "key": "MapModule",
+    "value": {
+        "plugin": {
+            "LogoPlugin" : {
+                "dataSources": "Copyright",
+                "layersHeader": "&copy; ELF and listed service providers"
+            }
+        }
+    }
+}, true);
+
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "catalogue.bundle.metadatacatalogue",
+    "value": {
+        "tabTitle": "Geo Product Finder",
+        "metadataCatalogueDescription": "Search spatial resources, such as spatial datasets, dataset series or services."
+    }
+}, true);
+
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "admin-layerselector",
+    "value": {
+        "admin": {
+            "en": {
+                "title": "En",
+                "placeholder": "Name in English",
+                "descplaceholder": "Description in English"
+            },
+            "fi": {
+                "title": "Fi",
+                "placeholder": "Name in Finnish",
+                "descplaceholder": "Description in Finnish"
+            },
+            "sv": {
+                "title": "Sv",
+                "placeholder": "Name in Swedish",
+                "descplaceholder": "Description in Swedish"
+            },
+            "es": {
+                "title": "Es",
+                "placeholder": "Name in Spanish",
+                "descplaceholder": "Description in Spanish"
+            }
+        }
+    }
+}, true);
+
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "PersonalData",
+    "value": {
+        "notLoggedIn": "My data contains your user profile and all your saved features, map views, embedded maps and datasets."
+    }
+}, true);
+
+
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "Publisher2",
+    "value": {
+        "tile": {
+            "title": "Create embedded map"
+        },
+        "published": {
+            "desc": "The embedded map has been saved. Embed it on your website by copying this code to the html code on your website:"
+        },
+        "layerselection": {
+            "info": "Select the map layers to show on the embedded map"
+        },
+        "NotLoggedView": {
+            "text": "This tool allows you to create embedded maps from available services. Use of these are subject to licensing and requires that you log in."
+        },
+        "StartView": {
+            "text": "Welcome to create an embedded map.",
+            "layerlist_title": "Embeddable map layers open in map window:",
+            "layerlist_empty": "No embeddable map layers are open in the map window.",
+            "layerlist_denied": "Non-embeddable map layers open in map window:",
+            "denied_tooltip": "These map layers cannot be included in embedded maps. Data producers have not granted permissions for embedding."
+        },
+        "layerFilter": {
+            "buttons": {
+                "publishable": "Embeddable"
+            },
+            "tooltips": {
+                "publishable": "Show only embeddable map layers."
+            }
+        }
+    }
+}, true);
+
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "LayerSelection",
+    "value": {
+        "layer": {
+            "rights": {
+                "can_be_published_map_user": {
+                    "label": "",
+                    "tooltip": ""
+                }
+            }
+        }
+    }
+}, true);
+
+
+Oskari.registerLocalization({
+    "lang": "en",
+    "key": "catalogue.bundle.metadataflyout",
+    "value": {
+        "flyout": {
+            "heading": {
+                "datasetInformation": "Information about the resource",
+                "contactInformation": "Contact information",
+                "metadataContact": "Metadata contact",
+                "technicalInformation": "Technical information",
+                "dataQuality":"Data quality"
+            },
+            "tableHeaders": {
+                "datasetInformation": {
+                    "referenceDate":"Reference date",
+                    "temporalInformation":"Temporal information",
+                    "resourceLanguage":"Language",
+                    "bbox":"Geographic bounding box",
+                    "crs":"CRS Identifier"
+                },
+                "contactInformation": {
+                    "pointOfContact":"Point of contact, owner, custodian"
+                },
+                "metadataContact": {
+                    "pointOfContact":"Point of contact"
+                },
+                "technicalInformation": {
+                    "accessConstraintInformation":"Access constraint information",
+                    "lineage":"Lineage",
+                    "metadataChangeDate":"Metadata change date",
+                    "uniqueIdentifier":"Unique identifier"
+                },
+                "dataQuality": {
+                    "conformance":"Conformance"
+                }
+            }
+        }
+    }
+}, true);
+
+Oskari.registerLocalization(
+{
+    "lang": "en",
     "key": "findbycoordinates",
     "value": {
         "tool": {
