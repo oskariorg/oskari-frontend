263dd9eb5f55b4abe4934c9aab30f4b50d528dca
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-02-27 11:33:24 +0200

    [AH-3438] new event for layer load progress

---
+++ bundles/mapping/mapmodule/event/ProgressEvent.js
@@ -0,0 +1,36 @@
+/**
+ * @class Oskari.mapframework.bundle.mapmodule.event.ProgressEvent
+ *
+ * Event is sent when decides to
+ */
+Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.event.ProgressEvent',
+/**
+ * @method create called automatically on construction
+ * @static
+ */
+function(status, id) {
+    this._status = status;
+    this._id = id;
+}, {
+    /** @static @property __name event name */
+    __name : "ProgressEvent",
+    /**
+     * @method getName
+     * @return {String} the name for the event
+     */
+    getName : function() {
+        return this.__name;
+    },
+    layerId: function(){
+      return this._id;
+    },
+    isLoaded: function(){
+      return this._status;
+    }
+}, {
+    /**
+     * @property {String[]} protocol array of superclasses as {String}
+     * @static
+     */
+    'protocol' : ['Oskari.mapframework.event.Event']
+});
