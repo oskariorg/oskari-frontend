2f3da3b0a3804c575184c2b759f35c0fc9c9cf13
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-03-15 16:21:22 +0200

    tuning

--- bundles/framework/rpc/instance.js
+++ bundles/framework/rpc/instance.js
@@ -130,6 +130,10 @@ Oskari.clazz.define(
             me._bindFunctions(channel);
             me._channel = channel;
         },
+        /**
+-         * Initialize allowed requests/events/functions based on config
+-         * @param  {Object} conf bundle configuration
+-         */
         __init : function(conf) {
             var me = this;
             // sanitize conf to prevent unnecessary errors
@@ -151,7 +155,6 @@ Oskari.clazz.define(
 
                 // Special handling for getScreenshot() since it's not always present
                 var mapModule = this.sandbox.findRegisteredModuleInstance('MainMapModule');
-                // var img = null;
                 if(typeof mapModule.getScreenshot === 'function') {
                     // this is only available in Openlayers3 implementation of mapmodule
                     funcs['getScreenshot'] = function(transaction) {
@@ -411,7 +414,7 @@ Oskari.clazz.define(
                             message: 'Invalid origin: ' + trans.origin
                         };
                     }
-                    params = params ||[];
+                    params = params || [];
                     params.unshift(trans);
 
                     var value =  me._availableFunctions[name].apply(me, params);
@@ -523,8 +526,70 @@ Oskari.clazz.define(
          *
          */
         init: function () {
-            'use strict';
-            return null;
+          var me = this;
+          // sanitize conf to prevent unnecessary errors
+          var conf = this.conf || {};
+          var allowedEvents = conf.allowedEvents;
+          var allowedFunctions = conf.allowedfunctions;
+          var allowedRequests = conf.allowedRequests;
+
+          if (allowedEvents === null || allowedEvents === undefined) {
+              allowedEvents = ['AfterMapMoveEvent', 'MapClickedEvent', 'AfterAddMarkerEvent', 'MarkerClickEvent',
+              'RouteResultEvent','FeedbackResultEvent','SearchResultEvent', 'UserLocationEvent', 'DrawingEvent', "FeatureEvent", 'InfoboxActionEvent', 'InfoBox.InfoBoxEvent',
+              'RPCUIEvent'];
+          }
+
+          if (allowedFunctions === null || allowedFunctions === undefined) {
+              allowedFunctions = [];
+              // allow all available functions by default
+              var funcs = this._availableFunctions;
+
+              // Special handling for getScreenshot() since it's not always present
+              var mapModule = this.sandbox.findRegisteredModuleInstance('MainMapModule');
+              if(typeof mapModule.getScreenshot === 'function') {
+                  // this is only available in Openlayers3 implementation of mapmodule
+                  funcs['getScreenshot'] = function(transaction) {
+                    mapModule.getScreenshot(function(image){
+                      transaction.complete(image);
+                    });
+                }
+              }
+
+              for(var name in funcs) {
+                  if(funcs.hasOwnProperty(name)) {
+                      allowedFunctions.push(name);
+                  }
+              }
+          }
+          if (allowedRequests === null || allowedRequests === undefined) {
+              allowedRequests = ['InfoBox.ShowInfoBoxRequest',
+                  'InfoBox.HideInfoBoxRequest',
+                  'MapModulePlugin.AddMarkerRequest',
+                  'MapModulePlugin.AddFeaturesToMapRequest',
+                  'MapModulePlugin.RemoveFeaturesFromMapRequest',
+                  'MapModulePlugin.GetFeatureInfoRequest',
+                  'MapModulePlugin.MapLayerVisibilityRequest',
+                  'MapModulePlugin.RemoveMarkersRequest',
+                  'MapModulePlugin.MarkerVisibilityRequest',
+                  'MapMoveRequest',
+                  'ShowProgressSpinnerRequest',
+                  'GetRouteRequest',
+                  'GetFeedbackServiceRequest',
+                  'GetFeedbackServiceDefinitionRequest',
+                  'GetFeedbackRequest',
+                  'PostFeedbackRequest',
+                  'SearchRequest',
+                  'ChangeMapLayerOpacityRequest',
+                  'MyLocationPlugin.GetUserLocationRequest',
+                  'DrawTools.StartDrawingRequest',
+                  'DrawTools.StopDrawingRequest',
+                  'MapModulePlugin.ZoomToFeaturesRequest',
+                  'MapModulePlugin.MapLayerUpdateRequest'];
+          }
+          me._allowedFunctions = this.__arrayToObject(allowedFunctions);
+          // try to get event/request builder for each of these to see that they really are supported!!
+          me.__setupAvailableEvents(allowedEvents);
+          me.__setupAvailableRequests(allowedRequests);
         },
 
         /**
