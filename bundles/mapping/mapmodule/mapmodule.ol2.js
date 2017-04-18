cf95e9c629b0fef06e93ff1f88100a103f8689e6
Marko Kuosmanen <marko.kuosmanen@maanmittauslaitos.fi>
2017-04-18 14:05:58 +0300

    Now default opacity is same ol2 and ol3

--- bundles/mapping/mapmodule/mapmodule.ol2.js
+++ bundles/mapping/mapmodule/mapmodule.ol2.js
@@ -466,6 +466,8 @@ Oskari.clazz.define('Oskari.mapframework.ui.module.common.MapModule',
             olStyle.graphicWidth = size;
             olStyle.graphicHeight = size;
 
+            olStyle.fillOpacity = 0.8;
+
             // If svg marker
             if(me.isSvg(style.image)) {
                 var svg = this.getSvg(style.image);
