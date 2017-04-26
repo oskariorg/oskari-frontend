21843bf869d0226f276f94521ae35d31e391356f
Marko Kuosmanen <marko.kuosmanen@maanmittauslaitos.fi>
2017-04-26 10:48:07 +0300

    Fixed null error when editing published map

--- bundles/framework/maplegend/instance.js
+++ bundles/framework/maplegend/instance.js
@@ -254,8 +254,10 @@ Oskari.clazz.define("Oskari.mapframework.bundle.maplegend.MapLegendBundleInstanc
             return jQuery('#contentMap').hasClass('published');
         },
         refreshUI: function() {
-            var plugin = this.isEmbedded() ? (this.plugin || this.plugins['Oskari.userinterface.Flyout']): this.plugins['Oskari.userinterface.Flyout'];
-            plugin.refresh();
+            var plugin = this.isEmbedded() ? this.plugin : this.plugins['Oskari.userinterface.Flyout'];
+            if(plugin) {
+                plugin.refresh();
+            }
         },
         /**
          * @method createUi
