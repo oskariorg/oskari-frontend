d6b47c89ec00d8cea2bd6517a34272be8599c222
Sami Mäkinen <sami.makinen@nls.fi>
2017-05-11 16:24:15 +0300

    Bump version for hotfix

--- src/oskari.js
+++ src/oskari.js
@@ -7,7 +7,7 @@ Oskari = (function () {
     var _markers = [];
 
     return {
-        VERSION : "1.42.0",
+        VERSION : "1.42.1",
 
         /**
          * @public @static @method Oskari.setMarkers
