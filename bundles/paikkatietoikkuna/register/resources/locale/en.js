e281864baf11207217aa14e427cec1b8344e992b
Hanna Visuri <hanna.vellonen@gmail.com>
2017-06-01 06:10:32 +0300

    new loginbar

---
+++ bundles/paikkatietoikkuna/register/resources/locale/en.js
@@ -0,0 +1,20 @@
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "Register",
+    "value": {
+        "title": "Rekisteröidy",
+        "desc": "",
+        "login": "Kirjaudu",
+        "register": "Rekisteröidy",
+        "logout": "Log out",
+        "popup": {
+            "title": "Rekisteröidy Paikkatietoikkunaan",
+            "registerInfo": "Rekisteröityneenä käyttäjänä voit julkaista karttoja, käyttää analyysitoimintoa sekä tuoda kartalle omia aineistoja. Rekisteröityminen tapahtuu Maanmittauslaitoksen tunnistuspalvelussa. Rekisteröitymisen jälkeen sinut ohjataan takaisin Paikkatietoikkunaan.",
+            "registerInfo2": "Rekisteröitymällä hyväksyt Paikkatietoikkunan ",
+            "termsOfUseLink": "käyttöehdot",
+            "continueBtn": "Jatka rekisteröitymään",
+            "cancelBtn": "Peruuta"
+        }
+    }
+});
---
+++ packages/paikkatietoikkuna/bundle/register/bundle.js
@@ -0,0 +1,83 @@
+/**
+ * @class Oskari.mapframework.bundle.register.RegisterBundle
+ *
+ * Definition for bundle. See source for details.
+ */
+Oskari.clazz.define(
+    "Oskari.mapframework.bundle.register.RegisterBundle",
+    function () {}, {
+        "create": function () {
+            return Oskari.clazz.create("Oskari.mapframework.bundle.register.RegisterBundleInstance");
+        },
+        "update": function (manager, bundle, bi, info) {}
+    }, {
+        "protocol": ["Oskari.bundle.Bundle", "Oskari.mapframework.bundle.extension.ExtensionBundle"],
+        "source": {
+
+            "scripts": [{
+                "type": "text/javascript",
+                "src": "../../../../bundles/paikkatietoikkuna/register/instance.js"
+            }, {
+                "type": "text/css",
+                "src": "../../../../bundles/paikkatietoikkuna/register/resources/css/register.css"
+            }],
+
+            "locales": [{
+                "lang": "en",
+                "type": "text/javascript",
+                "src": "../../../../bundles/paikkatietoikkuna/register/resources/locale/en.js"
+            }, {
+                "lang": "fi",
+                "type": "text/javascript",
+                "src": "../../../../bundles/paikkatietoikkuna/register/resources/locale/fi.js"
+            }, {
+                "lang": "sv",
+                "type": "text/javascript",
+                "src": "../../../../bundles/paikkatietoikkuna/register/resources/locale/sv.js"
+            }]
+        },
+        "bundle": {
+            "manifest": {
+                "Bundle-Identifier": "register",
+                "Bundle-Name": "register",
+                "Bundle-Author": [{
+                    "Name": "tm",
+                    "Organisation": "nls.fi",
+                    "Temporal": {
+                        "Start": "2014",
+                        "End": "2020"
+                    },
+                    "Copyleft": {
+                        "License": {
+                            "License-Name": "EUPL",
+                            "License-Online-Resource": "http://www.paikkatietoikkuna.fi/license"
+                        }
+                    }
+                }],
+                "Bundle-Name-Locale": {
+                    "fi": {
+                        "Name": " style-1",
+                        "Title": " style-1"
+                    },
+                    "en": {}
+                },
+                "Bundle-Version": "1.0.0",
+                "Import-Namespace": ["Oskari", "jquery"],
+                "Import-Bundle": {}
+
+                /**
+                 *
+                 */
+
+            }
+        },
+
+        /**
+         * @static
+         * @property dependencies
+         */
+        "dependencies": ["jquery"]
+
+    });
+
+Oskari.bundle_manager.installBundleClass("register", "Oskari.mapframework.bundle.register.RegisterBundle");
