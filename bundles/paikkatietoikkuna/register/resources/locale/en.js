639c387480822a0271c47055cd17eda21ae99595
Hanna Visuri <hanna.vellonen@gmail.com>
2017-06-07 10:47:49 +0300

    unnecessary localizations removed and english localizations updated

--- bundles/paikkatietoikkuna/register/resources/locale/en.js
+++ bundles/paikkatietoikkuna/register/resources/locale/en.js
@@ -3,18 +3,18 @@ Oskari.registerLocalization(
     "lang": "en",
     "key": "Register",
     "value": {
-        "title": "Rekisteröidy",
+        "title": "Register",
         "desc": "",
-        "login": "Kirjaudu",
-        "register": "Rekisteröidy",
-        "logout": "Log out",
+        "login": "Sign in",
+        "register": "Register",
+        "logout": "Sign out",
         "popup": {
-            "title": "Rekisteröidy Paikkatietoikkunaan",
-            "registerInfo": "Rekisteröityneenä käyttäjänä voit julkaista karttoja, käyttää analyysitoimintoa sekä tuoda kartalle omia aineistoja. Rekisteröityminen tapahtuu Maanmittauslaitoksen tunnistuspalvelussa. Rekisteröitymisen jälkeen sinut ohjataan takaisin Paikkatietoikkunaan.",
-            "registerInfo2": "Rekisteröitymällä hyväksyt Paikkatietoikkunan ",
-            "termsOfUseLink": "käyttöehdot",
-            "continueBtn": "Jatka rekisteröitymään",
-            "cancelBtn": "Peruuta"
+            "title": "Sign Up to Paikkatietoikkuna",
+            "registerInfo": "As a registered user, you can publish embedded maps, use analysis function and import your own datasets. Signing up is done in the service hosted by the National Land Survey of Finland. After submitting your registration, you will be automatically forwarded back to Paikkatietoikkuna.",
+            "registerInfo2": "By signing up, you agree to our ",
+            "termsOfUseLink": "Terms of Use",
+            "continueBtn": "Continue",
+            "cancelBtn": "Cancel"
         }
     }
 });
