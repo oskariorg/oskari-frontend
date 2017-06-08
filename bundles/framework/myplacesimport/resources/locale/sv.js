4bca2dff6b97d44b12b4ff822dd88397c3c3a7d0
okauppinen <olli.kauppinen@nls.fi>
2017-06-08 11:48:32 +0300

    added error messages and some localization changes

--- bundles/framework/myplacesimport/resources/locale/sv.js
+++ bundles/framework/myplacesimport/resources/locale/sv.js
@@ -10,18 +10,18 @@ Oskari.registerLocalization(
         },
         "flyout": {
             "title": "Dataset importering",
-            "description": "Du kan importera dina egna dataset i shape-, gpx- eller mif/mid filformat i zip-paketet eller kmz filformat. Du kan importera filer med högst 5000 objekt.",
-            "help": "Välj en fil från din dator eller ge en länk till den fil som innehåller ditt dataset. Filen kan vara i shp, zip eller kml / kmz-format. Du kan skapa zip-filer från shapefiler genom zippa filerna med ändelsen shp, SHX, DBF och PRJ till samma zip-fil. \nOckså kml / KMZ-filer från Google maps kan kopplas på samma sätt. \nMif/mid data CRS måste vara kartans CRS",
+            "description": "Importera en datamängd från din dator som en zip-fil, vilken innehåller alla erforderliga filer utav ett av de följande filformatsalternativen:<ul><li>Shapefile (.shp, .shx och .dbf, frivilligt .prj och .cpg)</li><li>GPX (.gpx)</li><li>MapInfo (.mif och .mid)</li><li>Google Map (.kml eller .kmz)</li></ul>Zip-filen kan innehålla endast en datamängd och dess storlek kan vara högst <xx> Mb.",
+            "help": "Importera en datamängd från din dator som ett zip-paket. Var vänlig och kontrollera, att alla filerna är i ett lämpligt filformat och koordinatsystem.",
             "actions": {
                 "cancel": "Avbryt",
-                "next": "Nästa"
+                "next": "Nästa",
+                "close": "Stäng"
             },
             "file": {
                 "submit": "Skicka",
                 "fileOverSizeError": {
                     "title": "Fel",
-                    "message": "Din datamängd är för stor. Datamängden kan vara högst <xx> mb.",
-                    "close": "Stäng"
+                    "message": "Din datamängd är för stor. Datamängden kan vara högst <xx> mb."
                 }
             },
             "layer": {
@@ -45,7 +45,27 @@ Oskari.registerLocalization(
                 "failure": {
                     "title": "Dataset importen lyckades inte. Försök på nytt senare."
                 }
-            }
+            },
+            "error":{
+                "title": "Importerningen av datamängden misslyckades.",
+                "unknown_projection": "Datamängdens koordinatsystem kunde inte identifieras. Kontrollera att datamängden är antingen i kartans koordinatsystem eller försäkra, att datamängden innehåller de nödvändiga uppgifterna av koordinatsystemet.",
+                "invalid_file": "Lämpliga filer för importeringen kunde inte hittas från zip-filen. Var vänlig och kontrollera att filformatet understött och att datamänderna är packade till en zip-fil.",
+                "unable_to_store_data": "Objekten kunde inte sparas till databasen eller den inmatade datamängden innehöll inga objekt.",
+                "short_file_prefix": "Hämtningen av datamängderna från zip-filen misslyckades. Kontrollera, att prefixerna av de packade filerna innehåller åtminstone tre tecken.",
+                "file_over_size": "Den valda filen är för stor. Den högsta tillåtna storleken är <xx> Mb.",
+                "malformed": "Kontrollera, att filnamnen inte innehåller diakritiska tecken (t.ex. bokstäverna Å,Ä,Ö).",
+                "kml": "Ett kartlager kunde inte skapas från KML-filen.",
+                "shp": "Ett kartlager kunde inte skapas från SHP-filen.",
+                "mif": "Ett kartlager kunde inte skapas från MIF-filen.",
+                "gpx": "Ett kartlager kunde inte skapas från GPX-filen.",
+                "timeout": "Importeringen av datamändgen kunde inte slutföras på grund av tidutlösning.",
+                "abort": "Importeringen av datamängden avbröts.",
+                "parsererror": "Datamängden kunde inte behandlas.",
+                "generic": "Ett okänt fel uppstod i systemet. Importerningen av datamängden misslyckades."
+            },
+            "warning":{
+                "features_skipped":"OBS! <xx> objekt övergavs vid importeringen på grund av saknande eller felaktiga koordinater eller geometri."
+            },
         },
         "tab": {
             "title": "Dataset",
