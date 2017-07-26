eb12d70b83297dfa00532fd4d14a032ad8cfe12a
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-07-26 15:01:46 +0300

    pass the instance instead of sandbox

--- bundles/statistics/statsgrid2016/Flyout.js
+++ bundles/statistics/statsgrid2016/Flyout.js
@@ -65,7 +65,7 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.Flyout',
           if( this.datacharts ) {
             return this.datacharts;
           }
-          this.datacharts = Oskari.clazz.create('Oskari.statistics.statsgrid.DataVisualizer', Oskari.getSandbox(), this.instance.getLocalization());
+          this.datacharts = Oskari.clazz.create('Oskari.statistics.statsgrid.DataVisualizer', this.instance, this.instance.getLocalization());
           return this.datacharts;
         },
         showDataCharts: function () {
