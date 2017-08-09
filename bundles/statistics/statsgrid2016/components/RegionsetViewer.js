4d0c0c035cf10f33bb28b9da9577e13814b8570a
Marko Kuosmanen <marko.kuosmanen@nls.fi>
2017-08-09 12:44:07 +0300

    Added thematic map vectorlayer to Oskari layer and selected layer

--- bundles/statistics/statsgrid2016/components/RegionsetViewer.js
+++ bundles/statistics/statsgrid2016/components/RegionsetViewer.js
@@ -121,7 +121,12 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.RegionsetViewer', function(inst
                         featureStyle: defaultFeatureStyle,
                         optionalStyles: optionalStyles,
                         layerId: me.LAYER_ID,
-                        prio: index
+                        prio: index,
+                        showLayer: true,
+                        opacity: 80,
+                        layerName: locale.layer.name,
+                        layerInspire: locale.layer.inspireName,
+                        layerOrganizationName: locale.layer.organizationName
                     }];
 
 
@@ -141,7 +146,6 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.RegionsetViewer', function(inst
         var style = null;
         var strokeWidth = (highlightRegion && (highlightRegion.toString() === region.toString())) ? 4 : 1;
         var strokeColor = Oskari.util.isDarkColor('#'+color) ? '#ffffff' : '#000000';
-        var opacity = (classification.transparency) ? (parseFloat(classification.transparency))/100 : 0.8;
         if(mapStyle === 'points') {
             var svg = me._pointSymbol.clone();
             svg.attr('width', 64);
@@ -160,7 +164,7 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.RegionsetViewer', function(inst
                     key: 'id'
                 },
                 image: {
-                    opacity: opacity,
+                    opacity: 1,
                     shape:{
                         data: svg.html(),
                         x: 32,
@@ -184,7 +188,7 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.RegionsetViewer', function(inst
                     width: strokeWidth
                 },
                 image: {
-                    opacity: 0.8
+                    opacity: 1
                 }
             };
         }
