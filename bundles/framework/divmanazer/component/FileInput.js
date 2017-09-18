9a5071a90c506af41dd6408a3c4f55c26c2ba0fc
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-09-18 16:04:22 +0300

    window.webkitRequestFileSystem supported only by chrome so implementation changed

--- bundles/framework/divmanazer/component/FileInput.js
+++ bundles/framework/divmanazer/component/FileInput.js
@@ -139,60 +139,18 @@ Oskari.clazz.define('Oskari.userinterface.component.FileInput', function (locali
         },
         exportToFile: function ( data, filename ) {
             var me = this;
-            function onInitFs(fs) {
-
-            fs.root.getFile( filename, {create: true}, function(fileEntry) {
-                console.log(fs.root);
-
-                // Create a FileWriter object for our FileEntry (log.txt).
-                fileEntry.createWriter( function( fileWriter ) {
-
-                fileWriter.onwriteend = function(e) {
-                    console.log('Write completed.');
-                };
-
-                fileWriter.onerror = function(e) {
-                    console.log('Write failed: ' + e.toString());
-                };
-
-                // Create a new Blob and write it to log.txt.
-                var blob = new Blob(['Lorem Ipsum'], {type: 'text/plain'});
-
-                fileWriter.write(blob);
-
-                }, me.errorHandler);
-
-            }, me.errorHandler);
-
+            var blob = new Blob([data], {type: 'text'});
+            if( window.navigator.msSaveOrOpenBlob ) {
+                window.navigator.msSaveBlob(blob, filename);
             }
-
-            window.webkitRequestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, me.errorHandler);
-        },
-         errorHandler: function( e ) {
-            var msg = '';
-
-            switch (e.code) {
-                case FileError.QUOTA_EXCEEDED_ERR:
-                msg = 'QUOTA_EXCEEDED_ERR';
-                break;
-                case FileError.NOT_FOUND_ERR:
-                msg = 'NOT_FOUND_ERR';
-                break;
-                case FileError.SECURITY_ERR:
-                msg = 'SECURITY_ERR';
-                break;
-                case FileError.INVALID_MODIFICATION_ERR:
-                msg = 'INVALID_MODIFICATION_ERR';
-                break;
-                case FileError.INVALID_STATE_ERR:
-                msg = 'INVALID_STATE_ERR';
-                break;
-                default:
-                msg = 'Unknown Error';
-                break;
-            };
-
-            console.log('Error: ' + msg);
+            else {
+                var elem = window.document.createElement('a');
+                elem.href = window.URL.createObjectURL(blob);
+                elem.download = filename;        
+                document.body.appendChild(elem);
+                elem.click();        
+                document.body.removeChild(elem);
+            }
         }
 }, {
 
