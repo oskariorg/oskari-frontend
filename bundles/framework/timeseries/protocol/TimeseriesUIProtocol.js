b0c8b75eedd5193589faad95e20e13809532e153
Jan Wolski <jan@wolski.fi>
2017-10-10 13:31:06 +0300

    TimeseriesUIProtocol

---
+++ bundles/framework/timeseries/protocol/TimeseriesUIProtocol.js
@@ -0,0 +1,52 @@
+/**
+ * @class Oskari.mapframework.bundle.timeseries.TimeseriesUIProtocol
+ *
+ * Interface/protocol definition for Timeseries Control UI delegate
+ */
+Oskari.clazz.define('Oskari.mapframework.bundle.timeseries.TimeseriesUIProtocol',
+/**
+ * @method create called automatically on construction
+ * @static
+ *
+ * Implement protocol, never instantiate
+ */
+function() {
+    throw new Error("Oskari.mapframework.bundle.timeseries.TimeseriesUIProtocol should not be instantiated");
+}, {
+    /**
+     * @method getTimes
+     * Returns all available time instants that can be shown
+     * @return {String[]} list of available timeseries times
+     * @throws always override this
+     */
+    getTimes : function() {
+        throw new Error("Implement your own");
+    },
+    /**
+     * @method getCurrentTime
+     * Returns current selected time instant
+     * @return {String} current time as ISO-string
+     * @throws always override this
+     */
+    getCurrentTime : function() {
+        throw new Error("Implement your own");
+    },
+    /**
+     * @method requestNewTime
+     * Requests change in current selected time
+     * @param {function} doneCallback callback that will be called after new time has been loaded
+     * @throws always override this
+     */
+    requestNewTime : function(doneCallback) {
+        throw new Error("Implement your own");
+    },
+    /**
+     * @method registerStateUpdate
+     * Registers state update function that implementing class can use to update UI state
+     * @param {function} callback 
+     * @throws always override this
+     */
+    registerStateUpdate : function(callback) {
+        throw new Error("Implement your own");
+    }
+});
