59472e47047a339f6852b968468a6fadc4515a02
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-10-13 16:12:02 +0300

    ol3

--- packages/framework/bundle/heatmap/bundle.js
+++ packages/framework/bundle/heatmap/bundle.js
@@ -29,7 +29,7 @@ Oskari.clazz.define("Oskari.mapframework.bundle.heatmap.HeatmapBundle", function
             "src": "../../../../bundles/framework/heatmap/HeatmapDialog.js"
         }, {
             "type": "text/javascript",
-            "src": "../../../../bundles/framework/heatmap/plugin/HeatmapLayerPlugin.js"
+            "src": "../../../../bundles/framework/heatmap/plugin/HeatmapLayerPlugin.ol3.js"
         }, {
             "type": "text/javascript",
             "src": "../../../../bundles/framework/heatmap/domain/HeatmapLayer.js"
