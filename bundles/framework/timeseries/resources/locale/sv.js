67af42af1c829b76b36e7234e26e491df0b81ffc
Jan Wolski <jan@wolski.fi>
2017-10-24 15:32:01 +0300

    Swedish localization

--- bundles/framework/timeseries/resources/locale/sv.js
+++ bundles/framework/timeseries/resources/locale/sv.js
@@ -5,6 +5,29 @@ Oskari.registerLocalization(
     "value": {
         "title": "",
         "desc": "",
-        "display": {}
+        "label": {
+            "animationSpeed": "Animering hastighet",
+            "skipAhead": "Hoppa över"
+        },
+        "animationSpeed": {
+            "slow": "Långsamt",
+            "normal": "Normal",
+            "fast": "Snabb"
+        },
+        "skip": {
+            "none": "Ingen",
+            "minute": "1 minut",
+            "hour": "1 timme",
+            "day": "1 dag",
+            "week": "1 vecka",
+            "month": "1 månad"
+        },
+        "dateRender": "{val, date, short} {val, time, short}",
+        "alert": {
+            "title": "Obs!",
+            "message": "Du har flera valda tidsserie kartlager. Bara den placerad högst upp kan kotrolleras.",
+            "ok": "OK"
+        }
     }
 });
+    
