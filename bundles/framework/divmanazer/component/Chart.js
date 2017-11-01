dce23d5f9a2bf5d89449091f1cd6c71512ad518c
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-11-01 09:22:06 +0200

    refactor

--- bundles/framework/divmanazer/component/Chart.js
+++ bundles/framework/divmanazer/component/Chart.js
@@ -17,16 +17,16 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
     _template: {
         graph: jQuery('<div id="graphic" style="width:100%"></div>')
     },
-    _checkColors: function(opts){
+    _checkColors: function ( opts ) {
         var options = opts || {};
         options.colors = options.colors || this._options.colors;
-        if(typeof options.colors === 'string') {
+        if ( typeof options.colors === 'string' ) {
             options.colors = [options.colors];
-        } else if(options.colors){
+        } else if ( options.colors ) {
             options.colors = options.colors;
         }
     },
-    chartIsInitialized: function() {
+    chartIsInitialized: function () {
         return this.svg !== null;
     },
     sortData: function () {
@@ -59,38 +59,41 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
         return dimensions;
     },
     createGridlines: function () {
-        return d3.axisBottom(this.x).ticks(10);
+        return d3.axisBottom( this.x ).ticks(10);
     },
     initScales: function () {
         this.x = d3.scaleLinear()
-        .domain([ d3.min( this.data, function (d) {
+        .domain([ d3.min( this.data, function ( d ) {
                 return d.value;
-            }), d3.max( this.data, function (d) {
+            }), d3.max( this.data, function ( d ) {
                 return d.value;
             })
         ]);
 
         this.y = d3.scaleBand()
-        .domain(this.data.map(function (d) {
+        .domain( this.data.map ( function ( d ) {
             return d.name;
         }));
     },
-    initSVG: function() {
-            var svg = d3.select(this._template.graph[0]).append("svg")
-                .attr("width", this.dimensions.width() + this.dimensions.margin.left + this.dimensions.margin.right)
-                .attr("height", this.dimensions.height() + this.dimensions.margin.top + this.dimensions.margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + this.dimensions.margin.left + "," + this.dimensions.margin.top + ")"
-            );
-            this.svg = svg;
+    initSVG: function () {
+        var svg = d3.select( this._template.graph[0] ).append("svg")
+            .attr( "width", this.dimensions.width() + this.dimensions.margin.left + this.dimensions.margin.right )
+            .attr( "height", this.dimensions.height() + this.dimensions.margin.top + this.dimensions.margin.bottom )
+            .append( "g" )
+            .attr( "transform", "translate(" + this.dimensions.margin.left + "," + this.dimensions.margin.top + ")" );
+        this.svg = svg;
         return svg;
     },
+    /**
+     * d3 axis are functions that generate svg-elements based on the scale given
+     * @method initAxis
+     *
+     */
     initAxis: function () {
-        // Init axes
-        this.yAxis = d3.axisLeft(this.y)
+        this.yAxis = d3.axisLeft( this.y )
         .tickSize(10);
 
-        this.xAxis = d3.axisBottom(this.x)
+        this.xAxis = d3.axisBottom( this.x )
         .tickSize(10);
     },
     /**
@@ -102,7 +105,7 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
     initChart: function () {
         var svg = this.initSVG();
         var scales = this.initScales();
-        var chart = this.chart(svg);
+        var chart = this.chart( svg );
         return chart;
     },
     /**
@@ -111,7 +114,7 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
      * @method handleData
      * @param  [] data only supports following format: [ { name: "", value: int } ]
      */
-    handleData: function( data ) {
+    handleData: function ( data ) {
         this.data = data;
         this.sortData( this.data );
     },
@@ -121,11 +124,8 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
      * @method parseOptions
      * @param  {} options
      */
-    parseOptions: function (options) {
-        if( options.activeIndicator ) {
-            this.activeIndicator = options.activeIndicator;
-        }
-        if(options.width) {
+    parseOptions: function ( options ) {
+        if( options.width ) {
             this.containerWidth = options.width;
         }
     },
@@ -137,29 +137,32 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
         var padding = this.dimensions.label.padding;
         var lblCenterPadding = this.dimensions.label.verticalCenterPadding;
         //groups
-        var gy = this.svg.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(0," + (padding + lblCenterPadding) + ")")
-            .call(this.yAxis);
+        var gy = this.svg.append( "g" )
+            .attr( "class", "y axis" )
+            .attr( "transform", "translate(0," + (padding + lblCenterPadding) + ")" )
+            .call( this.yAxis );
 
-        var gx = this.svg.append("g")
-            .attr("class", "x axis")
-            .call(this.xAxis);
+        var gx = this.svg.append( "g" )
+            .attr( "class", "x axis" )
+            .call( this.xAxis );
     },
+    /**
+     * @method setColorScale
+     */
     setColorScale: function ( colors ) {
         this.colorScale = d3.scaleQuantize()
-            .domain([0, this.data.length])
-            .range(colors);
+            .domain( [0, this.data.length] )
+            .range( colors );
     },
     /**
      * handles data & options passed to it, initializes skeleton chart and then applies barchart specific options to the element
      * @method createBarChart
-     * @param [Array] data, the indicator data
+     * @param [Array] data
      * @param { Object } options if options color is passed it needs to be an array for d3 to apply it
      */
     createBarChart: function ( data, options ) {
         if( data != undefined ) {
-            this.handleData(data);
+            this.handleData( data );
         }
         var opts = options || this._options;
 
@@ -182,9 +185,9 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
             .attr("class", "bar")
             .attr("text-anchor", "middle")
             .attr("y", function (d) {
-                return me.y(d.name) + me.y.bandwidth() / 2 + me.dimensions.label.padding;
+                return me.y( d.name ) + me.y.bandwidth() / 2 + me.dimensions.label.padding;
             })
-            .style('fill',function(d,i){ return me.colorScale(i); })
+            .style('fill', function( d,i ){ return me.colorScale(i); })
             .attr("height", 15)
             .attr("x", 0)
             .attr("width", function (d) {
@@ -201,26 +204,26 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
      */
     createLineChart: function ( data, options ) {
         if( data != undefined ) {
-            this.handleData(data);
+            this.handleData( data );
         }
 
         var opts = options || this._options;
-        if( Object.keys(opts).length !== 0 ) {
+        if ( Object.keys( opts ).length !== 0 ) {
             this.parseOptions( opts );
         }
-        this._checkColors(opts);
-        this.setColorScale(opts.colors);
+        this._checkColors( opts );
+        this.setColorScale( opts.colors );
         this.initChart();
         var me = this;
         var linegen = d3.line()
-            .x(function(d) { return me.x(d.value); })
-            .y(function(d) { return me.y(d.name); });
+            .x(function(d) { return me.x( d.value ); })
+            .y(function(d) { return me.y( d.name ); });
 
         this.svg.append('path')
-            .attr('d', linegen(this.data))
-            .attr('stroke', function(d,i){ return me.colorScale(i); })
-            .attr('stroke-width', 2)
-            .attr('fill', 'none');
+            .attr( 'd', linegen( this.data ) )
+            .attr( 'stroke', function( d,i ){ return me.colorScale( i ); } )
+            .attr( 'stroke-width', 2 )
+            .attr( 'fill', 'none' );
 
         this.chartType = 'linechart';
         return this.getGraph();
@@ -230,13 +233,13 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
      * @method chart
      * @param  d3 svg-element
      */
-    chart: function (selection) {
+    chart: function ( selection ) {
         var me = this;
         //update x & y scales
-        this.y.range([ this.dimensions.height(), 0 ]);
-        this.x.range([ 0, this.dimensions.width() ]);
+        this.y.range( [ this.dimensions.height(), 0 ] );
+        this.x.range( [ 0, this.dimensions.width() ] );
 
-        selection.each( function (data) {
+        selection.each( function ( data ) {
             // this._g = me.svg.select(this).append('g');
             me.initAxis();
             me.callGroups();
@@ -245,8 +248,8 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
                 .attr("class", "grid")
                 .attr("text-anchor", "middle")
                 .attr("transform", "translate(0," +  me.dimensions.height() + ")")
-                .call(me.createGridlines()
-                .tickSize(-me.dimensions.height() +30, 0, 0)
+                .call( me.createGridlines()
+                .tickSize( -me.dimensions.height() +30, 0, 0 )
                 .tickFormat("")
             );
         });
@@ -259,10 +262,10 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
      * @method redraw
      * @param  [data]
      */
-    redraw: function (data, options) {
+    redraw: function ( data, options ) {
         var chart;
         if( data != undefined ) {
-            this.handleData(data);
+            this.handleData( data );
         }
 
         var opts = options || {};
@@ -270,9 +273,9 @@ Oskari.clazz.define('Oskari.userinterface.component.Chart', function(sandbox, lo
         //Clear previous graphs
         this.clear();
         if( this.chartType === 'barchart' ) {
-            chart = this.createBarChart(this.data, opts);
+            chart = this.createBarChart( this.data, opts );
         } else if( this.chartType === 'linechart' ) {
-            chart = this.createLineChart(this.data, opts);
+            chart = this.createLineChart( this.data, opts );
         }
 
         return chart;
