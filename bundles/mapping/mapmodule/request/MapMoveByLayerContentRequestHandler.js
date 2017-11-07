1919de3ad5e93309a4ff8bdd7969ac511c97b36f
okauppinen <olli.kauppinen@nls.fi>
2017-11-07 17:09:28 +0200

    added zoomToExtent functionality to handler

--- bundles/mapping/mapmodule/request/MapMoveByLayerContentRequestHandler.js
+++ bundles/mapping/mapmodule/request/MapMoveByLayerContentRequestHandler.js
@@ -3,6 +3,7 @@
  * Provides functionality to:
  * - Move the map to zoom level that is in scale with the requested maplayer.
  * - Move the map to given layers geometry if it has one.
+ * - Optionally if zoomToExtent is true, then move and zoom the map to given layer's extent
  */
 Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.request.MapMoveByLayerContentRequestHandler',
     /**
@@ -22,6 +23,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.request.MapMoveByLayer
          * @method handleRequest
          * Moves the map to zoom level that is in scale with the requested maplayer.
          * Also moves the map to given layers geometry if it has one.
+         * Optionally if zoomToExtent is true, then move and zoom the map to given layer's extent
          * @param {Oskari.mapframework.core.Core} core
          *      reference to the application core (reference sandbox core.getSandbox())
          * @param {Oskari.mapframework.bundle.mapmodule.request.MapMoveByLayerContentRequest} request
@@ -29,24 +31,35 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.request.MapMoveByLayer
          */
         handleRequest: function (core, request) {
             var layerId = request.getMapLayerId();
+            var zoomToExtent = request.getZoomToExtent();
             var layer = this.sandbox.findMapLayerFromSelectedMapLayers(layerId);
             if (!layer) {
                 return;
             }
 
-            // set zoom level by layer scales
-            var newZoom = this.layersPlugin.getMapModule().getClosestZoomLevel(layer.getMinScale(), layer.getMaxScale());
-            // suppress mapmove-event here and send it after we have possibly also moved the map
-            this.layersPlugin.getMapModule().setZoomLevel(newZoom, true);
-
-            // move map to geometries if available
-            // this needs to be done after the zoom since it's comparing to viewport which changes in zoom
-            if (layer.getGeometry().length > 0) {
-                var containsGeometry = this.layersPlugin.isInGeometry(layer);
-                // only move if not currently in geometry
-                if (!containsGeometry) {
-                    var center = layer.getGeometry()[0].getCentroid();
-                    this.layersPlugin.getMapModule().moveMapToLonLat({ lon: center.x, lat : center.y});
+            if (zoomToExtent){
+                // move and zoom map to layer extent
+                if (layer.getGeometry().length > 0){
+                    var bounds = this.layersPlugin.getGeometryBounds(layer.getGeometry()[0]);
+                    // suppress mapmove-event
+                    this.layersPlugin.getMapModule().zoomToExtent(bounds, true, true);
+                    var center = this.layersPlugin.getGeometryCenter(layer.getGeometry()[0]);
+                    this.layersPlugin.getMapModule().moveMapToLonLat(center);
+                }
+            } else{
+                // set zoom level by layer scales
+                var newZoom = this.layersPlugin.getMapModule().getClosestZoomLevel(layer.getMinScale(), layer.getMaxScale());
+                // suppress mapmove-event here and send it after we have possibly also moved the map
+                this.layersPlugin.getMapModule().setZoomLevel(newZoom, true);
+                // move map to geometries if available
+                // this needs to be done after the zoom since it's comparing to viewport which changes in zoom
+                if (layer.getGeometry().length > 0) {
+                    var containsGeometry = this.layersPlugin.isInGeometry(layer);
+                    // only move if not currently in geometry
+                    if (!containsGeometry) {
+                        var center = this.layersPlugin.getGeometryCenter(layer.getGeometry()[0]);
+                        this.layersPlugin.getMapModule().moveMapToLonLat(center);
+                    }
                 }
             }
             // notify components that the map has moved
