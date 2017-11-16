125d28d8cc00e891d8ba6337ce052742aac0e3aa
Jan Wolski <jan@wolski.fi>
2017-11-16 16:36:47 +0200

    Geometry operations

--- bundles/framework/geometrycutter/GeometryProcessor.js
+++ bundles/framework/geometrycutter/GeometryProcessor.js
@@ -4,25 +4,73 @@
 Oskari.clazz.define('Oskari.mapframework.bundle.geometrycutter.GeometryProcessor', function () {
     this._reader = new jsts.io.GeoJSONReader();
     this._writer = new jsts.io.GeoJSONWriter();
+    this._geomFactory = new jsts.geom.GeometryFactory();
 }, {
     splitByLine: function(sourceFeature, drawnFeature) {
-        var source = this._reader.read(sourceGeometry);
-        var drawn = this._reader.read(drawnGeometry);
+        var source = this._reader.read(sourceFeature.geometry);
+        var drawn = this._reader.read(drawnFeature.geometry);
+        var sourceType = source.getGeometryType();
 
-        switch (source.getGeometryType()) {
-            case 'jsts.geom.Polygon':
-                    // use polygonizer
+        switch (sourceType) {
+            case 'MultiPolygon':
+            case 'Polygon':
+                return this._polygonSplit(source, drawn);
+            case 'MultiLineString':
+            case 'LineString':
+                return this._lineSplit(source, drawn);
+            default:
+                throw new Error('Unsupported split source geometry: ' + sourceType);
+        }
+    },
+    _polygonSplit: function(sourceGeometry, drawnGeometry) {
+        var linework = sourceGeometry.getBoundary().union(drawnGeometry);
+        var polygonizer = new jsts.operation.polygonize.Polygonizer();
+        polygonizer.add(linework);
+
+        var polygons = polygonizer.getPolygons();
+        var out = [];
+        for (var i = polygons.iterator(), c = 0; i.hasNext(); c++) {
+            var polygon = i.next();
+            var point = polygon.getInteriorPoint();
+            if(sourceGeometry.contains(point)) {
+                out.push({
+                    type: 'Feature',
+                    geometry: this._writer.write(polygon),
+                    properties: {
+                        id: c
+                    }
+                });
+            }
+        }
+        return out;
+    },
+
+    _lineSplit: function(sourceGeometry, drawnGeometry) {
+        var splitted = sourceGeometry.difference(drawnGeometry);
+        var parts = [];
+        switch (splitted.getGeometryType()) {
+            case 'LineString':
+                parts.push(splitted);
                 break;
-            case 'jsts.geom.MultiPolygon':
-                break;
-            case 'jsts.geom.LineString':
+            case 'MultiLineString':
+                for (let i = 0; i < splitted.getNumGeometries(); i++) {
+                    parts.push(splitted.getGeometryN(i));
+                }
                 break;
             default:
-                break;
+                throw new Error('Unsupported split geometry result!');
         }
-        
-
+        return parts.map(function(part, i) {
+            return {
+                type: 'Feature',
+                geometry: this._writer.write(part),
+                properties: {
+                    id: i
+                }
+            }
+        }, this);
     },
+
     clipByPolygon: function(sourceFeature, drawnFeature) {
         var source = this._reader.read(sourceFeature.geometry);
         var drawn = this._reader.read(drawnFeature.geometry);
