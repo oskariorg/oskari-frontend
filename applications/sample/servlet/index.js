ff5e9ff24e6216f9c3d4083de494d7980c676897
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2017-12-01 10:40:46 +0200

    application/sample/index.js was accidentally commited, this returns it to original state

--- applications/sample/servlet/index.js
+++ applications/sample/servlet/index.js
@@ -1,13 +1,5 @@
 jQuery(document).ready(function() {
     Oskari.app.loadAppSetup(ajaxUrl + 'action_route=GetAppSetup', window.controlParams, function() {
         jQuery('#mapdiv').append('Unable to start');
-    }, function () {
-
-    }, function (appSetup){
-                  var l = appSetup.startupSequence.length;
-            appSetup.startupSequence[l] = {
-                "bundlename":"coordinateconversion" ,
-            }
-            appSetup.startupSequence[l].metadata= { "Import-Bundle": { "coordinateconversion": { "bundlePath": "/Oskari/packages/paikkatietoikkuna/bundle/" } } };
     });
 });
