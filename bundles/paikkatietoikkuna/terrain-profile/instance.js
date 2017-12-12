24f1a25b03b24649321421da74b4ed2759e0226f
Jan Wolski <jan@wolski.fi>
2017-12-12 17:13:17 +0200

    Basic bundle

---
+++ bundles/paikkatietoikkuna/terrain-profile/instance.js
@@ -0,0 +1,129 @@
+Oskari.clazz.define('Oskari.mapframework.bundle.terrain-profile.TerrainProfileBundleInstance',
+    function () {
+        this.active = false;
+        this.feature = null;
+        this.popup = null;
+    },
+    {
+        __name: 'TerrainProfile',
+        _startImpl: function (sandbox) {
+            var addToolButtonBuilder = Oskari.requestBuilder('Toolbar.AddToolButtonRequest');
+            var buttonConf = {
+                iconCls: 'upload-material',
+                tooltip: 'Terrain height profile',
+                sticky: true,
+                callback: function (el) { }
+            };
+            sandbox.request(this, addToolButtonBuilder('TerrainProfile', 'basictools', buttonConf));
+        },
+        setActive: function (activeState) {
+            if (activeState) {
+                if (this.active) {
+                    return;
+                }
+                this.popup = this.createPopup();
+                this.startDrawing();
+                this.active = true;
+            } else {
+                if (!this.active) {
+                    return;
+                }
+                this.popup.close();
+                this.stopDrawing();
+                this.popup = null;
+                this.feature = null;
+                this.active = false;
+            }
+        },
+        cancelTool: function () {
+            var builder = Oskari.requestBuilder('Toolbar.SelectToolButtonRequest');
+            this.sandbox.request(this, builder());
+        },
+        createPopup: function () {
+            var me = this;
+            var title = 'Terrain Profile';
+            var popup = Oskari.clazz.create('Oskari.userinterface.component.Popup');
+            var buttons = [];
+            var cancelBtn = Oskari.clazz.create('Oskari.userinterface.component.buttons.CancelButton');
+
+            cancelBtn.setHandler(function () {
+                me.cancelTool();
+            });
+            buttons.push(cancelBtn);
+
+            var queryButton = Oskari.clazz.create('Oskari.userinterface.component.buttons.OkButton');
+
+            queryButton.setTitle('Show profile');
+            queryButton.setHandler(function () {
+                me.doQuery();
+            });
+            buttons.push(queryButton);
+
+            var content = jQuery('<div></div>');
+
+            popup.show(title, content, buttons);
+            popup.addClass('myplaces2');
+            popup.moveTo('#toolbar div.tool[tool=TerrainProfile]', 'top');
+
+            return popup;
+        },
+        startDrawing: function () {
+            var builder = Oskari.requestBuilder('DrawTools.StartDrawingRequest');
+            this.sandbox.request(this, builder(this.__name, 'LineString', {modifyControl: true}));
+        },
+        stopDrawing: function () {
+            var builder = Oskari.requestBuilder('DrawTools.StopDrawingRequest');
+            this.sandbox.request(this, builder(this.__name, true));
+        },
+        doQuery: function () {
+            if (!this.feature) {
+                return;
+            }
+            this.feature.properties = {numPoints: 100};
+            var url = this.sandbox.getAjaxUrl() + 'action_route=TerrainProfile&route=' + encodeURIComponent(JSON.stringify(this.feature));
+            jQuery.ajax({
+                type: 'GET',
+                dataType: 'json',
+                url: url,
+                success: function (response) {
+                    console.log(response);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+
+                }
+            });
+        },
+        eventHandlers: {
+            'DrawingEvent': function (event) {
+                var drawId = event.getId();
+                if (drawId !== this.__name) {
+                    return;
+                }
+                if (event.getIsFinished()) {
+                    this.feature = event.getGeoJson().features[0];
+                    this.doQuery();
+                } else {
+                    var sketch = event.getGeoJson().features[0];
+                    if (!sketch || sketch.geometry.coordinates.length <= 2) {
+                        this.feature = null;
+                        return;
+                    }
+                    var feature = {type: 'Feature', geometry: {type: 'LineString'}};
+                    feature.geometry.coordinates = sketch.geometry.coordinates.slice(sketch.geometry.coordinates.length - 1);
+                    this.feature = feature;
+                }
+            },
+            'Toolbar.ToolSelectedEvent': function (event) {
+                if (event.getToolId() === 'TerrainProfile') {
+                    this.setActive(true);
+                } else {
+                    this.setActive(false);
+                }
+            }
+        }
+    },
+    {
+        extend: ['Oskari.mapframework.bundle.terrain-profile.BundleModule'],
+        protocol: ['Oskari.bundle.BundleInstance', 'Oskari.mapframework.module.Module']
+    }
+);
---
+++ packages/paikkatietoikkuna/bundle/terrain-profile/bundle.js
@@ -0,0 +1,83 @@
+/**
+ * @class Oskari.mapframework.bundle.terrain-profile.TerrainProfileBundle
+ *
+ * Definition for bundle. See source for details.
+ */
+Oskari.clazz.define(
+    'Oskari.mapframework.bundle.terrain-profile.TerrainProfileBundle',
+    function () {}, {
+        create: function () {
+            return Oskari.clazz.create('Oskari.mapframework.bundle.terrain-profile.TerrainProfileBundleInstance');
+        },
+        update: function (manager, bundle, bi, info) {}
+    }, {
+        'protocol': ['Oskari.bundle.Bundle'],
+        'source': {
+            'scripts': [{
+                'type': 'text/javascript',
+                'src': '../../../../bundles/paikkatietoikkuna/terrain-profile/BundleModule.js'
+            }, {
+                'type': 'text/javascript',
+                'src': '../../../../bundles/paikkatietoikkuna/terrain-profile/instance.js'
+            }]
+            /*,
+            'locales': [{
+                'lang': 'en',
+                'type': 'text/javascript',
+                'src': '../../../../bundles/paikkatietoikkuna/terrain-profile/resources/locale/en.js'
+            }, {
+                'lang': 'fi',
+                'type': 'text/javascript',
+                'src': '../../../../bundles/paikkatietoikkuna/terrain-profile/resources/locale/fi.js'
+            }, {
+                'lang': 'sv',
+                'type': 'text/javascript',
+                'src': '../../../../bundles/paikkatietoikkuna/terrain-profile/resources/locale/sv.js'
+            }]
+            */
+        },
+        'bundle': {
+            'manifest': {
+                'Bundle-Identifier': 'terrain-profile',
+                'Bundle-Name': 'terrain-profile',
+                'Bundle-Author': [{
+                    'Name': 'Jan Wolski',
+                    'Organisation': 'Karttakeskus',
+                    'Temporal': {
+                        'Start': '2017',
+                        'End': '2017'
+                    },
+                    'Copyleft': {
+                        'License': {
+                            'License-Name': 'EUPL',
+                            'License-Online-Resource': 'http://www.paikkatietoikkuna.fi/license'
+                        }
+                    }
+                }],
+                'Bundle-Name-Locale': {
+                    'fi': {
+                        'Name': ' style-1',
+                        'Title': ' style-1'
+                    },
+                    'en': {}
+                },
+                'Bundle-Version': '1.0.0',
+                'Import-Namespace': ['Oskari', 'jquery'],
+                'Import-Bundle': {}
+
+                /**
+                 *
+                 */
+
+            }
+        },
+
+        /**
+         * @static
+         * @property dependencies
+         */
+        'dependencies': ['jquery']
+
+    });
+
+Oskari.bundle_manager.installBundleClass('terrain-profile', 'Oskari.mapframework.bundle.terrain-profile.TerrainProfileBundle');
