62895d4a6594a939b607d5033b07b61edef7eaa3
Jan Wolski <jan@wolski.fi>
2018-01-10 17:41:33 +0200

    Tracking of extensions

--- bundles/paikkatietoikkuna/telemetry/instance.js
+++ bundles/paikkatietoikkuna/telemetry/instance.js
@@ -6,14 +6,34 @@ Oskari.clazz.define('Oskari.mapframework.bundle.telemetry.TelemetryBundleInstanc
 }, {
     __name: 'TelemetryBundleInstance',
     eventHandlers: {
-        'extensionUpdatedEvent': function (event) {
-            
+        'userinterface.ExtensionUpdatedEvent': function (event) {
+            if(event.getViewState() === 'attach') {
+                this._pushEvent('SideBar', event.getExtension().getName());
+            }
         }
     },
     _startImpl: function() {
-        
+        this._initTelemetry();
+    },
+    _initTelemetry: function() {
+        var _paq = window._paq = window._paq || [];
+        _paq.push(["setDomains", ["*.kartta.paikkatietoikkuna.fi"]]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//piwik.nls.fi/";
+          _paq.push(['setTrackerUrl', u+'piwik.php']);
+          _paq.push(['setSiteId', 9]);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        })();
+    },
+    _pushEvent() {
+        var args = Array.prototype.slice.call(arguments);
+        args.unshift('trackEvent');
+        window._paq.push(args);
     }
 }, {
-    'extend': ['Oskari.mapframework.bundle.geometrycutter.BundleModule'],
-    'protocol': ['Oskari.bundle.BundleInstance']
+    'extend': ['Oskari.BasicBundle'],
+    'protocol': ['Oskari.bundle.BundleInstance', 'Oskari.mapframework.module.Module']
 });
