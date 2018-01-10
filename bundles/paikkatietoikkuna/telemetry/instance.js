e00119a3915c65f657d0f6e9fcd243ca7f0a49a6
Jan Wolski <jan@wolski.fi>
2018-01-10 15:16:04 +0200

    Instance

---
+++ bundles/paikkatietoikkuna/telemetry/instance.js
@@ -0,0 +1,19 @@
+/**
+ * @class Oskari.mapframework.bundle.telemetry.TelemetryBundleInstance
+ */
+Oskari.clazz.define('Oskari.mapframework.bundle.telemetry.TelemetryBundleInstance', function () {
+
+}, {
+    __name: 'TelemetryBundleInstance',
+    eventHandlers: {
+        'extensionUpdatedEvent': function (event) {
+            
+        }
+    },
+    _startImpl: function() {
+        
+    }
+}, {
+    'extend': ['Oskari.mapframework.bundle.geometrycutter.BundleModule'],
+    'protocol': ['Oskari.bundle.BundleInstance']
+});
