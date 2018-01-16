7c8b61670c8c09510cff49443ada315607e0634e
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2018-01-16 13:36:05 +0200

    simplify bundle

--- packages/asdi/bundle/asdi-lang-overrides/bundle.js
+++ packages/asdi/bundle/asdi-lang-overrides/bundle.js
@@ -1,26 +1,27 @@
 /**
  * Definition for bundle. See source for details.
  *
- * @class Oskari.elf.lang.overrides.Bundle
+ *  var obj = {
+        "bundlename":"asdi-lang-overrides" ,
+        "metadata": {
+            "Import-Bundle": { "asdi-lang-overrides": { "bundlePath": "/Oskari/packages/asdi/bundle/" } }
+        }
+    }
+    appSetup.startupSequence.unshift(obj);
+    
+ * @class Oskari.asdi.lang.overrides.Bundle
  */
-Oskari.clazz.define("Oskari.asdi.lang.overrides.Bundle", function() {
+Oskari.clazz.define("Oskari.asdi.lang.overrides", function() {
 
 }, {
     "create" : function() {
         return this;
     },
-    "update" : function(manager, bundle, bi, info) {
-    },
     "start": function() {},
     "stop": function() {}
 }, {
-    "protocol" : [
-        "Oskari.bundle.Bundle",
-        "Oskari.mapframework.bundle.extension.ExtensionBundle"
-    ],
     "source" : {
-        "scripts" : [
-        ],
+        "scripts" : [],
         "locales" : [
             {
                 "lang": "en",
@@ -28,43 +29,7 @@ Oskari.clazz.define("Oskari.asdi.lang.overrides.Bundle", function() {
                 "src": "../../../../bundles/asdi/asdi-lang-overrides/resources/locale/en.js"
             }
         ]
-    },
-    "bundle" : {
-        "manifest" : {
-            "Bundle-Identifier" : "asdi-lang-overrides",
-            "Bundle-Name" : "asdi-lang-overrides",
-            "Bundle-Author" : [{
-                "Name" : "mmldev",
-                "Organisation" : "nls.fi",
-                "Temporal" : {
-                    "Start" : "2018"
-                },
-                "Copyleft" : {
-                    "License" : {
-                        "License-Name" : "EUPL",
-                        "License-Online-Resource" : "http://www.oskari.org/documentation/development/license"
-                    }
-                }
-            }],
-            "Bundle-Name-Locale" : {
-                "fi" : {
-                    "Name" : "lang-overrides",
-                    "Title" : "lang-overrides"
-                },
-                "en" : {}
-            },
-            "Bundle-Version" : "1.0.0",
-            "Import-Namespace" : ["Oskari", "jquery"],
-            "Import-Bundle" : {}
-        }
-    },
-    /**
-     * @static
-     * @property dependencies
-     */
-    "dependencies" : ["jquery"]
-
+    }
 });
 
-Oskari.bundle_manager.installBundleClass("asdi-lang-overrides",
-    "Oskari.asdi.lang.overrides.Bundle");
+Oskari.bundle_manager.installBundleClass("asdi-lang-overrides", "Oskari.asdi.lang.overrides");
