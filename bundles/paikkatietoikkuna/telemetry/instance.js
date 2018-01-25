09e8c32083044b0c903c0e1e4edffac0ea5aab53
Jan Wolski <jan@wolski.fi>
2018-01-25 14:03:10 +0200

    Allow endpoint config

--- bundles/paikkatietoikkuna/telemetry/instance.js
+++ bundles/paikkatietoikkuna/telemetry/instance.js
@@ -7,42 +7,50 @@ Oskari.clazz.define('Oskari.mapframework.bundle.telemetry.TelemetryBundleInstanc
     __name: 'TelemetryBundleInstance',
     eventHandlers: {
         'userinterface.ExtensionUpdatedEvent': function (event) {
-            if(event.getViewState() === 'attach') {
+            if (event.getViewState() === 'attach') {
                 this._pushEvent('Tile', event.getExtension().getName());
             }
         }
     },
-    _startImpl: function() {
-        this._initTelemetry();
+    _startImpl: function () {
+        var endpoint = this.conf.endpoint;
+        if (!endpoint) {
+            Oskari.log(this.getName()).warn('No "endpoint" in conf. Telemetry bundle will not start.');
+            return;
+        }
+        if (location.hostname === 'localhost') {
+            return;
+        }
+        this._initTelemetry(endpoint);
         var me = this;
-        jQuery('#maptools').on('click', '#toolbar .toolrow .tool', function(event){
+        jQuery('#maptools').on('click', '#toolbar .toolrow .tool', function (event) {
             var el = jQuery(this);
-            if(el.hasClass('disabled')) {
+            if (el.hasClass('disabled')) {
                 return;
             }
             me._pushEvent('Toolbar', el.attr('tool'));
         });
-        jQuery('#mapdiv .mapplugin.mylocationplugin').click(function(event){
+        jQuery('#mapdiv .mapplugin.mylocationplugin').click(function (event) {
             me._pushEvent('Maptools', 'mylocationtool');
         });
-        jQuery('#mapdiv .mapplugin.coordinatetool').click(function(event){
+        jQuery('#mapdiv .mapplugin.coordinatetool').click(function (event) {
             me._pushEvent('Maptools', 'coordinatetool');
         });
     },
-    _initTelemetry: function() {
+    _initTelemetry: function (endpoint) {
         var _paq = window._paq = window._paq || [];
-        _paq.push(["setDomains", ["*.kartta.paikkatietoikkuna.fi"]]);
+        _paq.push(['setDomains', ['*.kartta.paikkatietoikkuna.fi']]);
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="//piwik.nls.fi/";
-          _paq.push(['setTrackerUrl', u+'piwik.php']);
-          _paq.push(['setSiteId', 9]);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+        (function () {
+            var u = '//' + endpoint + '/';
+            _paq.push(['setTrackerUrl', u + 'piwik.php']);
+            _paq.push(['setSiteId', 9]);
+            var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+            g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'piwik.js'; s.parentNode.insertBefore(g, s);
         })();
     },
-    _pushEvent() {
+    _pushEvent(/* variadic */) {
         var args = Array.prototype.slice.call(arguments);
         args.unshift('trackEvent');
         window._paq.push(args);
