f3de8f26c96fb3e90ac119f127ff37a9eab94452
Sami Mäkinen <sami.makinen@nls.fi>
2018-03-14 13:51:14 +0200

    Remove use strict from files as openlayers breaks with minified build if they are present

--- bundles/framework/admin-layerrights/Flyout.js
+++ bundles/framework/admin-layerrights/Flyout.js
@@ -1,4 +1,3 @@
-'use strict';
 /**
  * @class Oskari.framework.bundle.admin-layerrights.Flyout
  *
--- bundles/framework/rpc/instance.js
+++ bundles/framework/rpc/instance.js
@@ -1,4 +1,3 @@
-'use strict';
 /**
  * @class Oskari.mapframework.bundle.rpc.RemoteProcedureCallInstance
  *
