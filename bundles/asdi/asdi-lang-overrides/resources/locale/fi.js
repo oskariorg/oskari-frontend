f54b49a684005fd4916b7c03b92e41c68cd6f150
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2018-03-29 14:25:57 +0300

    add overriding locale for asdi

---
+++ bundles/asdi/asdi-lang-overrides/resources/locale/fi.js
@@ -0,0 +1,8 @@
+Oskari.registerLocalization(
+{
+    "lang": "fi",
+    "key": "PersonalData",
+    "value": {
+        "notLoggedIn": "Kirjautuneena käyttäjänä voit tarkastella ja muokata täällä käyttäjätietojasi sekä omiakarttanäkymiäsi ja karttajulkaisujasi."
+    }
+}, true);
