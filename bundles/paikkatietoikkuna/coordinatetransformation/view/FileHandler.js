00204dac6bf7375bbf962e4d3fe3d9ea1ba0abdb
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2018-04-05 14:50:11 +0300

    add new options, hardocded

--- bundles/paikkatietoikkuna/coordinatetransformation/view/FileHandler.js
+++ bundles/paikkatietoikkuna/coordinatetransformation/view/FileHandler.js
@@ -1,11 +1,11 @@
 Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
-    function (dataHandler, loc, type) {
+    function (dataHandler, loc, flag) {
         var me = this;
         Oskari.makeObservable(this);
         me.dataHandler = dataHandler;
         me.loc = loc;
         me.element = null;
-        me.type = type;
+        me.flag = flag;
         me._template = {
             settings: _.template(' <div class="oskari-coordinate-form">' +
                                     '<% if (typeof(filename) !== "undefined") { %> '+
@@ -18,6 +18,11 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
                                             '<option value="degree">${degree}</option>'+
                                             '<option value="gradian">${gradian}</option>'+
                                             '<option value="radian">${radian}</option>'+
+                                            '<option value="DDD.dd">DDD.dd</option>'+
+                                            '<option value="DD MM SS.ss">DD MM SS.ss</option>'+
+                                            '<option value="DD MM.mm">DD MM.mm</option>'+
+                                            '<option value="DDMMSS.ss">DDMMSS.ss</option>'+
+                                            '<option value="DDMM.mm">DDMM.mm</option>'+
                                         '</select>'+
                                     '</div>'+
                                     '<label class="lbl"><input id="prefixId" class="chkbox" type="checkbox">${id}</label>'+
@@ -58,7 +63,7 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
         },
         create: function() {
             var fileSettings;
-            if ( this.type === "export" ) {
+            if ( this.flag === "export" ) {
                 fileSettings = this._template.settings({
                     title: this.loc.filesetting.export.title,
                     filename:this.loc.filesetting.export.filename,
@@ -94,22 +99,24 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
 
             this.setElement( fileSettings );
         },
-        createEventHandlers: function () {
+        createEventHandlers: function ( dialog ) {
             var me = this;
             jQuery( '.oskari-coordinate-form' ).on('click', '.import', function () {
-                me.trigger('GetSettings', me.getImportSettings() );
+                me.trigger('GetSettings', me.getFormSelections() );
+                dialog.close();
             });
             jQuery( 'oskari-coordinate-form' ).on('click', '.export', function () {
-                me.trigger('GetSettings', me.getExportSettings() );
+                me.trigger('GetSettings', me.getFormSelections() );
+                dialog.close();
             });
             jQuery( '.oskari-coordinate-form' ).on('click', '.cancel', function () {
-
+                dialog.close();
             });
         },
         /**
-         * @method getExportSettings
+         * @method getFormSelections
          */
-        getExportSettings: function () {
+        getFormSelections: function () {
             var element = jQuery('.oskari-coordinate-form');
             var settings = {
                 filename: element.find('#filename').val(),
@@ -121,27 +128,13 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
             }
             return settings;
         },
-        /**
-         * @method getImportSettings
-         */
-        getImportSettings: function () {
-            var element = jQuery('.oskari-coordinate-form');
-            var settings = {
-                unit: element.find('#unit option:checked').val(),
-                decimalseparator: element.find('#decimalseparator option:checked').val(),
-                prefixId: element.find('#prefixId').is(":checked"),
-                axisFlip: element.find('#reversecoordinates').is(":checked"),
-                headerLineCount: element.find('#headerrow').val()
-            }
-            return settings;
-        },
         showFileDialogue: function() {
             var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
-            var title = this.type === "import" ? this.loc.filesetting.import.title : this.loc.filesetting.export.title;
+            var title = this.flag === "import" ? this.loc.filesetting.import.title : this.loc.filesetting.export.title;
             dialog.makeDraggable();
             dialog.createCloseIcon();
             dialog.show(title, jQuery( this.getElement() ));
-            this.createEventHandlers();
+            this.createEventHandlers( dialog );
         },
         exportFile: function ( settings ) {
             var exportArray = [];
