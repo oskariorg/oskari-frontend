002e90b0e7c4e7c98901b9586e459498692677a1
okauppinen <olli.kauppinen@nls.fi>
2018-04-24 21:17:56 +0300

    refactor file handler

--- bundles/paikkatietoikkuna/coordinatetransformation/view/FileHandler.js
+++ bundles/paikkatietoikkuna/coordinatetransformation/view/FileHandler.js
@@ -1,16 +1,17 @@
 Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
-    function (dataHandler, loc, flag) {
+    function (dataHandler, loc, type) {
         var me = this;
         Oskari.makeObservable(this);
         me.dataHandler = dataHandler;
         me.loc = loc;
         me.element = null;
-        me.flag = flag;
+        me.type = type; // import, export
+        me.settings;
         me._template = {
             settings: _.template('<div class="oskari-coordinate-form">' +
-                                    '<% if (typeof(filename) !== "undefined") { %> '+
+                                    '<% if (typeof fileName  !== "undefined") { %> '+
                                         '<div class="fileRow">' +
-                                            '<b class="title">${filename}</b>' +
+                                            '<b class="title">${fileName}</b>' +
                                             '<input id="filename" type="text">' +
                                         '</div>'+
                                     '<% } %>'+            
@@ -34,7 +35,7 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
                                         '</label>'+
                                     '</div>'+
                                     '<div class="decimalSeparator"> '+
-                                        '<b class="title">${decimalseparator}</b> '+
+                                        '<b class="title">${decimalSeparator}</b> '+
                                         '<div class="settingsSelect">'+
                                             '<select id="decimalseparator">'+
                                                 '<option value="point">${point}</option>'+
@@ -43,21 +44,21 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
                                         '</div>' +
                                         '<label class="lbl">' +
                                             '<input id="reversecoordinates" class="chkbox" type="checkbox">' +
-                                            '${reversecoords}' +
+                                            '${reverseCoords}' +
                                         '</label> '+
                                     '</div>' +
                                     '<div class="headerLineCount"> '+
-                                        '<b class="title">${headercount}</b>'+
+                                        '<b class="title">${headerCount}</b>'+
                                         '<input id="headerrow" type="number"> '+
                                     '</div>'+
-                                    '<input id="overlay-btn" class="cancel" type="button" value="${cancel} " />' +
-                                    '<% if(typeof(fileExport) !== "undefined") { %>'+
-                                        '<input id="overlay-btn" class="done" type="button" value="${fileExport}" />'+
-                                    '<% } else { %>'+
-                                        '<input id="overlay-btn" class="done" type="button" value="${done}" />' +
-                                    '<% } %>' +
-                                    '</div>' +
-                                '</div>'
+                                    //'<input id="overlay-btn" class="cancel" type="button" value="${cancel} " />' +
+                                    //'<% if(typeof(fileExport) !== "undefined") { %>'+
+                                    //    '<input id="overlay-btn" class="done" type="button" value="${fileExport}" />'+
+                                    //'<% } else { %>'+
+                                    //    '<input id="overlay-btn" class="done" type="button" value="${done}" />' +
+                                    //'<% } %>' +
+                                '</div>' +
+                            '</div>'
                                 )
         } 
     }, {
@@ -70,20 +71,23 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
         getName: function() {
             return 'Oskari.coordinatetransformation.view.FileHandler';
         },
+        getSettings: function (){
+            return this.settings;
+        },
         create: function() {
             var fileSettings;
-            if ( this.flag === "export" ) {
+            if ( this.type === "export" ) {
                 fileSettings = this._template.settings({
                     //title: this.loc('fileSettings.export.title'),
-                    fileName: this.loc('fileSettings.export.filename'),
-                    //fileExport: this.loc('fileSettings'),
+                    fileName: this.loc('fileSettings.export.fileName'),
+                    //fileExport: this.loc('fileSettings.export.fileName'),
                     format: this.loc('fileSettings.options.format'),
-                    decimalseparator: this.loc('fileSettings.options.decimalSeparator'),
+                    decimalSeparator: this.loc('fileSettings.options.decimalSeparator'),
                     id: this.loc('fileSettings.options.useId'),
-                    reversecoords: this.loc('fileSettings.options.reverseCoords'),
-                    headercount: this.loc('fileSettings.options.headerCount'),
-                    cancel: this.loc('actions.cancel'),
-                    done: this.loc('actions.done'),
+                    reverseCoords: this.loc('fileSettings.options.reverseCoords'),
+                    headerCount: this.loc('fileSettings.options.headerCount'),
+                    //cancel: this.loc('actions.cancel'),
+                    //done: this.loc('actions.done'),
                     degree: this.loc('fileSettings.options.degree'),
                     gradian: this.loc('fileSettings.options.gradian'),
                     radian: this.loc('fileSettings.options.radian'),
@@ -95,12 +99,12 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
                     //title: this.loc('fileSettings.import.title'),
                     //fileExport: this.loc('fileSettings'),
                     format: this.loc('fileSettings.options.format'),
-                    decimalseparator: this.loc('fileSettings.options.decimalSeparator'),
+                    decimalSeparator: this.loc('fileSettings.options.decimalSeparator'),
                     id: this.loc('fileSettings.options.useId'),
-                    reversecoords: this.loc('fileSettings.options.reverseCoords'),
-                    headercount: this.loc('fileSettings.options.headerCount'),
-                    cancel: this.loc('actions.cancel'),
-                    done: this.loc('actions.done'),
+                    reverseCoords: this.loc('fileSettings.options.reverseCoords'),
+                    headerCount: this.loc('fileSettings.options.headerCount'),
+                    //cancel: this.loc('actions.cancel'),
+                    //done: this.loc('actions.done'),
                     degree: this.loc('fileSettings.options.degree'),
                     gradian: this.loc('fileSettings.options.gradian'),
                     radian: this.loc('fileSettings.options.radian'),
@@ -111,7 +115,7 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
 
             this.setElement( fileSettings );
         },
-        createEventHandlers: function ( dialog ) {
+        /*createEventHandlers: function ( dialog ) {
             var me = this;
             jQuery( '.oskari-coordinate-form' ).on('click', '.done', function () {
                 me.trigger('GetSettings', me.getFormSelections() );
@@ -121,7 +125,7 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
             jQuery( '.oskari-coordinate-form' ).on('click', '.cancel', function () {
                 dialog.close();
             });
-        },
+        },*/
         /**
          * @method getFormSelections
          */
@@ -137,19 +141,33 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.FileHandler',
             }
             return settings;
         },
-        showFileDialogue: function() {
+        showFileDialogue: function(callback) {
+            var me = this;
             var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
-            var title = this.flag === "import" ? this.loc('fileSettings.import.title') : this.loc('fileSettings.export.title');
-            dialog.makeDraggable();
+            var title = this.type === "import" ? this.loc('fileSettings.import.title') : this.loc('fileSettings.export.title');
+            var btnText = this.type === "import" ? this.loc('actions.done') : this.loc('actions.export');
+            var cancelBtn =  dialog.createCloseButton(this.loc('actions.cancel'));
+            var btn = Oskari.clazz.create('Oskari.userinterface.component.Button');
+            btn.setTitle(btnText);
+            btn.setHandler(function() {
+                me.settings = me.getFormSelections();
+                if (typeof callback === "function"){
+                    callback(me.settings);
+                }
+                dialog.close();
+            });
+
             dialog.createCloseIcon();
-            dialog.show(title, jQuery( this.getElement() ));
-            this.createEventHandlers( dialog );
+            dialog.makeDraggable();
+
+            dialog.show(title, jQuery( this.getElement() ), [cancelBtn, btn]);
+            //this.createEventHandlers( dialog );
         },
         exportFile: function ( settings ) {
-            var exportArray = [];
+            var exportArray = this.dataHandler.getOutputCoords();
             //this.dataHandler.getCoordinateObject().forEach( function ( pair ) {
             //    exportArray.push( pair.input );
-            //});  
+            //});
             if( exportArray.length !== 0 ) {
                 this.fileInput.exportToFile( exportArray, settings.filename+'.txt' );
             } else {
