358c7f23817a42567b083ef62fea536c7ec5e039
nyholmi <ilkka.nyholm@maanmittauslaitos.fi>
2018-05-07 16:19:13 +0300

    accordion & table

--- bundles/statistics/statsgrid2016/components/UserIndicatorDataForm.js
+++ bundles/statistics/statsgrid2016/components/UserIndicatorDataForm.js
@@ -9,11 +9,24 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.UserIndicatorDataForm', functio
     }
 }, {
     __templates: {
-        main: _.template('<div class="indicator-data-form oskari-hidden"> '+
-                            '<form id="user-indicator">'+
+        main: _.template('<div class="user-indicator-main"> '+
+                        '</div>'),
+        form: _.template('<form id="indicator-restriction-form">'+
                             '   <input class="stats-indicator-form-item" type="text" name="year" placeholder="${year}"><br>'+
-                            '</form>'+
-                        '</div>')
+                            '</form>'),
+        insertTable: _.template('<table class="user-indicator-table">'+
+                                        '<tbody></tbody>'+
+                                    '</table>'),
+        header: _.template('<thead>'+
+                            '<tr>' +
+                                '<th id="nort">${north}</th>'+
+                                '<th id="east">${east}</th>'+
+                            '</tr>'+
+                        '</thead>'),
+        row: _.template('<tr>' +
+                '<td class="cell regionset" style=" border: 1px solid black ;">${regionset}</td>'+
+                '<td class="cell uservalue" contenteditable=true style=" border: 1px solid black ;"></td>'+
+        '</tr> '),
     },
     setElement: function (el) {
         this.element = el;
@@ -31,11 +44,32 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.UserIndicatorDataForm', functio
             data[key] = element.val();
         });
     },
+    createTable: function() {
+        return jQuery( this.__templates.insertTable() );
+    },
+    refreshTable: function ( region, mountPoint, tableRef) {
+        var me = this;
+        tableRef.empty();
+        this.service.getRegions( Number(region), function (err, regionlist) {
+
+            regionlist.forEach( function (region) {
+                tableRef.append( me.__templates.row({
+                    regionset: region.name
+                }));
+            });
+            mountPoint.append(tableRef);
+        });
+    },
     toggle: function () {
-        if( this.getElement().hasClass('oskari-hidden') ) {
-            this.getElement().removeClass('oskari-hidden');
+        var form = this.getElement().find('#indicator-restriction-form');
+        var table = this.getElement().find('table');
+
+        if ( form.hasClass('oskari-hidden') ) {
+            form.removeClass('oskari-hidden');
+            table.addClass('oskari-hidden');
         } else {
-            this.getElement().addClass('oskari-hidden');
+            form.addClass('oskari-hidden');
+            table.removeClass('oskari-hidden');
         }
     },
     clearUi: function () {
@@ -48,17 +82,31 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.UserIndicatorDataForm', functio
         var me = this;
         this.clearUi();
 
-        var main = this.__templates.main({
+        var main = jQuery(this.__templates.main());
+        var form = jQuery(this.__templates.form({
             year: this.locale.userIndicators.formYear
-        });
-        var jMain = jQuery(main);
+        }));
+
         var ds = this.service.getDatasource(this.datasourceid);
         var region = this.regionselect.create(ds.regionsets);
-        jMain.append(region.container);
-        this.setElement(jMain);
+        form.append(region.container);
+        main.prepend(form);
+
+        var indBtn = Oskari.clazz.create('Oskari.userinterface.component.Button');
+        indBtn.setTitle(this.locale.userIndicators.buttonAddIndicator);
+        indBtn.insertTo( main );
+        var table = me.createTable();
+        // var header = this.__templates.header();
+
+        indBtn.setHandler(function (event) {
+            event.stopPropagation();
+            me.toggle();
+            me.refreshTable( region.value(), main, table );
+        });
+        this.setElement(main);
     },
-    render: function (el) {
-        el.append( this.getElement() );
+    render: function (panel) {
+        panel.setContent( this.getElement() );
     }
 
 });
--- bundles/statistics/statsgrid2016/components/UserIndicatorForm.js
+++ bundles/statistics/statsgrid2016/components/UserIndicatorForm.js
@@ -2,26 +2,42 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.UserIndicatorForm', function ( 
     this.locale = locale;
     this.service = service;
     this.addIndicatorDataForm = Oskari.clazz.create('Oskari.statistics.statsgrid.UserIndicatorDataForm', service, locale, datasource);
+    this._accordion = Oskari.clazz.create('Oskari.userinterface.component.Accordion');
     this.element = null;
     if ( !this.getElement() ) {
         this.createUi();
     }
 }, {
     __templates: {
-        main: _.template('<div class="stats-indicator-form"> '+
-                            '<div class="stats-not-logged-in oskari-hidden">${warning}</div>'+
-                            '<form id="stats-user-indicator">'+
+        main: _.template('<div class="stats-user-indicator-form">'+
+                            '<div class="stats-not-logged-in oskari-hidden">${warning}</div>'+                
+                        '</div>'),
+        form: _.template('<form id="stats-user-indicator">'+
                             '   <input class="stats-indicator-form-item" type="text" name="name" placeholder="${name}"><br>'+
                             '   <textarea class="stats-indicator-form-item" name="description" form="stats-user-indicator" placeholder="${description}"></textarea> '+ 
                             '   <input class="stats-indicator-form-item" type="text" name="datasource" placeholder="${source}"><br>'+
-                            '</form>'+
-                        '</div>')
+                            '</form>')
     },
     setElement: function (el) {
         this.element = el;
     },
     getElement: function () {
         return this.element;
+    },
+    /**
+     * Creates an accordion panel for legend and classification edit with eventlisteners on open/close
+     * @param  {String} title UI label
+     * @return {Oskari.userinterface.component.AccordionPanel} panel without content
+     */
+    _createAccordionPanel : function(title) {
+        var me = this;
+        var panel = Oskari.clazz.create('Oskari.userinterface.component.AccordionPanel');
+        panel.on('open', function() {
+        });
+        panel.on('close', function() {
+        });
+        panel.setTitle(title);
+        return panel;
     },
     isLoggedIn: function () {
         return Oskari.user().isLoggedIn();
@@ -53,33 +69,39 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.UserIndicatorForm', function ( 
         var me = this;
         this.clearUi();
 
-        var main = this.__templates.main({
-            warning: this.locale.userIndicators.notLoggedInWarning,
+        var accordion = this._accordion;
+        var main = this.__templates.main({ 
+            warning: this.locale.userIndicators.notLoggedInWarning 
+        });
+        var form = this.__templates.form({
             name: this.locale.userIndicators.formName,
             description: this.locale.userIndicators.formDescription,
             source: this.locale.userIndicators.formDatasource
         });
+
+        var panel = this._createAccordionPanel('Tiedot');
+        var dataPanel = this._createAccordionPanel('Data');
+        // panel.getHeader().remove();
+        panel.open();
+        accordion.addPanel(panel);
+        accordion.addPanel(dataPanel);
+
         var jMain = jQuery(main);
+        var jForm = jQuery(form);
+        panel.setContent(jForm);
+        accordion.insertTo(jMain);
+
         if ( !this.isLoggedIn() ) {
             jMain.find('.stats-not-logged-in').removeClass('oskari-hidden');
         }
-        me.addIndicatorDataForm.render(jMain);
+        me.addIndicatorDataForm.render(dataPanel);
         var btn = Oskari.clazz.create('Oskari.userinterface.component.Button');
         btn.setTitle(this.locale.userIndicators.buttonSave);
         btn.insertTo( jMain );
-
+        btn.getButton().css('float','right');
         btn.setHandler(function (event) {
             event.stopPropagation();
             me.getFormData();
-        });
-        var indBtn = Oskari.clazz.create('Oskari.userinterface.component.Button');
-        indBtn.setTitle(this.locale.userIndicators.buttonAddIndicator);
-        indBtn.insertTo( jMain );
-
-        indBtn.setHandler(function (event) {
-            event.stopPropagation();
-            me.toggle();
-            me.addIndicatorDataForm.toggle();
         });
 
         this.setElement(jMain);
