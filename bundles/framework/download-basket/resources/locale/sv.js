f2ad32787da2dd70efa0f2bdac445fa1126c900c
ollijakobsson <olli.jakobsson@dimenteq.fi>
2018-05-28 15:56:44 +0300

    updated url

--- bundles/framework/download-basket/resources/locale/sv.js
+++ bundles/framework/download-basket/resources/locale/sv.js
@@ -36,7 +36,7 @@ Oskari.registerLocalization({
                 "basket-send" : "Ladda data",
                 "insert-email-for-download" : "Fyll i din e-postadress för nedladdning:",
                 "privacy-policy" : "Dataskyddsbeskrivning",
-                "privacy-policy-url" : "https://www.liikennevirasto.fi/yhteystiedot/kansalaisen-tietosuoja-liikenneviraston-palveluissa",
+                "privacy-policy-url" : "https://www.liikennevirasto.fi/yhteystiedot/tietosuoja",
                 "basket-user-email" : "E-postadress",
                 "basket-user-email-sure" : "Bekräfta e-postadress",
                 "check-form-error-huom" : "Observera",
