70788eadcfbc9bd2153b075d250c1cf0f6ede280
Jan Wolski <jan@wolski.fi>
2018-06-26 16:15:43 +0300

    CSS minfication

--- webpack.common.js
+++ webpack.common.js
@@ -13,18 +13,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS
-        ]
-      },
-      {
         test: /\.css$/,
         use: [
           "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
+          {loader: "css-loader", options: {minimize: true}},
         ]
       },
       {
