7dabd5415b943014a964a89f393cb2217825f590
Jan Wolski <jan@wolski.fi>
2018-06-28 16:43:05 +0300

    Auto-replace CSS with CSS if found

--- webpack/oskariBundleLoader.js
+++ webpack/oskariBundleLoader.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const fs = require('fs');
 
-module.exports = function(source) {
+const isCSS = /\.css$/;
+
+module.exports = function (source) {
 
     dependencies = [];
 
@@ -14,9 +17,25 @@ module.exports = function(source) {
                 if (metadata.source.scripts) {
                     metadata.source.scripts.forEach(script => {
                         dependencies.push(script);
+
+                        if (!isCSS.test(script.src)) {
+                            return;
+                        }
+
+                        let scssPath = script.src
+                                .replace(/\/css\//, '/scss/')
+                                .replace(/\.css$/, '.scss');
+
+                        const scssAbsolutePath = path.join(this.context, scssPath);
+                        if (!fs.existsSync(scssAbsolutePath)) {
+                            return;
+                        }
+
+                        console.log('Replacing CSS with SCSS:', scssAbsolutePath);
+                        script.src = scssPath;
                     });
                 }
-                
+
                 if (metadata.source.locales) {
                     metadata.source.locales.forEach(l => {
                         this.addDependency(path.join(this.context, l.src));
@@ -25,7 +44,7 @@ module.exports = function(source) {
             }
         },
         bundle_manager: {
-            installBundleClass: function(id, path) {}
+            installBundleClass: function (id, path) { }
         }
     }
     eval(source);
