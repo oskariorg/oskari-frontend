b207dbaf71f0b14661576a6aac0036aa52b81d0f
Sami Mäkinen <sami.makinen@nls.fi>
2018-09-18 17:45:32 +0300

    Add readability

--- bundles/statistics/statsgrid2016/components/IndicatorSelection.js
+++ bundles/statistics/statsgrid2016/components/IndicatorSelection.js
@@ -75,7 +75,7 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.IndicatorSelection', function (
             if (result.complete) {
                 me.spinner.stop();
                 var userDatasource = me.service.getUserDatasource();
-                var isUserDatasource = !!userDatasource && '' + me.service.getUserDatasource().id === '' + datasrc;
+                var isUserDatasource = !!userDatasource && '' + userDatasource.id === '' + datasrc;
                 if (!isUserDatasource && result.indicators.length === 0) {
                     // show notification about empty indicator list for non-myindicators datasource
                     errorService.show(locale('errors.title'), locale('errors.indicatorListIsEmpty'));
