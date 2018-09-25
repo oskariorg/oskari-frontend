45ed56e545813cdd5af81a7b4d83092fc4444b9f
Jan Wolski <jan@wolski.fi>
2018-09-25 11:27:46 +0300

    Style function

--- bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
+++ bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
@@ -2,6 +2,9 @@ import olSourceVectorTile from 'ol/source/VectorTile';
 import olLayerVectorTile from 'ol/layer/VectorTile';
 import olFormatMVT from 'ol/format/MVT';
 import TileGrid from 'ol/tilegrid/TileGrid';
+import olStyleStyle from 'ol/style/Style';
+
+const invisible = new olStyleStyle();
 
 /**
  * @class Oskari.mapframework.mapmodule.VectorTileLayerPlugin
@@ -24,9 +27,9 @@ Oskari.clazz.define('Oskari.mapframework.mapmodule.VectorTileLayerPlugin',
          * @private @method _initImpl
          * Interface method for the module protocol.
          */
-        _initImpl: function () {
+        _initImpl() {
             // register domain builder
-            var mapLayerService = this.getSandbox().getService('Oskari.mapframework.service.MapLayerService');
+            const mapLayerService = this.getSandbox().getService('Oskari.mapframework.service.MapLayerService');
             if (mapLayerService) {
                 mapLayerService.registerLayerModel(
                     'vectortile',
@@ -40,7 +43,7 @@ Oskari.clazz.define('Oskari.mapframework.mapmodule.VectorTileLayerPlugin',
          * @param  {Oskari.mapframework.domain.AbstractLayer}  layer
          * @return {Boolean}       true if this plugin handles the type of layers
          */
-        isLayerSupported: function (layer) {
+        isLayerSupported(layer) {
             if (!layer) {
                 return false;
             }
@@ -54,23 +57,67 @@ Oskari.clazz.define('Oskari.mapframework.mapmodule.VectorTileLayerPlugin',
          * @param {Boolean} keepLayerOnTop
          * @param {Boolean} isBaseMap
          */
-        addMapLayerToMap: function (layer, keepLayerOnTop, isBaseMap) {
-            var options = layer.getOptions();
-            var sourceOpts = {
+        addMapLayerToMap(layer, keepLayerOnTop, isBaseMap) {
+            const options = layer.getOptions();
+            const sourceOpts = {
                 format: new olFormatMVT(),
-                url: layer.getLayerUrl()
+                url: layer.getLayerUrl().replace('{epsg}', this.mapModule.getProjection())
             };
             if(options.tileGrid) {
                 sourceOpts.tileGrid = new TileGrid(options.tileGrid);
             }
-            var vectorTileLayer = new olLayerVectorTile({
+            const layerOpts = {
                 opacity: layer.getOpacity() / 100,
                 renderMode: 'hybrid',
                 source: new olSourceVectorTile(sourceOpts)
-            });
+            };
+            if (options.style && options.style.default) {
+                layerOpts.style = this._generateStyleFunction(options.style.default);
+            }
+            const vectorTileLayer = new olLayerVectorTile(layerOpts);
 
             this.mapModule.addLayer(vectorTileLayer, !keepLayerOnTop);
             this.setOLMapLayers(layer.getId(), vectorTileLayer);
+        },
+        _generateStyleFunction(styleDef) {
+            const styleCache = {};
+            Object.keys(styleDef).forEach((layerName) => {
+                const styles = {};
+                const layerStyleDef = styleDef[layerName];
+                const featureStyle = layerStyleDef.featureStyle;
+                if (featureStyle) {
+                    styles.base = this.mapModule.getStyle(featureStyle);
+                }
+                const optionalStyles = layerStyleDef.optionalStyles;
+                if (optionalStyles) {
+                    styles.optional = optionalStyles.map((optionalDef) => {
+                        return {
+                            key: optionalDef.property.key,
+                            value: optionalDef.property.value,
+                            style: this.mapModule.getStyle(Object.assign({}, featureStyle, optionalDef))
+                        }
+                    });
+                }
+                styleCache[layerName] = styles;
+            })
+            return (feature, resolution) => {
+                var styles = styleCache[feature.get('layer')];
+                if (!styles) {
+                    return invisible;
+                }
+                if (styles.optional) {
+                    var found = styles.optional.find((op) => {
+                        return feature.get(op.key) === op.value;
+                    });
+                    if (found) {
+                        return found.style;
+                    }
+                }
+                if (styles.base) {
+                    return styles.base;
+                }
+                return invisible;
+            }
         }
     }, {
         /**
