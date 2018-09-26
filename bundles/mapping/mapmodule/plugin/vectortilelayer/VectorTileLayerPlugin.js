1c4839a7dd42bc1c3d5f7ce8592504d291999b75
Jan Wolski <jan@wolski.fi>
2018-09-26 15:11:44 +0300

    as ES6 class

--- bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
+++ bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
@@ -2,101 +2,98 @@ import olSourceVectorTile from 'ol/source/VectorTile';
 import olLayerVectorTile from 'ol/layer/VectorTile';
 import olFormatMVT from 'ol/format/MVT';
 import TileGrid from 'ol/tilegrid/TileGrid';
-import {createDefaultStyle} from 'ol/style/Style';
+import { createDefaultStyle } from 'ol/style/Style';
 
 import VectorTileModelBuilder from './VectorTileModelBuilder';
 import VectorTileLayer from './VectorTileLayer';
 import styleGenerator from './styleGenerator';
 
+const AbstractMapLayerPlugin = Oskari.clazz.get('Oskari.mapping.mapmodule.AbstractMapLayerPlugin');
 const layertype = 'vectortile';
 
 /**
  * @class Oskari.mapframework.mapmodule.VectorTileLayerPlugin
  * Provides functionality to draw vector tile layers on the map
  */
-Oskari.clazz.define('Oskari.mapframework.mapmodule.VectorTileLayerPlugin',
-
+Oskari.clazz.defineES('Oskari.mapframework.mapmodule.VectorTileLayerPlugin',
+class VectorTileLayerPlugin extends AbstractMapLayerPlugin {
+    constructor(config) {
+        super(config);
+        this.__name = 'VectorTileLayerPlugin';
+        this._clazz = 'Oskari.mapframework.mapmodule.VectorTileLayerPlugin';
+    }
     /**
-     * @static @method create called automatically on construction
+     * @private @method _initImpl
+     * Interface method for the module protocol.
      */
-    function () {},
-    {
-        __name: 'VectorTileLayerPlugin',
-        _clazz: 'Oskari.mapframework.mapmodule.VectorTileLayerPlugin',
+    _initImpl() {
+        // register domain builder
+        const mapLayerService = this.getSandbox().getService('Oskari.mapframework.service.MapLayerService');
+        if (mapLayerService) {
+            mapLayerService.registerLayerModel(layertype + 'layer', VectorTileLayer);
+            mapLayerService.registerLayerModelBuilder(layertype + 'layer', new VectorTileModelBuilder());
+        }
+    }
+    _createPluginEventHandlers() {
+        return {
+            AfterChangeMapLayerStyleEvent(event) {
+                const oskariLayer = event.getMapLayer();
+                const olLayers = this.getOLMapLayers(oskariLayer);
 
-        /**
-         * @private @method _initImpl
-         * Interface method for the module protocol.
-         */
-        _initImpl() {
-            // register domain builder
-            const mapLayerService = this.getSandbox().getService('Oskari.mapframework.service.MapLayerService');
-            if (mapLayerService) {
-                mapLayerService.registerLayerModel(layertype + 'layer', VectorTileLayer);
-                mapLayerService.registerLayerModelBuilder(layertype + 'layer', new VectorTileModelBuilder());
-            }
-        },
-        _createPluginEventHandlers() {
-            return {
-                AfterChangeMapLayerStyleEvent(event) {
-                    const oskariLayer = event.getMapLayer();
-                    const olLayers = this.getOLMapLayers(oskariLayer);
-
-                    if (olLayers && olLayers.length > 0) {
-                        olLayers[0].setStyle(this._getLayerCurrentStyleFunction(oskariLayer));
-                    }
+                if (olLayers && olLayers.length > 0) {
+                    olLayers[0].setStyle(this._getLayerCurrentStyleFunction(oskariLayer));
                 }
-            };
-        },
-        _getLayerCurrentStyleFunction(layer) {
-            const styleDef = layer.getCurrentStyleDef();
-            return styleDef ? styleGenerator(this.mapModule.getStyle.bind(this.mapModule), styleDef) : createDefaultStyle;
-        },
-        /**
-         * Checks if the layer can be handled by this plugin
-         * @method  isLayerSupported
-         * @param  {Oskari.mapframework.domain.AbstractLayer}  layer
-         * @return {Boolean}       true if this plugin handles the type of layers
-         */
-        isLayerSupported(layer) {
-            if (!layer) {
-                return false;
             }
-            return layer.isLayerOfType(layertype);
-        },
-        /**
-         * @method addMapLayerToMap
-         * @private
-         * Adds a single vector tile layer to this map
-         * @param {Oskari.mapframework.domain.VectorTileLayer} layer
-         * @param {Boolean} keepLayerOnTop
-         * @param {Boolean} isBaseMap
-         */
-        addMapLayerToMap(layer, keepLayerOnTop, isBaseMap) {
-            const options = layer.getOptions();
-            const sourceOpts = {
-                format: new olFormatMVT(),
-                url: layer.getLayerUrl().replace('{epsg}', this.mapModule.getProjection())
-            };
-            if(options.tileGrid) {
-                sourceOpts.tileGrid = new TileGrid(options.tileGrid);
-            }
-            const vectorTileLayer = new olLayerVectorTile({
-                opacity: layer.getOpacity() / 100,
-                renderMode: 'hybrid',
-                source: new olSourceVectorTile(sourceOpts),
-                style: this._getLayerCurrentStyleFunction(layer)
-            });
+        };
+    }
+    _getLayerCurrentStyleFunction(layer) {
+        const styleDef = layer.getCurrentStyleDef();
+        return styleDef ? styleGenerator(this.mapModule.getStyle.bind(this.mapModule), styleDef) : createDefaultStyle;
+    }
+    /**
+     * Checks if the layer can be handled by this plugin
+     * @method  isLayerSupported
+     * @param  {Oskari.mapframework.domain.AbstractLayer}  layer
+     * @return {Boolean}       true if this plugin handles the type of layers
+     */
+    isLayerSupported(layer) {
+        if (!layer) {
+            return false;
+        }
+        return layer.isLayerOfType(layertype);
+    }
+    /**
+     * @method addMapLayerToMap
+     * @private
+     * Adds a single vector tile layer to this map
+     * @param {Oskari.mapframework.domain.VectorTileLayer} layer
+     * @param {Boolean} keepLayerOnTop
+     * @param {Boolean} isBaseMap
+     */
+    addMapLayerToMap(layer, keepLayerOnTop, isBaseMap) {
+        const options = layer.getOptions();
+        const sourceOpts = {
+            format: new olFormatMVT(),
+            url: layer.getLayerUrl().replace('{epsg}', this.mapModule.getProjection())
+        };
+        if (options.tileGrid) {
+            sourceOpts.tileGrid = new TileGrid(options.tileGrid);
+        }
+        const vectorTileLayer = new olLayerVectorTile({
+            opacity: layer.getOpacity() / 100,
+            renderMode: 'hybrid',
+            source: new olSourceVectorTile(sourceOpts),
+            style: this._getLayerCurrentStyleFunction(layer)
+        });
 
-            this.mapModule.addLayer(vectorTileLayer, !keepLayerOnTop);
-            this.setOLMapLayers(layer.getId(), vectorTileLayer);
-        },
-    }, {
+        this.mapModule.addLayer(vectorTileLayer, !keepLayerOnTop);
+        this.setOLMapLayers(layer.getId(), vectorTileLayer);
+    }
+}, {
         /**
          * @property {String[]} protocol array of superclasses as {String}
          * @static
          */
-        'protocol': ['Oskari.mapframework.module.Module', 'Oskari.mapframework.ui.module.common.mapmodule.Plugin'],
-        'extend': ['Oskari.mapping.mapmodule.AbstractMapLayerPlugin']
+        'protocol': ['Oskari.mapframework.module.Module', 'Oskari.mapframework.ui.module.common.mapmodule.Plugin']
     }
 );
