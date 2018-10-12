ad5c5ea52ecca1dc5593ffcab15aa28eb9d5ed4c
Joonas Heijari <joonas.heijari@gmail.com>
2018-10-12 14:56:39 +0300

    Layer builder for 3D tiles

---
+++ plugin/Tiles3DModelBuilder.js
@@ -0,0 +1,15 @@
+const Style = Oskari.clazz.get('Oskari.mapframework.domain.Style');
+
+export default class Tiles3DModelBuilder {
+    parseLayerData (layer, mapLayerJson, maplayerService) {
+        if (mapLayerJson.options.styles) {
+            Object.keys(mapLayerJson.options.styles).forEach((styleName) => {
+                const style = new Style();
+                style.setName(styleName);
+                style.setTitle(styleName);
+                layer.addStyle(style);
+            });
+            layer.selectStyle('default');
+        }
+    }
+}
