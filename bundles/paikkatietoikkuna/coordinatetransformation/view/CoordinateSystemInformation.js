8f1466f6cf94ba6dbdbe90702b956fc45bdca1f4
okauppinen <olli.kauppinen@nls.fi>
2018-10-25 11:52:02 +0300

    add precision table

--- bundles/paikkatietoikkuna/coordinatetransformation/view/CoordinateSystemInformation.js
+++ bundles/paikkatietoikkuna/coordinatetransformation/view/CoordinateSystemInformation.js
@@ -9,7 +9,18 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.CoordinateSystemInform
                     '</div>'),
         list: jQuery('<ul class="info-list"></ul>'),
         listItem: jQuery('<li></li>'),
-        paragraph: jQuery('<p></p>')
+        paragraph: jQuery('<p></p>'),
+        precisionTable: _.template('<div class="precision-table-wrapper">' +
+                '<h5 class="precision-table"><%= title %></h5>' +
+                '<table class="precisionTable">' +
+                    '<tr><th><%= unit %></th><th>~1 m</th><th>~0.1 m</th><th>~1 cm</th><th>~1 mm</th><th>~0.1 mm</th></tr>' +
+                    '<tr><td><%= deg %></td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td></tr>' +
+                    '<tr><td><%= rad %></td><td>7</td><td>8</td><td>9</td><td>10</td><td>11</td></tr>' +
+                    '<tr><td><%= min %></td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td></tr>' +
+                    '<tr><td><%= sec %></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr>' +
+                '</table>' +
+            '</div>'
+        )
     },
     setElement: function (el) {
         this.element = el;
@@ -32,25 +43,35 @@ Oskari.clazz.define('Oskari.coordinatetransformation.view.CoordinateSystemInform
         dialog.show(title, content, [btn]);
         dialog.moveTo(parentElement);
         dialog.makeDraggable();
+
         this.dialog = dialog;
+    },
+    addPrecisionTable: function (toElem) {
+        var headers = this.loc('infoPopup.decimalPrecision.precisionTable');
+        var table = this._template.precisionTable(headers);
+        toElem.append(table);
     },
     _createContent: function (key, skipInfo) {
         var me = this;
         var content = this._template.content.clone();
         var infoLoc = this.loc('infoPopup')[key];
+        var infoContent = content.find('.info-content');
         content.find('.info-description').html(this.loc('infoPopup.description'));
         if (skipInfo !== true) {
-            content.find('.info-content').html(infoLoc.info);
+            infoContent.html(infoLoc.info);
         }
         if (Array.isArray(infoLoc.paragraphs) && infoLoc.paragraphs.length !== 0) {
             infoLoc.paragraphs.forEach(function (item) {
                 var paragraph = me._template.paragraph.clone();
                 paragraph.text(item);
-                content.append(paragraph);
+                infoContent.append(paragraph);
             });
         }
         if (Array.isArray(infoLoc.listItems) && infoLoc.listItems.length !== 0) {
-            content.find('.info-content').append(this._createList(infoLoc.listItems));
+            infoContent.append(this._createList(infoLoc.listItems));
+        }
+        if (key === 'decimalPrecision') {
+            this.addPrecisionTable(infoContent);
         }
         return content;
     },
