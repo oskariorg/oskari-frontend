3bc7bad4f597d1390de3b56352161d291b7604ea
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2018-11-05 14:41:19 +0200

    Checked documentation

--- bundles/framework/download-basket/Flyout.js
+++ bundles/framework/download-basket/Flyout.js
@@ -25,15 +25,9 @@ Oskari.clazz.define('Oskari.mapframework.bundle.downloadBasket.Flyout',
         this.tabsContainer = null;
         this._localization = this.instance.getLocalization('flyout');
     }, {
-  /*      tabs: [{
-            'id': 'download-basket-cropping-tab',
-            'clazz': 'Oskari.mapframework.bundle.downloadBasket.Cropping'
-        },{
-            'id': 'download-basket-tab',
-            'clazz': 'Oskari.mapframework.bundle.downloadBasket.Basket'
-        }],*/
         /**
          * @method getName
+         * @public
          * @return {String} the name for the component
          */
         getName: function () {
@@ -41,78 +35,66 @@ Oskari.clazz.define('Oskari.mapframework.bundle.downloadBasket.Flyout',
         },
 
         /**
-         * @public @method setEl
          * Interface method implementation
-         *
-         * @param {Object} el
-         * Reference to the container in browser
-         * @param {Number} width
-         * Container size(?) - not used
-         * @param {Number} height
-         * Container size(?) - not used
-         *
+         * @public @method setEl
+         * @param {Object} el reference to the container in browser         *
          */
-        setEl: function (el, width, height) {
+        setEl: function (el) {
             this.container = jQuery(el[0]);
-            // if (!jQuery(this.container).hasClass('download-basket-cropping-tab')) {
-            //     jQuery(this.container).addClass('download-basket-cropping-tab');
-            // }
         },
         /**
-        * @public @method startPlugin
         * Interface method implementation, assigns the HTML templates
         * that will be used to create the UI
-        *
-        *
+        * @public @method startPlugin
         */
         startPlugin: function () {
             this.createUI();
         },
-        /* App specific methods */
+        /**
+         * Creates UI
+         * @method createUI
+         * @public
+         */
         createUI: function () {
             if (this.tabsContainer) {
                 return;
             }
-            var me = this,
-                tabsContainer = Oskari.clazz.create('Oskari.userinterface.component.TabContainer');
+            var me = this;
+            var tabsContainer = Oskari.clazz.create('Oskari.userinterface.component.TabContainer');
             tabsContainer.addTabChangeListener(me.tabChanged);
-            this.tabsContainer = tabsContainer;
+            me.tabsContainer = tabsContainer;
 
             tabsContainer.addPanel(me.instance.cropping);
             tabsContainer.addPanel(me.instance.basket);
-            tabsContainer.insertTo(this.container);
+            tabsContainer.insertTo(me.container);
         },
+        /**
+         * Handles tab changes
+         * @method tabChanged
+         * @public
+         * @param  {Object}   previous previous tab
+         * @param  {Object}   current  current tab
+         */
         tabChanged: function(previous, current){
             if(current.getId() === 'download-basket-tab'){
                 current.createBasket();
             }
         },
-        getEventHandlers: function () {
 
-        },
-        onEvent: function (event) {
-
-        },
         /**
+         * Gets localization
          * @method _getLocalization
+         * @private
          */
         _getLocalization: function (key) {
             return this._localization[key];
         },
-        /**
-         * @public @method stopPlugin
-         * Interface method implementation, does nothing atm
-         *
-         *
-         */
-        stopPlugin: function () {
 
-        },
 
         /**
-         * @public @method getTitle
-         *
-         *
+         * Gets title
+         * @method getTitle
+         * @public
          * @return {String} localized text for the title of the flyout
          */
         getTitle: function () {
@@ -120,9 +102,9 @@ Oskari.clazz.define('Oskari.mapframework.bundle.downloadBasket.Flyout',
         },
 
         /**
-         * @public @method getDescription
-         *
-         *
+         * Gets description
+         * @method getDescription
+         * @public
          * @return {String} localized text for the description of the flyout.
          */
         getDescription: function () {
@@ -130,22 +112,19 @@ Oskari.clazz.define('Oskari.mapframework.bundle.downloadBasket.Flyout',
         },
 
         /**
-         * @public @method getOptions
          * Interface method implementation, does nothing atm
-         *
-         *
+         * @method getOptions
+         * @public
          */
         getOptions: function () {
 
         },
 
         /**
-         * @public @method setState
          * Interface method implementation, does nothing atm
-         *
-         * @param {Object} state
-         * State that this component should use
-         *
+         * @method setState
+         * @public
+         * @param {Object} state state that this component should use
          */
         setState: function (state) {
             this.state = state;
@@ -153,7 +132,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.downloadBasket.Flyout',
         },
         /**
          * @method refresh
-         * utitity to temporarily support rightjs sliders (again)
+         * @public
          */
         refresh: function () {
 
