58c66c0d2932faceb2e9bad51a7d02707cd02922
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2018-11-08 14:13:39 +0200

    Removed jsts

--- packages/framework/bundle/download-basket/bundle.js
+++ packages/framework/bundle/download-basket/bundle.js
@@ -3,89 +3,85 @@
  *
  * Definition for bundle. See source for details.
  */
-Oskari.clazz.define("Oskari.mapframework.bundle.downloadBasket.Bundle", function () {
+Oskari.clazz.define('Oskari.mapframework.bundle.downloadBasket.Bundle', function () {
 
 }, {
-    "create": function () {
+    'create': function () {
         var me = this;
-        var inst = Oskari.clazz.create("Oskari.mapframework.bundle.downloadBasket.BundleInstance");
+        var inst = Oskari.clazz.create('Oskari.mapframework.bundle.downloadBasket.BundleInstance');
 
         return inst;
-
     },
-    "update": function (manager, bundle, bi, info) {
+    'update': function (manager, bundle, bi, info) {
 
     }
 }, {
 
-    "protocol" : ["Oskari.bundle.Bundle", "Oskari.bundle.BundleInstance", "Oskari.mapframework.bundle.extension.ExtensionBundle"],
-    "source": {
+    'protocol': ['Oskari.bundle.Bundle', 'Oskari.bundle.BundleInstance', 'Oskari.mapframework.bundle.extension.ExtensionBundle'],
+    'source': {
 
-        "scripts": [{
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/instance.js"
+        'scripts': [{
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/instance.js'
         }, {
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/Flyout.js"
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/Flyout.js'
         }, {
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/Cropping.js"
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/Cropping.js'
         }, {
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/Basket.js"
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/Basket.js'
         }, {
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/Tile.js"
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/Tile.js'
         }, {
-            "type": "text/css",
-            "src": "../../../../bundles/framework/download-basket/resources/scss/style.scss"
-        }, {
-            "type" : "text/javascript",
-            "src" : "../../../../libraries/jsts/jsts-1.4.0.min.js"
+            'type': 'text/css',
+            'src': '../../../../bundles/framework/download-basket/resources/scss/style.scss'
         }],
 
-        "locales": [{
-            "lang": "en",
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/resources/locale/en.js"
+        'locales': [{
+            'lang': 'en',
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/resources/locale/en.js'
         }, {
-            "lang": "fi",
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/resources/locale/fi.js"
+            'lang': 'fi',
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/resources/locale/fi.js'
         }, {
-            "lang": "sv",
-            "type": "text/javascript",
-            "src": "../../../../bundles/framework/download-basket/resources/locale/sv.js"
+            'lang': 'sv',
+            'type': 'text/javascript',
+            'src': '../../../../bundles/framework/download-basket/resources/locale/sv.js'
         }]
     },
-    "bundle": {
-        "manifest": {
-            "Bundle-Identifier": "download-basket",
-            "Bundle-Name": "download-basket",
-            "Bundle-Author": [{
-                "Name": "Tuomas Ruohonen",
-                "Organisation": "Dimenteq Oy",
-                "Temporal": {
-                    "Start": "2015",
-                    "End": "2016"
+    'bundle': {
+        'manifest': {
+            'Bundle-Identifier': 'download-basket',
+            'Bundle-Name': 'download-basket',
+            'Bundle-Author': [{
+                'Name': 'Tuomas Ruohonen',
+                'Organisation': 'Dimenteq Oy',
+                'Temporal': {
+                    'Start': '2015',
+                    'End': '2016'
                 },
-                "Copyleft": {
-                    "License": {
-                        "License-Name": "EUPL",
-                        "License-Online-Resource": "http://www.paikkatietoikkuna.fi/license"
+                'Copyleft': {
+                    'License': {
+                        'License-Name': 'EUPL',
+                        'License-Online-Resource': 'http://www.paikkatietoikkuna.fi/license'
                     }
                 }
             }],
-            "Bundle-Name-Locale": {
-                "fi": {
-                    "Name": "download-basket",
-                    "Title": "download-basket"
+            'Bundle-Name-Locale': {
+                'fi': {
+                    'Name': 'download-basket',
+                    'Title': 'download-basket'
                 },
-                "en": {}
+                'en': {}
             },
-            "Bundle-Version": "1.0.0",
-            "Import-Namespace": ["Oskari", "jquery"],
-            "Import-Bundle": {}
+            'Bundle-Version': '1.0.0',
+            'Import-Namespace': ['Oskari', 'jquery'],
+            'Import-Bundle': {}
         }
     },
 
@@ -93,8 +89,8 @@ Oskari.clazz.define("Oskari.mapframework.bundle.downloadBasket.Bundle", function
      * @static
      * @property dependencies
      */
-    "dependencies": ["jquery"]
+    'dependencies': ['jquery']
 
 });
 
-Oskari.bundle_manager.installBundleClass("download-basket", "Oskari.mapframework.bundle.downloadBasket.Bundle");
+Oskari.bundle_manager.installBundleClass('download-basket', 'Oskari.mapframework.bundle.downloadBasket.Bundle');
