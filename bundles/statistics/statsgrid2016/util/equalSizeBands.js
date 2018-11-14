76d889da99033efd868818b7860b39cdb41b593a
Jan Wolski <jan@wolski.fi>
2018-11-14 16:23:48 +0200

    More docs

--- bundles/statistics/statsgrid2016/util/equalSizeBands.js
+++ bundles/statistics/statsgrid2016/util/equalSizeBands.js
@@ -1,3 +1,10 @@
+/**
+ * @function equalSizeBands
+ * Creates array of numbers between min & max so that delta between numbers is the same
+ * @param {Number} classCount number of classed requested. Array length will be classCount+1 
+ * @param {Number} min 
+ * @param {Number} max 
+ */
 export default function equalSizeBands (classCount, min, max) {
     var output = [];
     if (classCount < 1) {
