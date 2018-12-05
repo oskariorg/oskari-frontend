5a243f3fc3e20cc308fd3004e791f1bc455b95fe
Jan Wolski <jan@wolski.fi>
2018-12-05 09:50:19 +0200

    remove unused import

--- bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
+++ bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileLayerPlugin.js
@@ -5,7 +5,6 @@ import TileGrid from 'ol/tilegrid/TileGrid';
 import { createDefaultStyle } from 'ol/style/Style';
 
 import VectorTileModelBuilder from './VectorTileModelBuilder';
-import VectorTileLayer from './VectorTileLayer';
 import styleGenerator from './styleGenerator';
 import mapboxStyleFunction from 'ol-mapbox-style/stylefunction';
 import { LAYER_ID, LAYER_HOVER, LAYER_TYPE, FTR_PROPERTY_ID } from '../../domain/constants';
