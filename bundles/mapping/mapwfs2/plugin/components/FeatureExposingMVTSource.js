02bcb4cf000466faf8396992b6e940af76b73ca3
Jan Wolski <jan@wolski.fi>
2018-12-10 14:14:30 +0200

    Param for converting

--- bundles/mapping/mapwfs2/plugin/components/FeatureExposingMVTSource.js
+++ bundles/mapping/mapwfs2/plugin/components/FeatureExposingMVTSource.js
@@ -1,22 +1,30 @@
 import olSourceVectorTile from 'ol/source/VectorTile';
 import {oskariIdKey} from './FeatureUtil';
 import {intersects} from 'ol/extent';
+import unRenderFeatures from './unRenderFeatures';
 
 export default class FeatureExposingMVTSource extends olSourceVectorTile {
-    getFeaturesIntersectingExtent (extent) {
+    getFeaturesIntersectingExtent (extent, convertFeatures) {
         const featuresById = new Map();
         Object.values(this.sourceTiles_)
             .filter(tile => {
                 const tileExtent = this._getTileExtent(tile);
-                return intersects(tileExtent, extent);
-            })
-            .forEach(tile => {
+                if (!intersects(tileExtent, extent)) {
+                    return;
+                }
                 const features = tile.getFeatures();
                 if (!features) {
                     return;
                 }
-                features
-                    .filter(f => intersects(f.getExtent(), extent))
+                let matchingFeatures = features
+                    .filter(f => intersects(f.getExtent(), extent));
+
+                // TODO: optimize
+                if (convertFeatures) {
+                    matchingFeatures = unRenderFeatures(matchingFeatures, tile, this);
+                }
+
+                matchingFeatures
                     .forEach(f => {
                         featuresById.set(f.get(oskariIdKey), f);
                     });
