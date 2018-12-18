7df596d30fda3b7d3ce7ef35ee6dadc480f572ae
okauppinen <olli.kauppinen@nls.fi>
2018-12-18 10:24:09 +0200

    add error to userlocationevent

--- bundles/mapping/mapmodule/event/UserLocationEvent.js
+++ bundles/mapping/mapmodule/event/UserLocationEvent.js
@@ -8,10 +8,11 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.event.UserLocationEven
      * @method create called automatically on construction
      * @static
      */
-    function (lon, lat, accuracy) {
+    function (lon, lat, accuracy, error) {
         this._lon = lon;
         this._lat = lat;
         this._accuracy = accuracy;
+        this._error = error;
     }, {
         __name: 'UserLocationEvent',
 
@@ -28,6 +29,9 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.event.UserLocationEven
         getAccuracy: function () {
             return this._accuracy;
         },
+        getError: function () {
+            return this._error;
+        },
         /**
          * Serialization for RPC
          * @return {Object} object has key id which has the marker id of the clicked
@@ -36,7 +40,8 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.event.UserLocationEven
             return {
                 lon: this.getLon(),
                 lat: this.getLat(),
-                accuracy: this.getAccuracy()
+                accuracy: this.getAccuracy(),
+                error: this.getError()
             };
         }
     }, {
