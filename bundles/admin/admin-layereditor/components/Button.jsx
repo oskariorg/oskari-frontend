f8584598e46667034c4b0b9e7cd4b682c9096fa3
atlain <laine.atte@gmail.com>
2019-03-13 09:29:51 +0200

    Minor refactoring

--- bundles/admin/admin-layereditor/components/Button.jsx
+++ bundles/admin/admin-layereditor/components/Button.jsx
@@ -3,15 +3,10 @@ import PropTypes from 'prop-types';
 import Button from 'antd/lib/button';
 import 'antd/lib/button/style/css';
 
-const ButtonComponent = (props) => {
-    return (
-        <Button {...props}>{props.children}</Button>
-    );
-};
+export const ButtonComponent = (props) => (
+    <Button {...props}>{props.children}</Button>
+);
 
 ButtonComponent.propTypes = {
     children: PropTypes.any
 };
-
-export default ButtonComponent;
-
