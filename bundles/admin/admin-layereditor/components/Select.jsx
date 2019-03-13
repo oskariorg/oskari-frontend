9e63f036126e36819566082d5bc4e975ffb59359
atlain <laine.atte@gmail.com>
2019-03-13 10:52:57 +0200

    Refactored naming conventions: Select component

--- bundles/admin/admin-layereditor/components/Select.jsx
+++ bundles/admin/admin-layereditor/components/Select.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Select from 'antd/lib/select';
+import AntSelect from 'antd/lib/select';
 import 'antd/lib/select/style/css';
 
-const Option = Select.Option;
+const AntOption = AntSelect.Option;
 
-export const SelectComponent = (props) => {
+export const Select = (props) => {
     const {options, ...other} = props;
     return (
-        <Select {...other}>
+        <AntSelect {...other}>
             {options.map((option, key) => (
-                <Option {...option} key={key}>{option.title}</Option>
+                <AntOption {...option} key={key}>{option.title}</AntOption>
             ))}
-        </Select>
+        </AntSelect>
     );
 };
 
-SelectComponent.propTypes = {
+Select.propTypes = {
     options: PropTypes.array
 };
