eba3d167ece7e95450428e1a073df7384e06de4d
Sami Mäkinen <sami.makinen@nls.fi>
2019-03-14 18:07:34 +0200

    Add protocol select for UrlInput

--- bundles/admin/admin-layereditor/components/UrlInput.jsx
+++ bundles/admin/admin-layereditor/components/UrlInput.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import Input from 'antd/lib/input';
+import { Select, Option } from './Select';
 
+const protocols = ['https', 'http'];
+const protocolSelect = (
+    <Select defaultValue="https://" style={{ width: 90 }}>
+        {protocols.map((title, key) => (
+            <Option key={key}>{title}://</Option>
+        ))}
+    </Select>
+);
 export const UrlInput = (props) => (
-    <Input {...props} />
+    <Input {...props} addonBefore={protocolSelect} />
 );
