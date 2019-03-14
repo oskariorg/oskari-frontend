3f6c54c3b0b0b3dd963656df97ed3994cde751aa
atlain <laine.atte@gmail.com>
2019-03-14 10:37:30 +0200

    Add style imports

--- bundles/admin/admin-layereditor/components/Checkbox.jsx
+++ bundles/admin/admin-layereditor/components/Checkbox.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AntCheckbox from 'antd/lib/checkbox';
+import 'antd/lib/checkbox/style/css';
 
 export const Checkbox = (props) => (
     <AntCheckbox {...props} />
--- bundles/admin/admin-layereditor/components/RadioButton.jsx
+++ bundles/admin/admin-layereditor/components/RadioButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AntRadio from 'antd/lib/radio';
+import 'antd/lib/radio/style/css';
 
 export const Radio = (props) => (
     <AntRadio {...props} />
