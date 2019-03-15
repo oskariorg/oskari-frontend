1dde37cb9494ecca3b144a711af32bb9e1406573
okauppinen <olli.kauppinen@nls.fi>
2019-03-15 14:03:14 +0200

    add optional prefix param

--- src/react/util.jsx
+++ src/react/util.jsx
@@ -14,11 +14,11 @@ export function withContext (Component) {
 
 // re-binds all object methods starting with 'handle',
 // so that methods can be used detached from instance. For use in React
-export function handleBinder (ob) {
+export function handleBinder (ob, prefix = 'handle') {
     const proto = Object.getPrototypeOf(ob);
     Object.getOwnPropertyNames(proto).forEach(propertyName => {
         const desc = Object.getOwnPropertyDescriptor(proto, propertyName);
-        if (!!desc && typeof desc.value === 'function' && propertyName.startsWith('handle')) {
+        if (!!desc && typeof desc.value === 'function' && propertyName.startsWith(prefix)) {
             ob[propertyName] = desc.value.bind(ob);
         }
     });
