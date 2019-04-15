95bc6bbf6743932c476bcdec058ca5c69f4fc48e
okauppinen <olli.kauppinen@nls.fi>
2019-04-15 20:51:51 +0300

    validate stroke and border width in visu form

--- bundles/framework/divmanazer/component/VisualizationForm.js
+++ bundles/framework/divmanazer/component/VisualizationForm.js
@@ -25,6 +25,7 @@ Oskari.clazz.define(
         this.dialog = null;
 
         var defaultOptions = {
+            validateValues: true,
             // include all forms by default
             forms: ['dot', 'line', 'area'],
             formValues: {
@@ -73,7 +74,23 @@ Oskari.clazz.define(
             '<div class="renderButton"></div>'
         );
     }, {
-
+        validateWidth: function (key, width) {
+            if (!this._options.validateValues) {
+                return true;
+            }
+            const isValid = Oskari.util.isNumberBetween(width, 1, 50);
+            if (!isValid) {
+                this.showMessage(this._loc.validation.title, this._loc.validation[key]);
+            }
+            return isValid;
+        },
+        showMessage: function (title, message) {
+            var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
+            var okBtn = dialog.createCloseButton();
+            okBtn.setPrimary(true, true);
+            dialog.fadeout();
+            dialog.show(title, message, [okBtn]);
+        },
         /**
          * @public @method getForm
          * Creates dom elements for each forms and binds click events to them
