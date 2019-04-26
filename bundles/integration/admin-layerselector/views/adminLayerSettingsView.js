85afd39a9e6da10c6950158e172a2f0fd0544ee4
Sami Mäkinen <sami.makinen@nls.fi>
2019-04-26 17:30:40 +0300

    Show WFS 3 only when supported

--- bundles/integration/admin-layerselector/views/adminLayerSettingsView.js
+++ bundles/integration/admin-layerselector/views/adminLayerSettingsView.js
@@ -471,6 +471,8 @@ function (
                 urlSource = [],
                 i,
                 j;
+            const wfsPlugin = Oskari.getSandbox().findRegisteredModuleInstance('MainMapModule').getLayerPlugins('wfs');
+            const supportsWFS3 = wfsPlugin && wfsPlugin.oskariStyleSupport === true;
             if (!me.model) {
                 me.model = this._createNewModel(layerType);
                 this.listenTo(this.model, 'change', this.render);
@@ -490,6 +492,7 @@ function (
 
             me.$el.append(me.layerTemplate({
                 model: me.model,
+                supportsWFS3: supportsWFS3,
                 header: layerTypeData.headerTemplate,
                 footer: layerTypeData.footerTemplate,
                 instance: me.options.instance,
