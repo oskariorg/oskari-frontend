d62600b29af2967e86afa2e983d2b6b064ada154
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-06-03 13:13:15 +0300

    Add comment to describe purpose of babel.config.js vs. babel configs in webpack.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
+/* Settings defined here are for Jest testing framework.
+ * Actual babel configurations related to building are defined in webpack.config.js.
+ */
 module.exports = {
     presets: [
         [
