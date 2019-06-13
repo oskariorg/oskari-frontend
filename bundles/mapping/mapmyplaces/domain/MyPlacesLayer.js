f6d94b2700b9d9541bd8dea7de588e585627dc1c
jheijari <joonas.heijari@gmail.com>
2019-06-13 12:03:41 +0300

    Use ES classes for layers

--- bundles/mapping/mapmyplaces/domain/MyPlacesLayer.js
+++ bundles/mapping/mapmyplaces/domain/MyPlacesLayer.js
@@ -1,60 +1,54 @@
+const WFSLayer = Oskari.clazz.get('Oskari.mapframework.bundle.mapwfs2.domain.WFSLayer');
 /**
  * @class Oskari.mapframework.bundle.mapmyplaces.domain.MyPlacesLayer
  *
  * MapLayer of type MyPlaces
  */
-Oskari.clazz.define('Oskari.mapframework.bundle.mapmyplaces.domain.MyPlacesLayer',
-
-    /**
- * @method create called automatically on construction
- * @static
- */
-    function () {
-    /* Layer Type */
+export class MyPlacesLayer extends WFSLayer {
+    constructor () {
+        super();
+        /* Layer Type */
         this._layerType = 'MYPLACES';
         this._metaType = 'MYPLACES';
-    }, {
-        /* Layer type specific functions */
-
-        /**
+    }
+    /**
      * Sets the wms url for the layer.
      *
      * @method setWmsUrl
      * @param {String} wmsUrl
      */
-        setWmsUrl: function (wmsUrl) {
-            this._wmsUrl = wmsUrl;
-        },
-        /**
+    setWmsUrl (wmsUrl) {
+        this._wmsUrl = wmsUrl;
+    }
+    /**
      * Returns the wms url of the layer.
      *
      * @method getWmsUrl
      * @return {String}
      */
-        getWmsUrl: function () {
-            return this._wmsUrl;
-        },
-        /**
+    getWmsUrl () {
+        return this._wmsUrl;
+    }
+    /**
      * @method setWmsName
      * @param {String} wmsName used to identify service f.ex. in GetFeatureInfo queries.
      */
-        setWmsName: function (wmsName) {
-            this._wmsName = wmsName;
-        },
-        /**
+    setWmsName (wmsName) {
+        this._wmsName = wmsName;
+    }
+    /**
      * @method getWmsName
      * @return {String} wmsName used to identify service f.ex. in GetFeatureInfo queries.
      */
-        getWmsName: function () {
-            return this._wmsName;
-        },
-        isFilterSupported: function () {
-            // this defaults to false in AbstractLayer, but WFSLayer returns true.
-            // Not sure if this is something we want, but it's the same behavior as before but NOT having
-            // WFS and analysis referenced in AbstractLayer
-            return false;
-        }
+    getWmsName () {
+        return this._wmsName;
+    }
+    isFilterSupported () {
+        // this defaults to false in AbstractLayer  but WFSLayer returns true.
+        // Not sure if this is something we want  but it's the same behavior as before but NOT having
+        // WFS and analysis referenced in AbstractLayer
+        return false;
+    }
+};
 
-    }, {
-        'extend': ['Oskari.mapframework.bundle.mapwfs2.domain.WFSLayer']
-    });
+Oskari.clazz.defineES('Oskari.mapframework.bundle.mapmyplaces.domain.MyPlacesLayer', MyPlacesLayer);
