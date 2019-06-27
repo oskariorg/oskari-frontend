6c631ffbf2023569eab73f22c001c15106cb27e3
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-06-27 17:57:53 +0300

    Add guided tour texts for maplegend and userguide bundles

--- bundles/framework/maplegend/resources/locale/fr.js
+++ bundles/framework/maplegend/resources/locale/fr.js
@@ -10,6 +10,12 @@ Oskari.registerLocalization(
         "tooltip": "Afficher les légendes de cartes",
         "singleLegend": "Légendes de cartes : ",
         "infotext": "Choisir la couche cartographique pour laquelle la légende est affichée :",
-        "newtab": "Ouvrir dans un nouvel onglet"
+        "newtab": "Ouvrir dans un nouvel onglet",
+        "guidedTour": {
+            "title": "Légendes des cartes",
+            "message": "Voir les légendes des couches ajoutées à l'heure actuelle pour mieux comprendre les données.",
+            "openLink": "Afficher Légendes des cartes",
+            "closeLink": "Masquer Légendes des cartes"
+        }
     }
 });
