a7af6fef65eb7921b0ef6da5009e3d86a5cd80c0
Sami Mäkinen <sami.makinen@nls.fi>
2019-09-19 10:59:56 +0300

    Change init for renderMode <> 3d detection

--- bundles/mapping/mapwfs2/plugin/WfsVectorLayerPlugin.ol.js
+++ bundles/mapping/mapwfs2/plugin/WfsVectorLayerPlugin.ol.js
@@ -72,7 +72,11 @@ export class WfsVectorLayerPlugin extends AbstractMapLayerPlugin {
     _initImpl () {
         super._initImpl();
         const sandbox = this.getSandbox();
-        this.renderMode = this.renderMode || (this.getMapModule().getSupports3D() ? RENDER_MODE_VECTOR : RENDER_MODE_MVT);
+        if (this.getMapModule().getSupports3D() && this.renderMode !== RENDER_MODE_VECTOR) {
+            // 3D mapmodule doesn't support MVT
+            this._log.warn('Changing WFS render mode (3d only supports GeoJSON). Configuration was for: ' + this.renderMode);
+            this.renderMode = RENDER_MODE_VECTOR;
+        }
         this.reqEventHandler = new ReqEventHandler(sandbox);
         this.visualizationForm = new VisualizationForm();
         this.WFSLayerService = new WFSLayerService(sandbox);
