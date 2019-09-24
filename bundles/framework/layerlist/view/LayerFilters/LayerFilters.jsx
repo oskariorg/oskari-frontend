778e7835638e15d3a997479af3f4e923ad0adbea
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-09-24 16:57:25 +0300

    Refactor code to use service mutator and to generate jsx from filters passed as props

--- bundles/framework/layerlist/view/LayerFilters/LayerFilters.jsx
+++ bundles/framework/layerlist/view/LayerFilters/LayerFilters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { LayerFilter } from './LayerFilter/LayerFilter';
 import styled from 'styled-components';
@@ -9,51 +9,31 @@ const StyledFilters = styled.div`
     display: flex;
 `;
 
-export const LayerFilters = ({ layerListRenderHandler }) => {
-    const layerlistService = Oskari.getSandbox().getService('Oskari.mapframework.service.LayerlistService');
-    const mapLayerService = Oskari.getSandbox().getService('Oskari.mapframework.service.MapLayerService');
-    const [buttons, addButton] = useState([]);
-    // TODO: Fix currentFilter setting through state.
-    // const [currentFilter, setFilter] = useState('');
-    var currentFilter = '';
-    useEffect(() => {
-        layerlistService.on('Layerlist.Filter.Button.Add', (button) => {
-            addButton(buttons => {
-                const buttonCreated = buttons.filter(b => b.key === button.properties.id).length > 0;
-                if (!buttonCreated) {
-                    const filterButton =
-                        <LayerFilter key={button.properties.id} text={button.properties.text}
-                            tooltip={button.properties.tooltip} filterName = {button.filterId}
-                            classNameDeactive = {button.properties.cls.deactive}
-                            clickHandler={(event) => {
-                                const filterName = event.target.attributes.filtername || event.target.parentElement.attributes.filtername;
-                                const filter = filterName.value;
-                                var layers;
-                                if (currentFilter !== filter) {
-                                    layers = mapLayerService.getFilteredLayers(filter);
-                                } else if (currentFilter === filter) {
-                                    layers = mapLayerService.getAllLayers();
-                                }
-                                // setFilter(filter);
-                                currentFilter = filter;
-                                const layersCopy = layers.slice(0);
-                                layerListRenderHandler(layersCopy);
-                            }}>
-                        </LayerFilter>
-                    return [...buttons, filterButton];
-                } else {
-                    return buttons;
-                }
-            });
+export const LayerFilters = ({ filters, service }) => {
+    const mappedFilters = [];
+
+    if (filters) {
+        Object.values(filters).forEach(button => {
+            mappedFilters.push(
+                <LayerFilter key={button.id} text={button.text}
+                    tooltip={button.tooltip} filterName = {button.id}
+                    currentStyle = {button.cls.current}
+                    clickHandler={(event) => {
+                        const filterName = event.target.attributes.filtername || event.target.parentElement.attributes.filtername;
+                        service.setCurrentFilter(filterName.value);
+                    }}>
+                </LayerFilter>
+            );
         });
-    });
+    }
     return (
         <StyledFilters>
-            {buttons}
+            {mappedFilters}
         </StyledFilters>
     );
 };
 
 LayerFilters.propTypes = {
-    layerListRenderHandler: PropTypes.func.isRequired
+    filters: PropTypes.object.isRequired,
+    service: PropTypes.object.isRequired
 };
