da1313e83f4de6c9686c169361433db5bdd51372
jheijari <joonas.heijari@gmail.com>
2019-10-15 13:14:34 +0300

    Fix ant-design missing non-global styles

--- bundles/admin/admin-layereditor/components/Button.jsx
+++ bundles/admin/admin-layereditor/components/Button.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { Button as AntButton } from 'antd';
 import 'antd/es/button/style/index.js';
 
-const Styled = styled(AntButton)`
-    > .anticon {
-        vertical-align: -0.125em;
-        text-rendering: optimizeLegibility;
-        -webkit-font-smoothing: antialiased;
-    }
-`;
-
 export const Button = ({ children, ...other }) => (
-    <Styled {...other}>{children}</Styled>
+    <AntButton {...other}>{children}</AntButton>
 );
 
 Button.propTypes = {
