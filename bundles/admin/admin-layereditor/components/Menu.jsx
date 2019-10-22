380df7f848c07045b46af4c6cc6b783e66284e95
Markus Ylinen <markus.ylinen@siili.com>
2019-10-22 13:22:59 +0300

    Import styles to menu

--- bundles/admin/admin-layereditor/components/Menu.jsx
+++ bundles/admin/admin-layereditor/components/Menu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Menu as AntMenu } from 'antd';
+import 'antd/es/menu/style/index.js';
 
 export const Menu = ({ items }) => (
     <AntMenu>
