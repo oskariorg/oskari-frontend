d84398d4cbaa358e549cd2c087579419dc96a8b3
jheijari <joonas.heijari@gmail.com>
2019-10-23 12:56:49 +0300

    Don't mock Oskari for tests

--- bundles/framework/layerlist/view/LayerViewTabs/test.util.js
+++ bundles/framework/layerlist/view/LayerViewTabs/test.util.js
@@ -3,7 +3,6 @@ import '../../../layerselector2/resources/locale/en';
 import '../../../../mapping/mapmodule/service/map.state';
 
 export const initServices = () => {
-    Oskari.setLang('en');
     const sandbox = Oskari.getSandbox();
     const layerlistService = Oskari.clazz.create('Oskari.mapframework.service.LayerlistService', sandbox);
     const mapStateService = Oskari.clazz.create('Oskari.mapframework.domain.Map', sandbox);
