b2e15a177f6714c3a46fe10671e52afbecbc80ef
Markus Ylinen <markus.ylinen@siili.com>
2019-10-25 16:04:54 +0300

    SelectedTab refactored to own component

--- bundles/framework/layerlist/view/LayerViewTabs/LayerViewTabs.jsx
+++ bundles/framework/layerlist/view/LayerViewTabs/LayerViewTabs.jsx
@@ -3,42 +3,11 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Tabs, TabPane } from 'oskari-ui';
 import { LayerList } from './LayerList/';
-import { SelectedLayers } from './SelectedLayers/';
+import { SelectedLayers, SelectedTab } from './SelectedLayers/';
 
 const StyledTabs = styled(Tabs)`
     max-width: 600px;
 `;
-
-const StyledBadge = styled.div`
-    min-width: 20px;
-    height: 20px;
-    color: #000;
-    background: #ffd400;
-    border-radius: 4px;
-    text-align: center;
-    padding: 2px 8px;
-    font-size: 12px;
-    display: inline;
-    line-height: 20px;
-    margin-left: 5px;
-    font-weight: 700;
-`;
-
-const SelectedTab = ({ num, text }) => {
-    return (
-        <span>
-            {text}
-            <StyledBadge>
-                {num}
-            </StyledBadge>
-        </span>
-    );
-};
-
-SelectedTab.propTypes = {
-    num: PropTypes.number.isRequired,
-    text: PropTypes.string.isRequired
-};
 
 export const LayerViewTabs = props => {
     const layerKey = 0;
