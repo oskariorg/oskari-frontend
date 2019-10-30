6fbfc02ae874e099496510babbae0a9fea3f295b
okauppinen <olli.kauppinen@nls.fi>
2019-10-30 10:48:06 +0200

    remove Paikkatietoikkuna from localization

--- bundles/mapping/dimension-change/resources/locale/fi.js
+++ bundles/mapping/dimension-change/resources/locale/fi.js
@@ -4,7 +4,7 @@ Oskari.registerLocalization(
         "key": "dimensionchange",
         "value": {
             "to3Dview": "Siirry 3D-näkymään. Käytettävissä vain osa karttatasoista ja työkaluista.",
-            "backTo2Dview": "Palaa Paikkatietoikkunan oletusnäkymään.",
+            "backTo2Dview": "Palaa oletusnäkymään.",
             "change-dimension-2D": "Siirry 2D-näkymään.",
             "change-dimension-3D": "Siirry 3D-näkymään."
         }
