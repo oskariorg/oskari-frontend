ffe9817f60b8f201259442a1858c717a1b71f2bb
jheijari <joonas.heijari@gmail.com>
2019-11-11 16:21:15 +0200

    Slider fixes

--- bundles/framework/layerlist/view/LayerViewTabs/SelectedLayers/LayerBox/Footer/OpacitySlider.jsx
+++ bundles/framework/layerlist/view/LayerViewTabs/SelectedLayers/LayerBox/Footer/OpacitySlider.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Slider, NumberInput, InputGroup } from 'oskari-ui';
+import { Timeout } from 'oskari-ui/util';
 
-const StyledSlider = styled.div`
+const Border = styled('div')`
     border-radius: 4px;
     border: 1px solid #d9d9d9;
     width: 120px;
-    padding: 10px;
+    padding: 10px 15px;
+`;
+const StyledSlider = styled(Slider)`
+    margin: 0 !important;
 `;
 
 const StyledNumberInput = styled(NumberInput)`
@@ -17,24 +21,37 @@ const StyledNumberInput = styled(NumberInput)`
     height: 34px !important;
 `;
 
-export const OpacitySlider = ({ value, onChange }) => (
-    <InputGroup compact>
-        <StyledSlider>
-            <Slider
-                value={value}
+export const OpacitySlider = ({ value, onChange }) => {
+    const [sliderValue, updateSlider] = useState(value);
+    const [previousValue, setPreviousValue] = useState(value);
+    const [eventTimeout, setEventTimeout] = useState(null);
+    if (previousValue !== value) {
+        // equal to get derived state from props
+        updateSlider(value);
+        setPreviousValue(value);
+    }
+    const instantValueChange = val => {
+        updateSlider(val);
+        if (eventTimeout && eventTimeout.isPending()) {
+            eventTimeout.cancel();
+        }
+        setEventTimeout(new Timeout(() => onChange(val), 200));
+    };
+    return (
+        <InputGroup compact>
+            <Border>
+                <StyledSlider value={sliderValue} onChange={instantValueChange} />
+            </Border>
+            <StyledNumberInput
+                min={0}
+                max={100}
+                value={sliderValue}
                 onChange={onChange}
-                style={{ margin: '0px' }}
+                formatter={value => `${value} %`}
             />
-        </StyledSlider>
-        <StyledNumberInput
-            min={0}
-            max={100}
-            value={value}
-            onChange={onChange}
-            formatter={value => `${value} %`}
-        />
-    </InputGroup>
-);
+        </InputGroup>
+    );
+};
 
 OpacitySlider.propTypes = {
     value: PropTypes.number.isRequired,
