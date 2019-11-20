99aa25b684c172be032003e758e37a7fb1f4856a
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-11-20 10:36:21 +0200

    Add service to handle user styles

---
+++ bundles/mapping/mapwfs2/service/UserStyleService.js
@@ -0,0 +1,71 @@
+export class UserStyleService {
+    constructor () {
+        this.styles = new Map();
+        Oskari.makeObservable(this);
+        this.mutator = this.initializeMutator();
+    }
+    initializeMutator () {
+        const me = this;
+        return {
+            saveUserStyle (layerId, style) {
+                if (!layerId || !style) {
+                    return;
+                }
+                const layerStyles = me.styles.get(layerId);
+                if (layerStyles) {
+                    const index = layerStyles.findIndex(s => s.id === style.id);
+
+                    if (index !== -1) {
+                        layerStyles[index] = style;
+                    } else {
+                        layerStyles.push(style);
+                    }
+                    me.styles.set(layerId, layerStyles);
+                } else {
+                    me.styles.set(layerId, [style]);
+                }
+                me.trigger('update');
+            },
+            removeUserStyle (layerId, styleId) {
+                if (!layerId || !styleId) {
+                    return;
+                }
+
+                const layerStyles = me.styles.get(layerId);
+                if (layerStyles) {
+                    const index = layerStyles.findIndex(s => s.id === styleId);
+                    if (index !== -1) {
+                        layerStyles.splice(index, 1);
+                        me.styles.set(layerId, layerStyles);
+                        me.trigger('update');
+                    }
+                }
+            },
+            removeUserStyleFromLayer (layerId, styleId) {
+                const layer = Oskari.getSandbox().findMapLayerFromSelectedMapLayers(layerId);
+                if (layer && layer.getCustomStyle() && layer.getCustomStyle().id === styleId) {
+                    layer.resetStyleToDefault();
+                    Oskari.getSandbox().postRequestByName('ChangeMapLayerStyleRequest', [layerId]);
+                }
+            }
+        };
+    }
+    getMutator () {
+        return this.mutator;
+    }
+    getQName () {
+        return 'Oskari.mapframework.bundle.mapwfs2.service.UserStyleService';
+    }
+    getUserStylesForLayer (layerId) {
+        return this.styles.get(layerId);
+    }
+    getUserStyle (layerId, styleId) {
+        const layerStyles = this.styles.get(layerId);
+        var style;
+        if (layerStyles) {
+            const s = layerStyles.filter(style => style.id === styleId);
+            style = s ? s[0] : undefined;
+        }
+        return style;
+    }
+}
---
+++ bundles/mapping/mapwfs2/service/UserStyleService.test.js
@@ -0,0 +1,68 @@
+import { UserStyleService } from './UserStyleService';
+
+describe('saveUserStyle function ', () => {
+    test('adds style correctly when styles with given layerId and style id does not exists', () => {
+        const service = new UserStyleService();
+        const mutator = service.getMutator();
+        const layerId = 1;
+        const style = {
+            id: 1
+        };
+        mutator.saveUserStyle(layerId, style);
+        const result = service.getUserStylesForLayer(layerId);
+        expect(result.length).toEqual(1);
+        expect(result[0]).toEqual(style);
+    });
+    test('adds style correctly when styles with given layerId exists but not with given style id', () => {
+        const service = new UserStyleService();
+        const mutator = service.getMutator();
+        const layerId = 1;
+        const style1 = {
+            id: 1
+        };
+        const style2 = {
+            id: 2
+        };
+        mutator.saveUserStyle(layerId, style1);
+        mutator.saveUserStyle(layerId, style2);
+        const result = service.getUserStylesForLayer(layerId);
+        expect(result.length).toEqual(2);
+        expect(result[0]).toEqual(style1);
+        expect(result[1]).toEqual(style2);
+    });
+    test('saves style correctly when style with given layerId and given style id exists', () => {
+        const service = new UserStyleService();
+        const mutator = service.getMutator();
+        const layerId = 1;
+        const originalStyle = {
+            id: 1
+        };
+        const updatedStyle = {
+            id: 1,
+            someNewField: 'someValue'
+        };
+        mutator.saveUserStyle(layerId, originalStyle);
+        mutator.saveUserStyle(layerId, updatedStyle);
+        const result = service.getUserStylesForLayer(layerId);
+        expect(result.length).toEqual(1);
+        expect(result[0]).toEqual(updatedStyle);
+    });
+});
+
+describe('removeUserStyle function ', () => {
+    test('removes user style correctly when found', () => {
+        const service = new UserStyleService();
+        const mutator = service.getMutator();
+        const layerId = 1;
+        const styleId = 1;
+        const style = {
+            id: styleId
+        };
+        mutator.saveUserStyle(layerId, style);
+        const result1 = service.getUserStylesForLayer(layerId);
+        expect(result1.length).toEqual(1);
+        mutator.removeUserStyle(layerId, styleId);
+        const result2 = service.getUserStylesForLayer(layerId);
+        expect(result2.length).toEqual(0);
+    });
+});
