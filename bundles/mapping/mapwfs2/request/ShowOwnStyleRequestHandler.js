886667c01005e676bf5f574768c26205b07706b9
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-11-20 14:59:01 +0200

    Update request and handler to enable handling of multiple own styles

--- bundles/mapping/mapwfs2/request/ShowOwnStyleRequestHandler.js
+++ bundles/mapping/mapwfs2/request/ShowOwnStyleRequestHandler.js
@@ -1,3 +1,4 @@
+import { UserStylesFlyout } from '../UserStylesFlyout';
 /**
  * @class Oskari.mapframework.bundle.mapwfs2.request.ShowOwnStyleRequestHandler
  *
@@ -13,8 +14,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapwfs2.request.ShowOwnStyleRequ
 
     function (plugin) {
         this.plugin = plugin;
-        this.localization = Oskari.getMsg.bind(null, 'MapWfs2')('popup');
-
+        this.localization = Oskari.getMsg.bind(null, 'MapWfs2');
         /* templates */
         this.template = {};
         var p;
@@ -23,6 +23,8 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapwfs2.request.ShowOwnStyleRequ
                 this.template[p] = jQuery(this.__templates[p]);
             }
         }
+        this.service = Oskari.getSandbox().getService(
+            'Oskari.mapframework.bundle.mapwfs2.service.UserStyleService');
     }, {
         __templates: {
             'wrapper': '<div></div>',
@@ -39,31 +41,50 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapwfs2.request.ShowOwnStyleRequ
          *      request to handle
          */
         handleRequest: function (core, request) {
-            var layerId = request.getId();
-            var layer = this.plugin.getSandbox().findMapLayerFromSelectedMapLayers(layerId);
-
+            const layerId = request.getId();
+            const styleId = request.getStyleId();
+            const createNew = request.isCreateNew();
+            const userStylesForLayer = this.plugin.getUserStylesForLayer(layerId);
+            if (userStylesForLayer && userStylesForLayer.length > 0 && !styleId && !createNew) {
+                this._showUserStylesList(layerId);
+            } else {
+                this._showVisualizationForm(layerId, styleId);
+            }
+        },
+        _showVisualizationForm (layerId, styleId, createNew) {
             // init popup
             var dialog = Oskari.clazz.create('Oskari.userinterface.component.Popup');
-            var title = this.localization.title;
+            var title = this.localization('popup.title');
             var content = this.template.wrapper.clone();
+            const layer = this.plugin.getSandbox().findMapLayerFromSelectedMapLayers(layerId);
+            var customStyle;
 
+            if (styleId) {
+                customStyle = this.service.getUserStyle(layerId, styleId);
+            } else {
+                customStyle = null;
+            }
             // add form
-            content.append(this.plugin.getCustomStyleEditorForm(layer));
+            content.append(this.plugin.getCustomStyleEditorForm(customStyle));
 
             // buttons
             var self = this;
             var saveOwnStyleBtn = Oskari.clazz.create('Oskari.userinterface.component.Button');
-            saveOwnStyleBtn.setTitle(this.localization.button.save);
+            saveOwnStyleBtn.setTitle(this.localization('popup.button.save'));
             saveOwnStyleBtn.addClass('primary saveOwnStyle');
             saveOwnStyleBtn.setHandler(function () {
-                self.plugin.applyEditorStyle(layer);
+                if (!styleId) {
+                    styleId = Date.now();
+                }
+                self.plugin.applyEditorStyle(layer, styleId);
+                self.plugin.saveUserStyle(layerId, styleId);
                 var event = Oskari.eventBuilder('MapLayerEvent')(layerId, 'update');
                 self.plugin.getSandbox().notifyAll(event);
                 dialog.close();
             });
 
             var cancelBtn = Oskari.clazz.create('Oskari.userinterface.component.Button');
-            cancelBtn.setTitle(this.localization.button.cancel);
+            cancelBtn.setTitle(this.localization('popup.button.cancel'));
             cancelBtn.setHandler(function () {
                 dialog.close();
             });
@@ -71,6 +92,30 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapwfs2.request.ShowOwnStyleRequ
             // show popup
             dialog.addClass('wfs_own_style');
             dialog.show(title, content, [cancelBtn, saveOwnStyleBtn]);
+        },
+        _showUserStylesList (layerId) {
+            const flyout = this._getFlyout();
+            flyout.setLayerId(layerId);
+            flyout.show();
+        },
+        /**
+         * @private @method _getFlyout
+         * Ensure flyout exists and return it
+         * @return {LayerEditorFlyout}
+         */
+        _getFlyout () {
+            if (!this.flyout) {
+                const xPosition = jQuery('#mapdiv').position().left;
+                const offset = 150;
+
+                this.flyout = new UserStylesFlyout(this.localization('own-styles-flyout-title'));
+                this.flyout.move(xPosition + offset, 15, true);
+                this.flyout.makeDraggable({
+                    handle: '.oskari-flyouttoolbar',
+                    scroll: false
+                });
+            }
+            return this.flyout;
         }
     }, {
         /**
