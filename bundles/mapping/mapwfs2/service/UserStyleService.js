a4f8c203b14abf002620879f3f4400c26c565917
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-11-20 14:52:26 +0200

    Add new function to combine logic to remove user style from service and from layer

--- bundles/mapping/mapwfs2/service/UserStyleService.js
+++ bundles/mapping/mapwfs2/service/UserStyleService.js
@@ -26,6 +26,10 @@ export class UserStyleService {
                 }
                 me.trigger('update');
             },
+            removeStyle (layerId, styleId) {
+                this.removeUserStyle(layerId,styleId);
+                this.removeUserStyleFromLayer(layerId,styleId);
+            },
             removeUserStyle (layerId, styleId) {
                 if (!layerId || !styleId) {
                     return;
