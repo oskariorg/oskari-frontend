e30fdef62b3f4103b2284e14edbc8e3d894b86f6
Mikko Mustikkamaa <mikko.mustikkamaa@outlook.com>
2019-11-20 14:06:31 +0200

    Add new flyout and react components

---
+++ bundles/mapping/mapwfs2/UserStylesFlyout.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { UserStyles } from './view/UserStyles';
+import { LocaleContext, MutatorContext } from 'oskari-ui/util';
+
+const ExtraFlyout = Oskari.clazz.get('Oskari.userinterface.extension.ExtraFlyout');
+
+export class UserStylesFlyout extends ExtraFlyout {
+    constructor (title, options) {
+        super(title, options);
+        this.element = null;
+        this.loc = Oskari.getMsg.bind(null, 'MapWfs2');
+        this.service = Oskari.getSandbox().getService(
+            'Oskari.mapframework.bundle.mapwfs2.service.UserStyleService');
+        this.on('show', () => {
+            if (!this.getElement()) {
+                this.createUi();
+            }
+        });
+        this.on('hide', () => {
+            this.cleanUp();
+        });
+        this.service.on('update', () => {
+            this.update();
+        });
+    }
+    setElement (el) {
+        this.element = el;
+    }
+    getElement () {
+        return this.element;
+    }
+    setLocale (loc) {
+        this.loc = loc;
+    }
+    createUi () {
+        this.setElement(jQuery('<div></div>'));
+        this.addClass('user-styles-flyout');
+        this.setContent(this.getElement());
+        this.update();
+    }
+    setLayerId (layerId) {
+        this.layerId = layerId;
+    }
+    update () {
+        const el = this.getElement();
+        if (!el) {
+            return;
+        }
+        const uiCode = this.getEditorUI();
+
+        ReactDOM.render(uiCode, el.get(0));
+    }
+    getEditorUI () {
+        const styles = this.service.getUserStylesForLayer(this.layerId);
+        return (
+            <LocaleContext.Provider value={this.loc}>
+                <MutatorContext.Provider value={this.service}>
+                    <UserStyles layerId={this.layerId} styles={styles}></UserStyles>
+                </MutatorContext.Provider>
+            </LocaleContext.Provider>
+        );
+    }
+    cleanUp () {
+        const el = this.getElement();
+        if (!el) {
+            return;
+        }
+        ReactDOM.unmountComponentAtNode(el.get(0));
+        delete this.element;
+    }
+}
---
+++ bundles/mapping/mapwfs2/view/UserStyles.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { List, ListItem, Icon } from 'oskari-ui';
+import { withLocale, withMutator } from 'oskari-ui/util';
+import { UserStyleRow } from './UserStyles/UserStyleRow';
+
+const Header = styled.div`
+    display: flex;
+    margin-bottom: 10px;
+`;
+
+const HeaderText = styled.div`
+    padding-top: 10px;
+    font-weight: bold;
+    flex: 0 0 300px;
+`;
+
+const AddStyle = styled.div`
+    font-weight: bold;
+    flex-grow: 1;
+    background-color: #ffffff;
+    padding: 10px;
+    cursor: pointer;
+    color: #006ce8;
+`;
+
+const StyledListItem = styled(ListItem)`
+    &:nth-child(even) {
+        background-color: #f3f3f3;
+    }
+    &:nth-child(odd) {
+        background-color: #ffffff;
+    }
+    border-style: solid;
+    border-width: 0.5px;
+    border-color: #d9d9d9;
+`;
+
+const AddStyleIcon = styled(Icon)`
+    margin-right: 10px;
+`;
+
+const showVisualizationForm = (layerId, styleId, isCreateNew) => {
+    Oskari.getSandbox().postRequestByName('ShowOwnStyleRequest', [layerId, styleId, false, isCreateNew]);
+};
+
+const UserStyles = ({ mutator, getMessage, layerId, styles }) => {
+    return (
+        <div>
+            <Header>
+                <HeaderText>{getMessage('styles')}</HeaderText>
+                <AddStyle onClick={() => showVisualizationForm(layerId, undefined, true)}>
+                    <AddStyleIcon type="plus"/>
+                    {getMessage('add-style')}
+                </AddStyle>
+            </Header>
+            { styles && styles.length > 0 &&
+            <List bordered={false} dataSource={styles} renderItem={style => {
+                return (
+                    <StyledListItem>
+                        <UserStyleRow styleName={style.name}
+                            editUserStyleHandler={() => showVisualizationForm(layerId, style.id, false)}
+                            removeUserStyleHandler={() => mutator.removeStyle(layerId, style.id)}/>
+                    </StyledListItem>
+                );
+            }}/>
+            }
+        </div>
+    );
+};
+
+UserStyles.propTypes = {
+    mutator: PropTypes.object.isRequired,
+    getMessage: PropTypes.func.isRequired,
+    layerId: PropTypes.number.isRequired,
+    styles: PropTypes.array.isRequired
+};
+
+const contextWrap = withMutator(withLocale(UserStyles));
+export { contextWrap as UserStyles };
