cc6f2d8f5121d59a43f032eb748be29c2e713735
jheijari <joonas.heijari@gmail.com>
2019-12-03 15:10:06 +0200

    Add comments

--- src/react/util/contexts/LocaleContext.jsx
+++ src/react/util/contexts/LocaleContext.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Message } from 'oskari-ui';
 
+/**
+ * The context takes an object with two properties as a parameter. { bundleKey, getMessage }
+ * getMessage is optional and can be used to provide a custom function taking the same params as Oskari.getMsg.
+ */
 export const LocaleContext = React.createContext();
 
+/**
+ * The context provides Message component and getMessage functions for it's children.
+ * Developers should use the Message component whenever they can and avoid using the getMessage function.
+ * The getMessage function may be used when an element can't use a ReactNode as a prop. (e.g. placeholder in TextInput)
+ */
 export function withLocale (Component) {
     const LocalizedComponent = (props, ref) => {
         return (
