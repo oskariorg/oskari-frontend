771edeaceda699489ef6c028be5ae9889fedf79f
jheijari <joonas.heijari@gmail.com>
2019-12-04 15:54:31 +0200

    Fix tests

--- bundles/framework/layerlist/view/LayerViewTabs/test.util.js
+++ bundles/framework/layerlist/view/LayerViewTabs/test.util.js
@@ -1,12 +1,15 @@
 import '../../../layerselector2/service/layerlist';
 import '../../../layerlist/resources/locale/en';
 import '../../../../mapping/mapmodule/service/map.state';
+import '../../service/LayerListToolingService';
 
 export const initServices = () => {
     const sandbox = Oskari.getSandbox();
     const layerlistService = Oskari.clazz.create('Oskari.mapframework.service.LayerlistService', sandbox);
+    const toolingService = Oskari.clazz.create('Oskari.mapframework.service.LayerListToolingService', sandbox);
     const mapStateService = Oskari.clazz.create('Oskari.mapframework.domain.Map', sandbox);
     sandbox.registerService(layerlistService);
+    sandbox.registerService(toolingService);
     sandbox.registerService(mapStateService);
 };
 
