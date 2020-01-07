ee6325e26e55d6745f9527ca6f7f9facd534f754
Sami Mäkinen <sami.makinen@nls.fi>
2020-01-07 17:34:36 +0200

    Switch id -> ref

--- bundles/mapping/shadow-plugin-3d/resources/icons/PlayPauseIcon.jsx
+++ bundles/mapping/shadow-plugin-3d/resources/icons/PlayPauseIcon.jsx
@@ -6,6 +6,7 @@ const PLAYPATH = 'M11,8 L18,11.74 18,20.28 11,24 11,8 M18,11.74 L26,16 26,16 18,
 const PAUSEPATH = 'M9,8 L14,8 14,24 9,24 9,8 M19,8 L24,8 24,24 19,24 19,8';
 
 export const PlayPauseIcon = ({ initial }) => {
+    const moi = React.createRef();
     const [status, setStatus] = React.useState(initial);
     const [d, setD] = React.useState('M11,8 L26,16 11,24 11,8');
     const [from, setFrom] = React.useState('');
@@ -15,7 +16,10 @@ export const PlayPauseIcon = ({ initial }) => {
         setD(d);
         setFrom(from);
         setTo(to);
-        document.getElementById('play-svg').getElementsByTagName('animate')[0].beginElement();
+        if (moi.current) {
+            moi.current.beginElement();
+        }
+        //document.getElementById('play-svg').getElementsByTagName('animate')[0]
     };
     const changeState = () => {
         const d = status ? PLAYPATH : PAUSEPATH;
@@ -25,9 +29,9 @@ export const PlayPauseIcon = ({ initial }) => {
         setStatus(!status);
     };
     return (
-        <svg id='play-svg' xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 40 40' onClick={changeState}>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 40 40' onClick={changeState}>
             <path d={d}>
-                <animate from={from} to={to} attributeName='d' fill='freeze' dur={dur} calcMode='spline' keySplines='0.19 1 0.22 1'/>
+                <animate ref={moi} from={from} to={to} attributeName='d' fill='freeze' dur={dur} calcMode='spline' keySplines='0.19 1 0.22 1'/>
             </path>
         </svg>
     );
