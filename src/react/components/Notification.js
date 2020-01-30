ab2adc40d16743f86e27699b6291e6a93c705fcf
jheijari <joonas.heijari@gmail.com>
2020-01-30 13:43:33 +0200

    Remove previous notification impl

--- src/react/components/Notification.js
+++
@@ -1,9 +0,0 @@
-import { notification } from 'antd';
-import 'antd/es/notification/style/index.js';
-/**
- * Function to open antd notification to desired location.
- * Should be called from useEffect in React components.
- */
-export const openNotification = (type, options) => {
-    notification[type](options);
-};
