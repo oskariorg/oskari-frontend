53f1529df3549755f5643fb4a0f53fa17909ed93
Sami Mäkinen <sami.makinen@nls.fi>
2020-03-30 16:20:52 +0300

    Release 1.55.0

--- src/oskari.es6.js
+++ src/oskari.es6.js
@@ -45,7 +45,7 @@ function appendQueryToURL(url, query) {
 }
 
 const Oskari = {
-    VERSION: '1.55.0-dev',
+    VERSION: '1.55.0',
     setMarkers (markers) {
         _markers = markers || [];
     },
