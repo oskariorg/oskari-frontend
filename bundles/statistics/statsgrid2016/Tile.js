83f6fa7aea64a6f91791b232521405dc2af87bc3
okauppinen <olli.kauppinen@nls.fi>
2020-04-02 14:06:19 +0300

    don't unselect extensions on hide

--- bundles/statistics/statsgrid2016/Tile.js
+++ bundles/statistics/statsgrid2016/Tile.js
@@ -131,13 +131,9 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.Tile', function (instance, serv
      * Hides all the extra options (used when tile is "deactivated")
      */
     hideExtensions: function () {
-        var me = this;
-        var extraOptions = me.getExtensions();
+        var extraOptions = this.getExtensions();
         Object.keys(extraOptions).forEach(function (key) {
-            // hide the tile "extra selection"
-            var extension = extraOptions[key];
-            extension.removeClass('material-selected');
-            extension.addClass('hidden');
+            extraOptions[key].addClass('hidden');
         });
         this.flyoutManager.tileClosed();
     },
@@ -146,9 +142,7 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.Tile', function (instance, serv
      * @return {[type]} [description]
      */
     showExtensions: function () {
-        var me = this;
-        var extraOptions = me.getExtensions();
-        this.flyoutManager.init();
+        var extraOptions = this.getExtensions();
         Object.keys(extraOptions).forEach(function (key) {
             extraOptions[key].removeClass('hidden');
         });
