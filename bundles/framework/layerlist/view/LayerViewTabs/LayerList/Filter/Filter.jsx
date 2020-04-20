a4c61c9704e477578d8558c26a67b3c7b5be3aa3
okauppinen <olli.kauppinen@nls.fi>
2020-04-20 09:58:55 +0300

    add tooltip for options

--- bundles/framework/layerlist/view/LayerViewTabs/LayerList/Filter/Filter.jsx
+++ bundles/framework/layerlist/view/LayerViewTabs/LayerList/Filter/Filter.jsx
@@ -5,26 +5,20 @@ import { Controller, LocaleConsumer } from 'oskari-ui/util';
 import { Labelled } from '../Labelled';
 
 const Filter = ({ filters, activeFilterId, controller }) => {
-    let tooltip;
-    // Show tooltip of the active filter.
-    const active = filters.find(cur => cur.id === activeFilterId);
-    if (active) {
-        tooltip = active.tooltip;
-    }
     return (
         <Labelled messageKey='filter.title'>
-            <Tooltip title={tooltip}>
-                <Select
-                    onChange={controller.setActiveFilterId}
-                    value={activeFilterId}
-                >
-                    {
-                        filters.map(({ id, text }) => (
-                            <Option key={id} value={id} >{text}</Option>
-                        ))
-                    }
-                </Select>
-            </Tooltip>
+            <Select
+                onChange={controller.setActiveFilterId}
+                value={activeFilterId}
+            >
+                {
+                    filters.map(({ id, text, tooltip }) => (
+                        <Option key={id} value={id} >
+                            <Tooltip title={tooltip}>{text}</Tooltip>
+                        </Option>
+                    ))
+                }
+            </Select>
         </Labelled>
     );
 };
