a29b002f7b7f7ad0c78e4a846b0aecc45e118f95
Sami Mäkinen <sami.makinen@nls.fi>
2020-04-29 14:39:18 +0300

    Consistency for AntD imports

--- src/react/components/Confirm.jsx
+++ src/react/components/Confirm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Popconfirm from 'antd/lib/popconfirm';
+import { Popconfirm } from 'antd';
 import 'antd/es/popconfirm/style/index.js';
 
 export const Confirm = ({ children, ...other }) => (
