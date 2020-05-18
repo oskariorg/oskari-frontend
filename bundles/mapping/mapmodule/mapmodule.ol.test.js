5f04bfafec67edd3019319dade86c26480f04805
Sami Mäkinen <sami.makinen@nls.fi>
2020-05-18 11:44:51 +0300

    Rename test to point to actual impl

--- bundles/mapping/mapmodule/mapmodule.ol.test.js
+++ bundles/mapping/mapmodule/mapmodule.ol.test.js
@@ -1,4 +1,3 @@
-
 import '../../../src/global';
 import proj4 from '../../../libraries/Proj4js/proj4js-2.2.1/proj4-src.js';
 import './AbstractMapModule';
@@ -6,11 +5,11 @@ import './mapmodule.ol';
 import './resources/locale/en.js';
 import './service/map.state';
 import jQuery from 'jquery';
-// import * as olProjProj4 from 'ol/proj/proj4';
 
 const Oskari = window.Oskari;
 global.jQuery = jQuery;
 
+// defaults from mapfull
 const projections = {
     'EPSG:3067': '+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs',
     'EPSG:4326': '+title=WGS 84 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
@@ -23,18 +22,23 @@ window.proj4 = proj4;
 // const mapModule = Oskari.clazz.create('Oskari.mapping.mapmodule.AbstractMapModule', 'Test');
 const mapModule = Oskari.clazz.create('Oskari.mapframework.ui.module.common.MapModule', 'Test');
 
-describe('AbstractMapModule ', () => {
+describe('MapModule', () => {
     const res = mapModule.getResolutionArray();
 
-    test('has 13 resolutions by default.', () => {
+    test('has 13 resolutions by default', () => {
         expect(res[0]).toEqual(2000);
         expect(res[res.length - 1]).toEqual(0.25);
         expect(res.length).toEqual(13);
     });
-    test('init.', () => {
+    test('uses EPSG:3067 by default', () => {
+        // for legacy reasons. This should be changed to 3857
         expect(mapModule.getProjection()).toEqual('EPSG:3067');
-        expect(mapModule.getScaleArray().length).toEqual(0);
-        mapModule.init(Oskari.getSandbox());
-        expect(mapModule.getScaleArray().length).toEqual(13);
+    });
+    describe('init', () => {
+        test('calculates scales based on resolutions', () => {
+            expect(mapModule.getScaleArray().length).toEqual(0);
+            mapModule.init(Oskari.getSandbox());
+            expect(mapModule.getScaleArray().length).toEqual(13);
+        });
     });
 });
