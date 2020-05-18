a73ac257e9f1947e6c0ad8e3d21799bf5d6afed9
Sami Mäkinen <sami.makinen@nls.fi>
2020-05-18 15:49:09 +0300

    Add scale limit support for arcgis layers

--- bundles/mapping/maparcgis/domain/ArcGisLayer.js
+++ bundles/mapping/maparcgis/domain/ArcGisLayer.js
@@ -14,7 +14,11 @@ Oskari.clazz.define('Oskari.arcgis.bundle.maparcgis.domain.ArcGisLayer',
         /* Layer Type */
         this._layerType = 'arcgis';
     }, {
-
+        getQueryable: function () {
+            // not sure why but previously the plugin called layer.setQueryable(true); when layer was added to the map
+            // and this makes it more explicit
+            return true;
+        }
     }, {
         'extend': ['Oskari.mapframework.domain.AbstractLayer']
     });
