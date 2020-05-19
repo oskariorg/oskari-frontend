542ccb9f894cf3d84c8b0ae6e4a84ce896f992c2
Sami Mäkinen <sami.makinen@nls.fi>
2020-05-19 16:50:02 +0300

    Tests for getLayerIds()

--- bundles/mapping/mapmodule/plugin/vectorlayer/VectorLayerPlugin.ol.test.js
+++ bundles/mapping/mapmodule/plugin/vectorlayer/VectorLayerPlugin.ol.test.js
@@ -1,5 +1,5 @@
 import '../../../../../src/global';
-// import proj4 from '../../../libraries/Proj4js/proj4js-2.2.1/proj4-src.js';
+import '../AbstractMapModulePlugin';
 import './VectorLayerPlugin.ol';
 import jQuery from 'jquery';
 import _ from 'lodash';
@@ -11,9 +11,21 @@ global._ = _;
 const plugin = Oskari.clazz.create('Oskari.mapframework.mapmodule.VectorLayerPlugin');
 
 describe('VectorLayerPlugin', () => {
+    plugin._olLayers['test_1'] = {};
+    plugin._olLayers['test_2'] = {};
     test('getLayerIds without param returns 2 faked layers', () => {
-        plugin._olLayers['test_1'] = {};
-        plugin._olLayers['test_2'] = {};
         expect(plugin.getLayerIds().length).toEqual(2);
+    });
+    test('getLayerIds with valid param returns 1', () => {
+        expect(plugin.getLayerIds({ layer: [12] }).length).toEqual(1);
+    });
+    test('getLayerIds with valid param returns 3', () => {
+        expect(plugin.getLayerIds({ layer: [1, 2, 3] }).length).toEqual(3);
+    });
+    test('getLayerIds with invalid param returns 0', () => {
+        expect(plugin.getLayerIds({ layer: 12 }).length).toEqual(0);
+    });
+    test('getLayerIds with invalid param 2 returns 0', () => {
+        expect(plugin.getLayerIds({ testing: true }).length).toEqual(0);
     });
 });
