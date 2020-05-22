fe142b21567e30e2b46c440b1e7825f9ee21453f
Sami Mäkinen <sami.makinen@nls.fi>
2020-05-22 17:36:48 +0300

    Testing for VectorLayerPlugin

--- bundles/mapping/mapmodule/plugin/vectorlayer/VectorLayerPlugin.ol.test.js
+++ bundles/mapping/mapmodule/plugin/vectorlayer/VectorLayerPlugin.ol.test.js
@@ -1,31 +1,112 @@
+// Oskari global
 import '../../../../../src/global';
-import '../AbstractMapModulePlugin';
+// VectorLayerPlugin stuff
 import './VectorLayerPlugin.ol';
+import './vectorlayer';
+
+// mapmodule 
+import '../../mapmodule.ol';
+import '../../resources/locale/en.js';
+
 import jQuery from 'jquery';
 import _ from 'lodash';
+import olSourceVector from 'ol/source/Vector';
+import olLayerVector from 'ol/layer/Vector';
 
-const Oskari = window.Oskari;
 global.jQuery = jQuery;
 global._ = _;
 
+// for mapmodule
+// defaults from mapfull
+
+const projections = {
+    'EPSG:3067': '+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs',
+    'EPSG:4326': '+title=WGS 84 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+};
+Object.keys(projections).forEach(code => {
+    // mapmodule introduces proj4 global
+    window.proj4.defs(code, projections[code]);
+});
+//  -/for mapmodule
+
+const sandbox = Oskari.getSandbox();
 const plugin = Oskari.clazz.create('Oskari.mapframework.mapmodule.VectorLayerPlugin');
+const layerService = Oskari.clazz.create('Oskari.mapframework.service.MapLayerService', sandbox);
+sandbox.registerService(layerService);
+
+const mapModule = Oskari.clazz.create('Oskari.mapframework.ui.module.common.MapModule', 'Test');
+sandbox.register(mapModule);
+mapModule.requestHandlers = {};
+mapModule.registerPlugin(plugin);
+mapModule.start(sandbox);
+/// plugin.startPlugin(sandbox);
+plugin._sandbox = sandbox;
+// sandbox.register();
+
+const createFakeLayer = () => {
+    return new olLayerVector({
+        source: new olSourceVector()
+    });
+};
 
 describe('VectorLayerPlugin', () => {
-    plugin._olLayers['test_1'] = {};
-    plugin._olLayers['test_2'] = {};
-    test('getLayerIds without param returns 2 faked layers', () => {
-        expect(plugin.getLayerIds().length).toEqual(2);
+    plugin._olLayers['test_1'] = createFakeLayer();
+    plugin._olLayers['test_2'] = createFakeLayer();
+
+    describe('getLayerIds', () => {
+        test('without param returns 2 faked layers', () => {
+            expect(plugin.getLayerIds().length).toEqual(2);
+        });
+        test('with valid param returns 1', () => {
+            expect(plugin.getLayerIds({ layer: [12] }).length).toEqual(1);
+        });
+        test('with valid param returns 3', () => {
+            expect(plugin.getLayerIds({ layer: [1, 2, 3] }).length).toEqual(3);
+        });
+        test('with invalid param returns 0', () => {
+            expect(plugin.getLayerIds({ layer: 12 }).length).toEqual(0);
+        });
+        test('with invalid param 2 returns 0', () => {
+            expect(plugin.getLayerIds({ testing: true }).length).toEqual(0);
+        });
     });
-    test('getLayerIds with valid param returns 1', () => {
-        expect(plugin.getLayerIds({ layer: [12] }).length).toEqual(1);
-    });
-    test('getLayerIds with valid param returns 3', () => {
-        expect(plugin.getLayerIds({ layer: [1, 2, 3] }).length).toEqual(3);
-    });
-    test('getLayerIds with invalid param returns 0', () => {
-        expect(plugin.getLayerIds({ layer: 12 }).length).toEqual(0);
-    });
-    test('getLayerIds with invalid param 2 returns 0', () => {
-        expect(plugin.getLayerIds({ testing: true }).length).toEqual(0);
+
+    describe('addFeaturesToMap', () => {
+        test('add geojson', () => {
+            const geojson = generateGeoJSON(1, 2);
+            plugin.addFeaturesToMap(geojson);
+            expect(plugin.getLayerIds().length).toEqual(3);
+        });
     });
 });
+
+const generateGeoJSON = (x, y) => {
+    return {
+        'type': 'FeatureCollection',
+        'crs': {
+            'type': 'name'
+        },
+        'features': [
+            {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Polygon',
+                    'coordinates': [[[x, y], [x + 1, y + 1], [x + 2, y + 5]]]
+                },
+                'properties': {
+                    'test_property': 1
+                }
+            },
+            {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [x + 4, y + 3]
+                },
+                'properties': {
+                    'test_property': 2
+                }
+            }
+        ]
+    };
+};
