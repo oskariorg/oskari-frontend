5eae612db68141fd88defb42bd8ead456d3292a5
Sami Mäkinen <sami.makinen@nls.fi>
2020-06-03 14:58:01 +0300

    Add tests for html formatter

--- bundles/mapping/mapmodule/plugin/getinfo/GetFeatureInfoFormatter.test.js
+++ bundles/mapping/mapmodule/plugin/getinfo/GetFeatureInfoFormatter.test.js
@@ -1,13 +1,36 @@
-import { afterAll } from '@jest/globals';
 import './GetInfoPlugin';
 import './GetFeatureInfoFormatter';
 
 const plugin = Oskari.clazz.create('Oskari.mapframework.mapmodule.GetInfoPlugin');
+const removeWhitespace = (content) => content.replace(/\s/g, '');
 
 describe('GetInfoPlugin', () => {
     describe('formatters', () => {
-        test('has html function', () => {
-            expect(typeof plugin.formatters.html).toEqual('function');
+        describe('html', () => {
+            const content = `
+                <div>
+                    <script>alert('Bazinga!')</script>
+                </div>
+            `;
+            test('is function', () => {
+                expect(typeof plugin.formatters.html).toEqual('function');
+            });
+            test('returns jQuery object', () => {
+                const result = plugin.formatters.html(content);
+                expect(result instanceof jQuery).toEqual(true);
+            });
+            test('wraps content to additional div', () => {
+                const result = plugin.formatters.html(`My data`);
+                expect(result.outerHTML()).toEqual('<div>My data</div>');
+
+                const result2 = plugin.formatters.html(`<div></div>`);
+                expect(removeWhitespace(result2.outerHTML())).toEqual('<div><div></div></div>');
+            });
+            test('removes script tags', () => {
+                const result = plugin.formatters.html(content);
+                const scriptTags = result.find('script');
+                expect(scriptTags.length).toEqual(0);
+            });
         });
     });
 });
