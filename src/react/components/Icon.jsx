f1c50fda75b811ac64b4b628e3eb2a7027b210be
Sami Mäkinen <zakarfin@gmail.com>
2020-06-08 00:09:19 +0300

    Use icon from new package, note API change as it doesnt provide any icons with type param

--- src/react/components/Icon.jsx
+++ src/react/components/Icon.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Icon as AntIcon } from 'antd';
+import AntIcon from '@ant-design/icons';
 import 'antd/es/icon/style/index.js';
 
+// Note! Ant icon doesn't have any graphics included to optimize bundle size
+// This is purely to provide custom SVG icons to be implemented with
+// antd icon API
 export const Icon = props => <AntIcon {...props} />;
