4fb5afca43930bd0a36b637769262c2199a3edd5
okauppinen <olli.kauppinen@nls.fi>
2020-10-27 12:50:50 +0200

    add ru and fr localization for layerlist

---
+++ bundles/framework/layerlist/resources/locale/fr.js
@@ -0,0 +1,69 @@
+Oskari.registerLocalization(
+    {
+        'lang': 'fr',
+        'key': 'LayerList',
+        'value': {
+            'title': 'Couches cartographiques',
+            'desc': '',
+            'errors': {
+                'loadFailed': 'Une erreur s’est produite dans le chargement des couches cartographiques. Rechargez la page web dans votre navigateur puis sélectionnez à nouveau les couches cartographiques.',
+                'noResults': 'Pas de résultats. Vérifiez vos critères de recherches.'
+            },
+            'tabs': {
+                'layerList': 'Rechercher parmi les couches cartographiques',
+                'selectedLayers': 'Couches sélectionnées'
+            },
+            'filter': {
+                'title': 'Afficher',
+                'all': {
+                    'title': 'All layers',
+                    'tooltip': ''
+                },
+                'search': {
+                    'placeholder': 'Search map layers by name, data producer or keyword',
+                    'tooltip': 'Search map layers by name, data producer or keyword. You can type a whole term or a part of it.'
+                },
+                'newest': {
+                    'title': 'Plus récent',
+                    'tooltip': 'Montrer les ## couches cartographiques les plus récentes'
+                },
+                'raster': {
+                    'title': 'Raster layers',
+                    'tooltip': 'Show only raster layers'
+                }
+            },
+            'grouping': {
+                'title': 'Grouping',
+                'inspire': 'Par thème',
+                'organization': 'Par fournisseur de données',
+                'noGroup' : 'No group'
+            },
+            'layer': {
+                'tooltip': {
+                    'timeseries': 'Couche chronologique'
+                },
+                'styles': {
+                    'title': 'Style'
+                },
+                'moveToScale': "Veuillez choisir un niveau d'échelle adapté",
+                'moveToContentArea': 'Veuillez vous déplacer sur un emplacement adapté',
+                'publishable': 'Publiable',
+                'hidden': 'La couche cartographique est temporairement masquée'
+            },
+            'backendStatus': {
+                'OK': 'La couche cartographique est actuellement disponible.',
+                'DOWN': 'La couche cartographique n’est pas disponible actuellement.',
+                'ERROR': 'La couche cartographique n’est pas disponible actuellement.',
+                'MAINTENANCE': 'Il est possible que la couche cartographique soit momentanément indisponible les jours prochains.',
+                'UNKNOWN': '',
+                'UNSTABLE': ''
+            },
+            'guidedTour': {
+                'title': 'Couches cartographiques',
+                'message': 'In the Map Layers menu you can find all the map layers in the map service.<br/><br/>Sort map layers by theme or data provider.<br/><br/>Search map layers by a map layer name, a data provider name or a keyword. Newest map layers, vector layers and publishable layers you can find in pre-defined lists.<br/><br/>You can check currently open map layers in the Selected Layers tab. You can rearrange the layers by dragging, adjust their opacity and change the styling of some layers.',
+                'openLink': 'Afficher les couches cartographiques',
+                'closeLink': 'Masquer les couches cartographiques',
+                'tileText': 'Couches cartographiques'
+            }
+        }
+    });
