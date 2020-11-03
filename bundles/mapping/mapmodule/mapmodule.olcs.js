a1546ee5b17434c0da4c91b787f4ac2d4dbed1f3
Sami Mäkinen <sami.makinen@nls.fi>
2020-11-03 14:42:00 +0200

    Move polyfill from index.js

--- bundles/mapping/mapmodule/mapmodule.olcs.js
+++ bundles/mapping/mapmodule/mapmodule.olcs.js
@@ -15,6 +15,26 @@ import './event/TimeChangedEvent';
 
 // OL-cesium expects to find this global
 window.Cesium = Cesium;
+
+// Polyfills DOM4 MouseEvent for olcs if needed
+(function (window) {
+    try {
+        // eslint-disable-next-line no-new, no-use-before-define
+        new MouseEvent('test');
+        return false; // No need to polyfill
+    } catch (e) {
+        // Need to polyfill - fall through
+    }
+    var MouseEvent = function (eventType, params) {
+        params = params || { bubbles: false, cancelable: false };
+        var mouseEvent = document.createEvent('MouseEvent');
+        mouseEvent.initMouseEvent(eventType, params.bubbles, params.cancelable, window, 0, params.screenX || 0, params.screenY || 0, params.clientX || 0, params.clientY || 0, false, false, false, false, 0, null);
+        return mouseEvent;
+    };
+    MouseEvent.prototype = Event.prototype;
+    window.MouseEvent = MouseEvent;
+})(window);
+
 const TILESET_DEFAULT_COLOR = '#ffd2a6';
 
 class MapModuleOlCesium extends MapModuleOl {
