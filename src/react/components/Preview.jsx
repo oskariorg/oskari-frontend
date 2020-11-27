242e6d1158a8004e6ffc2017277f0ecc8faeeaeb
kasparia <superkasperi@gmail.com>
2020-11-27 16:48:32 +0200

    Clean up preview

--- src/react/components/Preview.jsx
+++ src/react/components/Preview.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const previewBaseSvg = (path) => {
-    return (
-        <svg viewBox="0 0 80 80" width="80" height="80" xmlns="http://www.w3.org/2000/svg">
-            <svg viewBox="0 0 80 80" width="120" height="120" x="0" y="0" id="marker">
-                { path }
-            </svg>
-        </svg>
-    );
-};
-
+//Size for preview
 const previewSize = '80px';
 
+// Style settings for preview rectangle
 const previewStyling = {
     border: '1px solid #d9d9d9',
     height: previewSize,
     width: previewSize
 }
-
 
 /**
  * @class Preview
@@ -26,30 +17,46 @@ const previewStyling = {
  * @memberof module:oskari-ui
  * @see {@link module:oskari-ui/util.LocaleProvider|LocaleProvider}
  * @param {Object} props - { }
- * @param {Function} previewIcon - callback for creating icon
+ * @param {Component|Callback} previewIcon - callback for creating icon
  *
  * @example <caption>Basic usage</caption>
- * <Preview props={{ ...exampleProps }}/>
+ * <Preview previewIcon={ svgIconCallback }}/>
  */
 
 export class Preview extends React.Component {
     constructor (props) {
         super(props);
-
-        this.ref = React.createRef();
     }
 
-    _combineSvg (path) {
-        return(
-            previewBaseSvg(path)
+    /**
+     * @method _addBaseSvg
+     * @param {Component} path - svg image or callback for creating preview icon
+     *
+     * @returns {Component} - combined svg icon with preview base wrapping around provided icon
+     *
+     */
+    _addBaseSvg (path) {
+        return (
+            <svg viewBox={ '0 0 ' + previewSize + ' ' + previewSize } width={ previewSize } height={ previewSize } xmlns="http://www.w3.org/2000/svg">
+                <svg viewBox="0 0 80 80" width="120" height="120" x="0" y="0" id="marker">
+                    { path }
+                </svg>
+            </svg>
         );
     }
 
     render () {
         return (
             <div style={ previewStyling }>
-                { this._combineSvg(this.props.previewIcon) }
+                { this._addBaseSvg(this.props.previewIcon) }
             </div>
         );
     }
+};
+
+Preview.propTypes = {
+    previewIcon: PropTypes.oneOfType([
+        PropTypes.func,
+        PropTypes.element
+    ]).isRequired
 };
