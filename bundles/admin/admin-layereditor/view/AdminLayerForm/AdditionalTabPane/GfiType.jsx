bffd64e91a831bccae89bed1bb89865f9abd1c96
okauppinen <olli.kauppinen@nls.fi>
2021-01-25 11:48:52 +0200

    add dropdown for selecting gfi type

--- bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/GfiType.jsx
+++ bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/GfiType.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Message } from 'oskari-ui';
-import { InlineFlex } from '../InlineFlex';
+import { Message, Select, Option } from 'oskari-ui';
+import { Controller } from 'oskari-ui/util';
 import { InfoTooltip } from '../InfoTooltip';
+import { StyledFormField } from '../styled';
 
-export const GfiType = ({ layer }) => {
-    if (!layer.gfiType) {
+export const GfiType = ({ layer, controller }) => {
+    const { capabilities, gfiType } = layer;
+    const { formats = {} } = capabilities;
+    const options = formats.available || [];
+    const value = gfiType || formats.value || '';
+    if (options.length === 0) {
         return null;
     }
     return (
-        <InlineFlex>
-            <div>
-                <Message messageKey='fields.gfiType'/>
-                <InfoTooltip messageKeys='gfiTypeDesc'/>
-            </div>
-            <div>{ layer.gfiType }</div>
-        </InlineFlex>
+        <Fragment>
+            <Message messageKey='fields.gfiType'/>
+            <InfoTooltip messageKeys='gfiTypeDesc'/>
+            <StyledFormField>
+                <Select
+                    defaultValue={value}
+                    onChange={value => controller.setGfiType(value)}
+                >
+                    { options.map(option => (
+                        <Option key={option} value={option}>
+                            {option}
+                        </Option>
+                    )) }
+                </Select>
+            </StyledFormField>
+        </Fragment>
     );
 };
 GfiType.propTypes = {
-    layer: PropTypes.object.isRequired
+    layer: PropTypes.object.isRequired,
+    controller: PropTypes.instanceOf(Controller).isRequired
 };
