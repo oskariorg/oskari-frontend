1a107f1e4ae64302fe7bb8bd319ded5fa666e16e
Mingfeng Yu <mfdevworld@gmail.com>
2021-04-12 10:52:37 +0300

    Update translations for swipe alert messages

--- bundles/mapping/layerswipe/resources/locale/sv.js
+++ bundles/mapping/layerswipe/resources/locale/sv.js
@@ -2,6 +2,11 @@ Oskari.registerLocalization({
     lang: 'sv',
     key: 'LayerSwipe',
     value: {
-        toolLayerSwipe: 'Jämför kartlagret högst uppe med nedanstående kartlager'
+        toolLayerSwipe: 'Jämför kartlagret högst uppe med nedanstående kartlager',
+        alert: {
+            ok: 'OK',
+            swipeNoRasterTitle: 'Inget kartlager i rasterformat har valts för jämföring',
+            swipeNoRasterMessage: 'Lägg till raster kartlager i kartvyn.'
+        }
     }
 });
