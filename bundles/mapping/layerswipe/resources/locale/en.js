5c30f876572745ba8db32438e723cc1b9e235574
Mingfeng Yu <mfdevworld@gmail.com>
2021-04-13 11:36:02 +0300

    Update translations for swipe layer not visible alert message

--- bundles/mapping/layerswipe/resources/locale/en.js
+++ bundles/mapping/layerswipe/resources/locale/en.js
@@ -6,7 +6,9 @@ Oskari.registerLocalization({
         alert: {
             ok: 'OK',
             swipeNoRasterTitle: 'There is no raster layer selected to use with the map swipe tool',
-            swipeNoRasterMessage: 'Set a raster map layer visible.'
+            swipeNoRasterMessage: 'Set a raster map layer visible.',
+            swipeLayerNotVisibleTitle: 'Topmost layer is not visible in the current view',
+            swipeLayerNotVisibleMessage: 'Swipe feature is not working properly. Move the map view to a location with data visible in the top most layer'
         }
     }
 });
--- bundles/mapping/layerswipe/resources/locale/fi.js
+++ bundles/mapping/layerswipe/resources/locale/fi.js
@@ -6,7 +6,9 @@ Oskari.registerLocalization({
         alert: {
             ok: 'OK',
             swipeNoRasterTitle: 'Yhtään rasterikarttatasoa ei ole valittuna käytettäväksi vertailutyökalun kanssa',
-            swipeNoRasterMessage: 'Aseta karttatasovalikosta rasteritaso näkyväksi'
+            swipeNoRasterMessage: 'Aseta karttatasovalikosta rasteritaso näkyväksi',
+            swipeLayerNotVisibleTitle: 'Ylin karttataso ei näy tässä näkymässä',
+            swipeLayerNotVisibleMessage: 'Karttatasojen vertailu ei toimi. Siirry näkymässä sijaintiin, jossa ylin karttataso on näkyvissä'
         }
     }
 });
