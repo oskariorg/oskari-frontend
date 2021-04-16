e757de3eae635519654492220e191e57284d79b7
Mingfeng Yu <mfdevworld@gmail.com>
2021-04-16 13:41:01 +0300

    Update the alert message when the swipe layer is not visible on map

--- bundles/mapping/layerswipe/resources/locale/en.js
+++ bundles/mapping/layerswipe/resources/locale/en.js
@@ -7,8 +7,8 @@ Oskari.registerLocalization({
             ok: 'OK',
             swipeNoRasterTitle: 'There is no raster layer selected to use with the map swipe tool',
             swipeNoRasterMessage: 'Set a raster map layer visible.',
-            swipeLayerNotVisibleTitle: 'Topmost layer is not visible in the current view',
-            swipeLayerNotVisibleMessage: 'Swipe feature is not working properly. Move the map view to a location with data visible in the top most layer'
+            swipeLayerNotVisibleTitle: 'Swipe feature is not working',
+            swipeLayerNotVisibleMessage: 'The topmost layer is not visible in the current view. Make it visible in Selected layers or move to a location where it can be seen.'
         }
     }
 });
