1a3575c9d9ea504a7faea5eb61810553bc1e7f7c
Sami Mäkinen <sami.makinen@nls.fi>
2021-04-19 16:36:24 +0300

    Prevent 'Function components cannot be given refs'

--- src/react/components/Tooltip.jsx
+++ src/react/components/Tooltip.jsx
@@ -2,4 +2,8 @@ import React from 'react';
 import { Tooltip as AntTooltip } from 'antd';
 import 'antd/es/tooltip/style/index.js';
 
-export const Tooltip = (props) => <AntTooltip {...props} />;
+export const Tooltip = ({children, ...restOfProps}) => {
+    return (<AntTooltip {...restOfProps}>
+        <span>{children}</span>
+    </AntTooltip>);
+};
