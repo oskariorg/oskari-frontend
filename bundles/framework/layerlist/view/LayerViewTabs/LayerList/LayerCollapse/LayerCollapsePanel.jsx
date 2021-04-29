ee374a23304cde0035c42c46c4214ecb5bfe48e5
Sami Mäkinen <sami.makinen@nls.fi>
2021-04-29 09:57:37 +0300

    Separate styling from model

--- bundles/framework/layerlist/view/LayerViewTabs/LayerList/LayerCollapse/LayerCollapsePanel.jsx
+++ bundles/framework/layerlist/view/LayerViewTabs/LayerList/LayerCollapse/LayerCollapsePanel.jsx
@@ -49,14 +49,15 @@ const PanelToolContainer = React.memo(({group, layerCount, allLayersOnMap, contr
 
 /* ----- Layer list ------ */
 const StyledListItem = styled(ListItem)`
+    background-color: ${props => props.even ? '#ffffff' : '#f3f3f3'};
     padding: 0 !important;
     display: block !important;
 `;
 
-const renderLayer = ({ model, even, selected, controller }) => {
-    const itemProps = { model, even, selected, controller };
+const renderLayer = ({ model, selected, controller }, index) => {
+    const itemProps = { model, selected, controller };
     return (
-        <StyledListItem>
+        <StyledListItem even={index % 2 === 0}>
             <Layer key={model.getId()}  {...itemProps} />
         </StyledListItem>
     );
@@ -122,11 +123,10 @@ const StyledCollapsePanel = styled(CollapsePanel)`
 `;
 
 const getLayerRowModels = (layers = [], selectedLayerIds = [], controller) => {
-    return layers.map((oskariLayer, index) => {
+    return layers.map(oskariLayer => {
         return {
             id: oskariLayer.getId(),
             model: oskariLayer,
-            even: index % 2 === 0,
             selected: selectedLayerIds.includes(oskariLayer.getId()),
             controller
         };
