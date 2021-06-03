5d36d0de01e31b7c97ca91bfe75c422e0918d922
Sami Mäkinen <sami.makinen@nls.fi>
2021-06-03 13:46:20 +0300

    Filter out empty groups later for non-admins to make code easier to follow

--- bundles/framework/layerlist/view/LayerViewTabs/LayerList/LayerCollapse/util.js
+++ bundles/framework/layerlist/view/LayerViewTabs/LayerList/LayerCollapse/util.js
@@ -32,22 +32,14 @@ const group = {
 };
 createGroupModel(group, ...)
 */
-const createGroupModel = (group, method, allLayers, tools, admin) => {
-    const groupLayers = group.layers || [];
-    // TODO: check if subgroups have layers?
-    if (group.groups.length === 0 && groupLayers.length === 0 && !admin) {
-        // non-admin users get only groups with layers
-        return;
-    }
-    // check that group has layers
-    const name = group.name;
-    // const name = group.getName();
+const createGroupModel = (group, method, allLayers, tools) => {
     const newGroup = Oskari.clazz.create(
         'Oskari.mapframework.bundle.layerselector2.model.LayerGroup',
-        group.id, method, name
+        group.id, method, group.name
     );
     newGroup.setTools(tools);
     // attach layers to group
+    const groupLayers = group.layers || [];
     const groupLayerIds = groupLayers.map(l => l.id);
     const layerModels = allLayers.filter(layer => {
         if (typeof layer.getId !== 'function') {
@@ -60,17 +52,12 @@ const createGroupModel = (group, method, allLayers, tools, admin) => {
 
     // group has subgroups
     if (!group.groups.length) {
-        if (!layerModels.length && !admin) {
-            // no layers AND no subgroups -> remove group from list
-            //  for non-admins, we want to retain empty groups for admins
-            return;
-        }
         // has layers but no subgroups
         return newGroup;
     }
     const mappedSubgroups = group.groups
         // recursion for subgroups
-        .map(subgroup => createGroupModel(subgroup, method, allLayers, tools, admin))
+        .map(subgroup => createGroupModel(subgroup, method, allLayers, tools))
         // remove any subgroups that mapped to null:
         //  (groups without layers for non-admins etc)
         .filter(g => typeof g !== 'undefined');
@@ -127,11 +114,11 @@ export const groupLayers = (layers, method, tools, allGroups = [], noGroupTitle,
                 layers: [{ id: layer.getId() }],
                 groups: []
             };
-            groupForOrphans = createGroupModel(newGroup, method, layers, tools, isUserAdmin);
+            groupForOrphans = createGroupModel(newGroup, method, layers, tools);
         });
     // recursively map groups and layers together
     const groupList = allGroups
-        .map(rootGroup => createGroupModel(rootGroup, method, layers, tools, isUserAdmin))
+        .map(rootGroup => createGroupModel(rootGroup, method, layers, tools))
         .filter(group => typeof group !== 'undefined');
     const emptyGroupsShouldBeListed = isUserAdmin && !isPresetFiltered;
     const sortedGroups = sortGroupsAlphabetically(emptyGroupsShouldBeListed ? groupList : filterOutEmptyGroups(groupList));
