d5ff9d03c85d6b64aa8fcae4b27096dadd5d20eb
kasparia <superkasperi@gmail.com>
2021-06-24 14:03:28 +0300

    Remove optional chaining

--- src/react/components/StyleEditor.jsx
+++ src/react/components/StyleEditor.jsx
@@ -61,11 +61,14 @@ export const StyleEditor = (props) => {
 
     const styleExceptionHandler = (style) => {
         // if fill pattern is set to null, set color as empty
-        if (style.fill?.area?.pattern === 'null') {
-            style.fill.color = '';
-        } else if (style.fill?.area?.pattern !== 'null' && style.fill?.color === '') {
-            style.fill.color = OSKARI_BLANK_STYLE.fill.color;
+        if (typeof style.fill.area.pattern !== 'undefined') {
+            if (style.fill.area.pattern === 'null') {
+                style.fill.color = '';
+            } else if (style.fill.area.pattern !== 'null' && style.fill.color === '') {
+                style.fill.color = OSKARI_BLANK_STYLE.fill.color;
+            }
         }
+
 
         return style;
     };
