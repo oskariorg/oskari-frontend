6b1f016ba6c3dfa0867e7e09123cb2de55d9ab2e
okauppinen <olli.kauppinen@nls.fi>
2021-07-14 23:56:30 +0300

    add localization

---
+++ bundles/mapping/userstyle/resources/locale/fr.js
@@ -0,0 +1,16 @@
+Oskari.registerLocalization(
+{
+    "lang": "fr",
+    "key": "userstyle",
+    "value": {
+        "title": "Style personnel",
+        "defaultName": "Style personnel",
+        "popup": {
+            "title": "Style personnel",
+            "button": {
+                "cancel": "Annuler",
+                "save": "Enregistrer"
+            }
+        }
+    }
+});
---
+++ bundles/mapping/userstyle/resources/locale/ru.js
@@ -0,0 +1,16 @@
+Oskari.registerLocalization(
+{
+    "lang": "ru",
+    "key": "userstyle",
+    "value": {
+        "title": "Свой стиль",
+        "defaultName": "Свой стиль",
+        "popup": {
+            "title": "Свой стиль",
+            "button": {
+                "cancel": "Отменить",
+                "save": "Сохранить"
+            }
+        }
+    }
+});
