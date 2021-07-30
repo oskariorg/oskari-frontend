ec46a8e59de5c2d88a778d228e5e15e36acda565
kasparia <superkasperi@gmail.com>
2021-07-30 17:51:51 +0300

    Add MyPlaces editor component

---
+++ bundles/framework/myplaces3/MyPlacesStyleForm.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { LocaleProvider, Controller } from 'oskari-ui/util';
+import { Message, Button } from 'oskari-ui';
+import { VectorStyleModal } from 'oskari-ui/components/VectorStyle';
+
+const hasValidName = (name) => {
+    return name.length > 0;
+};
+
+const getMessage = (key, args) => <Message messageKey={key} messageArgs={args} bundleKey='MyPlaces3' />;
+
+export const MyPlacesStyleForm = ({layer, saveCategory, deleteCategory}) => {
+    const [editorState, setEditorState] = useState({
+        modalVisibility: false,
+        currentStyle: layer.style,
+        styleId: '',
+        styleName: layer.name || '',
+        originalName: '',
+        validates: false
+    });
+
+    const saveStyle = () => saveCategory({ categoryId: layer.categoryId, style: editorState.currentStyle, name: editorState.styleName });
+    const onModalCancel = () => setEditorState({ ...editorState, modalVisibility: false });
+    const resetNewStyle = () => setEditorState({ ...editorState, styleId: '', styleName: '', originalName: '', currentStyle: {}, modalVisibility: true });
+    const onModalOk = () => {
+        if (hasValidName(editorState.styleName)) {
+            saveStyle();
+            setEditorState({ ...editorState, modalVisibility: false });
+        }
+    };
+
+    const setName = (name) => {
+        setEditorState({ ...editorState, styleName: name });
+    };
+
+    return (
+        <LocaleProvider value={{ bundleKey: 'MyPlaces3' }}>
+            <Button onClick={ () => setEditorState({ ...editorState, modalVisibility: true }) }>
+                { getMessage('tab.editCategory') }
+            </Button>
+            <Button onClick={ () => deleteCategory(layer.categoryId) }>
+                { getMessage('tab.deleteCategory') }
+            </Button>
+            <VectorStyleModal
+                editorState={ editorState }
+                okButtonPros={ 'disabled' }
+                onModalOk={ onModalOk }
+                onCancel={ () => onModalCancel() }
+                cancelText={ <Message messageKey="cancel" /> }
+                okText={ <Message messageKey="save" /> }
+                nameValidation={ hasValidName }
+                setName={ setName }
+                setEditorState={ setEditorState }
+            />
+        </LocaleProvider>
+    );
+};
+
+MyPlacesStyleForm.propTypes = {
+    layer: PropTypes.object.isRequired,
+    controller: PropTypes.instanceOf(Controller)
+};
