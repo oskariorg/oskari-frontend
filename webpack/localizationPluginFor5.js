a11c494924355fd5f2ff83e333ac0f6fa8f3c5cd
Sami Mäkinen <sami.makinen@nls.fi>
2021-08-30 17:27:26 +0300

    Refactor localization plugin for webpack 5

--- webpack/localizationPluginFor5.js
+++ webpack/localizationPluginFor5.js
@@ -11,18 +11,96 @@ function isLocaleFile (filePath) {
         return false;
     }
     const parts = filePath.split(path.sep);
-    if (parts[parts.length -2] !== 'resouces') {
-        // this could be: oskari-frontend\node_modules\moment\locale\af.js
-        // for oskari locs we want oskari-frontend\bundles\*\resources\locale\en.js
+    if (parts[parts.length -3] !== 'resources') {
+        // this could be things like:
+        // - oskari-frontend\node_modules\moment\locale\af.js
+        // - oskari-frontend\node_modules\antd\es\locale\en_US.js
+        // for oskari locs we only want to process files in paths like:
+        // - oskari-frontend\bundles\*\resources\locale\en.js
         return false;
     }
     return fileRex.test(path.basename(filePath));
 }
 
+const langFromPath = (filePath) => {
+    const match = path.basename(filePath).match(fileRex);
+    return match ? match[1] : null;
+};
+
+const getLanguagesToWrite = (files) => {
+    const changedLanguages = new Set();
+    files.forEach(path => {
+        const lang = langFromPath(path);
+        if (lang) {
+            changedLanguages.add(lang);
+        }
+    });
+    return changedLanguages;
+};
+
+const readLocalizationContent = (localeFiles) => {
+    const changedLanguages = getLanguagesToWrite(localeFiles);
+
+    const langToLoc = new Map();
+    const langToOverride = new Map();
+    const allKeys = new Set();
+    /* eslint-disable-next-line */
+    const Oskari = {
+        registerLocalization: (loc, isOverride) => {
+            const lang = loc.lang;
+            if (!lang) {
+                throw new Error('Localization file has no "lang"!');
+            }
+            const collection = isOverride ? langToOverride : langToLoc;
+            let agg = new Map();
+            if (collection.has(lang)) {
+                agg = collection.get(lang);
+            } else {
+                collection.set(lang, agg);
+            }
+            agg.set(loc.key, loc);
+            allKeys.add(loc.key);
+        }
+    };
+    localeFiles
+        .filter(path => {
+            const lang = langFromPath(path);
+            return lang === 'en' || changedLanguages.has(lang); // Always process English as it might be needed as fallback
+        })
+        .forEach(path => {
+            const source = fs.readFileSync(path, 'utf8');
+            /* eslint-disable-next-line */
+            eval(source);
+        });
+
+    const englishLoc = langToLoc.get('en') || new Map();
+    const result = {};
+    for (const entry of langToLoc.entries()) {
+        const lang = entry[0];
+        const langLoc = entry[1];
+        const langOverride = langToOverride.get(lang) || new Map();
+
+        const keyContents = Array.from(allKeys)
+            .filter(key => englishLoc.has(key) || langLoc.has(key) || langOverride.has(key))
+            .map(key => {
+                const englishForKey = lang === 'en' ? {} : englishLoc.get(key) || {}; // don't merge English with itself
+                const locForKey = langLoc.get(key) || {};
+                const overrideForKey = langOverride.get(key) || {};
+
+                const mergedEnglish = merge.recursive(true, englishForKey, locForKey);
+                const mergedOverride = merge.recursive(true, mergedEnglish, overrideForKey);
+                mergedOverride.lang = lang; // value for "lang" key might be from fallback. Ensuring it's correct
+                return mergedOverride;
+            });
+
+        const fileContent = keyContents.map(content => `Oskari.registerLocalization(${JSON.stringify(content)});`).join('\n');
+        result[lang] = fileContent;
+    }
+    return result;
+}
+
 class LocalizationPlugin {
     constructor (appName) {
-        this.startTime = Date.now();
-        this.prevTimestamps = new Map();
         this.appPath = appName ? appName + '/' : '';
     }
 // https://stackoverflow.com/questions/65535038/webpack-processassets-hook-and-asset-source
@@ -30,115 +108,22 @@ class LocalizationPlugin {
 // https://survivejs.com/webpack/extending/plugins/
 // https://webpack.js.org/api/compilation-hooks/#processassets
     apply (compiler) {
-        compiler.hooks.thisCompilation.tap('LocalizationPlugin', (compilation) => {
-            const moi = Array.from(compilation.fileDependencies);
-            console.log('Number of files: ', moi.length);
-            const localeFiles = moi.filter(isLocaleFile);
-            console.log('Number of locales: ', localeFiles.length);
-
-            compilation.hooks.processAssets.tap({
-                name: pluginName,
-                stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL
-            }, (assets) => {
-
-            console.log('List of assets and their sizes:');
-            let files = [];
-            Object.entries(assets).forEach(([pathname, source]) => {
-                console.log(`— ${pathname}: ${source.size()} bytes`);
-                files.push(pathname);
-            });
-            //const localeFiles = files.filter(isLocaleFile);
-            // Array.from(compilation.fileDependencies).filter(isLocaleFile);
+        compiler.hooks.emit.tap(pluginName, (compilation) => {
+            const localeFiles = Array.from(compilation.fileDependencies).filter(isLocaleFile);
+            console.log('\n\nNumber of locales: ', localeFiles.length);
                 
-            const changedLanguages = new Set();
-            localeFiles
+            const oskariLangContents = readLocalizationContent(localeFiles)
             /*
-                .filter(path => {
-                    return (this.prevTimestamps.get(path) || this.startTime) < (compilation.fileTimestamps.get(path) || Infinity);
-                })
-                */
-                .forEach(path => {
-                    const lang = this.langFromPath(path);
-                    if (lang) {
-                        changedLanguages.add(lang);
-                    }
-                });
-
-            //this.prevTimestamps = compilation.fileTimestamps;
-
-            const langToLoc = new Map();
-            const langToOverride = new Map();
-            const allKeys = new Set();
-            /* eslint-disable-next-line */
-            const Oskari = {
-                registerLocalization: (loc, isOverride) => {
-                    const lang = loc.lang;
-                    if (!lang) {
-                        throw new Error('Localization file has no "lang"!');
-                    }
-                    const collection = isOverride ? langToOverride : langToLoc;
-                    let agg = new Map();
-                    if (collection.has(lang)) {
-                        agg = collection.get(lang);
-                    } else {
-                        collection.set(lang, agg);
-                    }
-                    agg.set(loc.key, loc);
-                    allKeys.add(loc.key);
-                }
-            };
-            localeFiles
-                .filter(path => {
-                    const lang = this.langFromPath(path);
-                    return lang === 'en' || changedLanguages.has(lang); // Always process English as it might be needed as fallback
-                })
-                .forEach(path => {
-                    const source = fs.readFileSync(path, 'utf8');
-                    /* eslint-disable-next-line */
-                    eval(source);
-                });
-
-            const englishLoc = langToLoc.get('en') || new Map();
-            for (const entry of langToLoc.entries()) {
-                const lang = entry[0];
-                const langLoc = entry[1];
-                const langOverride = langToOverride.get(lang) || new Map();
-
-                const keyContents = Array.from(allKeys)
-                    .filter(key => englishLoc.has(key) || langLoc.has(key) || langOverride.has(key))
-                    .map(key => {
-                        const englishForKey = lang === 'en' ? {} : englishLoc.get(key) || {}; // don't merge English with itself
-                        const locForKey = langLoc.get(key) || {};
-                        const overrideForKey = langOverride.get(key) || {};
-
-                        const mergedEnglish = merge.recursive(true, englishForKey, locForKey);
-                        const mergedOverride = merge.recursive(true, mergedEnglish, overrideForKey);
-                        mergedOverride.lang = lang; // value for "lang" key might be from fallback. Ensuring it's correct
-                        return mergedOverride;
-                    });
-
-                const fileContent = keyContents.map(content => `Oskari.registerLocalization(${JSON.stringify(content)});`).join('\n');
-                
+            [DEP_WEBPACK_COMPILATION_ASSETS] DeprecationWarning: Compilation.assets will be frozen in future, all modifications are deprecated.
+            BREAKING CHANGE: No more changes should happen to Compilation.assets after sealing the Compilation.
+                    Do changes to assets earlier, e. g. in Compilation.hooks.processAssets.
+                    Make sure to select an appropriate stage from Compilation.PROCESS_ASSETS_STAGE_*.
+            */
+            Object.keys(oskariLangContents).forEach(lang => {
+                const fileContent = oskariLangContents[lang];
                 compilation.emitAsset(`${this.appPath}oskari_lang_${lang}.js`, new sources.RawSource(fileContent));
-                /*
-                compilation.assets[`${this.appPath}oskari_lang_${lang}.js`] = {
-                    source () {
-                        return fileContent;
-                    },
-                    size () {
-                        return fileContent.length;
-                    }
-                };
-                */
-            }
             });
-            // callback();
         });
-    }
-
-    langFromPath (filePath) {
-        const match = path.basename(filePath).match(fileRex);
-        return match ? match[1] : null;
     }
 }
 
