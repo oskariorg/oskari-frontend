7e0d769ac41d445b4b8cf86df93ecd550f80adee
Sami Mäkinen <sami.makinen@nls.fi>
2021-09-23 17:37:34 +0300

    Remove unused localization

--- bundles/framework/timeseries/resources/locale/en.js
+++ bundles/framework/timeseries/resources/locale/en.js
@@ -33,8 +33,6 @@ Oskari.registerLocalization(
             "timeseries": "Time series"
         },
         "rangeControl": {
-            "helpMessage": "With this timeseries tool, you can select the years you want in the slider. The small circles indicate the years from which there exists data in current map view. In the default mode you will select a single year and see this data in the map view. Using the icon in the upper right corner of this tool you can change to a time range mode, in which you can select a range of years you want to see the images from. Click again and you will return to the default mode.",
-            
             "helpGeneric": 'With this timeseries tool, you can select the years you want in the slider. The small circles indicate the years from which there exists data in current map view.',
             "helpMsg_year": 'In this mode you can see data from a single year. At the fringes of yearly coverage areas it is easier to inspect the data in the time range mode. You can switch the mode by clicking the nearby icon:',
             "helpMsg_range": 'In this mode you can select a time range for the data. Note! The latest available image for the selected range is shown on any given location. You can switch to single year mode by clicking the near by icon:',
