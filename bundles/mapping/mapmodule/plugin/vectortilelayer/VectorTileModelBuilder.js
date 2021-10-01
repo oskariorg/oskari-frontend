7cd2552d9a8052f45c3d2548df58b0d6ea58cd6d
okauppinen <olli.kauppinen@nls.fi>
2021-10-01 13:33:31 +0300

    remove style parsing from vectortilemodelbuiler

--- bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileModelBuilder.js
+++ bundles/mapping/mapmodule/plugin/vectortilelayer/VectorTileModelBuilder.js
@@ -2,15 +2,10 @@ import { VectorStyle } from '../../domain/VectorStyle';
 export class VectorTileModelBuilder {
     parseLayerData (layer, mapLayerJson, maplayerService) {
         const { options = {}, style } = mapLayerJson;
-        const { styles = {}, externalStyles = {}, hover } = options;
+        const { externalStyles = {}, hover } = options;
 
-        Object.keys(styles).forEach(name => {
-            const style = new VectorStyle(name, null, 'normal', styles[name]);
-            layer.addStyle(style);
-        });
-        // Remove styles from options to be sure that VectorStyle is used
-        delete options.styles;
-
+        // normal styles are set in AbstractVectorLayer setOptions
+        // set external styles
         Object.keys(externalStyles).forEach(name => {
             // Use name as title
             const style = new VectorStyle(name, name, 'external', externalStyles[name]);
