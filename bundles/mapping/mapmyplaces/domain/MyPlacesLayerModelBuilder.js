073a86f49081a5ad65e5e3ae1228e854498dd213
Sami Mäkinen <sami.makinen@nls.fi>
2021-11-19 13:04:33 +0200

    Fix getting named sequence and remove multiplier since -1 works not for getting a group

--- bundles/mapping/mapmyplaces/domain/MyPlacesLayerModelBuilder.js
+++ bundles/mapping/mapmyplaces/domain/MyPlacesLayerModelBuilder.js
@@ -27,7 +27,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmyplaces.domain.MyPlacesLayer
             }
             if (!this.groupId) {
                 // negative value for group id means that admin isn't presented with tools for it
-                this.groupId = -10 * Oskari.seq.nextVal('usergeneratedGroup');
+                this.groupId = -1 * Oskari.getSeq('usergeneratedGroup').nextVal();
                 const mapLayerGroup = maplayerService.findLayerGroupById(this.groupId);
                 if (!mapLayerGroup) {
                     const group = {
