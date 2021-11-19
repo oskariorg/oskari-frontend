5649c9b3b05482bffff113e089fcdd46645cedcb
Sami Mäkinen <sami.makinen@nls.fi>
2021-11-19 17:45:34 +0200

    Documentation what plugins should implement

--- bundles/mapping/mapmodule/AbstractVectorLayerPlugin.js
+++ bundles/mapping/mapmodule/AbstractVectorLayerPlugin.js
@@ -15,9 +15,20 @@ const AbstractMapLayerPlugin = Oskari.clazz.get('Oskari.mapping.mapmodule.Abstra
     }
 
     /**
+     * Override in actual plugins to returns features.
      * 
-     * @param {Object} geojson geojson.geometry or geojson.properties can be used to filter features. Defaults to current viewport.
-     * @param {Object} opts options for operation
+     * Returns features that are currently on map filtered by given geometry and/or properties
+     * {
+     *   "[layer id]": {
+     *      runtime: true,
+     *      features: [{ geometry: {...}, properties: {...}}, ...]
+     *   },
+     *   ...
+     * }
+     * Runtime flag is true for features pushed with AddFeaturesToMapRequest etc and false/missing for features from WFS/OGC API sources.
+     * @param {Object} geojson an object with geometry and/or properties as filter for features. Geometry defaults to current viewport.
+     * @param {Object} opts additional options to narrow feature collection
+     * @returns {Object} an object with layer ids as keys with an object value with key "features" for the features on that layer and optional runtime-flag
      */
     getFeatures (geojson = {}, opts = {}) {
         // console.log('getting features from ', this.getName());
