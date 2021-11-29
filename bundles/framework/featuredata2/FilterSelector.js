f4402b99b42e2714d669fcfb39f3f2572974d5b8
Sami Mäkinen <sami.makinen@nls.fi>
2021-11-29 16:35:38 +0200

    Add docs

--- bundles/framework/featuredata2/FilterSelector.js
+++ bundles/framework/featuredata2/FilterSelector.js
@@ -4,11 +4,20 @@ import { getFilterAlternativesAsArray, filterFeaturesByAttribute } from '../../m
 
 const getFeatureId = (feature) => feature.id || feature.properties[WFS_ID_KEY] || feature.properties[WFS_FTR_ID_KEY];
 
+/**
+ * Helper for selecting features based on geometry or
+ * properties (by filters from Oskari.userinterface.component.FilterDialog)
+ */
 export class FilterSelector {
     constructor(featureQueryFn, selectionService) {
         this.featureQueryFn = featureQueryFn;
         this.selectionService = selectionService;
     }
+    /**
+     * Selects features matching the properties filters from the requested layer.
+     * @param {Object[]} filters array from Oskari.userinterface.component.FilterDialog
+     * @param {String|Number} layerId layer to make selection from
+     */
     selectWithProperties (filters = [], layerId) {
         if (!this.selectionService) {
             Oskari.log('FeatureData').error('VectorFeatureSelectionService is not available in appsetup');
@@ -45,7 +54,12 @@ export class FilterSelector {
         this.selectionService.setSelectedFeatureIds(layerId, [...filteredIds]);
 
     }
-
+    /**
+     * Selects features intersecting the geometry from the requested layers.
+     * @param {Object} filterFeature GeoJSONLike object with geometry key to query features for selection
+     * @param {String[]|Number[]} layers layers to query from
+     * @returns 
+     */
     selectWithGeometry (filterFeature = {}, layers = []) {
         if (!this.selectionService) {
             Oskari.log('FeatureData').error('VectorFeatureSelectionService is not available in appsetup');
@@ -64,10 +78,15 @@ export class FilterSelector {
             this.selectionService.setSelectedFeatureIds(layerId, selectedFeatureIds);
         });
     }
-
+    /**
+     * Returns a layer ids to query from by filtering out non-vector layers
+     * @param {Oskari.mapframework.domain.AbstractLayer[]} selectedLayers selection of layers for filtering
+     * @param {Boolean} fromAllLayers true to return all queryable layers or false for the top most layer
+     * @returns {String[]|Number[]} layer ids for querying
+     */
     getLayersToQuery(selectedLayers = [], fromAllLayers = false) {
         const selectedVectorLayers = selectedLayers
-            .filter(l => l.hasFeatureData())
+            .filter(l => l.hasFeatureData() && l.isVisible())
             .map(l => l.getId());
         if (!selectedVectorLayers.length) {
             Oskari.log('FeatureData').debug('No vector layers to select from');
