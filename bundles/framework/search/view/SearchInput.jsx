069cc0e738032e34c286b585ea6bf88ec75c073f
Sami Mäkinen <sami.makinen@nls.fi>
2021-12-15 15:58:23 +0200

    Hide search input clear icon added by chrome

--- bundles/framework/search/view/SearchInput.jsx
+++ bundles/framework/search/view/SearchInput.jsx
@@ -4,8 +4,25 @@ import { SearchInput as Input } from 'oskari-ui';
 import { AutoComplete } from 'antd';
 import styled from 'styled-components';
 
+/*
+The input type="search" has a duplicated "clear" function added by the browser.
+The css to hide it was copy/pasted from:
+https://blog.maximerouiller.com/post/remove-the-x-from-internet-explorer-and-chrome-input-type-search/
+
+This might go away by updating ant-d. It seems a bit weird that it isn't hidden
+*/
 const WideAutoComplete = styled(AutoComplete)`
     width: 100%;
+
+    /* clears the 'X' from Internet Explorer */
+    input[type=search]::-ms-clear {  display: none; width : 0; height: 0; }
+    input[type=search]::-ms-reveal {  display: none; width : 0; height: 0; }
+
+    /* clears the 'X' from Chrome */
+    input[type="search"]::-webkit-search-decoration,
+    input[type="search"]::-webkit-search-cancel-button,
+    input[type="search"]::-webkit-search-results-button,
+    input[type="search"]::-webkit-search-results-decoration { display: none; }
 `;
 
 const noop = () => {};
@@ -16,8 +33,7 @@ export const SearchInput = ({ query = '', suggestions = [], onChange = noop, onS
             className='t_searchInput'
             { ...rest }
             size='large'
-            // autocomplete/large setting will show it's own clear option
-            allowClear={false}
+            allowClear={true}
             enterButton={true}
             onChange={(event) => onChange(event.target.value)}
             onSearch={onSearch} />);
@@ -25,6 +41,7 @@ export const SearchInput = ({ query = '', suggestions = [], onChange = noop, onS
         return input;
     }
     return (<WideAutoComplete
+        allowClear={false}
         options={suggestions.map(word => ({ value: word }))}
         onSelect={onSearch}>
             {input}
