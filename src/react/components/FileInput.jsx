537935caa08fb246d508dc2ec6ca643d527fddaf
okauppinen <olli.kauppinen@nls.fi>
2022-01-20 12:16:24 +0200

    remove antd upload and implement own fileinput

--- src/react/components/FileInput.jsx
+++ src/react/components/FileInput.jsx
@@ -1,118 +1,150 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Message, Tooltip } from 'oskari-ui';
 import { Messaging } from 'oskari-ui/util';
-import Upload from 'antd/es/upload/Upload';
 import { CloudUploadOutlined, CloseCircleOutlined  } from '@ant-design/icons';
 
 const MAX_SIZE = 10; //MB
 const MAX_COUNT = 5;
 
-const Padding = styled('div')`
-    padding: 12px 24px;
-`;
-
 const Border = styled('div')`
+    display: block;
+    width: 100%;
+    min-height: 70px;
+    padding: 12px;
     background: #fafafa;
     border: 1px dashed #d9d9d9;
-    width: 100%;
-    cursor: pointer;
     transition: border-color .3s;
-    margin-bottom: 10px;
-    display: inline-block;
     &:hover{
         border-color: #40a9ff;
     }
 `;
-const Icon = styled(CloudUploadOutlined)`
+const StyledLabel = styled('label')`
+    cursor: pointer;
+    padding: 12px;
+    &:hover{
+        color: #40a9ff;
+    }
+`;
+const HiddenFileInput = styled('input')`
+    display: none;
+`;
+const StyledFileList = styled('div')`
+    display: inline-block;
+    padding: 0px 12px;
+    margin-top: 8px;
+`;
+const StyledUploadIcon = styled(CloudUploadOutlined)`
     padding-right: 10px;
+`;
+
+const StyledListItem = styled('span')`
+    white-space: nowrap;
+    padding-left: 5px;
+`;
+const StyledName = styled('span')`
+    color: #40a9ff;
+    padding-right: 5px;
 `;
 
 const getMsg = (path, args) => <Message messageKey={`FileInput.${path}`} bundleKey='oskariui' messageArgs={args}/>;
 
-const uploadListOptions = {
-    showPreviewIcon: false, 
-    removeIcon: <CloseCircleOutlined /> // override default trash icon to get rid of hard coded title
-};
-
-const uploadListItemIconRenderer = () => null; // remove paper clipper (needs some styling)
-
-// Another way to avoid uploadin files
-const fakeRequest = ({ file, onSuccess }) => {
-    setTimeout(() => {
-      onSuccess('ok');
-    }, 0);
-  };
+const prevent = e => e.preventDefault();
 
 const showError = (key, args) => {
     const content = getMsg(`error.${key}`, args);
     Messaging.error({ content, duration: 5 });
 }
 
-
-const onChange = (info, onFiles) => {
-    const { fileList } = info; // { file, fileList }
-    const files = fileList
-        .map(f => f.originFileObj)
-        .filter(f => {
-            if (typeof f !== 'undefined'){
-                return true;
-            }
-            Oskari.log('FileInput').warn('Could not find original File object');
-        });
-        
-    onFiles(files);
-};
-
-const validate = (file, fileList, maxCount, maxSize, allowedTypes  ) => {
-    if (fileList.length > maxCount) {
-        const errorKey = maxCount === 1 ? 'multipleNotAllowed' : 'maxCountExceeded';
-        showError(errorKey);
-        return Upload.LIST_IGNORE;
-    }
-    const validType = Array.isArray(allowedTypes) && allowedTypes.length ? allowedTypes.includes(file.type) : true;
-    if (!validType) {
-        showError('invalidType');
-    }
-    const validSize = file.size < (maxSize * 1024 * 1024);
-    if(!validSize) {
-        showError('fileSize', { maxSize });
-    }
-    // to avoid uploading files return false (Uploading will be stopped with false), LIST_IGNORE doesn't add file
-    // FIXME: LIST_IGNORE doesn't seem to work. Maybe use customized file listing or crate own (and get rid of antd)
-    return validType && validSize ? false : Upload.LIST_IGNORE;
-};
-
 export const FileInput = ({ 
     onFiles,
     multiple=true,
     tooltip,
-    allowedTypes,
-    maxSize = MAX_SIZE 
+    allowedTypes = [],
+    maxSize = MAX_SIZE
 }) => {
+    const [currentFiles, setFiles] = useState([]);
     const maxCount = multiple ? MAX_COUNT : 1;
+
+    const onDrop = event => {
+        event.preventDefault();
+        handleInputFiles(event.dataTransfer.files);
+    };
+    const handleInputFiles = inputFiles => {
+        // Special case for single option to override existing file
+        if (!multiple && inputFiles.length === 1) {
+            const file = inputFiles[0];
+            if(validate(file)){
+                onFiles([file]);
+                setFiles([file]);
+            }
+            return;
+        }
+        if (inputFiles.length + currentFiles.length > maxCount) {
+            const errorKey = maxCount === 1 ? 'multipleNotAllowed' : 'maxCountExceeded';
+            showError(errorKey);
+            return;
+        }
+        const validFiles = [];
+        //FileList doesn't have filter
+        inputFiles.forEach(file => validate(file) && validFiles.push(file));
+        const files = [...currentFiles, ...validFiles];
+        onFiles(files);
+        setFiles(files);
+    };
+
+    const validate = file => {
+        const validType = allowedTypes.length ? allowedTypes.includes(file.type) : true;
+        if (!validType) {
+            showError('invalidType');
+        }
+        const validSize = file.size < (maxSize * 1024 * 1024);
+        if(!validSize) {
+            showError('fileSize', { maxSize });
+        }
+        return validType && validSize;
+    };
+
+    const onFileRemove = name => {
+        const files = currentFiles.filter(file => file.name !== name);
+        setFiles(files);
+        onFiles(files);
+    };
 
     return (
         <Tooltip title={ tooltip } trigger={ ['focus', 'hover'] }>
-            <Border>
-                <Upload
-                    multiple = {multiple}
-                    maxCount = {maxCount}
-                    onChange = {info => onChange(info, onFiles)}
-                    className="upload-list-inline"
-                    beforeUpload = {(file, fileList) => validate(file, fileList, maxCount, maxSize, allowedTypes)} 
-                    showUploadList = {uploadListOptions}
-                    iconRender = {uploadListItemIconRenderer}
-                    customRequest = {fakeRequest}
-                >
-                    <Padding>
-                        <Icon style={{ fontSize: '30px', color: '#006ce8'}}/>
-                        { getMsg('drag',{ maxCount }) }
-                    </Padding>
-                </Upload>
+            <Border
+                className="t_fileinput_dropzone"
+                onDrop={onDrop}
+                onDragOver={prevent}
+                onDragEnter={prevent}
+                onDragLeave={prevent}
+            >
+                <StyledLabel>
+                    <HiddenFileInput
+                        type="file"
+                        multiple= {multiple}
+                        accept={allowedTypes.join(',')}
+                        onChange={e => handleInputFiles(e.target.files)}
+                    />
+                    <StyledUploadIcon className="t_fileinput_btn" style={{ fontSize: '30px', color: '#006ce8'}}/>
+                    { getMsg('drag',{ maxCount }) }
+                </StyledLabel>
+                <StyledFileList className="t_fileinput_list">
+                    { currentFiles.map( ({name}) => <FileListItem name={name} onRemoveClick={onFileRemove} key={name} />) }
+                </StyledFileList>
             </Border>
         </Tooltip>
+    );
+};
+
+const FileListItem = ({name, onRemoveClick}) => {
+    return (
+        <StyledListItem>
+            <StyledName>{ name }</StyledName>
+            <CloseCircleOutlined className="t_fileinput_remove" onClick={()=>onRemoveClick(name)}/>
+        </StyledListItem>
     );
 };
 
