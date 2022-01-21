98b720f8ba7b32cbf0d2fb500992505df6202a43
okauppinen <olli.kauppinen@nls.fi>
2022-01-21 16:05:33 +0200

    pass placeholder and required with element's props to localizationcomponent

--- bundles/framework/myplaces3/MyPlacesLayerForm.jsx
+++ bundles/framework/myplaces3/MyPlacesLayerForm.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Message, Divider, Modal, LocalizationComponent, TextInput } from 'oskari-ui';
-import { createLocalizedLabels } from 'oskari-ui/components/LocalizationComponent';
 
 import { StyleEditor } from 'oskari-ui/components/StyleEditor';
 import { OSKARI_BLANK_STYLE } from 'oskari-ui/components/StyleEditor/index';
@@ -12,20 +11,6 @@ const PaddedInput = styled(TextInput)`
     margin-bottom: 10px;
 `;
 
-const getPlaceholders = () => {
-    const localizedFields = { name: Oskari.getMsg(LOCALE_KEY, 'categoryform.layerName') };
-    return createLocalizedLabels(localizedFields);
-};
-
-const getMandatory = (defaultLang) => {
-    const mandatory = {};
-    mandatory[defaultLang] = ['name'];
-    return mandatory;
-};
-
-// name in default language is mandatory
-const validate = (lang, value) => lang !== Oskari.getDefaultLanguage() || (typeof value === 'string' && value.trim().length > 0);
-
 export const MyPlacesLayerForm = ({ locale: initLocale, style: initStyle, onSave, onCancel }) => {
     const [editorState, setEditorState] = useState({
         style: initStyle || OSKARI_BLANK_STYLE,
@@ -34,10 +19,18 @@ export const MyPlacesLayerForm = ({ locale: initLocale, style: initStyle, onSave
     const { locale, style } = editorState;
     const updateStyle = (style) => setEditorState({ ...editorState, style });
     const updateLocale = (locale) => setEditorState({ ...editorState, locale });
+
     const defaultLang = Oskari.getDefaultLanguage();
     const hasName = Oskari.util.keyExists(locale, `${defaultLang}.name`) && locale[defaultLang].name.trim().length > 0;
+
+    const placeholder = Oskari.getMsg(LOCALE_KEY, 'categoryform.layerName');
+    const required = [];
+    if (!hasName) {
+        required.push(defaultLang);
+    }
     // TODO: show warning in button tooltip
     // { !hasName && <Message messageKey='validation.categoryName' /> }
+
     return (
         <Modal
             title={ <Message messageKey={ 'categoryform.title' } /> }
@@ -50,13 +43,11 @@ export const MyPlacesLayerForm = ({ locale: initLocale, style: initStyle, onSave
             okText={ <Message messageKey="buttons.save" /> }
         >
             <LocalizationComponent
-                placeholders={ getPlaceholders() }
                 value={ locale }
                 languages={ Oskari.getSupportedLanguages() }
                 onChange={ updateLocale }
-                mandatoryFields = { getMandatory(defaultLang) }
             >
-                <PaddedInput type='text' name='name' validate={validate}/>
+                <PaddedInput type='text' name='name' placeholder={placeholder} required={required} />
             </LocalizationComponent>
             <Divider orientation="left"><Message messageKey={ 'categoryform.styleTitle' } /></Divider>
             <StyleEditor
