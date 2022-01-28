90c670cde4a2780f2f749f68b4072ffa70abae02
okauppinen <olli.kauppinen@nls.fi>
2022-01-28 10:10:03 +0200

    add button container to oskariui

--- src/react/components/buttons/index.js
+++ src/react/components/buttons/index.js
@@ -1,2 +1,13 @@
+import styled from 'styled-components';
+
 export { PrimaryButton } from './PrimaryButton';
 export { SecondaryButton } from './SecondaryButton';
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+    button {
+        margin-left: 5px;
+    }
+`;
