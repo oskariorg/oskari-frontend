b92e6b4531f76a058788c7ce456505e45c61b561
okauppinen <olli.kauppinen@nls.fi>
2022-02-14 08:03:58 +0200

    render pattern icons with react

--- src/react/components/StyleEditor/AreaTab.jsx
+++ src/react/components/StyleEditor/AreaTab.jsx
@@ -3,44 +3,35 @@ import PropTypes from 'prop-types';
 import { ColorPicker, Message, Tooltip } from 'oskari-ui';
 import { SvgRadioButton, SizeControl, constants, PreviewCol } from './index';
 import { Form, Row, Col } from 'antd';
-import { getAreaPattern } from './Preview/SVGHelper';
+import { FillPattern, isSolid } from './Preview/FillPattern';
 
-const getFillSVG = (name, counter) => {
-    const myId = name.toLowerCase() + '-' + counter;
-    const patternDef = getAreaPattern(myId, name, '#000000');
-    // TODO
-    let fillPattern = `url(#${myId})`;
-    if (name === 'SOLID') {
-        fillPattern = '#000000'
-    }
-    return `<svg viewBox="0 0 32 32" width="32" height="32" xmlns="http://www.w3.org/2000/svg">
-            ${patternDef}
-        <rect width="32" height="32" fill="${fillPattern}" />
-    </svg>`;
+const { FILLS } = constants;
+const SIZE = 32;
+const COLOR = '#000000';
+const ID_PREFIX = 'button-pattern-';
+
+const getFillIcon = fillCode => {
+    const id = ID_PREFIX + fillCode;
+    const solid = isSolid(fillCode);
+    const fillPattern = solid ? COLOR : `url(#${id})`;
+    return (
+        <svg width={SIZE} height={SIZE}>
+            { !solid && <FillPattern id={id} fillCode={fillCode} color={COLOR}/> }
+            <rect width={SIZE} height={SIZE} fill={fillPattern} />
+        </svg>
+    );
 };
 
-const areaFills = ['TRANSPARENT', 'SOLID', 'HORIZONTAL_THIN', 'HORIZONTAL_THICK', 'DIAGONAL_THIN', 'DIAGONAL_THICK'];
-
+let fillOtions;
 const getFillOptions = () => {
-    return areaFills.map(name => {
-        return {
-            name,
-            data: getFillSVG(name, counter)
-        };
-    });
-
+    if (!fillOtions) {
+        fillOtions = Object.keys(FILLS).map(name  => ({ name, data: getFillIcon(FILLS[name]) }));
+    }
+    return fillOtions;
 };
 
-// counter is used to generate changing ids for SVG to workaround conflicting ids when hard coded
-let counter = 0;
 export const AreaTab = ({oskariStyle, showPreview}) => {
-    counter++;
     const areaFillOptions = getFillOptions();
-
-    const isTransparentFill = Oskari.util.keyExists(oskariStyle, 'fill.area.pattern')
-        && oskariStyle.fill.area.pattern === constants.TRANSPARENT_FILL;
-
-    const fillColorTooltip = isTransparentFill ? <Message messageKey='StyleEditor.tooltips.noFillColor' /> : '';
     return (
         <React.Fragment>
             <Row>
@@ -55,15 +46,13 @@ export const AreaTab = ({oskariStyle, showPreview}) => {
                 </Col>
 
                 <Col span={ 12 } >
-                    <Tooltip title={fillColorTooltip} placement='topLeft'>
-                        <Form.Item
-                            { ...constants.ANTD_FORMLAYOUT }
-                            name='fill.color'
-                            label={ <Message messageKey='StyleEditor.fill.color' /> }
-                            >
-                            <ColorPicker disabled={isTransparentFill} />
-                        </Form.Item>
-                    </Tooltip>
+                    <Form.Item
+                        { ...constants.ANTD_FORMLAYOUT }
+                        name='fill.color'
+                        label={ <Message messageKey='StyleEditor.fill.color' /> }
+                        >
+                        <ColorPicker />
+                    </Form.Item>
                 </Col>
             </Row>
 
---
+++ src/react/components/StyleEditor/FillPattern.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { getFillPatternPath } from '../../../../../bundles/mapping/mapmodule/oskariStyle/generator.ol';
+import { FILLS } from '../constants';
+
+const PATTERN_SIZE = 32;
+
+export const isSolid = id => {
+    return id === FILLS.SOLID || id < 0;
+}
+
+export const FillPattern = ({ id, fillCode, color })=> {
+    if (fillCode === FILLS.TRANSPARENT) {
+        return (
+            <defs>
+                <pattern id={id} x="0" y="0" width="16" height="16" patternUnits="userSpaceOnUse">
+                    <rect fill="#eee" x="0" width="8" height="8" y="0"/>
+                    <rect fill="#eee" x="8" width="8" height="8" y="8"/>
+                </pattern>
+            </defs>
+        );
+    }
+    const { strokeWidth, path } = getFillPatternPath(PATTERN_SIZE, fillCode);
+    return (
+        <defs>
+            <pattern id={id} width={PATTERN_SIZE} height={PATTERN_SIZE}>
+                <path d={path} stroke={color} strokeWidth={strokeWidth}/>
+            </pattern>
+        </defs>
+    );
+};
+
+FillPattern.propTypes = {
+    id: PropTypes.string.isRequired,
+    fillCode: PropTypes.number.isRequired,
+    color: PropTypes.string.isRequired
+};
