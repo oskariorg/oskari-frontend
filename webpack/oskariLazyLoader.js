3dabfaa3f1fe5435d38f48fd6dc87a7711529234
Sami Mäkinen <zakarfin@gmail.com>
2022-03-01 23:02:58 +0200

    Downgrade loader-utils

--- webpack/oskariLazyLoader.js
+++ webpack/oskariLazyLoader.js
@@ -1,17 +1,11 @@
-// const { urlToRequest } = require('loader-utils');
-// TODO: remove loader-utils?
-// https://github.com/webpack/loader-utils/releases/tag/v3.0.0
-// https://github.com/webpack/loader-utils/commit/0d895223ca0e093ed6555459511c90096828066b
+const { stringifyRequest } = require('loader-utils');
+
 module.exports = function (source) {
     if (!this.query) {
         throw new Error('Param with bundle name must be given, eg. "import \'oskari-lazy-loader?bundlename!path/to/bundle.js\'"');
     }
-
-    const stringifyRequest = () => {
-        JSON.stringify(loaderContext.utils.contextify(this.context, request))
-    }
     const name = this.query.slice(1);
-    const bundlePath = stringifyRequest(`oskari-loader!${this.resourcePath}`);
+    const bundlePath = stringifyRequest(this, `oskari-loader!${this.resourcePath}`);
 
     return `Oskari.bundle_manager.registerDynamic('${name}', function() {return import(/* webpackChunkName: "chunk_${name}" */ ${bundlePath});});\n`;
 };
