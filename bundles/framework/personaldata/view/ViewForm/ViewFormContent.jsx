8e6e64cdbe68f7cc61486d6b565d8c8eded945a8
okauppinen <olli.kauppinen@nls.fi>
2022-03-10 00:23:54 +0200

    fix spinner, show tooltip only if mandatory is missing, change to use labeledinput, add default name

--- bundles/framework/personaldata/view/ViewForm/ViewFormContent.jsx
+++ bundles/framework/personaldata/view/ViewForm/ViewFormContent.jsx
@@ -1,55 +1,60 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Message, Tooltip, Spin, TextInput, TextAreaInput, Checkbox } from 'oskari-ui';
+import { Message, Tooltip, Spin, LabeledInput, Checkbox } from 'oskari-ui';
 import { SecondaryButton, PrimaryButton, ButtonContainer } from 'oskari-ui/components/buttons';
-
+import { LocaleConsumer } from 'oskari-ui/util';
 
 const Content = styled.div`
     margin: 12px 24px 24px;
 `;
 
-const Input = styled(TextInput)`
-    margin-bottom: 10px;
-`;
-
-export const ViewFormContent = ({ values = {}, onOk, onCancel, error, loading }) => {
-    const [state, setState] = useState({ ...values });
-
+const ViewFormContent = ({ values = {}, onOk, onCancel, getMessage }) => {
+    const [state, setState] = useState({
+        name: getMessage('tabs.myviews.popup.defaultName', { now: Date.now() }),
+        ...values
+    });
     const updateState = (newState) => setState({ ...state, ...newState });
-    const onOkClick = () => {
-        const values = {
-            name: state.name,
-            description: state.description,
-            isDefault: state.isDefault
-        };
-        onOk(values);
+    const getLabel = name => getMessage(`tabs.myviews.popup.label.${name}`);
+    const onSave = () => {
+        onOk({ ...state });
+        updateState({ loading: true });
     };
 
-    const hasMandatoryName = state.name?.trim().length > 0;
+    const hasMandatoryName = state.name.trim().length > 0;
+    const tooltip = hasMandatoryName ? '' : <Message messageKey={`tabs.myviews.popup.noName`} />;
 
     const Component = (
         <Content>
-            <Message messageKey='tabs.myviews.popup.name_placeholder' />
-            <Input type='text' name='name' onChange={e => updateState({[e.target.name]: e.target.value})} value={state.name} isRequired/>
-            <Message messageKey='tabs.myviews.popup.description_placeholder' />
-            <TextAreaInput name='description' onChange={e => updateState({[e.target.name]: e.target.value})} value={state.description} />
+            <LabeledInput
+                value={state.name}
+                onChange={e => updateState({ name: e.target.value })}
+                onFocus = {e => e.target.select()}
+                label={getLabel('name')}
+                mandatory
+                autoFocus
+            />
+            <LabeledInput
+                value={state.description}
+                type='textarea'
+                onChange={e => updateState({ description: e.target.value })}
+                label={getLabel('description')}
+            />
             <Checkbox
-                name='isDefault'
                 checked={state.isDefault}
-                onChange={() => updateState({isDefault: !state.isDefault})}
+                onChange={() => updateState({ isDefault: !state.isDefault })}
             >
-                <Message messageKey='tabs.myviews.popup.default' />
+                <Message messageKey='tabs.myviews.popup.label.default' />
             </Checkbox>
             <ButtonContainer>
                 <SecondaryButton type='cancel' onClick={() => onCancel()}/>
-                <Tooltip title={<Message messageKey={`tabs.myviews.popup.error_noname`} />}>
-                    <PrimaryButton disabled={!hasMandatoryName} type="save" onClick={onOkClick}/>
+                <Tooltip title={tooltip}>
+                    <PrimaryButton disabled={!hasMandatoryName} type="save" onClick={onSave}/>
                 </Tooltip>
             </ButtonContainer>
         </Content>
     );
-    if (!error && loading) {
+    if (state.loading) {
         return <Spin showTip={true}>{Component}</Spin>;
     }
     return Component;
@@ -61,3 +66,6 @@ ViewFormContent.propTypes = {
     onCancel: PropTypes.func.isRequired,
     error: PropTypes.string
 };
+
+const wrapped = LocaleConsumer(ViewFormContent);
+export { wrapped as ViewFormContent };
