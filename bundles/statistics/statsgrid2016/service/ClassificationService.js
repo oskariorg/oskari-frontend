1a30b570b111824f59478dfbabda3d951e1bb7c0
okauppinen <olli.kauppinen@nls.fi>
2022-03-22 10:24:29 +0200

    refactor classification to use own range calculations and remove unused parts

--- bundles/statistics/statsgrid2016/service/ClassificationService.js
+++ bundles/statistics/statsgrid2016/service/ClassificationService.js
@@ -13,8 +13,21 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
      */
     function (colorService) {
         this._colorService = colorService;
-        this.lastUsedBounds = null;
         this.log = Oskari.log(this.getQName());
+        // limits should be used when creating UI for classification
+        this.limits = {
+            // 2-7 used for points, colorservice's colorsets limits other mapStyles
+            count: { min: 2, max: 7 },
+            // values recognized by the code (and geostats)
+            methods: ['jenks', 'quantile', 'equal', 'manual'],
+            // values recognized by the code (and geostats)
+            modes: ['distinct', 'discontinuous'],
+            // values recognized by the code (and geostats)
+            mapStyles: ['choropleth', 'points'],
+            // values recognized by the code (and geostats)
+            types: this._colorService.getAvailableTypes(),
+            fractionDigits: 5
+        };
     }, {
         __name: 'StatsGrid.ClassificationService',
         __qname: 'Oskari.statistics.statsgrid.ClassificationService',
@@ -24,20 +37,6 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
         },
         getName: function () {
             return this.__name;
-        },
-        // limits should be used when creating UI for classification
-        limits: {
-            // 2-7 used for points, colorservice's colorsets limits other mapStyles
-            count: {
-                min: 2,
-                max: 7
-            },
-            // values recognized by the code (and geostats)
-            methods: ['jenks', 'quantile', 'equal', 'manual'],
-            // values recognized by the code (and geostats)
-            modes: ['distinct', 'discontinuous'],
-            // values recognized by the code (and geostats)
-            mapStyles: ['choropleth', 'points']
         },
         getRangeForPoints: function () {
             return { ...this.limits.count };
@@ -51,8 +50,12 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
         getAvailableMapStyles: function () {
             return [...this.limits.mapStyles];
         },
-        getLimits: function () {
-            return { ...this.limits };
+        getLimits: function (mapStyle, type) {
+            if (mapStyle === 'points') {
+                return { ...this.limits };
+            }
+            const count = this._colorService.getRange(type);
+            return { ...this.limits, count };
         },
         /**
          * Classifies given dataset.
@@ -82,59 +85,44 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
          * @param  {geostats} groupStats precalculated geostats | optional
          * @return {Object}               result with values and helper functions
          */
-        getClassification: function (indicatorData, options, groupStats) {
-            var me = this;
+        getClassification: function (indicatorData, opts, groupStats) {
             if (typeof indicatorData !== 'object') {
                 this.log.warn('Data expected as object with region/value as keys/values.');
                 return { error: 'noData' };
             }
-            var opts = me._validateOptions(options);
-            var list = me._getDataAsList(indicatorData);
-            var formatter = Oskari.getNumberFormatter(opts.fractionDigits);
-
-            if (me._hasNonNumericValues(list)) {
-                // geostats can handle this, but lets not support for now (gstats.getUniqueValues() used previously)
-                this.log.warn('Non-numeric data not supported for now');
-                return { error: 'noData' };
+            const dataAsList = Object.values(indicatorData);
+            if ((groupStats && groupStats.serie.length < 3) || dataAsList.length < 2) {
+                return { error: 'noEnough' };
             }
 
-            var stats = new geostats(list);
+            const { format } = Oskari.getNumberFormatter(opts.fractionDigits);
+            var stats = new geostats(dataAsList);
             stats.silent = true;
-            stats.setPrecision(opts.precision);
-
-            var response = {};
-
+            stats.setPrecision();
+            let bounds;
             const setBounds = (stats) => {
                 if (opts.method === 'jenks') {
                     // Luonnolliset välit
-                    response.bounds = stats.getClassJenks(opts.count);
-                    stats.setBounds(response.bounds);
-                    stats.setRanges();
+                    bounds = stats.getClassJenks(opts.count);
                 } else if (opts.method === 'quantile') {
                     // Kvantiilit
-                    response.bounds = stats.getQuantile(opts.count);
+                    bounds = stats.getQuantile(opts.count);
                 } else if (opts.method === 'equal') {
                     // Tasavälit
-                    response.bounds = stats.getEqInterval(opts.count);
+                    bounds = stats.getEqInterval(opts.count);
                 } else if (opts.method === 'manual') {
-                    const bounds = this.getBoundsFallback(opts.manualBounds, opts.count, stats.min(), stats.max());
-                    response.bounds = stats.setClassManually(bounds);
+                    bounds = this.getBoundsFallback(opts.manualBounds, opts.count, stats.min(), stats.max());
+                    stats.setBounds(bounds);
                 }
             };
 
             if (groupStats) {
-                if (groupStats.serie.length < 3) {
-                    return { error: 'noEnough' };
-                }
                 groupStats.silent = true;
-                groupStats.setPrecision(opts.precision);
-
                 if (opts.count >= groupStats.serie.length) {
                     opts.count = groupStats.serie.length - 1;
                 }
                 var groupOpts = groupStats.classificationOptions || {};
-                var calculateBounds =
-                    !groupStats.classificationOptions ||
+                const calculateBounds =
                     groupOpts.method !== opts.method ||
                     groupOpts.count !== opts.count ||
                     (opts.method === 'manual' && !Oskari.util.arraysEqual(groupStats.bounds, opts.manualBounds));
@@ -147,25 +135,49 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
                 }
                 // Set bounds manually.
                 stats.setBounds(groupStats.bounds);
-                stats.setRanges();
             } else {
-                if (list.length < 2) {
-                    return { error: 'noEnough' };
-                } else if (list.length === 2) {
-                    // TODO: these should be handled when options are edited
-                    opts.count = list.length;
-                    /* Switch method from jenks to quantile with value length of two since geostats.getClassJenks()
-                     * does not return bounds correctly with two values
-                     */
-                    if (opts.method === 'jenks') {
-                        opts.method = 'quantile';
-                    }
-                } else if (opts.count >= list.length) {
-                    opts.count = list.length - 1;
-                }
                 setBounds(stats);
             }
-            response.stats = {
+            if (bounds.some(bound => isNaN(bound))) {
+                this.log.warn('Failed to create bounds');
+                return { error: 'noEnough' };
+            }
+            const ranges = opts.mode === 'distinct'
+                ? this.getRangesFromBounds(bounds, opts, format)
+                : this.getValueRanges(dataAsList, bounds, format);
+            const colors = this._colorService.getColorsForClassification(opts);
+            const pixels = this.getPixelsForClassification(opts);
+
+            if (![colors, pixels, ranges].every(list => Array.isArray(list) && list.length === opts.count)) {
+                this.log.warn('Failed to create groups');
+                return { error: 'general' };
+            }
+            const groups = [];
+            for (let i = 0; i < opts.count; i++) {
+                const group = {
+                    sizePx: pixels[i],
+                    range: ranges[i],
+                    color: colors[i],
+                    regionIds: []
+                };
+                groups.push(group);
+            }
+
+            const getGroupForValue = value => {
+                const index = bounds.findIndex(bound => value < bound);
+                // first groups values is between bounds[0] and bounds[1]
+                return index === -1 ? bounds.length - 2 : index - 1;
+            };
+            for (const region in indicatorData) {
+                const value = indicatorData[region];
+                if (typeof value === 'undefined') {
+                    // no value for region -> skip
+                    continue;
+                }
+                var index = getGroupForValue(value);
+                groups[index].regionIds.push(region);
+            }
+            const statistics = {
                 min: stats.min(),
                 max: stats.max(),
                 sum: stats.sum(),
@@ -176,70 +188,15 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
                 stddev: stats.stddev(),
                 cov: stats.cov()
             };
-
-            let ranges;
-            if (opts.method === 'manual') {
-                ranges = this.getRangesFromBounds(response.bounds);
-            } else {
-                ranges = opts.mode === 'discontinuous' ? stats.getInnerRanges() : stats.getRanges();
-            }
-            // TODO: could use regionIds.length where count is needed
-            stats.doCount();
-            const counts = stats.counter;
-            const colors = this._colorService.getColorsForClassification(options);
-            const pixels = this.getPixelsForClassification(options);
-
-            if (![colors, pixels, counts, ranges].every(list => list.length === opts.count)) {
-                this.log.warn('Failed to create groups');
-                return { error: 'general' };
-            }
-            const groups = [];
-            for (let i = 0; i < opts.count; i++) {
-                const group = {
-                    sizePx: pixels[i],
-                    range: ranges[i],
-                    color: colors[i],
-                    count: counts[i],
-                    regionIds: []
-                };
-                groups.push(group);
-            }
-            for (const region in indicatorData) {
-                const value = indicatorData[region];
-                if (typeof value === 'undefined') {
-                    // no value for region -> skip
-                    continue;
-                }
-                var index = stats.getRangeNum(value);
-                groups[index].regionIds.push(region);
-            }
-            response.groups = groups;
-            response.format = formatter.format;
-
-            // TODO: could add bounds to groups (and maybe remove range as its parsed and formatted for legend)
-            this.lastUsedBounds = response.bounds;
-            var maxBounds = [];
-            if (response.bounds) {
-                // max bounds are calculated for color scale used in diagram
-                var values = stats.sorted();
-                var j = 1;
-                for (var i = 0; i < values.length; i++) {
-                    if (parseFloat(values[i]) > parseFloat(response.bounds[j])) {
-                        maxBounds.push(values[i]);
-                        j++;
-                    }
-                }
-            }
-            response.maxBounds = maxBounds;
-            response.getHtmlLegend = function (colors) {
-                // Choropleth  legend
-                return stats.getHtmlLegend(colors, '', true, formatter.format, opts.mode);
+            return {
+                groups,
+                bounds,
+                format,
+                stats: statistics
             };
-            return response;
         },
         getPixelsForClassification: function (classification, index) {
             const { min, max, count } = classification;
-
             const x = d3.scaleLinear()
                 .domain([0, count - 1])
                 .range([min, max]);
@@ -259,18 +216,48 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
             }
             return sizes;
         },
-        getRangesFromBounds: function (bounds) {
+
+        getRangeString: function (min, max) {
+            return `${min} - ${max}`;
+        },
+        getRangesFromBounds: function (bounds, opts, format) {
+            const { fractionDigits } = opts;
             const ranges = [];
             for (let i = 0; i < bounds.length - 1; i++) {
-                ranges.push(bounds[i] + ' - ' + bounds[i + 1]);
+                let min = bounds[i];
+                let max = bounds[i + 1];
+                const same = min === max;
+                max = format(max.toFixed(fractionDigits) - Math.pow(10, -fractionDigits));
+                if (same) {
+                    ranges.push(max);
+                } else {
+                    min = format(min);
+                    ranges.push(this.getRangeString(min, max));
+                }
+            }
+            return ranges;
+        },
+
+        getValueRanges: function (data, bounds, format) {
+            const sorted = [...data].sort((a, b) => a - b);
+            const ranges = [];
+            for (let i = 0; i < bounds.length - 1; i++) {
+                const max = bounds[i + 1];
+                console.log(max);
+                const min = bounds[i];
+                const values = sorted.filter(val => val >= min && val < max);
+                if (values.length === 0) {
+                    ranges[i] = '';
+                } else if (values.length === 1) {
+                    ranges[i] = format(values[0]);
+                } else {
+                    ranges[i] = this.getRangeString(format(values[0]), format(values[values.length - 1]));
+                }
             }
             return ranges;
         },
         getBoundsFallback: function (bounds, count, dataMin, dataMax) {
-            return this._tryKnownBounds(bounds, count, dataMin, dataMax) || equalSizeBands(count, dataMin, dataMax);
-        },
-        _tryKnownBounds: function (givenBounds, count, dataMin, dataMax) {
-            return this._tryBounds(givenBounds, count, dataMin, dataMax) || this._tryBounds(this.lastUsedBounds, count, dataMin, dataMax);
+            return this._tryBounds(bounds, count, dataMin, dataMax) || equalSizeBands(count, dataMin, dataMax);
         },
         _tryBounds: function (bounds, count, dataMin, dataMax) {
             if (!bounds) {
@@ -279,90 +266,9 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.ClassificationService',
             if (bounds[0] !== dataMin || bounds[bounds.length - 1] !== dataMax) {
                 return;
             }
-            const targetLength = count + 1;
-            if (bounds.length === targetLength) {
+            if (bounds.length === count + 1) {
                 return bounds.slice();
             }
-            if (bounds.length > targetLength) {
-                return bounds.slice(0, targetLength - 1).concat([dataMax]);
-            }
-
-            const extraNeeded = equalSizeBands(targetLength - bounds.length + 1, bounds[bounds.length - 2], dataMax);
-
-            return bounds
-                .slice(0, -2)
-                .concat(extraNeeded);
-        },
-        /**
-         * Validates and normalizes options
-         * @param  {Object} options options for classification
-         * @return {Object}         normalized options
-         */
-        // TODO: should validate earlier
-        _validateOptions: function (options) {
-            var opts = options || {};
-            opts.count = opts.count || 5;
-            opts.type = opts.type || 'seq';
-
-            var range = opts.mapStyle === 'points'
-                ? this.getRangeForPoints()
-                : this._colorService.getRange(opts.type, opts.mapStyle);
-            if (opts.count < range.min) {
-                // no need to classify if partitioning to less than 2 groups
-                throw new Error('Requires atleast ' + range.min + ' partitions. Count was ' + opts.count);
-            }
-            // TODO: can't update values on validate
-            if (opts.count > range.max) {
-                opts.count = range.max;
-            }
-            // maybe validate max count?
-            const methods = this.getAvailableMethods();
-            opts.method = opts.method || methods[0];
-            // method needs to be one of 'jenks', 'quantile', 'equal', 'manual'
-            if (methods.indexOf(opts.method) === -1) {
-                throw new Error('Requested method not allowed: ' + opts.method + '. Allowed values are: ' + methods.join());
-            }
-
-            // Jatkuva / epäjatkuva
-            const modes = this.getAvailableModes();
-            opts.mode = opts.mode || modes[0];
-            if (modes.indexOf(opts.mode) === -1) {
-                throw new Error('Requested mode not allowed: ' + opts.mode + '. Allowed values are: ' + modes.join());
-            }
-            return opts;
-        },
-        /**
-         * Transforms { key: value, key2 : null, key3: undefined, key4 : value2 } to [value, value2]
-         * @param  {Object} data input data object
-         * @return {Number[]}    values array
-         */
-        _getDataAsList: function (data) {
-            var list = [];
-            // object -> array and take out any missing values
-            for (var reg in data) {
-                var value = data[reg];
-                if (value !== undefined && value !== null) {
-                    list.push(value);
-                }
-            }
-            return list;
-        },
-        /**
-         * Determines if there are NaN-values in the input
-         * @param  {Array}  values Array of values
-         * @return {Boolean}       true if there are NaN values in the array
-         */
-        _hasNonNumericValues: function (values) {
-            var i, val, valLen;
-            for (i = 0, valLen = values.length; i < valLen; i += 1) {
-                val = values[i];
-                if (val !== undefined) {
-                    if (isNaN(val)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
         }
     }, {
         'protocol': ['Oskari.mapframework.service.Service']
