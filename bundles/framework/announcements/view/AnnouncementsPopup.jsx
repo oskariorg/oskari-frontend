00d7d8d16bb1b870f93c543bd3b18c96bcf2297b
okauppinen <olli.kauppinen@nls.fi>
2022-05-18 13:35:39 +0300

    move content to own files

--- bundles/framework/announcements/view/AnnouncementsPopup.jsx
+++ bundles/framework/announcements/view/AnnouncementsPopup.jsx
@@ -1,13 +1,12 @@
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { showPopup } from 'oskari-ui/components/window';
-import { Message, Pagination, Checkbox, Divider } from 'oskari-ui';
 import { Controller, LocaleProvider } from 'oskari-ui/util';
-import { SecondaryButton, PrimaryButton, ButtonContainer } from 'oskari-ui/components/buttons';
-import { AnnouncementsContent } from './';
+
 import { BUNDLE_KEY } from './Constants';
+import { PaginatedContent, SingleContent } from './';
 
 // Pop-up functionality for announcements
 
@@ -15,58 +14,6 @@ const Content = styled.div`
     margin: 12px 24px 24px;
     min-width: 300px;
 `;
-
-const StyledPagination = styled(Pagination)`
-    li.ant-pagination-next {
-        min-width: 0;
-    }
-`;
-
-const Announcement = ({
-    announcement,
-    controller,
-    count,
-    current,
-    dontShowAgain,
-    onClose
-}) => {
-    const onCheckboxChange = (e) => controller.setShowAgain(announcement.id, e.target.checked);
-    const onPageChange = (page) => controller.onPopupChange(page);
-    const isLast = current === count;
-
-    const itemRender = (current, type, originalElement) => {
-        if (type === 'prev') {
-            return <SecondaryButton type='previous'/>;
-        }
-        if (type === 'next') {
-            return isLast ? null : <PrimaryButton type='next' style={{ color: '#ffffff' }}/>;
-        }
-        return originalElement;
-    };
-
-    return (
-        <Fragment>
-            <AnnouncementsContent announcement={announcement}/>
-            <Divider/>
-            <Checkbox checked={dontShowAgain} onChange={onCheckboxChange}>
-                <Message messageKey='dontShow'/>
-            </Checkbox>
-            <ButtonContainer left>
-                <StyledPagination current={current} total={count} defaultPageSize={1} hideOnSinglePage onChange={onPageChange} itemRender={itemRender}/>
-                {isLast && <PrimaryButton type='close' onClick={() => onClose()}/>}
-            </ButtonContainer>
-        </Fragment>
-    );
-};
-
-Announcement.propTypes = {
-    announcement: PropTypes.object.isRequired,
-    controller: PropTypes.instanceOf(Controller).isRequired,
-    count: PropTypes.number.isRequired,
-    current: PropTypes.number.isRequired,
-    dontShowAgain: PropTypes.bool.isRequired,
-    onClose: PropTypes.func.isRequired
-};
 
 const getContent = (state, controller, onClose) => {
     const { showAsPopup, currentPopup = 1 } = state;
@@ -76,10 +23,11 @@ const getContent = (state, controller, onClose) => {
     }
     const dontShowAgain = state.dontShowAgain.includes(announcement.id);
     const { title } = Oskari.getLocalized(announcement.locale);
+    const PopupContent = showAsPopup.length === 1 ? SingleContent : PaginatedContent;
     const content = (
         <LocaleProvider value={{ bundleKey: BUNDLE_KEY }}>
             <Content>
-                <Announcement
+                <PopupContent
                     controller={controller}
                     announcement={announcement}
                     count={showAsPopup.length}
--- bundles/framework/announcements/view/index.js
+++ bundles/framework/announcements/view/index.js
@@ -2,3 +2,5 @@ export { showAnnouncementsPopup } from './AnnouncementsPopup';
 export { AnnouncementsCollapse } from './AnnouncementsCollapse';
 export { AnnouncementsContent } from './AnnouncementsContent';
 export { AnnouncementsHandler } from './AnnouncementsHandler';
+export { SingleContent } from './popup/SingleContent';
+export { PaginatedContent } from './popup/PaginatedContent';
