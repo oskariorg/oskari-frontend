a577038eee4baada3ec771e3c6810ac9843ec634
okauppinen <olli.kauppinen@nls.fi>
2022-05-20 11:25:01 +0300

    use Link from react components

--- bundles/framework/announcements/view/AnnouncementsContent.jsx
+++ bundles/framework/announcements/view/AnnouncementsContent.jsx
@@ -1,29 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Message } from 'oskari-ui';
-import styled from 'styled-components';
-import { SelectOutlined } from '@ant-design/icons';
-
-const LinkIcon = styled(SelectOutlined)`
-    margin-left: 6px;
-`;
-
-const Link = ({ url }) => (
-    <a href={url} target="_blank" rel="noreferrer noopener">
-        <Message messageKey={'externalLink'} />
-        <LinkIcon/>
-    </a>
-);
-Link.propTypes = {
-    url: PropTypes.string.isRequired
-};
+import { Message, Link } from 'oskari-ui';
 
 export const AnnouncementsContent = ({ announcement }) => {
     const { locale } = announcement;
     const { link, content } = Oskari.getLocalized(locale);
     if (link) {
         return (
-            <Link url ={link}/>
+            <Link url ={link}>
+                <Message messageKey={'externalLink'} />
+            </Link>
         );
     }
     return (
