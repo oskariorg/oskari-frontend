8d4a1352e0db1fc103125992c7d0832197956824
okauppinen <olli.kauppinen@nls.fi>
2022-05-20 11:21:35 +0300

    move formatData to oskari util

--- bundles/framework/announcements/service/util.js
+++ bundles/framework/announcements/service/util.js
@@ -1,20 +1,8 @@
 const RANGE_SEPARATOR = '\u2013';
-const TIME_OPTIONS = {
-    hour: '2-digit',
-    minute: '2-digit'
-};
-
-export const formatDate = (isoDateTime) => {
-    const dateTime = new Date(isoDateTime);
-    const date = dateTime.toLocaleDateString();
-    const time = dateTime.toLocaleTimeString([], TIME_OPTIONS);
-
-    return `${time} ${date}`;
-};
 
 export const getDateRange = (announcement) => {
-    const start = formatDate(announcement.beginDate);
-    const end = formatDate(announcement.endDate);
+    const start = Oskari.util.formatDate(announcement.beginDate);
+    const end = Oskari.util.formatDate(announcement.endDate);
     return start + RANGE_SEPARATOR + end;
 };
 
