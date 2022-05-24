c54f9055a970917d750af8ae9d20ad384f82d443
okauppinen <olli.kauppinen@nls.fi>
2022-05-24 17:27:32 +0300

    add tooltip for DeleteButton

--- src/react/components/buttons/DeleteButton.jsx
+++ src/react/components/buttons/DeleteButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Message, Confirm } from 'oskari-ui';
+import { Message, Confirm, Tooltip } from 'oskari-ui';
 import { SecondaryButton } from './';
 import { DeleteOutlined } from '@ant-design/icons';
 import { red } from '@ant-design/colors'
@@ -10,27 +10,34 @@ const DeleteIcon = styled(DeleteOutlined)`
     color: ${red.primary}
 `;
 
+const getMsg = key => <Message messageKey={key} bundleKey='oskariui'/>
+
 export const DeleteButton = ({ 
     onConfirm,
     icon,
-    title = <Message messageKey='messages.confirmDelete' bundleKey='oskariui'/>
+    tooltip = getMsg('buttons.delete'),
+    title = getMsg('messages.confirmDelete')
 }) => {
     const button = icon
         ? <DeleteIcon/>
         : <SecondaryButton danger type="delete"/>;
+    const placement = tooltip ? 'bottom' : 'top';
     return (
         <Confirm
             title={title}
             onConfirm={onConfirm}
-            okText={<Message messageKey='buttons.delete' bundleKey='oskariui'/>}
-            cancelText={<Message messageKey='buttons.cancel' bundleKey='oskariui'/>}
-            placement='top'>
-            {button}
+            okText={getMsg('buttons.delete')}
+            cancelText={getMsg('buttons.cancel')}
+            placement={placement}>
+            <Tooltip title={tooltip}>
+                {button}
+            </Tooltip>
         </Confirm>
     );
 };
 DeleteButton.propTypes = {
     onConfirm: PropTypes.func.isRequired,
     title: PropTypes.any,
+    tooltip: PropTypes.any,
     icon: PropTypes.bool
 };
