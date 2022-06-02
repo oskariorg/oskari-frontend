d3cbe2fecd3b33e2304b0059fb145eb39edb5ba0
okauppinen <olli.kauppinen@nls.fi>
2022-06-02 13:13:48 +0300

    add icon + button for DeleteButton and tooltip, disabled, type props

--- src/react/components/buttons/DeleteButton.jsx
+++ src/react/components/buttons/DeleteButton.jsx
@@ -1,36 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Message, Confirm } from 'oskari-ui';
+import { Message, Confirm, Button } from 'oskari-ui';
 import { SecondaryButton } from './';
 import { DeleteOutlined } from '@ant-design/icons';
 import { red } from '@ant-design/colors'
+import { Tooltip } from 'antd';
 
 const DeleteIcon = styled(DeleteOutlined)`
-    color: ${red.primary}
+    color: ${red.primary};
+    cursor: pointer;
 `;
 
+const getButton = type => {
+    if (type === 'icon') {
+        return <DeleteIcon/>;
+    }
+    if (type === 'button') {
+        return <Button><DeleteIcon/></Button>;
+    }
+    return <SecondaryButton danger type="delete"/>;
+};
+
+// TODO: update bundles to use type and remove icon
 export const DeleteButton = ({ 
     onConfirm,
     icon,
-    title = <Message messageKey='messages.confirmDelete' bundleKey='oskariui'/>
+    type = icon ? 'icon' : '',
+    title = <Message messageKey='messages.confirmDelete' bundleKey='oskariui'/>,
+    tooltip,
+    disabled
 }) => {
-    const button = icon
-        ? <DeleteIcon/>
-        : <SecondaryButton danger type="delete"/>;
+    const placement = tooltip ? 'bottom' : 'top';
     return (
         <Confirm
             title={title}
             onConfirm={onConfirm}
+            disabled={disabled}
             okText={<Message messageKey='buttons.delete' bundleKey='oskariui'/>}
             cancelText={<Message messageKey='buttons.cancel' bundleKey='oskariui'/>}
-            placement='top'>
-            {button}
+            placement={placement}>
+            <Tooltip title={tooltip}>
+                {getButton(type)}
+            </Tooltip>
         </Confirm>
     );
 };
 DeleteButton.propTypes = {
     onConfirm: PropTypes.func.isRequired,
-    title: PropTypes.any,
-    icon: PropTypes.bool
+    title: PropTypes.node,
+    icon: PropTypes.bool,
+    tooltip: PropTypes.node,
+    disabled: PropTypes.bool,
+    type: PropTypes.oneOf(['icon', 'button', 'text'])
 };
