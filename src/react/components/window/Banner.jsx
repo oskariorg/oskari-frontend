de284e6afc6235cbe702c3a9c55bef86e3cd5a4b
Aho Henri <vk585@maanmittauslaitos.fi>
2022-06-06 12:43:10 +0300

    Mobile optimization & color changes.

--- src/react/components/window/Banner.jsx
+++ src/react/components/window/Banner.jsx
@@ -3,30 +3,39 @@ import styled from 'styled-components';
 import { CloseCircleOutlined } from '@ant-design/icons';
 
 const StyledBanner = styled('div')`
-    position: absolute;
+    position: fixed;
     top: 0;
-    left: 270px;
-    right: 250px;
-    background-color: #fff0e2;
+    background-color: #fdf8d9;
     box-shadow: 0 5px 10px 0 #888888;
     height: auto;
     padding: 10px 15px 10px 15px;
+    left: 50%;
+    transform: translateX(-50%);
+    min-width: 950px;
+    @media only screen and (max-width: 1025px) {
+        min-width: 0;
+        width: 100%
+    }
+    z-index: 999999;
 `;
 
 const Container = styled('div')`
     display: flex;
     flex-direction: row;
     width: 100%;
+    @media only screen and (max-width: 1025px) {
+        flex-direction: column;
+    }
 `;
 
 const StyledTitle = styled('span')`
     font-weight: bold;
-    color: #5e2c00;
+    color: #3c3c3c;
 `;
 
 const CloseIcon = styled(CloseCircleOutlined)`
     cursor: pointer;
-    color: #5e2c00;
+    color: #3c3c3c;
     font-size: 18px;
     align-self: center;
 `;
@@ -41,6 +50,9 @@ const Actions = styled('div')`
     display: flex;
     flex-direction: column;
     margin: 0 20px 0 10px;
+    @media only screen and (max-width: 1025px) {
+        flex-direction: column;
+    }
 `;
 
 const Icon = styled('div')`
