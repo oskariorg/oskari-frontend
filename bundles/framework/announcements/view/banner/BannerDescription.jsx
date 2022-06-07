1c32a789b2abd28fb22365b72eec9ba173f0a1f6
okauppinen <olli.kauppinen@nls.fi>
2022-06-07 15:38:39 +0300

    add different id for popup opened from banner

--- bundles/framework/announcements/view/banner/BannerDescription.jsx
+++ bundles/framework/announcements/view/banner/BannerDescription.jsx
@@ -4,6 +4,10 @@ import { BUNDLE_KEY } from '../../constants';
 import { AnnouncementsContent } from '../AnnouncementsContent';
 import styled from 'styled-components';
 
+const POPUP_OPTIONS = {
+    id: BUNDLE_KEY + '-from-banner'
+};
+
 const StyledContent = styled.div`
     margin: 12px 24px 24px;
     min-width: 300px;
@@ -16,5 +20,5 @@ export const showBannerDescriptionPopup = (announcement, onClose) => {
         </StyledContent>
     );
     const { title } = Oskari.getLocalized(announcement.locale);
-    return showPopup(title, content, onClose, { id: BUNDLE_KEY });
+    return showPopup(title, content, onClose, POPUP_OPTIONS);
 };
