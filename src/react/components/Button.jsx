a5831b9879f1326fe663c83edc4e1404476dd4b3
Sami Mäkinen <sami.makinen@nls.fi>
2022-06-09 16:59:55 +0300

    Make button attach t_button class automatically

--- src/react/components/Button.jsx
+++ src/react/components/Button.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 import { Button as AntButton } from 'antd';
 import 'antd/es/button/style/index.js';
 
-export const Button = ({ children, ...other }) => (
-    <AntButton {...other}>{children}</AntButton>
-);
+export const Button = ({ children, className, ...other }) => {
+    let modifiedClass = className || '';
+    if (!modifiedClass.includes('t_button')) {
+        modifiedClass = 't_button ' + className;
+    }
+    return (<AntButton className={modifiedClass} {...other}>{children}</AntButton>);
+};
 
 Button.propTypes = {
     children: PropTypes.any
