5d984f57299b2f220f6995b83cf7a9f29e3c0c75
Sami Mäkinen <sami.makinen@nls.fi>
2022-06-15 11:45:28 +0300

    Remove lodash import

--- src/theming.js
+++ src/theming.js
@@ -1,6 +1,3 @@
-import merge from 'lodash/merge';
-import cloneDeep  from 'lodash/clonedeep';
-
 const DEFAULT_THEME = {
     color: {
         icon: '#3c3c3c',
@@ -9,26 +6,26 @@ const DEFAULT_THEME = {
     }
 };
 
-let theme = {
+let currentTheme = {
     ...DEFAULT_THEME
 };
-let  listeners = [];
+let listeners = [];
 
 export const THEMING = {
-    getTheme () {
-        return cloneDeep(theme);
+    getTheme() {
+        return cloneDeep(currentTheme);
     },
-    setTheme (newTheme = {}) {
+    setTheme(newTheme = {}) {
         // start with new object so we bust any memoized value for listeners and get a good value for reset as well
-        theme = merge({}, DEFAULT_THEME, newTheme);
-        listeners.forEach(l => l(theme));
+        currentTheme = merge({}, DEFAULT_THEME, newTheme);
+        listeners.forEach(l => l(currentTheme));
     },
     /**
      * 
      * @param {Function} listener function to call with new theme object if theme changes
      * @returns function to call for removing the listener (for cleanup)
      */
-    addListener (listener) {
+    addListener(listener) {
         if (typeof listener === 'function') {
             listeners.push(listener);
         }
@@ -36,4 +33,48 @@ export const THEMING = {
             listeners = [...listeners.filter(fn => fn !== listener)];
         }
     }
+};
+
+// simple deep cloning for theme purposes
+const cloneDeep = (original) => {
+    if (!original) {
+        return original;
+    }
+    const theClone = Array.isArray(original) ? [] : {};
+    for (const key in original) {
+        const value = original[key];
+        theClone[key] = (typeof value === "object") ? cloneDeep(value) : value;
+    }
+    return theClone;
+};
+
+// merge from https://thewebdev.info/2021/03/06/how-to-deep-merge-javascript-objects/
+const isObject = (item) => {
+    return (item && typeof item === 'object' && !Array.isArray(item));
+};
+
+const merge = (target, ...sources) => {
+    if (!sources.length) {
+        return target;
+    }
+    const source = sources.shift();
+
+    if (isObject(target) && isObject(source)) {
+        for (const key in source) {
+            if (isObject(source[key])) {
+                if (!target[key]) {
+                    Object.assign(target, {
+                        [key]: {}
+                    });
+                }
+                merge(target[key], source[key]);
+            } else {
+                Object.assign(target, {
+                    [key]: source[key]
+                });
+            }
+        }
+    }
+
+    return merge(target, ...sources);
 };
