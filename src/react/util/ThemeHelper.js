1e1a2cfe7863b92e142f94ad0c394213020f4ab2
Sami Mäkinen <sami.makinen@nls.fi>
2022-06-15 18:11:51 +0300

    Move theme related files to own folder

--- src/react/util/ThemeHelper.js
+++
@@ -1,9 +0,0 @@
-
-export const getHeaderTheme = (theme) => {
-    return {
-        getBgColor: () => theme.color.primary,
-        getAccentColor: () => theme.color.accent,
-        getToolIconColor: () => theme.color.icon,
-        getToolIconHoverColor: () => theme.color.accent
-    };
-};
