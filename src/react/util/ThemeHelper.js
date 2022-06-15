2bedf2c25392071a0fdfa25a2cfbc85b8bc1f07f
Sami Mäkinen <sami.makinen@nls.fi>
2022-06-15 13:10:24 +0300

    Use ThemeHelper so it's easier to change theme structure/build logic for getters

--- src/react/util/ThemeHelper.js
+++ src/react/util/ThemeHelper.js
@@ -2,6 +2,7 @@
 export const getHeaderTheme = (theme) => {
     return {
         getBgColor: () => theme.color.primary,
+        getAccentColor: () => theme.color.accent,
         getToolIconColor: () => theme.color.icon,
         getToolIconHoverColor: () => theme.color.accent
     };
