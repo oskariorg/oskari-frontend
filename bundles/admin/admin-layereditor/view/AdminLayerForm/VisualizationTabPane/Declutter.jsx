cd9f720833dd2a1fd5921101ed255f5f1c68be71
Aho Henri <vk585@maanmittauslaitos.fi>
2022-09-06 12:08:21 +0300

    Remove state.

--- bundles/admin/admin-layereditor/view/AdminLayerForm/VisualizationTabPane/Declutter.jsx
+++ bundles/admin/admin-layereditor/view/AdminLayerForm/VisualizationTabPane/Declutter.jsx
@@ -1,20 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Controller } from 'oskari-ui/util';
 import { Message, Switch } from 'oskari-ui';
 import { StyledFormField, SpacedLabel } from '../styled';
 
 export const Declutter = ({ layer, controller }) => {
-    const [checked, setChecked] = useState(layer.options && layer.options.declutter ? layer.options.declutter : false);
-    const toggle = checked => {
-        controller.toggleDeclutter(checked);
-        setChecked(checked);
-    };
-
+    const options = layer.options || {};
+    const value = options.declutter || false;
     return (
         <StyledFormField>
             <label>
-                <Switch size='small' checked={checked} onChange={checked => toggle(checked)} />
+                <Switch size='small' checked={value} onChange={checked => controller.toggleDeclutter(checked)} />
                 <Message messageKey='fields.declutter' LabelComponent={SpacedLabel} />
             </label>
         </StyledFormField>
