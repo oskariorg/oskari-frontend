e3439f7eb66dbbaeda138e36279832baee9ba2e1
Sami Mäkinen <zakarfin@gmail.com>
2022-09-07 17:25:04 +0300

    Lint fixes

--- bundles/mapping/mapmodule/AbstractMapModule.js
+++ bundles/mapping/mapmodule/AbstractMapModule.js
@@ -2677,13 +2677,13 @@ Oskari.clazz.define(
                     if (layer.getMaxScale() || layer.getMinScale()) {
                         const layerResolutions = me.calculateLayerResolutions(layer.getMaxScale(), layer.getMinScale());
                         const minRes = layerResolutions[0];
-                        const maxRes = layerResolutions[layerResolutions.length - 1]
+                        const maxRes = layerResolutions[layerResolutions.length - 1];
                         // only set if limiting
                         if (mapResolutions[0] !== layerResolutions[0]) {
                             layerObject.minZoom = mapResolutions.indexOf(minRes);
                         }
                         if (mapResolutions[mapResolutions.length - 1] !== layerResolutions[layerResolutions.length - 1]) {
-                            layerObject.maxZoom = mapResolutions.indexOf(maxRes);;
+                            layerObject.maxZoom = mapResolutions.indexOf(maxRes);
                         }
                     };
 
