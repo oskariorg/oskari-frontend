98876099bd99deeb934be7b5fd7b51821d8beabe
Sami Mäkinen <sami.makinen@nls.fi>
2022-09-19 14:10:36 +0300

    Testing out theme colored metadata-icon

--- src/react/components/icons/Metadata.jsx
+++ src/react/components/icons/Metadata.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { InfoCircleOutlined } from '@ant-design/icons';
-import { Tooltip } from 'oskari-ui';
+import { ThemeConsumer } from '../../util';
 import styled from 'styled-components';
 
+const getThemeColors = theme => {
+    let color = theme?.color?.accent;
+    if (!color || Oskari.util.isLightColor(color)) {
+        // default to light blue if:
+        // - accent color is missing
+        // - color is too light to be readable
+        return {
+            main: '#0290ff',
+            hover: 'rgba(24,144,255, 0.25)'
+        };
+    }
+    // hover = accent with 25% opacity
+    const components = Oskari.util.hexToRgb(color);
+    return {
+        main: color,
+        //hover
+        hover: `rgba(${components.r},${components.g},${components.b}, 0.25)`
+    };
+};
 
 const StyledMetadataIcon = styled(InfoCircleOutlined)`
     cursor: pointer;
-    color: #0290ff;
+    color: ${props => props.theme.main};
     border-radius: 50%;
     &:hover {
-        background-color: rgba(24,144,255, 0.25);
+        background-color: ${props => props.theme.hover};
     }
 `;
 
@@ -20,24 +39,25 @@ const StyledMetadataIcon = styled(InfoCircleOutlined)`
  * @param {Object} style Additional styles
  * @returns 
  */
-export const Metadata = ({ metadataId, size = 16, style }) => {
-
-    if (!metadataId) return null;
+export const Metadata = ThemeConsumer(({ metadataId, size = 16, style, theme }) => {
+    if (!metadataId) {
+        return null;
+    }
 
     const onClick = () => {
-        Oskari.getSandbox().postRequestByName('catalogue.ShowMetadataRequest', [
-            {uuid: metadataId}
-    ]);
+        Oskari.getSandbox()
+            .postRequestByName('catalogue.ShowMetadataRequest', [{ uuid: metadataId }]);
     };
 
     return (
         <StyledMetadataIcon
             className='t_icon t_metadata'
+            theme={getThemeColors(theme)}
             style={{ fontSize: `${size}px`, ...style }}
             onClick={onClick}
         />
     );
-};
+});
 
 Metadata.propTypes = {
     metadataId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
