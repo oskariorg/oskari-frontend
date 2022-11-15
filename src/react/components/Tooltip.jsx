bb1a118eebf01748b0a48b445175b5c194ef0af3
Aho Henri <vk585@maanmittauslaitos.fi>
2022-11-15 13:35:54 +0200

    Hide tooltip when hovering over the tooltip.

--- src/react/components/Tooltip.jsx
+++ src/react/components/Tooltip.jsx
@@ -26,7 +26,7 @@ There seems to be a problem with tooltips with styled-components as direct child
 https://stackoverflow.com/questions/61450739/understanding-warning-function-components-cannot-be-given-refs
 */
 export const Tooltip = ({children, ...restOfProps}) => {
-    return (<AntTooltip {...restOfProps}>
+    return (<AntTooltip overlayStyle={{ pointerEvents: 'none' }} {...restOfProps}>
         <span>{children}</span>
     </AntTooltip>);
 };
