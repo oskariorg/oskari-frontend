258deda858e87d503d29bbed860a91062149b746
Sami Mäkinen <sami.makinen@nls.fi>
2022-11-29 16:07:26 +0200

    Remove ref to #contentMap and remove unnecessary cleanup calls

--- bundles/framework/publisher2/view/PublisherSidebar.js
+++ bundles/framework/publisher2/view/PublisherSidebar.js
@@ -20,7 +20,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.publisher2.view.PublisherSidebar
         me.data = data;
         me.panels = [];
         me.instance = instance;
-        // basic_publisher needs a wrapper div for styles to work properly
+        // basic_publisher needs an extra wrapper-div for styles to work properly
         me.template = jQuery(
             '<div><div class="basic_publisher">' +
             '  <div class="header">' +
@@ -617,18 +617,5 @@ Oskari.clazz.define('Oskari.mapframework.bundle.publisher2.view.PublisherSidebar
          */
         destroy: function () {
             this.mainPanel.remove();
-            // Resets map position and size to cover the whole space. Maybe find another way to do this?
-            jQuery('#contentMap').width('');
-            jQuery('.oskariui-left')
-                .css({
-                    'width': '',
-                    'height': '',
-                    'float': ''
-                })
-                .empty();
-            jQuery('.oskariui-center').css({
-                'width': '100%',
-                'float': ''
-            });
         }
     });
