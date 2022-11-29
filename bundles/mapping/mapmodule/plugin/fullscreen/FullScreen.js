c4ea1f0e857e72696ebcdc679734a0f9cd2af487
Sami Mäkinen <sami.makinen@nls.fi>
2022-11-29 16:56:34 +0200

    Use React button as base

--- bundles/mapping/mapmodule/plugin/fullscreen/FullScreen.js
+++ bundles/mapping/mapmodule/plugin/fullscreen/FullScreen.js
@@ -1,4 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FullscreenOutlined, FullscreenExitOutlined } from '@ant-design/icons';
+import { MapModuleButton } from '../../MapModuleButton';
+import styled from 'styled-components';
+
 import './request/ToggleFullScreenControlRequest';
+
+// Icon is too small with defaults (18x18px)
+const StyledButton = styled(MapModuleButton)`
+> span {
+    font-size: 22px;
+    max-height: 22px;
+    max-width: 22px;
+    > svg {
+        max-height: 22px;
+        max-width: 22px;
+    }
+}
+`;
 
 /**
  * @class Oskari.mapframework.bundle.mapmodule.plugin.FullScreenPlugin
@@ -19,6 +38,9 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.plugin.FullScreenPlugi
         me._element = null;
         me.state = {};
         me._sandbox = null;
+        me._templates = {
+            plugin: jQuery('<div class="mapplugin fullscreen"></div>')
+        };
     },
     {
         /**
@@ -28,18 +50,7 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.plugin.FullScreenPlugi
          * Plugin jQuery element
          */
         _createControlElement: function () {
-            const el = jQuery(
-                    '<div class="mapplugin fullscreenDiv">' +
-                    '<img class="fullscreenDivImg" src="' + this.getImagePath('hide-navigation.png') + '"></img>' +
-                    '</div>'
-                );
-            el.find('.fullscreenDivImg').on('click', (event) => {
-                event.preventDefault();
-                this.setState({
-                    fullscreen: !this.state.fullscreen
-                });
-            });
-            return el;
+            return this._templates.plugin.clone();
         },
         /**
          * @method _startPluginImpl
@@ -52,13 +63,93 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.plugin.FullScreenPlugi
             return this.setVisible(this._visible);
         },
         /**
+         * @public @method refresh
+         */
+        refresh: function () {
+            const el = this.getElement();
+            if (!el) {
+                return;
+            }
+            const isFullscreen = !!this.state.fullscreen;
+            ReactDOM.render(
+                <StyledButton
+                    className='t_fullscreen'
+                    styleName={this.getStyleForRender()}
+                    icon={isFullscreen ? <FullscreenExitOutlined /> : <FullscreenOutlined />}
+                    iconActive={isFullscreen}
+                    onClick={() => {
+                        if (!this.inLayerToolsEditMode()) {
+                            this.setState({
+                                fullscreen: !isFullscreen
+                            });
+                        }
+                    }}
+                    position={this.getLocation()}
+                />
+                ,
+                el[0]
+            );
+        },
+        
+        getStyleForRender: function() {
+            const conf = this.getConfig();
+            let toolStyle;
+            // Change the style if in the conf
+            if (conf && conf.toolStyle) {
+                toolStyle = conf.toolStyle;
+            } else {
+                toolStyle = this.getToolStyleFromMapModule();
+            }
+            return toolStyle || 'rounded-dark';
+        },
+
+        /**
+         * @public @method changeToolStyle
+         * Changes the tool style of the plugin
+         *
+         * @param {Object} style
+         * @param {jQuery} div
+         *
+         */
+         changeToolStyle: function (style, div) {
+            const conf = this.getConfig();
+            // Change the style if in the conf
+            if (style) {
+                conf.toolStyle = style;
+            }
+            this.refresh();
+        },
+        /**
+         * Handle plugin UI and change it when desktop / mobile mode
+         * @method  @public createPluginUI
+         * @param  {Boolean} mapInMobileMode is map in mobile mode
+         * @param {Boolean} forced application has started and ui should be rendered with assets that are available
+         */
+        redrawUI: function (mapInMobileMode, forced) {
+            if (!this.isVisible() || !this.isEnabled()) {
+                // no point in drawing the ui if we are not visible or enabled
+                return;
+            }
+
+            this.teardownUI();
+
+            this.inMobileMode = mapInMobileMode;
+
+            this._element = this._createControlElement();
+            this.refresh();
+            this.addToPluginContainer(this.getElement());
+        },
+        teardownUI: function () {
+            this.removeFromPluginContainer(this.getElement());
+        },
+        /**
          * @method _stopPluginImpl
          *
          * @param {Oskari.mapframework.sandbox.Sandbox} sandbox
          *          reference to application sandbox
          */
         _stopPluginImpl: function (sandbox) {
-            this.removeFromPluginContainer(this.getElement());
+            this.teardownUI();
         },
 
         /**
@@ -91,25 +182,16 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapmodule.plugin.FullScreenPlugi
             } else {
                 this._showNavigation();
             }
+            this.refresh();
         },
         getState: function () {
             return this.state;
         },
         _showNavigation: function () {
-            const el = this.getElement();
-            if (!el) {
-                return;
-            }
-            el.find('.fullscreenDivImg').attr('src', this.getImagePath('hide-navigation.png'));
-            Oskari.dom.getMapContainerEl().classList.remove('oskari-map-window-fullscreen')
+            Oskari.dom.getMapContainerEl().classList.remove('oskari-map-window-fullscreen');
         },
         _hideNavigation: function () {
-            const el = this.getElement();
-            if (!el) {
-                return;
-            }
-            el.find('.fullscreenDivImg').attr('src', this.getImagePath('show-navigation.png'));
-            Oskari.dom.getMapContainerEl().classList.add('oskari-map-window-fullscreen')
+            Oskari.dom.getMapContainerEl().classList.add('oskari-map-window-fullscreen');
         }
     },
     {
