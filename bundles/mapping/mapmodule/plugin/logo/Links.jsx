933fef09b035fc8f016d2bd72d5b748189920703
Sami Mäkinen <sami.makinen@nls.fi>
2023-01-05 17:39:54 +0200

    With src the previous overrides don't work anyway (background-image -> content required)

--- bundles/mapping/mapmodule/plugin/logo/Links.jsx
+++ bundles/mapping/mapmodule/plugin/logo/Links.jsx
@@ -25,17 +25,14 @@ export const Links = ({ links }) => {
     const labels = [];
     links.forEach(link => {
         const callback = typeof link.options.callback === 'function' ? link.options.callback : null;
-        if (link.options.id !== 'icon') {
+        if (link.options.id !== 'logo') {
             labels.push(
                 <NonIconLink key={link.options.id} className={link.options.id && link.options.id.toLowerCase()}><a onClick={callback ? callback : null}>{link.title}</a></NonIconLink>
             );
         } else {
             if (link.options.src) {
-                // additional classes:
-                // - icon (link.options.id) for compatibility for overrides <2.10
-                // - logo for an additional identifier and override enabler for 2.10+
                 labels.push(
-                    <StyledLogo key={link.options.id} className={link.options.id + ' logo'} src={link.options.src} onClick={callback ? callback : null} />
+                    <StyledLogo key={link.options.id} className={link.options.id} src={link.options.src} onClick={callback ? callback : null} />
                 );
             } else {
                 labels.push(
