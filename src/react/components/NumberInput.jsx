c0cd406e89e6f68b7426f425c67187efe153f080
Aho Henri <vk585@maanmittauslaitos.fi>
2023-01-16 11:46:32 +0200

    Move validation to NumberInput.

--- src/react/components/NumberInput.jsx
+++ src/react/components/NumberInput.jsx
@@ -3,5 +3,12 @@ import { InputNumber } from 'antd';
 import 'antd/es/input-number/style/index.js';
 
 export const NumberInput = (props) => (
-    <InputNumber {...props} />
+    <InputNumber
+        onKeyDown={(e) => {
+            if (!/^[0-9\b]+$/.test(e.key) && e.key !== 'Backspace') {
+                e.preventDefault();
+            }
+        }}
+        {...props}
+    />
 );
