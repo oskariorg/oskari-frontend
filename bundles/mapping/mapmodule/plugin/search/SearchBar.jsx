0286ab47082b315af1279be164ed806c2c534993
Sami Mäkinen <zakarfin@gmail.com>
2023-02-28 23:46:14 +0200

    Make search bar a bit bigger

--- bundles/mapping/mapmodule/plugin/search/SearchBar.jsx
+++ bundles/mapping/mapmodule/plugin/search/SearchBar.jsx
@@ -9,16 +9,16 @@ import { getNavigationTheme } from 'oskari-ui/theme';
 // this would make the clear cross be closer to edge:
 // padding: 4px 0px 4px 11px;
 const StyledInput = styled(TextInput)`
-    height: 24px;
-    border-radius: calc(${props => props.rounding ? props.rounding.replace('%', '') / 100 : 0} * 24px);
+    height: 35px;
+    border-radius: calc(${props => props.rounding ? props.rounding.replace('%', '') / 100 : 0} * 35px);
 `;
 
 // filtering class means that the search uses other channels than what are used by default
 // -> highlight the button
 const StyledButton = styled(Button)`
     margin-left: 7px;
-    height: 24px;
-    border-radius: calc(${props => props.$rounding ? props.$rounding.replace('%', '') / 100 : 0} * 24px);
+    height: 35px;
+    border-radius: calc(${props => props.$rounding ? props.$rounding.replace('%', '') / 100 : 0} * 35px);
     background: ${props => props.$backgroundColor};
     color: ${props => props.$iconColor};
     border: none;
@@ -49,14 +49,21 @@ const StyledButton = styled(Button)`
 const SearchContainer = styled('div')`
     margin: 0 10px 10px 10px;
     display: flex;
-    height: 32px;
+    height: 40px;
     flex-direction: row;
     opacity: ${props => props.opacity};
     background: ${props => props.backgroundColor};
     align-items: center;
     padding: 3px 7px 3px 3px;
-    border-radius: calc(${props => props.rounding ? props.rounding.replace('%', '') / 100 : 0} * 32px);
+    border-radius: calc(${props => props.rounding ? props.rounding.replace('%', '') / 100 : 0} * 40px);
     box-shadow: 1px 1px 2px rgb(0 0 0 / 60%);
+
+    .ant-btn-icon-only > * {
+        font-size: 20px;
+    }
+    input {
+        font-size: 15px;
+    }
 `;
 
 export const SearchBar = ThemeConsumer(({ theme = {}, disabled = false, placeholder, state = {}, controller }) => {
