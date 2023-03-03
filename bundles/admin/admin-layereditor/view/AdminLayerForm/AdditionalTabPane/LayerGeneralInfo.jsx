74461fe4b5aa57071b0f964d94747dff9e9b29ba
okauppinen <olli.kauppinen@nls.fi>
2023-03-03 12:14:46 +0200

    use util date formatter

--- bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/LayerGeneralInfo.jsx
+++ bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/LayerGeneralInfo.jsx
@@ -4,46 +4,20 @@ import { Message } from 'oskari-ui';
 import { Row, Col } from 'antd';
 import styled from 'styled-components';
 
-
 const StyledRoot = styled('div')`
     padding-bottom: 20px;
 `;
 
 const dateLocale = 'fi-FI'; // we are not using here other locales than Finnish so we can hard core it into constant variable
-const localeDateOptions = {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit'
-};
-
-const formatTimestamp = (timestamp) => {
-    let date;
-    if (typeof timestamp !== 'undefined') {
-        date = new Date(timestamp);
+const dateOptions = {
+    time: {
+        second: '2-digit'
     }
-    return {
-        date: formatDate(date),
-        time: formatTime(date)
-    };
-};
-
-const formatDate = (date) => {
-    if (typeof date === 'undefined') {
-        return '--.--.----';
-    }
-    return date.toLocaleDateString(dateLocale, localeDateOptions);
-};
-
-const formatTime = (date) => {
-    if (typeof date === 'undefined') {
-        return '--:--:--';
-    }
-    return date.toLocaleTimeString(dateLocale).replace(/\./g, ':');
 };
 
 export const LayerGeneralInfo = ({ layer }) => {
-    const created = formatTimestamp(layer.created);
-    const updated = formatTimestamp(layer.updated);
+    const created = Oskari.util.formatDate(layer.created, dateOptions, dateLocale);
+    const updated = Oskari.util.formatDate(layer.updated, dateOptions, dateLocale);
 
     return (
         <StyledRoot>
@@ -53,11 +27,11 @@ export const LayerGeneralInfo = ({ layer }) => {
             </Row>
             <Row>
                 <Col span={ 12 }><Message messageKey='fields.created' /></Col>
-                <Col span={ 12 }>{ created.date } { created.time }</Col>
+                <Col span={ 12 }>{ created }</Col>
             </Row>
             <Row>
                 <Col span={ 12 }><Message messageKey='fields.updated' /></Col>
-                <Col span={ 12 }>{ updated.date } { updated.time }</Col>
+                <Col span={ 12 }>{ updated }</Col>
             </Row>
         </StyledRoot>
     );
