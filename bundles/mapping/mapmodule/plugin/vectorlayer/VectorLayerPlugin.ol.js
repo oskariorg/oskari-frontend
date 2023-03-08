0ec1fada885b9657bfd383d7bf8b95e13c3c7f20
Sami Mäkinen <sami.makinen@nls.fi>
2023-03-08 18:46:20 +0200

    Add names used by mapmodule

--- bundles/mapping/mapmodule/plugin/vectorlayer/VectorLayerPlugin.ol.js
+++ bundles/mapping/mapmodule/plugin/vectorlayer/VectorLayerPlugin.ol.js
@@ -69,6 +69,7 @@ Oskari.clazz.define(
         this._log = Oskari.log('VectorLayerPlugin');
         this._styleCache = {};
         this._animatingFeatures = {};
+        this._name = 'VectorLayerPlugin';
     }, {
         __name: 'Oskari.mapframework.mapmodule.VectorLayerPlugin',
         /**
