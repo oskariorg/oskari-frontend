1a270470e484c6496fda80ce1ab375ef63113c86
Sami Mäkinen <sami.makinen@nls.fi>
2023-03-15 12:12:00 +0200

    Move featuredata tool from publisher to featuredata

--- packages/framework/bundle/publisher2/bundle.js
+++ packages/framework/bundle/publisher2/bundle.js
@@ -139,10 +139,6 @@ Oskari.clazz.define("Oskari.mapframework.bundle.publisher2.PublisherBundle", fun
         },
         {
             "type": "text/javascript",
-            "src": "../../../../bundles/framework/publisher2/tools/FeaturedataTool.js"
-        },
-        {
-            "type": "text/javascript",
             "src": "../../../../bundles/framework/publisher2/tools/CrosshairTool.js"
         },
         {
