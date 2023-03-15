84175710ea6a4eac92d74dbcd670b209e1e594d8
okauppinen <olli.kauppinen@nls.fi>
2023-03-15 14:13:40 +0200

    wrap more than one style to collection

--- bundles/mapping/userstyle/view/UserStylesTab/UserStylesList.jsx
+++ bundles/mapping/userstyle/view/UserStylesTab/UserStylesList.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Message } from 'oskari-ui';
 import { IconButton } from 'oskari-ui/components/buttons';
 import { Table, getSorterFor, ToolsContainer } from 'oskari-ui/components/Table';
+
+const StyledCount = styled.span`
+    font-style: italic;
+`;
 
 export const UserStylesList = ({ controller, data = [], loading }) => {
     const columnSettings = [
@@ -11,13 +16,17 @@ export const UserStylesList = ({ controller, data = [], loading }) => {
             title: <Message messageKey='tab.grid.layer' />,
             dataIndex: 'layer',
             sorter: getSorterFor('layer'),
-            defaultSortOrder: 'ascend'
+            defaultSortOrder: 'ascend',
+            render: (value, { layerId, id }) => <a onClick={() => controller.addLayerToMap(layerId, id)}>{value}</a>
         },
         {
             align: 'left',
             title: <Message messageKey='tab.grid.name' />,
             dataIndex: 'name',
-            sorter: getSorterFor('name')
+            sorter: getSorterFor('name'),
+            render: (value, { count }) => count
+                ? <StyledCount>(<Message messageKey='tab.styleCount' messageArgs={{ count }}/>)</StyledCount>
+                : value
         },
         {
             align: 'left',
@@ -39,15 +48,20 @@ export const UserStylesList = ({ controller, data = [], loading }) => {
             align: 'left',
             title: <Message messageKey='tab.grid.actions' />,
             dataIndex: 'id',
-            width: 150,
-            render: (id) => {
+            width: 80,
+            render: (id, { count, layerId }) => {
+                const isCollection = count > 1;
+                const onEdit = isCollection ? () => controller.showLayerStyles(layerId) : () => controller.showStyleEditor(id);
+                const deleteTitle = isCollection ? <Message messageKey='tab.styleCount' messageArgs={{ count }}/> : undefined;
                 return (
                     <ToolsContainer>
                         <IconButton
                             type='edit'
-                            onClick={() => controller.showStyleEditor(id) }
+                            onClick={onEdit}
                         />
                         <IconButton
+                            disabled={isCollection}
+                            title={deleteTitle}
                             type='delete'
                             onConfirm={() => controller.deleteStyle(id) }
                         />
@@ -61,7 +75,7 @@ export const UserStylesList = ({ controller, data = [], loading }) => {
         <Table
             columns={columnSettings}
             dataSource={data.map((item) => ({
-                key: item.id,
+                key: item.layerId,
                 ...item
             }))}
             pagination={false}
