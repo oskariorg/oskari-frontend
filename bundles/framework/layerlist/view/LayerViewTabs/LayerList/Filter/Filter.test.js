bf14d90c5957a081555f488deea9023227bad9ed
Aho Henri <vk554@maanmittauslaitos.fi>
2023-06-01 16:34:22 +0300

    Fix test.

--- bundles/framework/layerlist/view/LayerViewTabs/LayerList/Filter/Filter.test.js
+++ bundles/framework/layerlist/view/LayerViewTabs/LayerList/Filter/Filter.test.js
@@ -3,8 +3,8 @@ import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
 import { Filter } from './Filter';
 import { testFilters } from './Filter.test.util';
-import { Controller, LocaleProvider } from 'oskari-ui/util';
-import { Select, Option } from 'oskari-ui';
+import { Controller, LocaleProvider, ThemeProvider } from 'oskari-ui/util';
+import { Option } from 'oskari-ui';
 import { getBundleInstance } from '../../test.util';
 
 // from https://github.com/ant-design/ant-design/blob/4.3.1/components/select/__tests__/index.test.js
@@ -23,7 +23,9 @@ describe('<Filter/>', () => {
 
     const wrapper = mount(
         <LocaleProvider value={{ bundleKey: instance.getName() }}>
-            <Filter filters={testFilters} activeFilterId={testFilters[1].id} controller={controller}/>
+            <ThemeProvider>
+                <Filter filters={testFilters} activeFilterId={testFilters[1].id} controller={controller}/>
+            </ThemeProvider>
         </LocaleProvider>
     );
 
@@ -31,7 +33,7 @@ describe('<Filter/>', () => {
         expect.assertions(3);
 
         // expect(wrapper.html()).toBe('uncomment to show html');
-        const select = wrapper.find(Select);
+        const select = wrapper.find('.t_filter');
         expect(wrapper.find(Filter).exists()).toBe(true);
 
         expect(select.exists()).toBe(true);
