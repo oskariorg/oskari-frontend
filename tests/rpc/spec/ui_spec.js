711bbec1c618b0adc77dbcc15b32dd097657e313
AleksiKaa <allu.kaariainen@gmail.com>
2023-07-12 12:53:28 +0300

    format spec with prettier

--- tests/rpc/spec/ui_spec.js
+++ tests/rpc/spec/ui_spec.js
@@ -1,29 +1,29 @@
-describe('Ui', function(){
+describe('Ui', function () {
 
-    beforeEach(function(done) {
-        channel.onReady(function() {
-            channel.resetState(function() {
+    beforeEach(function (done) {
+        channel.onReady(function () {
+            channel.resetState(function () {
                 counter = 0;
                 done();
             });
         });
     });
 
-    afterEach(function() {
+    afterEach(function () {
         // Spy callback.
         expect(counter).toEqual(1);
     });
 
-    describe('Show a progress spinner', function(){
+    describe('Show a progress spinner', function () {
 
-        it("Adds progress spinner", function(done) {
+        it("Adds progress spinner", function (done) {
             channel.postRequest('ShowProgressSpinnerRequest', [true]);
             channel.log('ShowProgressSpinnerRequest true done.');
             counter++;
             done();
         });
 
-        it("Removes progress spinner", function(done) {
+        it("Removes progress spinner", function (done) {
             channel.postRequest('ShowProgressSpinnerRequest', [false]);
             channel.log('ShowProgressSpinnerRequest false done.');
             counter++;
@@ -31,9 +31,9 @@ describe('Ui', function(){
         });
     });
 
-    describe('Show or hide infobox', function(){
+    describe('Show or hide infobox', function () {
 
-        it("Shows infobox", function(done) {
+        it("Shows infobox", function (done) {
             // Adds custom infobox.
             channel.postRequest('InfoBox.ShowInfoBoxRequest', myInfoBox);
             channel.log("ShowInfoBoxRequest done.");
@@ -41,9 +41,9 @@ describe('Ui', function(){
             done();
         });
 
-        it("Hides infobox", function(done) {
+        it("Hides infobox", function (done) {
             // Add event handler
-            handleEvent('InfoBox.InfoBoxEvent', function(data) {
+            handleEvent('InfoBox.InfoBoxEvent', function (data) {
                 channel.log('InfoBox.InfoBoxEvent launched!');
                 expect(data.id).toEqual(infoboxId);
                 expect(data.isOpen).toEqual(false);
@@ -57,48 +57,47 @@ describe('Ui', function(){
         });
     });
 
-    describe('Send UI event', function(){
+    describe('Send UI event', function () {
         // Toggles coordinatetool visible.
         // Toggles the map's center crosshair.
         using("sendUIEvent",
             [
-                    'coordinatetool',
-                    'mapmodule.crosshair'
+                'coordinatetool',
+                'mapmodule.crosshair'
             ],
             function (UIEvent) {
-                
-                    it("toggles on", function(done) {
-                        channel.sendUIEvent([UIEvent], function(data) {
-                            expect(data).toEqual(true);
-                            channel.log('sendUIEvent: ', data);
-                            counter++;
-                            done();
-                        });
+
+                it("toggles on", function (done) {
+                    channel.sendUIEvent([UIEvent], function (data) {
+                        expect(data).toEqual(true);
+                        channel.log('sendUIEvent: ', data);
+                        counter++;
+                        done();
                     });
-                    
-                    // toggle off repeats the same call
-                    it("toggles off", function(done) {
-                        channel.sendUIEvent([UIEvent], function(data) {
-                            expect(data).toEqual(true);
-                            channel.log('sendUIEvent: ', data);
-                            counter++;
-                            done();
-                        });
+                });
+
+                // toggle off repeats the same call
+                it("toggles off", function (done) {
+                    channel.sendUIEvent([UIEvent], function (data) {
+                        expect(data).toEqual(true);
+                        channel.log('sendUIEvent: ', data);
+                        counter++;
+                        done();
                     });
-                }
+                });
+            }
         );
     });
 
-    describe('Set cursor style', function(){
+    describe('Set cursor style', function () {
 
         all("Cursor style is visible",
 
-            cursorStyles
-            ,
-            function(cursorStyles, done) {
+            cursorStyles,
+            function (cursorStyles, done) {
                 // Sets the cursor style on map. The value can be any valid css cursor value. 
                 // Not all possible values are supported by all browsers.
-                channel.setCursorStyle([cursorStyles], function(data) {
+                channel.setCursorStyle([cursorStyles], function (data) {
                     channel.log('setCursorStyle: ', data);
                     counter++;
                     done();
