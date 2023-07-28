a2db63e7c7770b0fc4d8a3265511b1c6e018278b
okauppinen <olli.kauppinen@nls.fi>
2023-07-28 23:29:17 +0300

    use accordion for formatters

--- bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/VectorLayerAttributes/PropertiesFormatter.jsx
+++ bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/VectorLayerAttributes/PropertiesFormatter.jsx
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
-import { Message, Select, Option } from 'oskari-ui';
-import { StyledFormField } from '../styled';
+import styled from 'styled-components';
+import { Message, Select, Collapse, CollapsePanel, Checkbox } from 'oskari-ui';
 
-const Property = ({ name }) => {
-    return (
-        <span>{name}</span>
-    )
+const StyledSelect = styled(Select)`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+
+const CheckboxWrapper = styled.div`
+    display: flex;
+`;
+
+const TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'i', 'b', 'em'];
+const FORMATTERS = ['link', 'image', 'number'];
+const HIDDEN = 'hidden'; // TODO: support or notify to use filter
+
+const getOptions = () => {
+    const opts = [];
+    opts.push({ label: 'formatters', options: FORMATTERS.map(f => ({label: f, value: f}))});
+    opts.push({ label: 'tags', options: TAGS.map(f => ({label: f, value: f}))});
+    return opts;
 };
 
-export const PropertiesFormatter = ({ formatter, update, properties }) => {
-    if (properties) {
-        return (<span>test</span>);
-    }
+const CollapseContent = ({name, formatter, update }) => {
+    const { noLabel, skipEmpty, type, params = {} } = formatter[name] || {};
+    const { link } = params;
+
+    const onChange = value => console.log(value);
     return (
-        <div>
-            { properties.map(prop => <Property key={prop.name} {...prop}/>) }
-        </div>
+        <Fragment>
+            <StyledSelect
+                value={type}
+                onChange={onChange}
+                options={getOptions()}/>
+            <CheckboxWrapper>
+                <Checkbox checked={noLabel} onChange={onChange}>
+                    <Message messageKey='noLabel' />
+                </Checkbox>
+                <Checkbox checked={skipEmpty} onChange={onChange}>
+                    <Message messageKey='skipEmpty' />
+                </Checkbox>
+                <Checkbox checked={link} onChange={onChange}>
+                    <Message messageKey='link' />
+                </Checkbox>
+            </CheckboxWrapper>
+        </Fragment>
+    );
+};
+
+export const PropertiesFormatter = ({ properties, labels, ...rest }) => {
+    return (
+        <Collapse accordion>
+            { properties.map(name => {
+                return (
+                    <CollapsePanel header={labels[name] ? `${name} (${labels[name]})` : name}>
+                        <CollapseContent key={name} name={name} {...rest} />
+                    </CollapsePanel>
+                )})
+            }
+        </Collapse>
     );
 };
 
 PropertiesFormatter.propTypes = {
     formatter: PropTypes.object.isRequired,
     update: PropTypes.func.isRequired,
-    properties: PropTypes.array.isRequired
+    properties: PropTypes.array.isRequired,
+    labels: PropTypes.object.isRequired
 };
