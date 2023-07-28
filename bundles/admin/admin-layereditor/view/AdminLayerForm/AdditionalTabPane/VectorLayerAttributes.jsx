c9fe042d336a96c3f235e7fdcec8f8544fec2561
okauppinen <olli.kauppinen@nls.fi>
2023-07-28 17:49:25 +0300

    add modal, buttons and state handling for data

--- bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/VectorLayerAttributes.jsx
+++ bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/VectorLayerAttributes.jsx
@@ -1,13 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { Message, Select, Option } from 'oskari-ui';
+import { Message, Select, Option, Space, Button } from 'oskari-ui';
+import { Modal } from 'oskari-ui/components/Modal';
 import { Controller } from 'oskari-ui/util';
+import { PropertiesFilter, PropertiesLocale, PropertiesFormatter } from './VectorLayerAttributes/';
 import { StyledFormField } from '../styled';
-import { GEOMETRY_TYPES, getGeometryType } from '../../LayerHelper';
 import { InfoTooltip } from '../InfoTooltip';
+import { GEOMETRY_TYPES, getGeometryType } from '../../LayerHelper';
 
 export const VectorLayerAttributes = ({ layer, controller }) => {
-    const geometryTypeSource = layer.attributes.data?.geometryType ? 'Attributes' : 'Capabilities';
+    const { data = {} } = layer.attributes;
+    const [modal, setModal] = useState(null);
+    const [state, setState] = useState({
+        filter: data.filter || {},
+        locale: data.locale || {},
+        formatter: data.formatter || {}
+    });
+
     const onGeometryTypeChange = value => {
         if (GEOMETRY_TYPES[0] === value) {
             controller.setAttributesData('geometryType');
@@ -15,6 +24,25 @@ export const VectorLayerAttributes = ({ layer, controller }) => {
             controller.setAttributesData('geometryType', value);
         }
     };
+    const onModalOk = () => {
+        // TODO: clean empty objects??
+        controller.setAttributesData(modal, state[modal]);
+        setModal(null);
+    };
+    const onButtonClick = mode => {
+        if (!properties.length) {
+            // nothing to see, TODO: notify
+            return;
+        }
+        setModal(mode);
+    };
+    const onModalUpdate = (value) => {
+        setState({ ...state, [modal]: value });
+    };
+
+    const properties = layer.capabilities.featureProperties?.map(prop => prop.name) || []; // TODO: filter geom
+    const geometryTypeSource = data.geometryType ? 'Attributes' : 'Capabilities';
+
     return (
         <Fragment>
             <Message messageKey='attributes.geometryType.label'/>
@@ -30,6 +58,41 @@ export const VectorLayerAttributes = ({ layer, controller }) => {
                     )) }
                 </Select>
             </StyledFormField>
+            <Message messageKey='attributes.properties' />
+            <StyledFormField>
+                <Space direction='horizontal'>
+                    <Button onClick={() => onButtonClick('filter')}>
+                        <Message messageKey='attributes.filter.button' />
+                    </Button>
+                    <Button onClick={() => onButtonClick('locale')}>
+                        <Message messageKey='attributes.locale.button' />
+                    </Button>
+                    <Button onClick={() => onButtonClick('formatter')}>
+                        <Message messageKey='attributes.formatter.button' />
+                    </Button>
+                </Space>
+            </StyledFormField>
+            <Modal
+                mask={ false }
+                maskClosable= { false }
+                open={ !!modal }
+                onOk={ () => onModalOk() }
+                onCancel={ () => setModal(null) }
+                cancelText={ <Message messageKey="cancel" /> }
+                okText={ <Message messageKey="save" /> }
+                width={ 500 }
+            >
+                <h3><Message messageKey={`attributes.${modal}.title`} /></h3>
+                { modal === 'filter' &&
+                    <PropertiesFilter update={onModalUpdate} filter={state.filter} properties={properties}/>
+                }
+                { modal === 'locale' &&
+                    <PropertiesLocale update={onModalUpdate} locale={state.locale} properties={properties}/>
+                }
+                { modal === 'formatter' &&
+                    <PropertiesFormatter update={onModalUpdate} formatter={state.formatter} properties={properties}/>
+                }
+            </Modal>
         </Fragment>
     );
 };
