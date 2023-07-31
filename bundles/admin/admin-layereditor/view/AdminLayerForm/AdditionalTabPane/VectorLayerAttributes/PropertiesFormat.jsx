5a63faebe20c597b4ee5310768d1357057673d5b
okauppinen <olli.kauppinen@nls.fi>
2023-07-31 12:04:14 +0300

    add phone number formatter and link label input

--- bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/VectorLayerAttributes/PropertiesFormat.jsx
+++ bundles/admin/admin-layereditor/view/AdminLayerForm/AdditionalTabPane/VectorLayerAttributes/PropertiesFormat.jsx
@@ -1,7 +1,9 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Message, Select, Collapse, CollapsePanel, Checkbox } from 'oskari-ui';
+import { Message, Select, Collapse, CollapsePanel, Checkbox, TextInput } from 'oskari-ui';
+
+const BUNDLE_NAME = 'admin-layereditor';
 
 const StyledSelect = styled(Select)`
     min-width: 200px;
@@ -9,22 +11,44 @@ const StyledSelect = styled(Select)`
     margin-left: 10px;
 `;
 
-const CheckboxWrapper = styled.div`
+const Options = styled.div`
     display: flex;
+    flex-flow: row wrap;
 `;
 
 const TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'i', 'b', 'em'];
-const FORMATTERS = ['link', 'image', 'number'];
+const FORMATTERS = ['link', 'image', 'number', 'phone'];
 // eslint-disable-next-line no-unused-vars
 const HIDDEN = 'hidden'; // TODO: support or notify to use filter?
 
-const localize = key => Oskari.getMsg('admin-layereditor', `attributes.format.type.${key}`)
+const PARAM_OPTIONS = {
+    image: ['link'],
+    link: ['fullUrl', 'label'],
+    phone: ['label']
+};
 
-const getOptions = () => {
+const localize = key => Oskari.getMsg(BUNDLE_NAME, `attributes.format.type.${key}`)
+
+const getTypeOptions = () => {
     const opts = [];
     opts.push({ label: localize('typeFormats'), options: FORMATTERS.map(f => ({label: localize(f), value: f}))});
     opts.push({ label: localize('textFormats'), options: TAGS.map(f => ({label: f, value: f}))});
     return opts;
+};
+
+const ParamOption = ({name, value, onChange}) => {
+    if (name === 'label') {
+        return (
+            <TextInput value={value} allowClear
+                placeholder={Oskari.getMsg(BUNDLE_NAME, `attributes.format.params.${name}`)}
+                onChange={evt => onChange(name, evt.target.value)}/>
+        );
+    }
+    return (
+        <Checkbox checked={value} onChange={evt => onChange(name, evt.target.checked)}>
+            <Message messageKey={`attributes.format.params.${name}`}/>
+        </Checkbox>
+    );
 };
 
 const CollapseContent = ({name, format, update }) => {
@@ -40,26 +64,23 @@ const CollapseContent = ({name, format, update }) => {
         onChange('params', updated);
     };
 
+    const paramOptions = PARAM_OPTIONS[values.type] || [];
     return (
         <Fragment>
             <Message messageKey='attributes.format.type.label' />
             <StyledSelect allowClear
                 value={values.type}
                 onChange={value => onChange('type', value)}
-                options={getOptions()}/>
-            <CheckboxWrapper>
+                options={getTypeOptions()}/>
+            <Options>
                 <Checkbox checked={values.noLabel} onChange={evt => onChange('noLabel', evt.target.checked)}>
                     <Message messageKey='attributes.format.options.noLabel' />
                 </Checkbox>
                 <Checkbox checked={values.skipEmpty} onChange={evt => onChange('skipEmpty', evt.target.checked)}>
                     <Message messageKey='attributes.format.options.skipEmpty' />
                 </Checkbox>
-                { values.type === 'image' &&
-                    <Checkbox checked={params.link} onChange={evt => onParamChange('link', evt.target.checked)}>
-                        <Message messageKey='attributes.format.options.link' />
-                    </Checkbox>
-                }
-            </CheckboxWrapper>
+                { paramOptions.map(name => <ParamOption key={name} name={name} value={params[name]} onChange={onParamChange} />) }
+            </Options>
         </Fragment>
     );
 };
