0f8eacd6f0085cd687015048d0738fc0b7ed482c
Sami Mäkinen <sami.makinen@nls.fi>
2023-08-10 15:32:35 +0300

    Move CSS to global styles for it to work properly

--- src/react/components/Tabs.jsx
+++ src/react/components/Tabs.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Tabs as AntTabs } from 'antd';
 import 'antd/es/tabs/style/index.js';
-import styled from 'styled-components';
 
-// Fixes flickering when tabs reach the right side of the container element AND a scrollbar is shown on the container
-// The tabs flicker between showing the last tab name as whole and/or switching it to the "..." icon.
-// This seems to only happen on the Firefox browser
-const StyledTabs = styled(AntTabs)`
-.ant-tabs-nav-wrap {
-    padding-right: 2em;
-}
-`;
+// Note! There's CSS related to this under
+//  oskari-frontend/resources/css/portal.css
+//  to fix flickering on Firefox
 export const Tabs = ({ children, ...other }) => (
-    <StyledTabs {...other}>
+    <AntTabs {...other}>
         {children}
-    </StyledTabs>
+    </AntTabs>
 );
 
 Tabs.propTypes = {
