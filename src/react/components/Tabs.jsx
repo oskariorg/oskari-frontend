f916c3e75b970c555e7112322bcf1ae0ca8966d6
Sami Mäkinen <sami.makinen@nls.fi>
2023-08-10 14:30:29 +0300

    Flickering fix for tabs/scrollbar combo on Firefox

--- src/react/components/Tabs.jsx
+++ src/react/components/Tabs.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Tabs as AntTabs } from 'antd';
 import 'antd/es/tabs/style/index.js';
+import styled from 'styled-components';
 
+// Fixes flickering when tabs reach the right side of the container element AND a scrollbar is shown on the container
+// The tabs flicker between showing the last tab name as whole and/or switching it to the "..." icon.
+// This seems to only happen on the Firefox browser
+const StyledTabs = styled(AntTabs)`
+.ant-tabs-nav-wrap {
+    padding-right: 2em;
+}
+`;
 export const Tabs = ({ children, ...other }) => (
-    <AntTabs {...other}>
+    <StyledTabs {...other}>
         {children}
-    </AntTabs>
+    </StyledTabs>
 );
 
 Tabs.propTypes = {
