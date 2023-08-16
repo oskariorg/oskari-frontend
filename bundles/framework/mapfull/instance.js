62c448bf9fd74e55583012fb33301bd511fb6ab2
Sami Mäkinen <sami.makinen@nls.fi>
2023-08-16 17:56:27 +0300

    Add more well known transforms

--- bundles/framework/mapfull/instance.js
+++ bundles/framework/mapfull/instance.js
@@ -169,8 +169,10 @@ Oskari.clazz.define('Oskari.mapframework.bundle.mapfull.MapFullBundleInstance',
             // supported by default
             const supportedProjections = {
                 'EPSG:3067': '+proj=utm +zone=35 +ellps=GRS80 +units=m +no_defs',
-                'EPSG:4326': '+title=WGS 84 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
+                'EPSG:4326': '+title=WGS 84 +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs',
+                'EPSG:3857': '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs'
             };
+            supportedProjections['EPSG:900913'] = supportedProjections['EPSG:3857'];
             // shovel in additional projections
             Object.keys(defs).forEach(projection => {
                 if (!supportedProjections[projection]) {
