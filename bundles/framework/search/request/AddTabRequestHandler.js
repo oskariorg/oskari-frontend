62025f2a5098def9fde5a2c7901eb644e32d1de3
Sami Mäkinen <sami.makinen@nls.fi>
2023-09-08 13:58:24 +0300

    Update refs in comments

--- bundles/framework/search/request/AddTabRequestHandler.js
+++ bundles/framework/search/request/AddTabRequestHandler.js
@@ -7,8 +7,8 @@ Oskari.clazz.define('Oskari.mapframework.bundle.search.request.AddTabRequestHand
  * @static
  * @param {Oskari.Sandbox} sandbox
  *          reference to application sandbox
- * @param {Oskari.mapframework.bundle.search.StateHandlerBundleInstance} search
- *          reference to search
+ * @param {Oskari.mapframework.bundle.search.Flyout} search
+ *          reference to search flyout
  */
     function (sandbox, search) {
         this.sandbox = sandbox;
@@ -18,11 +18,16 @@ Oskari.clazz.define('Oskari.mapframework.bundle.search.request.AddTabRequestHand
      * @method handleRequest
      * @param {Oskari.mapframework.core.Core} core
      *      reference to the application core (reference sandbox core.getSandbox())
-     * @param {Oskari.mapframework.bundle.personaldata.request.AddTabRequestHandler} request
+     * @param {Oskari.mapframework.bundle.search.request.AddTabRequest} request
      *      request to handle
      */
         handleRequest: function (core, request) {
-            this.search.addTab({ 'title': request.getTitle(), 'content': request.getContent(), 'priority': request.getPriority(), 'id': request.getId() });
+            this.search.addTab({
+                title: request.getTitle(),
+                content: request.getContent(),
+                priority: request.getPriority(),
+                id: request.getId()
+            });
         }
     }, {
     /**
