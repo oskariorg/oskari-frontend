c1e48b60ec48ffd815a066311153d25a5b0e91e5
Pekka Helesuo <pekka.helesuo@siili.com>
2023-10-30 16:48:37 +0200

    white text on dark background

--- bundles/framework/announcements/view/AnnouncementsBanner.jsx
+++ bundles/framework/announcements/view/AnnouncementsBanner.jsx
@@ -1,22 +1,35 @@
 import React, { Fragment } from 'react';
 import { showBanner } from 'oskari-ui/components/window';
-import { Checkbox, Message, Pagination, Link } from 'oskari-ui';
+import { Checkbox, Message, Pagination } from 'oskari-ui';
 import { BUNDLE_KEY } from '../constants';
 import styled from 'styled-components';
 import { InfoCircleOutlined, SelectOutlined } from '@ant-design/icons';
-import { ThemeProvider } from 'oskari-ui/util';
+import { ThemeConsumer } from 'oskari-ui/util';
+import { getTextColor } from 'oskari-ui/theme';
 
+const DEFAULT_GREY_TEXT = '#3c3c3c';
+const DEFAULT_POPUP_LINK_COLOR = '#0091ff';
 const LinkIcon = styled(SelectOutlined)`
     margin-left: 6px;
 `;
 
-const PopupLink = styled('span')`
-    color: #0091ff;
+const TextColorWrapper = styled('span')`
+    color: ${props => props.textColor ? props.textColor : DEFAULT_POPUP_LINK_COLOR};
+`;
+
+const PopupLink = styled('div')`
+    color: ${props => props.textColor ? props.textColor : DEFAULT_POPUP_LINK_COLOR};
     cursor: pointer;
+    a {
+        color: ${props => props.textColor ? props.textColor : DEFAULT_POPUP_LINK_COLOR};
+    }
+
+    div {
+        text-decoration: underline
+    }
 `;
 
 const InfoIcon = styled(InfoCircleOutlined)`
-    color: #3c3c3c;
     font-size: 24px;
     margin-right: 10px;
 `;
@@ -24,64 +37,71 @@ const InfoIcon = styled(InfoCircleOutlined)`
 const StyledCheckbox = styled(Checkbox)`
     margin-left: 10px;
     margin-bottom: 10px;
+
 `;
+
 const StyledTitle = styled.span`
     font-weight: bold;
-    color: #3c3c3c;
+    color: ${props => props.textColor ? props.textColor : DEFAULT_GREY_TEXT};
 `;
+
 const Column = styled.div`
     display: flex;
     flex-direction: column;
 `;
+
 const InfoContainer = styled.div`
     display: flex;
     flex-direction: row;
+    color: ${props => props.textColor ? props.textColor : DEFAULT_GREY_TEXT};
 `;
+
 const Row = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     width: 100%;
 `;
+
 const Margin = styled.div`
     margin-right: auto;
 `;
 
-const getContent = (state, controller, renderDescriptionPopup) => {
-    const { bannerAnnouncements, currentBanner = 1 } = state;
-    const announcement = bannerAnnouncements[currentBanner - 1];
-
-    if (!announcement) return null;
-
-    const { title, link, content } = Oskari.getLocalized(announcement.locale);
-    const count = bannerAnnouncements.length;
-    const setShowAgain = (e) => controller.setShowAgain(announcement.id, e.target.checked);
-    const onPageChange = (page) => controller.onBannerChange(page);
-
-    let description = <span/>;
-
-    if (link) {
-        description = (
-            <Link url={link}>
-                <Message messageKey={'externalLink'} bundleKey={BUNDLE_KEY} />
-            </Link>
-        );
-    } else if (content) {
-        description = (
-            <PopupLink onClick={() => renderDescriptionPopup(announcement)}>
-                <Message messageKey={'externalLink'} bundleKey={BUNDLE_KEY} />
-                <LinkIcon/>
-            </PopupLink>
-        );
+const StyledPagination = styled(Pagination)`
+    ul {
+        color: ${props => props.textColor ? props.textColor : DEFAULT_GREY_TEXT};
+    }
+    .ant-pagination-simple-pager {
+        color: ${props => props.textColor ? props.textColor : DEFAULT_GREY_TEXT};
+    }
+    .ant-pagination-simple-pager > input {
+        color: #000000;
     }
 
-    const notificationContent = <>
-        <InfoContainer>
+    .ant-pagination-next :not(.ant-pagination-disabled) > .ant-pagination-item-link {
+        color: ${props => props.textColor ? props.textColor : DEFAULT_GREY_TEXT};
+    }
+
+    .ant-pagination-prev :not(.ant-pagination-disabled) > .ant-pagination-item-link {
+        color: ${props => props.textColor ? props.textColor : DEFAULT_GREY_TEXT};
+    }
+`;
+
+const ThemedNotificationContent = ThemeConsumer(({ theme, state, link, content, renderDescriptionPopup, setShowAgain, onPageChange }) => {
+    const { bannerAnnouncements, currentBanner = 1 } = state;
+    const announcement = bannerAnnouncements[currentBanner - 1];
+    const count = bannerAnnouncements.length;
+    const { title } = Oskari.getLocalized(announcement.locale);
+    theme.color.primary = '#01A0E3';
+    const textColor = getTextColor(theme?.color?.primary);
+
+    return <>
+        <InfoContainer textColor={textColor}>
             <InfoIcon/>
             <Column>
-                <StyledTitle>{title}</StyledTitle>
+                <StyledTitle textColor={textColor}>{title}</StyledTitle>
                 <span>
-                    {description}
+                    {getDescription(link, content, announcement, renderDescriptionPopup, textColor)}
                     <Margin/>
                 </span>
             </Column>
@@ -89,15 +109,61 @@ const getContent = (state, controller, renderDescriptionPopup) => {
         <Margin/>
         <Column style={{ whiteSpace: 'nowrap' }}>
             <StyledCheckbox checked={state.dontShowAgain.includes(announcement.id)} onChange={setShowAgain}>
-                <Message messageKey='dontShow' bundleKey={BUNDLE_KEY} />
+                <TextColorWrapper textColor={textColor}>
+                    <Message messageKey='dontShow' bundleKey={BUNDLE_KEY} />
+                </TextColorWrapper>
             </StyledCheckbox>
-            <Pagination simple hideOnSinglePage current={currentBanner} total={count} defaultPageSize={1} onChange={onPageChange}/>
+            <StyledPagination textColor={textColor} simple hideOnSinglePage current={currentBanner} total={count} defaultPageSize={1} onChange={onPageChange}/>
         </Column>
     </>;
+});
+
+const getDescription = (link, content, announcement, renderDescriptionPopup, textColor) => {
+    let description = <span/>;
+
+    if (link) {
+        description = (
+            <PopupLink textColor={textColor}>
+                <a href={link} target='_blank' rel='noreferrer noopener'>
+                    <Message messageKey={'externalLink'} bundleKey={BUNDLE_KEY} />
+                </a>
+                <LinkIcon/>
+            </PopupLink>
+        );
+    } else if (content) {
+        description = (
+            <PopupLink textColor={textColor} onClick={() => renderDescriptionPopup(announcement)}>
+                <Message messageKey={'externalLink'} bundleKey={BUNDLE_KEY} />
+                <LinkIcon/>
+            </PopupLink>
+        );
+    }
+
+    return description;
+};
+
+const getContent = (state, controller, renderDescriptionPopup) => {
+    const { bannerAnnouncements, currentBanner = 1 } = state;
+    const announcement = bannerAnnouncements[currentBanner - 1];
+
+    if (!announcement) return null;
+
+    const { link, content } = Oskari.getLocalized(announcement.locale);
+    const setShowAgain = (e) => controller.setShowAgain(announcement.id, e.target.checked);
+    const onPageChange = (page) => controller.onBannerChange(page);
 
     const isMobile = Oskari.util.isMobile();
+
+    const notificationContent = <ThemedNotificationContent
+        state={state}
+        link={link}
+        content={content}
+        renderDescriptionPopup={renderDescriptionPopup}
+        setShowAgain={setShowAgain}
+        onPageChange={onPageChange}/>;
+
     return (
-        <ThemeProvider>
+        <>
             { isMobile &&
                 <Column>
                     { notificationContent }
@@ -108,7 +174,7 @@ const getContent = (state, controller, renderDescriptionPopup) => {
                     { notificationContent }
                 </Row>
             }
-        </ThemeProvider>
+        </>
     );
 };
 
