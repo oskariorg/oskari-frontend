00f5277510a2daa4aba0fd355d1861bf5fa9627b
Aho Henri <vk554@maanmittauslaitos.fi>
2023-11-06 11:11:54 +0200

    Cleaning.

--- bundles/statistics/statsgrid/FlyoutManager.js
+++ bundles/statistics/statsgrid/FlyoutManager.js
@@ -1,5 +1,3 @@
-import { getNavigationDimensions } from 'oskari-ui/components/window';
-
 Oskari.clazz.define('Oskari.statistics.statsgrid.FlyoutManager', function (instance, service, handler) {
     this.instance = instance;
     this.flyouts = {};
@@ -29,13 +27,6 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.FlyoutManager', function (insta
             title: loc.tile.diagram,
             controller: this.diagramHandler.getController(),
             state: this.diagramHandler.getState()
-        },
-        {
-            id: 'indicatorForm',
-            hideTile: true,
-            title: loc.userIndicators.flyoutTitle || 'Add indicator',
-            oskariClass: 'Oskari.statistics.statsgrid.view.IndicatorFormFlyout',
-            cls: 'statsgrid-user-indicator-flyout'
         }
     ];
 }, {
@@ -45,50 +36,16 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.FlyoutManager', function (insta
             return;
         }
 
-        const navDimensions = getNavigationDimensions();
-        var position = navDimensions.right;
-        const y = this._positionY;
-        const container = jQuery(Oskari.dom.getRootEl());
         this.flyoutInfo.forEach((info) => {
-            if (info.id === 'search' || info.id === 'table' || info.id === 'diagram') {
-                this.flyouts[info.id] = {
-                    controller: info.controller,
-                    state: info.state
-                };
-            } else {
-                var flyout = Oskari.clazz.create(info.oskariClass, info.title, {
-                    width: 'auto',
-                    cls: info.cls,
-                    container,
-                    position: {
-                        x: position,
-                        y
-                    }
-                }, this.instance);
-                flyout.makeDraggable({
-                    handle: '.oskari-flyouttoolbar, .statsgrid-data-container > .header',
-                    scroll: false
-                });
-
-                flyout.bringToTop();
-                flyout.on('hide', () => {
-                    this.trigger('hide', info.id);
-                });
-                this.flyouts[info.id] = flyout;
-                position = position + flyout.getSize().width;
-                if (info.resizable) {
-                    const { minWidth, minHeight } = info;
-                    flyout.makeResizable({
-                        minWidth,
-                        minHeight
-                    });
-                }
-            }
+            this.flyouts[info.id] = {
+                controller: info.controller,
+                state: info.state
+            };
         });
     },
     open: function (type) {
         const flyout = this.flyouts[type];
-        if (!flyout || type === 'indicatorForm') {
+        if (!flyout) {
             return;
         }
         flyout.controller.toggleFlyout(true, () => this.trigger('hide', type));
@@ -97,7 +54,7 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.FlyoutManager', function (insta
     },
     hide: function (type) {
         const flyout = this.flyouts[type];
-        if (!flyout || type === 'indicatorForm') {
+        if (!flyout) {
             return;
         }
 
@@ -106,7 +63,7 @@ Oskari.clazz.define('Oskari.statistics.statsgrid.FlyoutManager', function (insta
     },
     toggle: function (type) {
         const flyout = this.getFlyout(type);
-        if (!flyout || type === 'indicatorForm') {
+        if (!flyout) {
             // unrecognized flyout
             return;
         }
