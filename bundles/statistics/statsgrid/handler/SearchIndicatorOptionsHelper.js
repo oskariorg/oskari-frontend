a7d65860f48f1cc3efaf7c9f8174de4853e768c2
Sami Mäkinen <sami.makinen@nls.fi>
2023-12-08 17:31:50 +0200

    Use partial updates for indicator listing

---
+++ bundles/statistics/statsgrid/handler/SearchIndicatorOptionsHelper.js
@@ -0,0 +1,104 @@
+
+const indicatorListPerDatasource = {};
+/**
+ * Fetches listing from server andd calls callback with a list of indicators for the datasource.
+ *
+ * This could be a long operation and successCallback might be called several times
+ * with the list of indicators.
+ * We use callbacks to enable partial updates as partial indicator list might be returned
+ * while parsing the responses from the backing services.
+ * If the results are already cached on the server the callback is only called once
+ * @param {Number}   datasourceId       datasource id
+ * @param {function} successCallback    called 0-n times with the whole list of indicators as param for all calls
+ * @param {function} errorCallback      called if there's an error. Called only once with error message as param.
+ */
+export const getIndicatorOptions = (datasourceId, successCallback, errorCallback) => {
+    if (!datasourceId) {
+        if (typeof errorCallback === 'function') {
+            errorCallback('Datasource missing');
+        }
+        return;
+    }
+    const cacheKey = 'ds_' + datasourceId;
+    const cachedResponse = indicatorListPerDatasource[cacheKey];
+    if (cachedResponse) {
+        // found a cached response
+        successCallback({
+            complete: true,
+            indicators: [...cachedResponse.indicators]
+        });
+        return;
+    }
+
+    try {
+        const response = populateIndicatorList(datasourceId, successCallback);
+        const value = {
+            // always signal complete when we are done with retries, even if server returned !complete
+            complete: true,
+            indicators: response.indicators || []
+        };
+        if (response.complete) {
+            // cache result if complete
+            // this allows the server to continue processing and return updated list when available
+            indicatorListPerDatasource[cacheKey] = value;
+        }
+        return value;
+    } catch (error) {
+        if (typeof errorCallback === 'function') {
+            errorCallback(error);
+        } else {
+            throw new Error(error);
+        }
+    }
+};
+
+const RETRY_LIMIT = 5;
+const populateIndicatorList = async (datasourceId, successCallback, retryCount = RETRY_LIMIT, previousResult = {}) => {
+    const response = await getIndicatorListFromServer(datasourceId);
+    if (response.complete) {
+        successCallback(response);
+        return;
+    }
+    if (retryCount === 0) {
+        // not succesful after {RETRY_LIMIT} tries
+        throw new Error('Error loading indicators. Tried ' + RETRY_LIMIT + ' times');
+    }
+    const newIndicatorCount = response.indicators?.length || 0;
+    const previousIndicatorCount = previousResult.indicators?.length || 0;
+    let newRetryCount = retryCount;
+    if (newIndicatorCount === previousIndicatorCount) {
+        // Same indicator list as in previous try -> not getting anything new.
+        // There might be some problems with the service -> reduce retries
+        newRetryCount = retryCount - 1;
+    }
+
+    return new Promise((resolve, reject) => {
+        // try again after 10 seconds
+        setTimeout(async () => {
+            try {
+                resolve(await populateIndicatorList(datasourceId, successCallback, newRetryCount, response));
+            } catch (err) {
+                reject(err);
+            }
+        }, 10000);
+    });
+};
+
+const getIndicatorListFromServer = async (datasourceId) => {
+    try {
+        const response = await fetch(Oskari.urls.getRoute('GetIndicatorList', {
+            datasource: datasourceId
+        }), {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json'
+            }
+        });
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        return await response.json();
+    } catch (error) {
+        throw new Error('errors.indicatorListIsEmpty');
+    }
+};
