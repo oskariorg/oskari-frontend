53cf64f6697f3212a0662fcf96d8ab1764dfdb84
okauppinen <olli.kauppinen@nls.fi>
2024-03-01 21:03:20 +0200

    revert changes from master

---
+++ bundles/statistics/statsgrid2016/components/classification/Legend.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { InactiveLegend } from './legend/InactiveLegend';
+import { LegendRow } from './legend/LegendRow';
+
+const Container = styled.div`
+    margin: 0 auto;
+    width: 90%;
+    overflow: hidden;
+`;
+
+export const Legend = ({
+    transparency,
+    mapStyle,
+    classifiedDataset
+}) => {
+    const { error } = classifiedDataset;
+    if (error) {
+        const errorKey = error === 'general' ? 'cannotCreateLegend' : error;
+        return (<InactiveLegend error = {errorKey} />);
+    }
+    const opacity = transparency / 100 || 1;
+    const { groups } = classifiedDataset;
+    const maxSizePx = groups.map(g => g.sizePx).reduce((max, val) => max < val ? val : max);
+    return (
+        <Container>
+            { groups.map((group, i) =>
+                <LegendRow key={`item-${i}`}
+                    opacity={opacity}
+                    mapStyle={mapStyle}
+                    maxSizePx={maxSizePx}
+                    { ...group }
+                />
+            )}
+        </Container>
+    );
+};
+
+Legend.propTypes = {
+    transparency: PropTypes.number.isRequired,
+    mapStyle: PropTypes.string.isRequired,
+    classifiedDataset: PropTypes.object.isRequired
+};
