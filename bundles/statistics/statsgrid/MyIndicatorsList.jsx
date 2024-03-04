442724fa5cee13360b167e3e0d8e92d9403ef502
okauppinen <olli.kauppinen@nls.fi>
2024-03-04 14:06:47 +0200

    useState for clipboard value, data -> indicators, confirm delete dataset

--- bundles/statistics/statsgrid/MyIndicatorsList.jsx
+++ bundles/statistics/statsgrid/MyIndicatorsList.jsx
@@ -16,7 +16,7 @@ const editIconStyle = {
     fontSize: '16px'
 };
 
-export const MyIndicatorsList = ({ controller, data = [], loading }) => {
+export const MyIndicatorsList = ({ controller, indicators = [], loading }) => {
     const columnSettings = [
         {
             dataIndex: 'name',
@@ -80,7 +80,7 @@ export const MyIndicatorsList = ({ controller, data = [], loading }) => {
             </ButtonContainer>
             <Table
                 columns={columnSettings}
-                dataSource={data.map(item => ({
+                dataSource={indicators.map(item => ({
                     key: item.id,
                     ...item
                 }))}
@@ -92,7 +92,7 @@ export const MyIndicatorsList = ({ controller, data = [], loading }) => {
 };
 
 MyIndicatorsList.propTypes = {
-    data: PropTypes.arrayOf(PropTypes.object),
+    indicators: PropTypes.arrayOf(PropTypes.object),
     controller: PropTypes.object.isRequired,
     loading: PropTypes.bool
 };
