2520068c5b611b137a5861628e2956708ebec4a0
okauppinen <olli.kauppinen@nls.fi>
2024-03-13 16:20:40 +0200

    update title and show warning for guest inside form

--- bundles/statistics/statsgrid/view/Form/IndicatorForm.jsx
+++ bundles/statistics/statsgrid/view/Form/IndicatorForm.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Message } from 'oskari-ui';
+import { Message, Button } from 'oskari-ui';
 import { LocaleProvider } from 'oskari-ui/util';
 import { PrimaryButton, SecondaryButton, ButtonContainer } from 'oskari-ui/components/buttons';
 import { showPopup } from 'oskari-ui/components/window';
 import { StatisticalData } from './StatisticalData';
 import { IndicatorCollapse } from './IndicatorCollapse';
+import { getRegionsets } from '../../helper/ConfigHelper';
 
 import styled from 'styled-components';
 
@@ -17,16 +18,43 @@ const Content = styled('div')`
     min-width: 365px;
 `;
 
+const Warning = styled.div`
+    width: 350px;
+    font-style: italic;
+    margin-top: 10px;
+`;
+
+const Title = ({ indicator, showDataTable, regionset, selection }) => {
+    if (showDataTable) {
+        const regionsetName = getRegionsets().find(rs => rs.id === regionset)?.name || '';
+        return (
+            <span>
+                <Message messageKey='parameters.year' bundleKey={BUNDLE_KEY}/>
+                {`: ${selection} - ${regionsetName}`}
+            </span>
+        );
+    }
+    const key = !indicator.id ? 'userIndicators.add' : 'userIndicators.edit';
+    return <Message messageKey={key} bundleKey={BUNDLE_KEY} />
+};
+
 const IndicatorForm = ({ state, controller, onClose }) => {
-    const { showDataTable } = state;
+    const { showDataTable, indicator } = state;
+    const showWarning = !showDataTable && !indicator.id && !Oskari.user().isLoggedIn();
     const Component = showDataTable ? StatisticalData : IndicatorCollapse;
     const onSave = () => showDataTable ? controller.saveData() : controller.saveIndicator();
     const onCancel = () => showDataTable ? controller.closeDataTable() : onClose();
     return (
         <Content>
             <Component state={state} controller={controller}/>
+            {showWarning && (<Warning><Message messageKey='userIndicators.notLoggedInWarning'/></Warning>)}
             <ButtonContainer>
                 <SecondaryButton type='cancel' onClick={onCancel}/>
+                {showDataTable && (
+                    <Button onClick={() => controller.showClipboardPopup()} >
+                        <Message messageKey='userIndicators.import.title' />
+                    </Button>
+                )}
                 <PrimaryButton type='save' onClick={onSave}/>
             </ButtonContainer>
         </Content>
@@ -34,12 +62,10 @@ const IndicatorForm = ({ state, controller, onClose }) => {
 };
 
 export const showIndicatorForm = (state, controller, onClose) => {
-
-    const title = <Message messageKey='userIndicators.flyoutTitle' bundleKey={BUNDLE_KEY} />;
     const controls = showPopup(
-        title,
+        <Title {...state}/>,
         <LocaleProvider value={{ bundleKey: BUNDLE_KEY }}>
-            <IndicatorForm state={state} controller={controller} />
+            <IndicatorForm state={state} controller={controller} onClose={onClose} />
         </LocaleProvider>,
         onClose
     );
@@ -47,9 +73,9 @@ export const showIndicatorForm = (state, controller, onClose) => {
     return {
         ...controls,
         update: (state) => controls.update(
-            title,
+            <Title {...state}/>,
             <LocaleProvider value={{ bundleKey: BUNDLE_KEY }}>
-                <IndicatorForm state={state} controller={controller} />
+                <IndicatorForm state={state} controller={controller} onClose={onClose}/>
             </LocaleProvider>
         )
     };
