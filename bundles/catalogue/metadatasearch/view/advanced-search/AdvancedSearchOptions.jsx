ccd20d5cf5c5ee507e25681ae61c11f5021f66f1
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-05-28 15:19:36 +0300

    use Message and defaultMsg, some refactoring to simplify code

--- bundles/catalogue/metadatasearch/view/advanced-search/AdvancedSearchOptions.jsx
+++ bundles/catalogue/metadatasearch/view/advanced-search/AdvancedSearchOptions.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { METADATA_BUNDLE_LOCALIZATION_ID } from '../../instance';
 import { AdvancedSearchDropdown } from './AdvancedSearchDropdown';
 import { FlexColumnContainer } from './AdvancedSearchStyledComponents';
 import { AdvancedSearchMulti } from './AdvancedSearchMulti';
 import { AdvancedSearchCoverage } from './AdvancedSearchCoverage';
+
 const COVERAGE_FIELD_NAME = 'coverage';
+
 export const AdvancedSearchOptions = (props) => {
     const { advancedSearchOptions, advancedSearchValues, drawing, controller } = props;
-    const hasCoverage = advancedSearchOptions?.fields?.find((field) => field.field === COVERAGE_FIELD_NAME) || null;
-    return <FlexColumnContainer>
-        { advancedSearchOptions &&
-            // coverage should be rendered separately
-            advancedSearchOptions?.fields?.filter((field) => field.field !== COVERAGE_FIELD_NAME).map((field) => {
-                if (field.multi) {
-                    return <AdvancedSearchMulti
-                        key={field.field}
-                        disabled={drawing}
-                        title={Oskari.getMsg(METADATA_BUNDLE_LOCALIZATION_ID, 'advancedSearch.' + field.field)}
-                        options={getByField(field.field, advancedSearchOptions)}
-                        selected={advancedSearchValues[field.field]}
-                        onChange={(value) => controller.advancedSearchParamsChangedMulti(field.field, value)}/>;
-                }
+    const { fields = [] } = advancedSearchOptions || {};
+    const hasCoverage = fields.some(({ field }) => field === COVERAGE_FIELD_NAME);
+    // coverage should be rendered separately
+    const fieldOptions = fields.filter(({ field }) => field !== COVERAGE_FIELD_NAME);
 
-                return <AdvancedSearchDropdown
-                    key={field.field}
+    return <FlexColumnContainer>
+        {
+            fieldOptions.map(({ field, multi, values }) => {
+                const Node = multi ? AdvancedSearchMulti : AdvancedSearchDropdown;
+                return <Node
+                    key={field}
+                    field={field}
                     disabled={drawing}
-                    title={Oskari.getMsg(METADATA_BUNDLE_LOCALIZATION_ID, 'advancedSearch.' + field.field)}
-                    options={getByField(field.field, advancedSearchOptions)}
-                    selected={advancedSearchValues[field.field]}
-                    onChange={(value) => controller.advancedSearchParamsChanged(field.field, value)}/>;
+                    options={values}
+                    selected={advancedSearchValues[field]}
+                    onChange={value => controller.advancedSearchParamsChanged(field, value)}/>;
             })
         }
 
@@ -42,10 +37,6 @@ export const AdvancedSearchOptions = (props) => {
                 coverageFeature={advancedSearchValues?.coverage}/>
         }
     </FlexColumnContainer>;
-};
-
-const getByField = (fieldName, optionsArray) => {
-    return optionsArray?.fields?.find((item) => item.field === fieldName) || null;
 };
 
 AdvancedSearchOptions.propTypes = {
