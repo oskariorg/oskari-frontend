ce11f1c135f8f1155588e05c79aadb64e1eb98dd
Sami Mäkinen <sami.makinen@nls.fi>
2024-08-30 14:05:16 +0300

    Check cesium before copying

--- webpack/hacks/postinstall.js
+++ webpack/hacks/postinstall.js
@@ -2,6 +2,10 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const srcFileCesiumHack = path.join(__dirname, 'cesium', 'buildModuleUrl.js.modified');
+if (!fs.existsSync(path.join(__dirname, srcFileCesiumHack))) {
+    // possibly running on oskari-frontend-contrib
+    return;
+}
 const destFileCesiumHack = path.join(__dirname, '..', '..', 'node_modules', '@cesium', 'engine', 'Source', 'Core', 'buildModuleUrl.js');
 // Overwrites node_modules/@cesium/engine/Source/Core/buildModuleUrl.js
 // with a modified on to fix issues with Webpack 4.x
