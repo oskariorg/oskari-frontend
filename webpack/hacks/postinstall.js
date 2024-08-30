9ad8b3ddfa6977e8d7f5524e45a39397b08c9fab
Sami Mäkinen <sami.makinen@nls.fi>
2024-08-30 14:06:34 +0300

    Dont use return as its not a function

--- webpack/hacks/postinstall.js
+++ webpack/hacks/postinstall.js
@@ -2,11 +2,11 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const srcFileCesiumHack = path.join(__dirname, 'cesium', 'buildModuleUrl.js.modified');
-if (!fs.existsSync(path.join(__dirname, srcFileCesiumHack))) {
-    // possibly running on oskari-frontend-contrib
-    return;
+if (fs.existsSync(path.join(__dirname, srcFileCesiumHack))) {
+    const destFileCesiumHack = path.join(__dirname, '..', '..', 'node_modules', '@cesium', 'engine', 'Source', 'Core', 'buildModuleUrl.js');
+    // Overwrites node_modules/@cesium/engine/Source/Core/buildModuleUrl.js
+    // with a modified on to fix issues with Webpack 4.x
+    fs.copyFile(srcFileCesiumHack, destFileCesiumHack);
+} else {
+    // possibly running on oskari-frontend-contrib or cesium is not available
 }
-const destFileCesiumHack = path.join(__dirname, '..', '..', 'node_modules', '@cesium', 'engine', 'Source', 'Core', 'buildModuleUrl.js');
-// Overwrites node_modules/@cesium/engine/Source/Core/buildModuleUrl.js
-// with a modified on to fix issues with Webpack 4.x
-fs.copyFile(srcFileCesiumHack, destFileCesiumHack);
