815c939141e4f3009021edaabf22773d6244a533
Sami Mäkinen <sami.makinen@nls.fi>
2024-09-04 11:30:42 +0300

    Try detecting cesium folder, not just node_modules folder

--- webpack/hacks/postinstall.js
+++ webpack/hacks/postinstall.js
@@ -3,17 +3,17 @@ const path = require('path');
 
 const srcFileCesiumHack = path.join(__dirname, 'cesium', 'buildModuleUrl.js.modified');
 // installed as ../oskari-frontend
-let pathToNodeModules = path.join(__dirname, '..', '..', 'node_modules');
-if (!fs.existsSync(pathToNodeModules)) {
+let pathToCesium = path.join(__dirname, '..', '..', 'node_modules', '@cesium');
+if (!fs.existsSync(pathToCesium)) {
     // installed in non-dev-mode
-    pathToNodeModules = path.join(__dirname, '..', '..', '..', 'node_modules');
+    pathToCesium = path.join(__dirname, '..', '..', '..', 'node_modules', '@cesium');
 }
 // try again with non-dev-mode
-if (!fs.existsSync(pathToNodeModules)) {
+if (!fs.existsSync(pathToCesium)) {
     // something went terribly wrong
 } else {
     // Overwrites node_modules/@cesium/engine/Source/Core/buildModuleUrl.js
     // with a modified on to fix issues with Webpack 4.x
-    const destFileCesiumHack = path.join(pathToNodeModules, '@cesium', 'engine', 'Source', 'Core', 'buildModuleUrl.js');
+    const destFileCesiumHack = path.join(pathToCesium, 'engine', 'Source', 'Core', 'buildModuleUrl.js');
     fs.copyFile(srcFileCesiumHack, destFileCesiumHack);
 }
