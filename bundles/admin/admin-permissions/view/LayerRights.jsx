39aeb221fcff3d3d65a563366ab9085a5dc387e6
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-11-01 10:08:34 +0200

    add summaries to select

--- bundles/admin/admin-permissions/view/LayerRights.jsx
+++ bundles/admin/admin-permissions/view/LayerRights.jsx
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Select, Message, Confirm } from 'oskari-ui';
 import { LayerRightsTable } from './LayerRightsTable';
+import { LayerRightsSummary } from './LayerRightsSummary';
 import { LayerRightsSearch } from './LayerRightsSearch';
+import { LayerDetails } from './LayerDetails';
 import styled from 'styled-components';
+
+const SUMMARY = ['permissions', 'layer'];
 
 const Container = styled.div`
     display: flex;
@@ -18,15 +22,38 @@ const Instruction = styled.span`
     font-style: italic;
 `;
 
+const Table = ({ controller, state }) => {
+    const { selectedRole } = state;
+    if (!selectedRole) {
+        return <Instruction><Message messageKey={`flyout.instruction`} /></Instruction>;
+    }
+    if (selectedRole === 'permissions') {
+        return <LayerRightsSummary controller={controller} state={state} />;
+    }
+    if (selectedRole === 'layer') {
+        return <LayerDetails controller={controller} state={state} />;
+    }
+    return <LayerRightsTable controller={controller} state={state} />;
+};
+Table.propTypes = {
+    controller: PropTypes.object.isRequired,
+    state: PropTypes.object.isRequired
+};
+
 const getRoleOptions = roles => {
+    const mapped = roles.map(r => ({ label: r.name, value: r.id, isSystem: r.type !== 'other' }));
     return [{
+        title: 'summary',
+        label: <Message messageKey='flyout.select.summary' />,
+        options: SUMMARY.map(value => ({ value, label: <Message messageKey={`flyout.select.${value}`} /> }))
+    }, {
         title: 'system',
         label: <Message messageKey='roles.type.system' />,
-        options: roles.filter(r => r.isSystem)
+        options: mapped.filter(r => r.isSystem)
     }, {
         title: 'other',
         label: <Message messageKey='roles.type.other' />,
-        options: roles.filter(r => !r.isSystem)
+        options: mapped.filter(r => !r.isSystem)
     }];
 };
 
@@ -34,7 +61,7 @@ export const LayerRights = ({ controller, state }) => {
     const [roleConfirmOpen, setRoleConfirmOpen] = useState(false);
     const [pendingRole, setPendingRole] = useState(null);
     const hasChanges = Object.keys(state.unSavedChanges).length > 0;
-    const roleSelected = !!state.selectedRole;
+    const showSearch = state.selectedRole && state.selectedRole !== 'layer';
 
     const onRoleChange = role => {
         if (hasChanges) {
@@ -54,7 +81,7 @@ export const LayerRights = ({ controller, state }) => {
     return (
         <div>
             <Container>
-                <Message messageKey='roles.title' />
+                <Message messageKey='flyout.select.label' />
                 <Confirm
                     title={<Message messageKey='flyout.unsavedChangesConfirm'/>}
                     open={roleConfirmOpen}
@@ -65,15 +92,14 @@ export const LayerRights = ({ controller, state }) => {
                     placement='top'
                     popupStyle={{ zIndex: '999999' }}>
                     <StyledSelect
-                        placeholder={<Message messageKey='roles.placeholder'/>}
+                        placeholder={<Message messageKey='flyout.select.placeholder'/>}
                         value={state.selectedRole}
                         options={getRoleOptions(state.roles)}
                         onChange={value => onRoleChange(value)}/>
                 </Confirm>
-                { roleSelected && <LayerRightsSearch controller={controller} state={state} /> }
+                { showSearch && <LayerRightsSearch controller={controller} state={state} /> }
             </Container>
-            { roleSelected && <LayerRightsTable controller={controller} state={state} /> }
-            { !roleSelected && <Instruction><Message messageKey={`flyout.instruction`} /></Instruction> }
+            <Table controller={controller} state={state} />
         </div>
     );
 };
