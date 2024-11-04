c70520cb817e43b1e3d7ef0a4263b6aaa0867871
Pekka Helesuo <pekka.helesuo@siili.com>
2024-11-04 11:19:05 +0200

    move maplayerlist - tool under mapmodule: localisations

--- bundles/framework/publisher2/resources/locale/sv.js
+++ bundles/framework/publisher2/resources/locale/sv.js
@@ -100,12 +100,7 @@ Oskari.registerLocalization(
                     "label": "Mina kartlager"
                 },
                 "layerselection": {
-                    "info": "Välj bakgrundskartlager. Du kan göra förval i förhandsgranskningsvyn.",
                     "selectAsBaselayer": "Välj bakgrundskartlager",
-                    "allowStyleChange": "Tillåt val av visningsstil",
-                    "showMetadata": "Visa länkar för metadata",
-                    "noMultipleStyles": "Endast en visningsstil är tillgänglig för valda kartlager.",
-                    "noMetadata": "Metadatalänkar är inte tillgängliga på valda kartlager"
                 },
                 "mylocation": {
                     "modes": {
--- bundles/mapping/mapmodule/resources/locale/fi.js
+++ bundles/mapping/mapmodule/resources/locale/fi.js
@@ -186,6 +186,14 @@ Oskari.registerLocalization(
             },
             "CrosshairTool": {
                 "toolLabel": "Näytä kartan keskipiste"
+            },
+            "LayerSelection": {
+                "toolLabel": "Karttatasovalikko",
+                "noMultipleStyles": "Vain yksi esitystyyli saatavilla valituilla karttatasoilla.",
+                "allowStyleChange": "Salli esitystyylin valinta",
+                "noMetadata": "Metatietolinkkejä ei saatavilla valituilla karttatasoilla",
+                "showMetadata": "Näytä metatietolinkit",
+                "selectAsBaselayer": "Valitse taustakartaksi",
             }
         }
     }
