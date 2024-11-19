9f619d871a3bdab94952e3b50aacc1563f04d86a
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-11-19 12:21:40 +0200

    add prop validation and fix required

--- bundles/statistics/statsgrid/components/description/MetadataContent.jsx
+++ bundles/statistics/statsgrid/components/description/MetadataContent.jsx
@@ -3,23 +3,9 @@ import PropTypes from 'prop-types';
 import { Message } from 'oskari-ui';
 import styled from 'styled-components';
 
-export const MetadataContent = ({description, source, metadata = {}}) => {
-    if (!description) {
-        return (<Message messageKey='metadataPopup.noMetadata' messageArgs={{ indicators: 1 }}/>);
-    }
-    // Description can include HTML so well have to wrap it as HTML content...
-    return (<React.Fragment>
-        <p dangerouslySetInnerHTML={{__html: description}} />
-        <p>
-            <DataLabel labelKey='panels.newSearch.datasourceTitle' value={source} />
-            <DataLabel labelKey='metadataPopup.updated' value={metadata.updated} />
-            <DataLabel labelKey='metadataPopup.nextUpdate' value={metadata.nextUpdate} />
-        </p>
-    </React.Fragment>);
-};
-
 const bTag = styled.b``;
-const DataLabel = ({labelKey, value}) => {
+
+const DataLabel = ({ labelKey, value }) => {
     if (typeof value !== 'number' && !value) {
         return null;
     }
@@ -27,10 +13,28 @@ const DataLabel = ({labelKey, value}) => {
         <React.Fragment>
             <Message LabelComponent={bTag} messageKey={labelKey} />: {value}<br />
         </React.Fragment>);
-}
+};
+DataLabel.propTypes = {
+    labelKey: PropTypes.string.isRequired,
+    value: PropTypes.any
+};
 
+export const MetadataContent = ({ description, source, metadata = {} }) => {
+    if (!description) {
+        return (<Message messageKey='metadataPopup.noMetadata' messageArgs={{ indicators: 1 }}/>);
+    }
+    // Description can include HTML so well have to wrap it as HTML content...
+    return (<React.Fragment>
+        <p dangerouslySetInnerHTML={{ __html: description }} />
+        <p>
+            <DataLabel labelKey='panels.newSearch.datasourceTitle' value={source} />
+            <DataLabel labelKey='metadataPopup.updated' value={metadata.updated} />
+            <DataLabel labelKey='metadataPopup.nextUpdate' value={metadata.nextUpdate} />
+        </p>
+    </React.Fragment>);
+};
 MetadataContent.propTypes = {
-    description: PropTypes.string.isRequired,
-    source: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    source: PropTypes.string,
     metadata: PropTypes.object
 };
