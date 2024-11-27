f2093d40f414d420b73832aadc399e7a5bcaacf7
Olli Kauppinen <olli.kauppinen@nls.fi>
2024-11-27 23:24:12 +0200

    add role to test

--- bundles/admin/admin-layereditor/view/AdminLayerForm/PermissionsTabPane/PermissionRow.test.js
+++ bundles/admin/admin-layereditor/view/AdminLayerForm/PermissionsTabPane/PermissionRow.test.js
@@ -4,18 +4,15 @@ import { PermissionRow } from './PermissionRow';
 import { Checkbox } from 'oskari-ui';
 
 describe('<PermissionRow/>', () => {
-    test('ignored', () => {});
-    /*
     test('renders one div with containing param text and as many checkboxes as passed as parameter', () => {
         expect.assertions(2);
         const mockText = 'Mock rolename text';
         const mockCheckboxes = [<Checkbox data-testid={mockText} key={'key1'} onChange={() => console.log('Not called in this test')}/>,
             <Checkbox data-testid={mockText} key={'key2'} onChange={() => console.log('Not called in this test')}/>];
-        const row = render(<PermissionRow text={mockText} checkboxes={mockCheckboxes}
+        const row = render(<PermissionRow text={mockText} checkboxes={mockCheckboxes} role={{ name: mockText }}
             isHeaderRow={false}/>);
 
         expect(screen.queryAllByText(mockText)).toHaveLength(1);
         expect(row.queryAllByRole('checkbox')).toHaveLength(mockCheckboxes.length);
     });
-    */
 });
