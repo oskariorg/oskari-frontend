e14bf6bdace6b6c00fcf615cb0a59cac553a2095
Pekka Helesuo <pekka.helesuo@siili.com>
2024-12-03 14:10:42 +0200

    add handler class and utilise localeprovider

--- bundles/framework/publisher2/view/dialog/ReplaceConfirmDialogContent.jsx
+++ bundles/framework/publisher2/view/dialog/ReplaceConfirmDialogContent.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'oskari-ui';
+import { Button, Message } from 'oskari-ui';
 import { SecondaryButton } from 'oskari-ui/components/buttons';
 import { ButtonContainer, DialogContentContainer } from './Styled';
+import { LocaleProvider } from 'oskari-ui/util';
 
-export const ReplaceConfirmDialogContent = ({ message, replaceButtonTitle, okCallback, closeCallback }) => {
-    return <DialogContentContainer>
-        {message}
-        <ButtonContainer>
-            <SecondaryButton type='cancel' onClick={closeCallback}/>
-            <Button type='primary' onClick={okCallback}>{replaceButtonTitle}</Button>
-        </ButtonContainer>
-    </DialogContentContainer>;
+export const ReplaceConfirmDialogContent = ({ okCallback, closeCallback }) => {
+    return <LocaleProvider value={{ bundleKey: 'Publisher2' }}>
+        <DialogContentContainer>
+            {<Message messageKey='BasicView.confirm.replace.msg'/>}
+            <ButtonContainer>
+                <SecondaryButton type='cancel' onClick={closeCallback}/>
+                <Button type='primary' onClick={okCallback}>
+                    <Message messageKey='BasicView.buttons.replace'/>
+                </Button>
+            </ButtonContainer>
+        </DialogContentContainer>
+    </LocaleProvider>;
 };
 
 ReplaceConfirmDialogContent.propTypes = {
