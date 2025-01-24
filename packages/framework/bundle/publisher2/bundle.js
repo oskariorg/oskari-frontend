e88857d45c9604a7efd33f9e3ba5e08628397cd2
Pekka Helesuo <pekka.helesuo@siili.com>
2025-01-24 11:30:04 +0200

    remove panellayout. Not used anymore

--- packages/framework/bundle/publisher2/bundle.js
+++ packages/framework/bundle/publisher2/bundle.js
@@ -75,10 +75,6 @@ Oskari.clazz.define("Oskari.mapframework.bundle.publisher2.PublisherBundle", fun
         },
         {
             "type": "text/javascript",
-            "src": "../../../../bundles/framework/publisher2/view/PanelLayout.js"
-        },
-        {
-            "type": "text/javascript",
             "src": "../../../../bundles/framework/publisher2/view/PanelRpc.js"
         },
         {
