f2724cca2f6ad48edd95fcc5a1c9875d644f48ed
Sami Mäkinen <sami.makinen@nls.fi>
2025-02-24 11:36:24 +0200

    Remove the request handler part for now as non-working

--- src/react/BasicBundleInstance.js
+++ src/react/BasicBundleInstance.js
@@ -6,7 +6,6 @@ export class BasicBundleInstance extends BaseModule {
     constructor (name = DEFAULT_NAME) {
         super(name);
         this._loc = undefined;
-        this._requestHandlers = undefined;
     }
 
     /**
@@ -18,17 +17,6 @@ export class BasicBundleInstance extends BaseModule {
         // do the usual startup like registering to sandboc
         super.start(sandbox);
         // start() here is for documentation purposes as it's something bundles will likely override
-    }
-
-    /**
-     * If you need to do any cleanup when the bundle is stopped, this is the place to do it.
-     * If you override this, remember to call super.stop() that handles the usual cleanup for event listeners and request handlers.
-     */
-    stop () {
-        // do the usual
-        super.stop();
-        // and remove any requests handlers in case the bundle added some
-        this.removeRequestHandler();
     }
 
     /**
@@ -45,73 +33,10 @@ export class BasicBundleInstance extends BaseModule {
         return this._loc(key, args);
     }
 
-    addRequestHandler (requestName, handlerFn) {
-        if (!requestName) {
-            throw new Error('Tried to register handler without request name');
-        }
-        if (typeof handlerFn !== 'function') {
-            throw new Error(`Tried to register handler for ${requestName} without handlerFn`);
-        }
-        if (!this._requestHandlers) {
-            this._requestHandlers = Oskari.createStore('handler');
-        }
-        this.getSandbox().requestHandler(requestName, (req) => handlerFn(req));
-        this._requestHandlers.handler(requestName, handlerFn);
-    }
-
-    removeRequestHandler (requestName) {
-        if (requestName) {
-            // remove single request handler
-            this.getSandbox().requestHandler(requestName, null);
-            this._listeners.request.reset(requestName);
-        } else {
-            // remove all request handlers
-            this._listeners.request.handler().forEach(reqName => this.removeRequestHandler(reqName));
-        }
-    }
-
-    // FIXME: this doesn't work:
-    /*
-    This was the plan:
-```
-class BaseRequest {
-    constructor (name = 'BaseRequest' + Oskari.getSeq('BaseModule').nextVal()) {
-        this.name = name;
-    }
-
-    getName () {
-        return this.name;
-    }
-}
-
-class HelloRequest extends BaseRequest {
-    constructor(id) {
-        super('HelloRequest');
-        this._id = id;
-    }
-
-    getId () {
-        return this._id;
-    }
-};
-```
-Then on bundle:
-```
-    this.registerRequestImpl(HelloRequest);
-    this.addRequestHandler('HelloRequest', (req) => console.log(req));
-```
-Or even just: `this.addRequestHandler(HelloRequest, (req) => console.log(req));`
-     */
-    registerRequestImpl (bundleRequestClass) {
-        // we should find a way for es classes to register for metadata without creating the random name
-        Oskari.clazz.defineES('Oskari.random.Request' + Oskari.getSeq('BaseRequest').nextVal(), bundleRequestClass, {
-            protocol: ['Oskari.mapframework.request.Request']
-        });
-    }
-
     // jump through some additional hoops to default name as the bundleId if it hasn't been overridden
     /**
      * Tries to use bundle id as name if name isn't given for constructor
+     * @see BaseModule.getName()
      * @returns String the name of the bundle (used for referencing on sandbox, mapping the correct localization file)
      */
     getName () {
