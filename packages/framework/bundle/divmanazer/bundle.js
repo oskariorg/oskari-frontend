00a09d9e020382c73fe2404a53b77c0edc9e1872
Sami Mäkinen <sami.makinen@nls.fi>
2025-03-04 00:42:54 +0200

    Remove chocen.js and the selectlist component that used it

--- packages/framework/bundle/divmanazer/bundle.js
+++ packages/framework/bundle/divmanazer/bundle.js
@@ -147,10 +147,6 @@ Oskari.clazz.define("Oskari.userinterface.bundle.ui.UserInterfaceBundle", functi
             }, {
                 "type": "text/javascript",
                 "src": "../../../../bundles/framework/divmanazer/component/Select.js"
-
-            }, {
-                "type": "text/javascript",
-                "src": "../../../../bundles/framework/divmanazer/component/SelectList.js"
             }, {
                 "type": "text/javascript",
                 "src": "../../../../bundles/framework/divmanazer/component/MultiLevelSelect.js"
@@ -250,10 +246,6 @@ Oskari.clazz.define("Oskari.userinterface.bundle.ui.UserInterfaceBundle", functi
             }, {
                 "type": "text/css",
                 "src": "../../../../bundles/framework/divmanazer/resources/scss/modal.scss"
-            },
-            {
-                "type": "text/css",
-                "src": "../../../../bundles/framework/divmanazer/resources/scss/selectlist.scss"
             }, {
                 "type": "text/css",
                 "src": "../../../../bundles/framework/divmanazer/resources/scss/badge.scss"
