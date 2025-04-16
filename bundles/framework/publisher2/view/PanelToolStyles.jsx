cf99c04fba341bc09a5becc892f7c1bcfe59190e
Olli Kauppinen <olli.kauppinen@nls.fi>
2025-04-16 23:43:01 +0300

    remove internal use state & effect and move some functions to handler

--- bundles/framework/publisher2/view/PanelToolStyles.jsx
+++ bundles/framework/publisher2/view/PanelToolStyles.jsx
@@ -1,11 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Divider, Message, Slider, Checkbox, Dropdown, Button, Select, Option, NumberInput } from 'oskari-ui';
+import { Divider, Message, Slider, Checkbox, Dropdown, Button, Select, NumberInput } from 'oskari-ui';
 import { ColorPicker } from 'oskari-ui/components/ColorPicker';
-import { PropTypes } from 'prop-types';
-import { getDefaultMapTheme } from '../../..//mapping/mapmodule/util/MapThemeHelper.js';
-import { INFOBOX_PREVIEW_ID } from '../handler/PanelLayoutHandler.js';
-const BUNDLE_KEY = 'Publisher2';
+import PropTypes from 'prop-types';
+import { BUNDLE_KEY } from '../constants.js';
+
+const FONTS = [
+    {
+        label: 'Arial (sans-serif)',
+        value: 'arial'
+    },
+    {
+        label: 'Georgia (serif)',
+        value: 'georgia'
+    },
+    {
+        label: 'Fantasy (sans-serif)',
+        value: 'fantasy'
+    },
+    {
+        label: 'Verdana (sans-serif)',
+        value: 'verdana'
+    }
+];
 
 const Content = styled('div')`
     margin-top: 10px;
@@ -54,149 +71,14 @@ const StyledSelect = styled(Select)`
     width: 200px;
 `;
 
-const toggleInfoboxPreview = (visible) => {
-    if (!visible) {
-        Oskari.getSandbox().postRequestByName('InfoBox.HideInfoBoxRequest', [INFOBOX_PREVIEW_ID]);
-        return;
-    }
-
-    showOrUpdateInfobox();
-};
-
-const showOrUpdateInfobox = () => {
-    const sandbox = Oskari.getSandbox();
-    const mapmodule = sandbox.findRegisteredModuleInstance('MainMapModule');
-    const location = mapmodule.getMapCenter();
-    const title = Oskari.getMsg(BUNDLE_KEY, 'BasicView.layout.popup.gfiDialog.title');
-    const featureName = Oskari.getMsg(BUNDLE_KEY, 'BasicView.layout.popup.gfiDialog.featureName');
-    const featureDesc = Oskari.getMsg(BUNDLE_KEY, 'BasicView.layout.popup.gfiDialog.featureDesc');
-
-    const infoboxContent = [
-        {
-            html: '<div class="getinforesult_header">' +
-                    '<div class="icon-bubble-left">' +
-                '</div>' +
-                '<div title="' + title + '" class="getinforesult_header_title">' + featureName + '</div>' +
-                '</div>' +
-                '<div>' +
-                    featureDesc +
-                '</div>'
-        }
-    ];
-
-    const infoboxData = [
-        INFOBOX_PREVIEW_ID,
-        Oskari.getMsg(BUNDLE_KEY, 'BasicView.layout.popup.gfiDialog.title'),
-        infoboxContent,
-        {
-            ...location
-        },
-        {
-            hidePrevious: true
-        }
-    ];
-    sandbox.postRequestByName('InfoBox.ShowInfoBoxRequest', infoboxData);
-};
-
-export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVisible, updateInfoBoxPreviewVisible }) => {
-    const [font, setFont] = useState(mapTheme?.font || fonts[0].val);
-    const [popupHeader, setPopupHeader] = useState(mapTheme?.color?.header?.bg);
-    const [popupHeaderText, setPopupHeaderText] = useState(mapTheme?.color?.header?.text);
-
-    const [infoboxHeader, setInfoboxHeader] = useState(mapTheme?.infobox?.header?.bg);
-    const [infoboxHeaderText, setInfoboxHeaderText] = useState(mapTheme?.infobox?.header?.text);
-
-    const [buttonBackground, setButtonBackground] = useState(mapTheme?.navigation?.color?.primary);
-    const [buttonText, setButtonText] = useState(mapTheme?.navigation?.color?.text);
-    const [buttonAccent, setButtonAccent] = useState(mapTheme?.navigation?.color?.accent);
-    const [buttonRounding, setButtonRounding] = useState(mapTheme?.navigation?.roundness);
-    const [buttonEffect, setButtonEffect] = useState(mapTheme?.navigation?.effect);
-
-    useEffect(() => {
-        const theme = {
-            ...mapTheme,
-            font,
-            color: {
-                ...mapTheme.color,
-                header: {
-                    ...mapTheme.color.header,
-                    bg: popupHeader,
-                    text: popupHeaderText,
-                    icon: popupHeaderText
-                },
-                accent: buttonAccent
-            },
-            navigation: {
-                ...mapTheme.navigation,
-                color: {
-                    ...mapTheme.navigation.color,
-                    primary: buttonBackground,
-                    accent: buttonAccent,
-                    text: buttonText
-                },
-                roundness: buttonRounding,
-                effect: buttonEffect
-            },
-            infobox: {
-                ...mapTheme.infobox,
-                header: {
-                    bg: infoboxHeader,
-                    text: infoboxHeaderText
-                }
-            }
-        };
-        changeTheme(theme);
-        toggleInfoboxPreview(infoBoxPreviewVisible);
-    }, [font, popupHeader, popupHeaderText, infoboxHeader, infoboxHeaderText, infoBoxPreviewVisible, buttonBackground, buttonText, buttonAccent, buttonRounding, buttonEffect]);
-
-    const setPreset = (style) => {
-        let rounding = 100;
-        let popupBg = '#3c3c3c';
-        let buttonBg = '#141414';
-        let icon = '#ffffff';
-        let effect;
-        const hover = '#ffd400';
-
-        // reset infobox to defaults
-        const infoboxDefaults = getDefaultMapTheme()?.infobox;
-        const infoboxHeader = infoboxDefaults?.header?.bg;
-        const infoboxHeaderText = infoboxDefaults?.header?.text;
-
-        if (style.includes('sharp')) {
-            rounding = 0;
-        }
-        if (style.includes('light')) {
-            popupBg = '#ffffff';
-            buttonBg = '#ffffff';
-            icon = '#000000';
-        }
-        if (style.includes('3d')) {
-            rounding = 20;
-            effect = '3D';
-        }
-
-        setPopupHeader(popupBg);
-        setButtonRounding(rounding);
-        setButtonBackground(buttonBg);
-        setButtonText(icon);
-        setButtonAccent(hover);
-        setButtonEffect(effect);
-        setPopupHeaderText(icon);
-        setInfoboxHeader(infoboxHeader);
-        setInfoboxHeaderText(infoboxHeaderText);
-    };
-
-    const toolStyles = Oskari.getMsg(BUNDLE_KEY, 'BasicView.layout.fields.toolStyles') || {};
+export const PanelToolStyles = ({ mapTheme, infoBoxPreviewVisible, presets, controller }) => {
+    const { font = FONTS[0].value, color = {}, navigation = {}, infobox = {} } = mapTheme;
+    const [buttonRounding, setButtonRounding] = useState(navigation.roundness);
 
     return (
         <Content>
             <Field>
-                <Dropdown items={Object.keys(toolStyles).map(key => (
-                    {
-                        title: toolStyles[key],
-                        action: () => setPreset(key)
-                    }
-                ))}>
+                <Dropdown items={presets}>
                     <Button>
                         <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.presets' />
                     </Button>
@@ -206,22 +88,16 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.fonts.label' />
                 <StyledSelect
                     value={font}
-                    onChange={val => {
-                        setFont(val);
-                    }}
-                >
-                    {fonts.map(font => (
-                        <Option key={font.val}>{font.name}</Option>
-                    ))}
-                </StyledSelect>
+                    onChange={val => controller.updateTheme('font', val) }
+                    options={FONTS} />
             </Field>
             <Divider><Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.title.buttons' /></Divider>
             <Field>
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.buttonBackgroundColor' />
                 <StyledColorPicker>
                     <ColorPicker
-                        value={buttonBackground}
-                        onChange={setButtonBackground}
+                        value={navigation.color?.primary}
+                        onChange={val => controller.updateTheme('navigation.color.primary', val)}
                     />
                 </StyledColorPicker>
             </Field>
@@ -229,8 +105,8 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.buttonTextColor' />
                 <StyledColorPicker>
                     <ColorPicker
-                        value={buttonText}
-                        onChange={setButtonText}
+                        value={navigation.color?.text}
+                        onChange={val => controller.updateTheme('navigation.color.text', val)}
                     />
                 </StyledColorPicker>
             </Field>
@@ -238,8 +114,8 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.buttonAccentColor' />
                 <StyledColorPicker>
                     <ColorPicker
-                        value={buttonAccent}
-                        onChange={setButtonAccent}
+                        value={navigation.color?.accent}
+                        onChange={val => controller.updateThemeMultiPath(['color.accent', 'navigation.color.accent'], val)}
                     />
                 </StyledColorPicker>
             </Field>
@@ -249,15 +125,15 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                     <SliderContainer>
                         <Slider noMargin
                             value={buttonRounding}
-                            onChange={val => setButtonRounding(val)}/>
+                            onChange={val => setButtonRounding(val)}
+                            onChangeComplete={val => controller.updateTheme('navigation.roundness', val)}/>
                     </SliderContainer>
                     <NumberInputContainer>
                         <StyledNumberInput
                             min={0}
                             max={100}
                             value={buttonRounding}
-                            onChange={val => setButtonRounding(val)}
-                        />
+                            onChange={val => controller.updateTheme('navigation.roundness', val)}/>
                         <NumberSuffix>
                             %
                         </NumberSuffix>
@@ -267,9 +143,8 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
             <Field>
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.effect' />
                 <Checkbox
-                    checked={buttonEffect === '3D'}
-                    onChange={e => setButtonEffect(e.target.checked ? '3D' : undefined)}
-                >
+                    checked={navigation.effect === '3D'}
+                    onChange={e => controller.updateTheme('navigation.effect', e.target.checked ? '3D' : undefined)}>
                     <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.3d' />
                 </Checkbox>
             </Field>
@@ -278,8 +153,8 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.popupHeaderColor' />
                 <StyledColorPicker>
                     <ColorPicker
-                        value={popupHeader}
-                        onChange={setPopupHeader}
+                        value={color.header?.bg}
+                        onChange={val => controller.updateTheme('color.header.bg', val)}
                     />
                 </StyledColorPicker>
             </Field>
@@ -287,8 +162,8 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.popupHeaderTextColor' />
                 <StyledColorPicker>
                     <ColorPicker
-                        value={popupHeaderText}
-                        onChange={setPopupHeaderText}
+                        value={color.header?.text}
+                        onChange={val => controller.updateThemeMultiPath(['color.header.text', 'color.header.icon'], val)}
                     />
                 </StyledColorPicker>
             </Field>
@@ -297,8 +172,8 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.infoboxHeaderColor' />
                 <StyledColorPicker>
                     <ColorPicker
-                        value={infoboxHeader}
-                        onChange={setInfoboxHeader}
+                        value={infobox.header?.bg}
+                        onChange={val => controller.updateTheme('infobox.header.bg', val)}
                     />
                 </StyledColorPicker>
             </Field>
@@ -306,13 +181,13 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
                 <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.infoboxHeaderTextColor' />
                 <StyledColorPicker>
                     <ColorPicker
-                        value={infoboxHeaderText}
-                        onChange={setInfoboxHeaderText}
+                        value={infobox.header?.text}
+                        onChange={val => controller.updateTheme('infobox.header.bg', val)}
                     />
                 </StyledColorPicker>
             </Field>
             <Field>
-                <Checkbox checked={infoBoxPreviewVisible} onChange={ () => updateInfoBoxPreviewVisible(!infoBoxPreviewVisible) }>
+                <Checkbox checked={infoBoxPreviewVisible} onChange={ () => controller.updateInfoBoxPreviewVisible(!infoBoxPreviewVisible) }>
                     <Message bundleKey={BUNDLE_KEY} messageKey='BasicView.layout.fields.infoboxPreview'/>
                 </Checkbox>
             </Field>
@@ -321,9 +196,8 @@ export const PanelToolStyles = ({ mapTheme, changeTheme, fonts, infoBoxPreviewVi
 };
 
 PanelToolStyles.propTypes = {
-    mapTheme: PropTypes.object,
-    changeTheme: PropTypes.func,
-    fonts: PropTypes.array,
-    infoBoxPreviewVisible: PropTypes.bool,
-    updateInfoBoxPreviewVisible: PropTypes.func
+    mapTheme: PropTypes.object.isRequired,
+    presets: PropTypes.array.isRequired,
+    infoBoxPreviewVisible: PropTypes.bool.isRequired,
+    controller: PropTypes.object.isRequired
 };
