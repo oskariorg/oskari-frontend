1c0d2ce2b3cbe80919dbacca24a7dccfbee6e982
Pekka Helesuo <pekka.helesuo@siili.com>
2025-11-13 15:24:32 +0200

    LabeledSelect options array

--- bundles/statistics/statsgrid/components/classification/editclassification/LabeledSelect.jsx
+++ bundles/statistics/statsgrid/components/classification/editclassification/LabeledSelect.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Select, Option, Message } from 'oskari-ui';
+import { Select, Message } from 'oskari-ui';
 
 const Margin = styled.div`
     padding-top: 5px;
@@ -16,6 +16,7 @@ export const LabeledSelect = ({
     handleChange,
     ...rest
 }) => {
+
     return (
         <Fragment>
             <Message messageKey={`classify.labels.${name}`}/>
@@ -24,14 +25,9 @@ export const LabeledSelect = ({
                     className={`t_option-${name}`}
                     onChange={value => handleChange(name, value)}
                     size='small'
+                    options={options}
                     {...rest}
-                >
-                    {options.map(({ label, ...rest }, i) => (
-                        <Option key={`option-${i}`} {...rest}>
-                            {label}
-                        </Option>
-                    ))}
-                </Select>
+                />
             </Margin>
         </Fragment>
     );
