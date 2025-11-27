ad2eadebb57272fb610410b90a565b064e14f50b
Pekka Helesuo <pekka.helesuo@siili.com>
2025-11-27 17:05:36 +0200

    hava data-value default to value in <Select/> options

--- src/react/components/Select.jsx
+++ src/react/components/Select.jsx
@@ -1,8 +1,25 @@
+import React from 'react';
 import { Select as AntSelect } from 'antd';
 import styled from 'styled-components';
 
 // Wrap to styled so components can be referred in component styling.
-const Select = styled(AntSelect)`
+
+const SelectWrapper = (props) => {
+
+    const { options } = props;
+    const newOptions = options.map((option) => ({
+        ...option,
+        'data-value': option['data-value'] ? option['data-value'] : option.value
+    }));
+
+    return <AntSelect
+        {...props}
+        options = { newOptions }
+    />;
+
+}
+
+const Select = styled(SelectWrapper)`
 `;
 Select.displayName = 'OskariUISelect';
 
