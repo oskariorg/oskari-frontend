25b6311e288bda0e1af3e5f2e9bc7aec7db7b79a
Pekka Helesuo <pekka.helesuo@siili.com>
2026-04-16 11:51:51 +0300

    fix buttons not wrapping on narrow screen - issue

--- src/react/components/FeatureEditor/FeaturePanel.jsx
+++ src/react/components/FeatureEditor/FeaturePanel.jsx
@@ -9,8 +9,14 @@ import { GeoJSONPanel } from './GeoJSONPanel';
 import { Helper } from './Helper';
 import { DrawingHelper } from './DrawingHelper';
 import { StyledSpace, Row } from './styled';
+import styled from 'styled-components';
 
-export const FeaturePanel = ({ layer = {}, feature = {}, onCancel, onSave, onDelete, startNewFeature}) => {
+const DrawingActionRow = styled(Row)`
+    flex-wrap: wrap;
+    width: 100%;
+`;
+
+export const FeaturePanel = ({ layer = {}, feature = {}, onCancel, onSave, onDelete, startNewFeature, showGeoJSONPanel = true, showGeometryNotRecognizedAlert = true }) => {
     const type = Helper.detectGeometryType(layer.geometryType);
     const isMulti = type.includes('Multi');
     const [isDrawing, setDrawingMode] = useState(false);
@@ -21,7 +27,7 @@ export const FeaturePanel = ({ layer = {}, feature = {}, onCancel, onSave, onDel
         setDrawingMode(false);
         DrawingHelper.stopDrawing(clearPrevious);
     };
-    
+
     const cancelCb = () => {
         stopDrawing(true);
         onCancel();
@@ -31,7 +37,7 @@ export const FeaturePanel = ({ layer = {}, feature = {}, onCancel, onSave, onDel
         stopDrawing(true);
         onSave(currentFeature);
     };
-    
+
     const startNewCb = () => {
         stopDrawing(true);
         startNewFeature();
@@ -79,18 +85,18 @@ export const FeaturePanel = ({ layer = {}, feature = {}, onCancel, onSave, onDel
                     { isDrawing &&
                         <React.Fragment>
                             <Message messageKey="FeatureEditorView.geometrylist.editing" />
-                            <Row>
+                            <DrawingActionRow>
                                 <Button type="primary" onClick={() => stopDrawing(false)}>
                                     <Message messageKey="FeatureEditorView.tools.finishSketch" />
                                 </Button>
-                                <Button type="default" onClick={() => { 
+                                <Button type="default" onClick={() => {
                                     // reset geometry with the original feature and clear drawing.
-                                    updateGeometry(feature); 
-                                    stopDrawing(true); 
+                                    updateGeometry(feature);
+                                    stopDrawing(true);
                                 }}>
                                     <Message messageKey="FeatureEditorView.restoreOriginal" />
                                 </Button>
-                            </Row>
+                            </DrawingActionRow>
                         </React.Fragment>
                     }
                     { !isDrawing &&
@@ -100,9 +106,10 @@ export const FeaturePanel = ({ layer = {}, feature = {}, onCancel, onSave, onDel
                             original={feature}
                             startDrawing={startDrawing}
                             stopDrawing={stopDrawing}
-                            updateGeometry={updateGeometry} />
+                            updateGeometry={updateGeometry}
+                            showGeometryNotRecognizedAlert={showGeometryNotRecognizedAlert} />
                     }
-                    { !isDrawing &&
+                    { !isDrawing && showGeoJSONPanel &&
                         <GeoJSONPanel feature={currentFeature} />
                     }
                 </StyledSpace>
@@ -143,5 +150,7 @@ FeaturePanel.propTypes = {
     editing: PropTypes.bool,
     onCancel: PropTypes.func,
     onSave: PropTypes.func,
-    onDelete: PropTypes.func
+    onDelete: PropTypes.func,
+    showGeoJSONPanel: PropTypes.bool,
+    showGeometryNotRecognizedAlert: PropTypes.bool
 };
