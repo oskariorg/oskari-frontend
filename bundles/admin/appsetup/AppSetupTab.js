60a004134cfd2e553f5a99c3dd275ab4db015bae
Pekka Helesuo <pekka.helesuo@siili.com>
2026-06-01 11:50:00 +0300

    appsetup tab es/react

---
+++ bundles/admin/appsetup/AppSetupTab.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import { Button, TextAreaInput, Link } from 'oskari-ui';
+import { Messaging } from 'oskari-ui/util';
+import { styled } from 'styled-components';
+
+const BUNDLE_KEY = 'AdminAppSetup';
+
+const Description = styled('div')`
+    margin-bottom: 0.375em;
+`;
+
+const ResponseData = styled('div')`
+    margin-left: 0.75em;
+`;
+
+const JsonInput = styled(TextAreaInput)`
+    width: 100%;
+    max-width: 25em;
+    min-height: 15.625em;
+    margin-bottom: 0.5em;
+`;
+
+const Actions = styled('div')`
+    margin-top: 0.5em;
+`;
+
+const Result = styled('div')`
+    margin-top: 0.75em;
+`;
+
+export const AppSetupTab = () => {
+    const [json, setJson] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [result, setResult] = useState(null);
+
+    const currentViewUrl = `${Oskari.urls.getRoute('Views')}&uuid=${Oskari.app.getUuid()}`;
+
+    const importJSON = async () => {
+        if (!json.trim()) {
+            Messaging.error(Oskari.getMsg(BUNDLE_KEY, 'error.checkValue'));
+            return;
+        }
+        let parsed;
+        try {
+            parsed = JSON.parse(json);
+        } catch (e) {
+            Messaging.error(Oskari.getMsg(BUNDLE_KEY, 'error.checkValue'));
+            return;
+        }
+        setLoading(true);
+        setResult(null);
+        try {
+            const response = await fetch(Oskari.urls.getRoute('Views'), {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+                body: JSON.stringify(parsed)
+            });
+            if (!response.ok) {
+                Messaging.error(Oskari.getMsg(BUNDLE_KEY, 'error.importError'));
+            } else {
+                const data = await response.json();
+                setResult(data);
+                setJson('');
+            }
+        } catch (e) {
+            Messaging.error(Oskari.getMsg(BUNDLE_KEY, 'error.importError'));
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <>
+            <Description>
+                <div>
+                    {Oskari.getMsg(BUNDLE_KEY, 'description.fillJSON')}{' '}
+                    (<Link url={currentViewUrl}>{Oskari.getMsg(BUNDLE_KEY, 'description.current')}</Link>).
+                </div>
+                <div>{Oskari.getMsg(BUNDLE_KEY, 'description.differentUuid')}</div>
+            </Description>
+            <JsonInput
+                value={json}
+                onChange={(e) => setJson(e.target.value)}
+                placeholder={Oskari.getMsg(BUNDLE_KEY, 'textAreaPlaceholder')}
+            />
+            <Actions>
+                <Button type="primary" loading={loading} onClick={importJSON}>
+                    {Oskari.getMsg(BUNDLE_KEY, 'importButtonText')}
+                </Button>
+            </Actions>
+            {result && (
+                <Result>
+                    <div>{Oskari.getMsg(BUNDLE_KEY, 'success.description')}</div>
+                    <ResponseData>
+                        <ul>
+                            <li><b>{Oskari.getMsg(BUNDLE_KEY, 'success.viewId')}</b>: {result.id}</li>
+                            <li><b>{Oskari.getMsg(BUNDLE_KEY, 'success.viewUuid')}</b>: {result.uuid}</li>
+                            <li>
+                                <b>{Oskari.getMsg(BUNDLE_KEY, 'success.viewUrl')}</b>:{' '}
+                                <Link url={result.url}>{result.url}</Link>
+                            </li>
+                        </ul>
+                    </ResponseData>
+                </Result>
+            )}
+        </>
+    );
+};
--- bundles/admin/appsetup/instance.js
+++ bundles/admin/appsetup/instance.js
@@ -1,143 +1,30 @@
-/**
- * @class Oskari.admin.bundle.appsetup.AppSetupAdminBundleInstance
- *
- * Appsetup bundle for admins. Allows import appsetup jsons.
- */
-Oskari.clazz.define('Oskari.admin.bundle.appsetup.AppSetupAdminBundleInstance',
+import React from 'react';
+import { getReactRoot } from 'oskari-ui/components/window';
+import { LocaleProvider, ThemeProvider } from 'oskari-ui/util';
+import { BasicBundleInstance } from 'oskari-ui/BasicBundleInstance';
+import { AppSetupTab } from './AppSetupTab.js';
 
-    /**
-     * @method create called automatically on construction
-     * @static
-     */
+const BUNDLE_KEY = 'AdminAppSetup';
 
-    function () {
-        this._localization = this.getLocalization();
-        this._popup = Oskari.clazz.create('Oskari.userinterface.component.Popup');
-        this._templates = {
-            tabContent: jQuery('<div><div class="appsetup__description"></div><div class="appsetup__textarea"></div><div class="appsetup__button"></div></div>'),
-            successMessage: jQuery('<div class="appsetup__message">' +
-                        '   <div>' + this._localization.success.description + '</div>' +
-                        '   <div class="response_data">' +
-                        '       <ul>' +
-                        '           <li><b>' + this._localization.success.viewId + '</b>: <span class="view-id"></span></li>' +
-                        '           <li><b>' + this._localization.success.viewUuid + '</b>: <span class="view-uuid"></span></li>' +
-                        '           <li><b>' + this._localization.success.viewUrl + '</b>: <a target="_blank"></a></li>' +
-                        '       </ul>' +
-                        '   </div>' +
-                        '</div>')
-        };
-    }, {
-        getName: function () {
-            return 'AdminAppSetup';
-        },
-        getLocalization: function (key) {
-            if (!this._localization) {
-                this._localization = Oskari.getLocalization(this.getName());
-            }
-            if (key) {
-                return this._localization[key];
-            }
-            return this._localization;
-        },
-        start: function () {
-            var me = this;
-            var sandbox = Oskari.getSandbox();
-            sandbox.register(this);
-            me.sandbox = sandbox;
+export class AppSetupBundleInstance extends BasicBundleInstance {
+    constructor () {
+        super(BUNDLE_KEY);
+    }
 
-            var content = me._templates.tabContent.clone();
-            var description = content.find('.appsetup__description');
-            var currentViewUrl = Oskari.urls.getRoute('Views') + '&uuid=' + Oskari.app.getUuid();
-            description.html('<div>' + me._localization.description.fillJSON + ' ' +
-                '(<a href="' + currentViewUrl + '" target="_blank">' + me._localization.description.current + '</a>).</div>' +
-                '<div>' + this._localization.description.differentUuid + '</div>'
-            );
-            me._jsonInput = Oskari.clazz.create('Oskari.userinterface.component.TextAreaInput');
-            me._jsonInput.setName('json');
-            me._jsonInput.setPlaceholder(me._localization.textAreaPlaceholder);
-            me._jsonInput.setValue('');
-            me._jsonInput.insertTo(content.find('.appsetup__textarea'));
-
-            me._importBtn = Oskari.clazz.create('Oskari.userinterface.component.Button');
-            me._importBtn.addClass('primary');
-            me._importBtn.setTitle(me._localization.importButtonText);
-            me._importBtn.setHandler(function () {
-                me.importJSON();
-            });
-            me._importBtn.insertTo(content.find('.appsetup__button'));
-
-            if (sandbox.hasHandler('Admin.AddTabRequest')) {
-                var reqBuilder = Oskari.requestBuilder('Admin.AddTabRequest');
-                var request = reqBuilder(me._localization.title, content, 3, 'appsetup');
-                sandbox.request(this, request);
-            }
-        },
-        importJSON: function () {
-            var me = this;
-            me._popup.close(true);
-
-            var value = me._jsonInput.getValue();
-            if (value === '') {
-                me._popup.show(me._localization.error.title, me._localization.error.checkValue);
-                me._popup.fadeout(5000);
-                return;
-            }
-
-            var parsed = null;
-
-            // test parse json
-            try {
-                parsed = JSON.parse(value);
-            } catch (e) {
-                me._popup.show(me._localization.error.title, me._localization.error.checkValue);
-                me._popup.fadeout(5000);
-                return;
-            }
-
-            me._importBtn.setEnabled(false);
-
-            jQuery.ajax({
-                type: 'POST',
-                dataType: 'json',
-                contentType: 'application/json; charset=UTF-8',
-                url: Oskari.urls.getRoute('Views'),
-                data: JSON.stringify(parsed),
-                error: function () {
-                    me._popup.show(me._localization.error.title, me._localization.error.importError);
-                    me._popup.fadeout(5000);
-                    me._importBtn.setEnabled(true);
-                },
-                success: function (response) {
-                    var btn = me._popup.createCloseButton(me._localization.ok);
-                    btn.addClass('primary');
-                    var message = me._templates.successMessage.clone();
-                    message.find('span.view-id').html(response.id);
-                    message.find('span.view-uuid').html(response.uuid);
-                    message.find('a').html(response.url);
-                    message.find('a').attr('href', response.url);
-                    me._popup.show(me._localization.success.title, message, [btn]);
-                    me._importBtn.setEnabled(true);
-                    me._jsonInput.setValue('');
-                }
-            });
-        },
-        // module boilerplate methods
-        init: function () {
-
-        },
-        stop: function () {
-
-        },
-        update: function () {
-
+    start (sandbox) {
+        super.start(sandbox);
+        if (!sandbox.hasHandler('Admin.AddTabRequest')) {
+            return;
         }
-    }, {
-        /**
-         * @property {String[]} protocol
-         * @static
-         */
-        protocol: [
-            'Oskari.bundle.BundleInstance',
-            'Oskari.mapframework.module.Module'
-        ]
-    });
+        const container = document.createElement('div');
+        getReactRoot(container).render(
+            <LocaleProvider value={{ bundleKey: BUNDLE_KEY }}>
+                <ThemeProvider>
+                    <AppSetupTab />
+                </ThemeProvider>
+            </LocaleProvider>
+        );
+        const reqBuilder = Oskari.requestBuilder('Admin.AddTabRequest');
+        sandbox.request(this, reqBuilder(Oskari.getMsg(BUNDLE_KEY, 'title'), container, 3, 'appsetup'));
+    }
+}
