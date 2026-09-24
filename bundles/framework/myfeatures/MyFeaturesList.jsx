c99a381c65ddc5c1691b9fec08d98e6dae5d6396
Pekka Helesuo <pekka.helesuo@siili.com>
2026-09-24 14:07:15 +0300

    check that featuredata exists

--- bundles/framework/myfeatures/MyFeaturesList.jsx
+++ bundles/framework/myfeatures/MyFeaturesList.jsx
@@ -10,6 +10,12 @@ const ICON_STYLE = {
     fontSize: '16px'
 };
 
+const FEATUREDATA_HANDLER_NAME = 'ShowFeatureDataRequest';
+
+function isFeatureDataAvailable() {
+    return Oskari.getSandbox().hasHandler(FEATUREDATA_HANDLER_NAME);
+}
+
 export const MyFeaturesList = ({ data = [], controller, loading }) => {
     const columnSettings = [
         {
@@ -52,12 +58,15 @@ export const MyFeaturesList = ({ data = [], controller, loading }) => {
             render: (title, item) => {
                 return (
                     <ToolsContainer>
-                        <IconButton
-                            className='t_edit'
-                            title={<Message messageKey='tab.grid.featuredata' />}
-                            icon={<TableOutlined style={ICON_STYLE} />}
-                            onClick={() => controller.openFeatureData(item.key)}
-                        />
+                        { isFeatureDataAvailable() &&
+                            <IconButton
+                                className='t_edit'
+                                title={<Message messageKey='tab.grid.featuredata' />}
+                                icon={<TableOutlined style={ICON_STYLE} />}
+                                onClick={() => controller.openFeatureData(item.key)}
+                            />
+
+                        }
                         <IconButton
                             className='t_edit'
                             title={<Message messageKey='tab.grid.edit' />}
